e0e6238c1c89399e1080f08fb54e0d591a50e2c0
郭胜强 <guoshengqiang@live.com>
2018-09-28 15:44:21 +0800

    初始化小程序组件示例工程

---
+++ examples/wxcomponents-template/App.vue
@@ -0,0 +1,26 @@
+<script>
+	export default {
+		onLaunch: function () {
+			console.log('App Launch')
+		},
+		onShow: function () {
+			console.log('App Show')
+		},
+		onHide: function () {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+
+	page,
+	view {
+		display: flex;/* uni-app默认使用flex布局。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。如不了解flex布局，请参考http://www.w3.org/TR/css3-flexbox/。若不需要flex布局可删除本行*/
+	}
+
+	page {
+		min-height: 100%;
+	}
+</style>
---
+++ examples/wxcomponents-template/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+		<text class="title">{{title}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data: {
+			title: 'Hello'
+		}
+	}
+</script>
+
+<style>
+	.content {
+		flex: 1;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
