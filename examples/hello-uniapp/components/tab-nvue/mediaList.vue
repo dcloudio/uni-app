518c29707db14770b3b020ebfe782de149d071bb
hbcui1984 <hbcui1984@163.com>
2018-10-30 12:22:58 +0800

    优化顶部选项卡模板，vue/nvue 复用共同的js逻辑和css样式

--- examples/hello-uniapp/components/tab-nvue/mediaList.vue
+++ examples/hello-uniapp/components/tab-nvue/mediaList.vue
@@ -1,29 +1,29 @@
 <template>
-    <div>
-        <div class="list-cell" @click="bindClick">
-            <div class="media-list" v-if="data.title">
-                <div :class="[isImgRight?'media-image-right':'',isImgLeft?'media-image-left':'']">
+    <view>
+        <view class="list-cell" hover-class="uni-list-cell-hover" @click="bindClick">
+            <view class="media-list" v-if="data.title">
+                <view :class="[isImgRight?'media-image-right':'',isImgLeft?'media-image-left':'']">
                     <text :class="['media-title',isImgRight||isImgLeft?'media-title2':'']">{{data.title}}</text>
-                    <div v-if="showImg" :class="['image-section',isImgRight?'image-section-right':'',isImgLeft?'image-section-left':'']">
+                    <view v-if="showImg" :class="['image-section',isImgRight?'image-section-right':'',isImgLeft?'image-section-left':'']">
                         <image :class="['image-list1',isImgRight||isImgLeft?'image-list2':'']" v-if="data.image_url"
                             :src="data.image_url"></image>
                         <image class="image-list3" v-if="data.image_list" :src="source.url" v-for="(source, i) in data.image_list"
                             :key="i" />
-                    </div>
-                </div>
-                <div class="media-foot">
-                    <div class="media-info">
+                    </view>
+                </view>
+                <view class="media-foot">
+                    <view class="media-info">
                         <text class="info-text">{{data.source}}</text>
                         <text class="info-text">{{data.comment_count}}条评论</text>
                         <text class="info-text">{{data.datetime}}</text>
-                    </div>
-                    <div class="max-close-view" @click="close">
-                        <div class="close-view"><text class="close">×</text></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                    </view>
+                    <view class="max-close-view" @click.stop="close">
+                        <view class="close-view"><text class="close">×</text></view>
+                    </view>
+                </view>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
@@ -60,22 +60,28 @@
 </script>
 
 <style>
-    .list-cell {
-        width: 750px;
-        padding: 0 30px;
+    view {
+        display: flex;
+        flex-direction: column;
+        box-sizing: border-box;
     }
 
-    .list-cell:active {
+    .list-cell {
+        width: 750upx;
+        padding: 0 30upx;
+    }
+
+    .uni-list-cell-hover {
         background-color: #eeeeee;
     }
 
     .media-list {
         flex: 1;
         flex-direction: column;
-        border-bottom-width: 1px;
+        border-bottom-width: 1upx;
         border-bottom-style: solid;
         border-bottom-color: #c8c7cc;
-        padding: 20px 0;
+        padding: 20upx 0;
     }
 
     .media-image-right {
@@ -93,49 +99,49 @@
     .media-title {
         lines: 3;
         text-overflow: ellipsis;
-        font-size: 32px;
+        font-size: 32upx;
         color: #555555;
     }
 
     .media-title2 {
         flex: 1;
-        margin-top: 6px;
-        line-height: 40px;
+        margin-top: 6upx;
+        line-height: 40upx;
     }
 
     .image-section {
-        margin-top: 20px;
+        margin-top: 20upx;
         flex-direction: row;
         justify-content: space-between;
     }
 
     .image-section-right {
-        margin-top: 0px;
-        margin-left: 10px;
-        width: 225px;
-        height: 146px;
+        margin-top: 0upx;
+        margin-left: 10upx;
+        width: 225upx;
+        height: 146upx;
     }
 
     .image-section-left {
-        margin-top: 0px;
-        margin-right: 10px;
-        width: 225px;
-        height: 146px;
+        margin-top: 0upx;
+        margin-right: 10upx;
+        width: 225upx;
+        height: 146upx;
     }
 
     .image-list1 {
-        width: 690px;
-        height: 481px;
+        width: 690upx;
+        height: 481upx;
     }
 
     .image-list2 {
-        width: 225px;
-        height: 146px;
+        width: 225upx;
+        height: 146upx;
     }
 
     .image-list3 {
-        width: 225px;
-        height: 146px;
+        width: 225upx;
+        height: 146upx;
     }
 
     .media-info {
@@ -143,13 +149,13 @@
     }
 
     .info-text {
-        margin-right: 20px;
+        margin-right: 20upx;
         color: #999999;
-        font-size: 28px;
+        font-size: 28upx;
     }
 
     .media-foot {
-        margin-top: 20px;
+        margin-top: 20upx;
         flex-direction: row;
         justify-content: space-between;
     }
@@ -158,24 +164,24 @@
         align-items: center;
         justify-content: flex-end;
         flex-direction: row;
-        height: 40px;
-        width: 80px;
+        height: 40upx;
+        width: 80upx;
     }
 
     .close-view {
         border-style: solid;
         border-width: 1px;
         border-color: #999999;
-        border-radius: 10px;
+        border-radius: 10upx;
         justify-content: center;
-        height: 30px;
-        width: 40px;
-        line-height: 30px;
+        height: 30upx;
+        width: 40upx;
+        line-height: 30upx;
     }
 
     .close {
         text-align: center;
         color: #999999;
-        font-size: 28px;
+        font-size: 28upx;
     }
 </style>
