03dc2167c59e9f7251b968b181b5766e6c987b03
xiaoyucoding <xiaoyucoding@126.com>
2018-11-11 18:11:57 +0800

    modify: 更新 web-view 示例加载的页面

--- examples/hello-uniapp/pages/component/web-view/web-view.vue
+++ examples/hello-uniapp/pages/component/web-view/web-view.vue
@@ -1,6 +1,6 @@
 <template>
     <view>
-        <web-view src="https://uniapp.dcloud.io"></web-view>
+        <web-view src="https://uniapp.dcloud.io/static/web-view.html"></web-view>
     </view>
 </template>
 
