a4c6ce3ce8bd328fe87a836eaaa6ef811043d3ae
xiaoyucoding <xiaoyucoding@126.com>
2018-11-11 18:54:30 +0800

    modify: navigator 示例调整为新的规范

--- examples/hello-uniapp/pages/component/navigator/navigator.vue
+++ examples/hello-uniapp/pages/component/navigator/navigator.vue
@@ -6,7 +6,7 @@
 				<navigator url="navigate/navigate?title=navigate" hover-class="navigator-hover">
 					<button type="default">跳转到新页面</button>
 				</navigator>
-				<navigator url="redirect/redirect?title=redirect" redirect hover-class="other-navigator-hover">
+				<navigator url="redirect/redirect?title=redirect" open-type="redirect" hover-class="other-navigator-hover">
 					<button type="default">在当前页打开</button>
 				</navigator>
 			</view>
