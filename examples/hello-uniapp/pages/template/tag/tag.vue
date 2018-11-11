ea663a4e2624dd05f7d1395f063ca425d5f02555
hcoder <5213606@qq.com>
2018-11-11 16:48:34 +0800

    刘海君
    tag 页面结构更新

--- examples/hello-uniapp/pages/template/tag/tag.vue
+++ examples/hello-uniapp/pages/template/tag/tag.vue
@@ -1,103 +1,97 @@
 <template>
-    <view class="page">
+    <view>
         <page-head :title="title"></page-head>
-        <view class="page-body">
-            <view class="page-section">
-                <view class="page-section-title">实心标签</view>
-                <view class="page-section">
-                    <view class="tag-view">
-                        <uni-tag text="标签"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="primary"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="success"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="warning"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="danger" :circle="true"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="royal" :circle="true"></uni-tag>
-                    </view>
-                </view>
+        <view class="uni-padding-wrap uni-common-mt">
+            <view>
+            	<view class="uni-title">实心标签</view>
+				<view class="tag-view">
+					<uni-tag text="标签"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" type="primary"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" type="success"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" type="warning"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" type="danger" :circle="true"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" type="royal" :circle="true"></uni-tag>
+				</view>
             </view>
-            <view class="page-section">
-                <view class="page-section-title">空心标签</view>
-                <view class="page-section">
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="primary"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="success"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="warning"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="danger" :circle="true"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="royal" :circle="true"></uni-tag>
-                    </view>
-                </view>
+            
+			<view class="uni-title uni-common-mt">空心标签</view>
+			<view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true" type="primary"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true" type="success"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true" type="warning"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true" type="danger" :circle="true"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true" type="royal" :circle="true"></uni-tag>
+				</view>
+			</view>
+			
+            <view class="uni-title uni-common-mt">点击事件</view>
+            <view>
+            	<view class="tag-view">
+            		<uni-tag text="标签" :type="type" @click="setType"></uni-tag>
+            	</view>
+            	<view class="tag-view">
+            		<uni-tag text="标签" type="primary" :circle="true" :inverted="inverted" @click="setInverted"></uni-tag>
+            	</view>
             </view>
+			
+			<view class="uni-title uni-common-mt">小标签</view>
+			<view>
+				<view class="tag-view">
+					<uni-tag text="标签" size="small"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" type="primary" size="small"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" type="success" size="small"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true" type="warning" size="small"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true" type="danger" size="small" :circle="true"></uni-tag>
+				</view>
+				<view class="tag-view">
+					<uni-tag text="标签" inverted="true" type="royal" size="small" :circle="true"></uni-tag>
+				</view>
+			</view>
+            
+            <view class="uni-title uni-common-mt">不可点击状态</view>
             <view class="page-section">
-                <view class="page-section-title">点击事件</view>
-                <view class="page-section">
-                    <view class="tag-view">
-                        <uni-tag text="标签" :type="type" @click="setType"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="primary" :circle="true" :inverted="inverted" @click="setInverted"></uni-tag>
-                    </view>
-                </view>
-            </view>
-            <view class="page-section">
-                <view class="page-section-title">小标签</view>
-                <view class="page-section">
-                    <view class="tag-view">
-                        <uni-tag text="标签" size="small"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="primary" size="small"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="success" size="small"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="warning" size="small"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="danger" size="small" :circle="true"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="royal" size="small" :circle="true"></uni-tag>
-                    </view>
-                </view>
-            </view>
-            <view class="page-section">
-                <view class="page-section-title">不可点击状态</view>
-                <view class="page-section">
-                    <view class="tag-view">
-                        <uni-tag text="标签" :disabled="true"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" type="primary" :disabled="true"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="danger" size="small" :disabled="true"></uni-tag>
-                    </view>
-                    <view class="tag-view">
-                        <uni-tag text="标签" inverted="true" type="royal" size="small" :circle="true" :disabled="true"></uni-tag>
-                    </view>
-                </view>
+            	<view class="tag-view">
+            		<uni-tag text="标签" :disabled="true"></uni-tag>
+            	</view>
+            	<view class="tag-view">
+            		<uni-tag text="标签" type="primary" :disabled="true"></uni-tag>
+            	</view>
+            	<view class="tag-view">
+            		<uni-tag text="标签" inverted="true" type="danger" size="small" :disabled="true"></uni-tag>
+            	</view>
+            	<view class="tag-view">
+            		<uni-tag text="标签" inverted="true" type="royal" size="small" :circle="true" :disabled="true"></uni-tag>
+            	</view>
             </view>
         </view>
     </view>
