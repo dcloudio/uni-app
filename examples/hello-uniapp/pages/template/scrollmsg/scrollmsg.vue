ea00b8e4f188c98b659f62d4c07eb0d6256d0942
shenhai <5213606@qq.com>
2018-11-14 15:35:57 +0800

    修改滚动公告 data 声明及消息数据

--- examples/hello-uniapp/pages/template/scrollmsg/scrollmsg.vue
+++ examples/hello-uniapp/pages/template/scrollmsg/scrollmsg.vue
@@ -10,14 +10,8 @@
 					<image src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png" mode="widthFix"></image>
 				</view>
 				<swiper vertical="true" autoplay="true" circular="true" interval="3000">
-					<swiper-item>
-						<navigator>内容......</navigator>
-					</swiper-item>
-					<swiper-item>
-						<navigator>内容......</navigator>
-					</swiper-item>
-					<swiper-item>
-						<navigator>内容......</navigator>
+					<swiper-item v-for="(item, index) in msg" :key="index">
+						<navigator>{{item}}</navigator>
 					</swiper-item>
 				</swiper>
 			</view>
@@ -30,14 +24,8 @@
 					<image src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png" mode="widthFix"></image>
 				</view>
 				<swiper autoplay="true" circular="true" interval="5000">
-					<swiper-item>
-						<navigator>内容......</navigator>
-					</swiper-item>
-					<swiper-item>
-						<navigator>内容......</navigator>
-					</swiper-item>
-					<swiper-item>
-						<navigator>内容......</navigator>
+					<swiper-item v-for="(item, index) in msg" :key="index">
+						<navigator>{{item}}</navigator>
 					</swiper-item>
 				</swiper>
 			</view>
@@ -46,15 +34,18 @@
 </template>
 <script>
 export default {
-	data:{
-		title:"滚动公告"
+	data() {
+		return {
+			title:"滚动公告",
+			msg : [
+				'uni-app行业峰会频频亮相，开发者反响热烈',
+				'DCloud完成B2轮融资，uni-app震撼发布',
+				'36氪热文榜推荐、CSDN公号推荐 DCloud CEO文章'
+			]
+		}
 	}
 }
 </script>
 <style>
-.uni-swiper-msg{width:100%; padding:12rpx 0; flex-wrap:nowrap; display:flex;}
-.uni-swiper-msg-icon{width:50upx; margin-right:20upx;}
-.uni-swiper-msg-icon image{width:100%; flex-shrink:0;}
-.uni-swiper-msg swiper{width:100%; height:50upx;}
-.uni-swiper-msg swiper-item{line-height:50upx;}
+
 </style>
