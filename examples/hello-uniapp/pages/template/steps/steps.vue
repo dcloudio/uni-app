ebe5d27d1543f3b7791afe62ebdc4133cda845bc
shenhai <5213606@qq.com>
2018-11-14 15:12:06 +0800

    修正data声明，统一样式

--- examples/hello-uniapp/pages/template/steps/steps.vue
+++ examples/hello-uniapp/pages/template/steps/steps.vue
@@ -28,20 +28,13 @@
 </template>
 <script>
 export default {
-	data:{
-		title:"步骤"
+	data() {
+		return {
+			title:"步骤"
+		}
 	}
 }
 </script>
 <style>
 page{background:#FFFFFF;}
-.uni-steps{padding:20upx 30upx; flex-grow: 1; display:flex; flex-wrap:wrap;}
-.uni-steps view{display:flex; flex-wrap:wrap; float:none;}
-.uni-steps .step{width:31.3%; margin:0 1%; flex-wrap:nowrap;}
-.uni-steps .step-circle{width:50upx; height:50upx; border-radius:50upx; background:#F1F1F3; justify-content:center; line-height:50upx; flex-shrink:0; margin-right:15upx; color:#666; font-size:28upx;}
-.uni-steps .step-content{width:100%; height:22upx; border-bottom:1px solid #F1F2F3;}
-.uni-steps .step-title{line-height:50upx; height:50upx; background:#FFFFFF; width:auto; overflow:hidden; padding-right:8upx;}
-.uni-steps .current .step-circle{background:#00B26A; color:#FFFFFF;}
-.uni-steps .current .step-content{border-color:#00B26A;}
-.uni-steps .current .step-title{color:#00B26A;}
 </style>
