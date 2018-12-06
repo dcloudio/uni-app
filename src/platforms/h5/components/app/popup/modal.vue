6e9d0048a15258cdbfb3919afb0c3ba31d6a002c
xiaoyucoding <xiaoyucoding@126.com>
2018-12-06 20:32:29 +0800

    fix: 限制 showModal 标题及内容的高度

--- src/platforms/h5/components/app/popup/modal.vue
+++ src/platforms/h5/components/app/popup/modal.vue
@@ -12,7 +12,9 @@
           class="uni-modal__hd">
           <strong class="uni-modal__title">{{ title }}</strong>
         </div>
-        <div class="uni-modal__bd">{{ content }}</div>
+        <div
+          class="uni-modal__bd"
+          @touchmove.stop>{{ content }}</div>
         <div class="uni-modal__ft">
           <div
             v-if="showCancel"
@@ -105,13 +107,19 @@ export default {
 	}
 
 	uni-modal .uni-modal__hd {
-		padding: 1.3em 1.6em 0;
-		line-height: 1;
+		padding: 1em 1.6em 0.3em;
 	}
 
 	uni-modal .uni-modal__title {
 		font-weight: 400;
 		font-size: 18px;
+		word-wrap:break-word;
+		word-break:break-all;
+		overflow : hidden;
+		text-overflow: ellipsis;
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+		-webkit-box-orient: vertical;
 	}
 
 	uni-modal .uni-modal__bd {
@@ -122,6 +130,8 @@ export default {
 		word-wrap: break-word;
 		word-break: break-all;
 		color: #999999;
+		max-height: 400px;
+		overflow-y: auto;
 	}
 
 	uni-modal .uni-modal__ft {
