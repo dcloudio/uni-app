cd711d40c2f5fce53a8732b2d5f6d81327112d84
fxy060608 <fxy060608@gmail.com>
2018-12-25 19:46:13 +0800

    uni-mp-baidu 0.0.4

--- packages/uni-mp-baidu/dist/index.js
+++ packages/uni-mp-baidu/dist/index.js
@@ -310,10 +310,26 @@ var extraApi = /*#__PURE__*/Object.freeze({
   getProvider: getProvider
 });
 
-
+function requestPayment (params) {
+  let parseError = false;
+  if (typeof params.orderInfo === 'string') {
+    try {
+      params.orderInfo = JSON.parse(params.orderInfo);
+    } catch (e) {
+      parseError = true;
+    }
+  }
+  if (parseError) {
+    params.fail({
+      errMsg: 'requestPayment:fail: 参数 orderInfo 数据结构不正确，参考：https://uniapp.dcloud.io/api/plugins/payment?id=orderinfo'
+    });
+  } else {
+    swan.requestPolymerPayment(params);
+  }
+}
 
 var api = /*#__PURE__*/Object.freeze({
-
+  requestPayment: requestPayment
 });
 
 let uni$1 = {};
