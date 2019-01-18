c79fd1aa60e825efa2ee729c7861d5a5489e55a9
xzs02 <service02@dcloud.io>
2019-01-18 20:00:40 +0800

    refactor: 内置页面导航栏按钮图标调整与其他页面图标一致

--- src/platforms/h5/components/system-routes/system-header.vue
+++ src/platforms/h5/components/system-routes/system-header.vue
@@ -4,18 +4,8 @@
       <slot />
     </div>
     <div
-      class="header-btn header-back"
-      @click="_back">
-      <svg
-        class="header-btn-img"
-        width="200px"
-        height="200.00px"
-        viewBox="0 0 1024 1024"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg">
-        <path d="M729.6 931.2l-416-425.6 416-416c9.6-9.6 9.6-25.6 0-35.2-9.6-9.6-25.6-9.6-35.2 0l-432 435.2c-9.6 9.6-9.6 25.6 0 35.2l432 441.6c9.6 9.6 25.6 9.6 35.2 0C739.2 956.8 739.2 940.8 729.6 931.2z" />
-      </svg>
-    </div>
+      class="header-btn header-back uni-btn-icon header-btn-icon"
+      @click="_back">&#xe601;</div>
     <div
       v-if="confirm"
       class="header-btn header-confirm"
@@ -84,15 +74,21 @@ div {
 
 .header-btn-img {
   fill: #fff;
-  width: 40%;
-  height: 40%;
-  margin: 30%;
+  width: 50%;
+  height: 50%;
+  margin: 25%;
 }
 
 .header-back {
   left: 0;
 }
+
 .header-confirm {
   right: 0;
+}
+
+.header-btn-icon {
+  font-size: 27px;
+  line-height: 44px;
 }
 </style>
