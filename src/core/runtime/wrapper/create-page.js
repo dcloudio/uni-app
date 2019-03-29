8e5cea324ab8293e8fc6b8f015ffa0d861d1e516
fxy060608 <fxy060608@gmail.com>
2019-03-29 19:10:12 +0800

    feat(runtime): mp page=>component

--- src/core/runtime/wrapper/create-page.js
+++ src/core/runtime/wrapper/create-page.js
@@ -31,6 +31,39 @@ const hooks = [
   'onNavigationBarSearchInputClicked'
 ]
 
+function attached (VueComponent, args) {
+  if (__PLATFORM__ === 'mp-baidu') {
+    this.$baiduComponentInstances = Object.create(null)
+  }
+
+  this.$vm = new VueComponent({
+    mpType: 'page',
+    mpInstance: this
+  })
+
+  this.$vm.__call_hook('created')
+  if (args) {
+    this.$vm.$mp.query = args // 又要兼容 mpvue
+    this.$vm.__call_hook('onLoad', args) // 开发者可能会在 onLoad 时赋值，提前到 mount 之前
+  }
+  this.$vm.$mount()
+}
+
+function ready () {
+  this.$vm.__call_hook('beforeMount')
+  this.$vm._isMounted = true
+  this.$vm.__call_hook('mounted')
+  this.$vm.__call_hook('onReady')
+}
+
+function detached () {
+  this.$vm.__call_hook('onUnload')
+  if (__PLATFORM__ === 'mp-baidu') { // 百度组件不会在页面 unload 时触发 detached
+    baiduPageDestroy(this.$vm)
+  } else {
+    this.$vm.$destroy()
+  }
+}
 export function createPage (vueOptions) {
   vueOptions = vueOptions.default || vueOptions
   let VueComponent
@@ -41,39 +74,32 @@ export function createPage (vueOptions) {
     VueComponent = Vue.extend(vueOptions)
   }
   const pageOptions = {
+    options: {
+      multipleSlots: true,
+      addGlobalClass: true
+    },
     data: getData(vueOptions, Vue.prototype),
-    onLoad (args) {
-      if (__PLATFORM__ === 'mp-baidu') {
-        this.$baiduComponentInstances = Object.create(null)
-      }
-
-      this.$vm = new VueComponent({
-        mpType: 'page',
-        mpInstance: this
-      })
-
-      this.$vm.__call_hook('created')
-      this.$vm.__call_hook('onLoad', args) // 开发者可能会在 onLoad 时赋值，提前到 mount 之前
-      this.$vm.$mount()
-    },
-    onReady () {
-      this.$vm._isMounted = true
-      this.$vm.__call_hook('mounted')
-      this.$vm.__call_hook('onReady')
-    },
-    onUnload () {
-      this.$vm.__call_hook('onUnload')
-      if (__PLATFORM__ === 'mp-baidu') { // 百度组件不会在页面 unload 时触发 detached
-        baiduPageDestroy(this.$vm)
-      } else {
-        this.$vm.$destroy()
+    lifetimes: { // 当页面作为组件时
+      attached () {
+        attached.call(this, VueComponent)
+      },
+      ready () {
+        ready.call(this)
+      },
+      detached () {
+        detached.call(this)
       }
     },
-    __e: handleEvent,
-    __l: handleLink
+    methods: { // 作为页面时
+      onLoad (args) {
+        attached.call(this, VueComponent, args)
+      },
+      __e: handleEvent,
+      __l: handleLink
+    }
   }
 
-  initHooks(pageOptions, hooks)
+  initHooks(pageOptions.methods, hooks)
 
-  return Page(pageOptions)
+  return Component(pageOptions)
 }
