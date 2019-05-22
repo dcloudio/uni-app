4114898293adf69215976e791a6cf3d36659599d
fxy060608 <fxy060608@gmail.com>
2019-05-22 21:06:37 +0800

    fix(h5): web-view 组件重复加载(http://ask.dcloud.net.cn/question/71466)

--- src/platforms/h5/view/components/web-view/index.vue
+++ src/platforms/h5/view/components/web-view/index.vue
@@ -1,11 +1,5 @@
 <template>
-  <uni-web-view>
-    <iframe
-      :src="$getRealPath(src)"
-      frameborder="0"
-      width="100%"
-      height="100%" />
-  </uni-web-view>
+  <uni-web-view />
 </template>
 <script>
 export default {
@@ -15,11 +9,49 @@ export default {
       type: String,
       default: ''
     }
+  },
+  watch: {
+    src (val, oldVal) {
+      this.iframe && (this.iframe.src = this.$getRealPath(this.src))
+    }
+  },
+  mounted () {
+    const {
+      top,
+      bottom,
+      width,
+      height
+    } = this.$el.getBoundingClientRect()
+
+    this.iframe = document.createElement('iframe')
+    this.iframe.style.position = 'absolute'
+    this.iframe.style.display = 'block'
+    this.iframe.style.border = 0
+    this.iframe.style.top = top + 'px'
+    this.iframe.style.bottom = bottom + 'px'
+    this.iframe.style.width = width + 'px'
+    this.iframe.style.height = height + 'px'
+    this.iframe.src = this.$getRealPath(this.src)
+
+    document.body.appendChild(this.iframe)
+  },
+  activated () {
+    this.iframe.style.display = 'block'
+  },
+  deactivated () {
+    this.iframe.style.display = 'none'
+  },
+  beforeDestroy () {
+    document.body.removeChild(this.iframe)
   }
 }
 </script>
 <style>
-	uni-web-view iframe {
-		position: absolute;
-	}
+    uni-web-view {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+    }
 </style>
