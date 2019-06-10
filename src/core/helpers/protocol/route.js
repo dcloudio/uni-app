96786a984e1094b4337437a2b85043c795024d42
fxy060608 <fxy060608@gmail.com>
2019-06-10 14:20:20 +0800

    fix(h5): improve url query #445 #446

--- src/core/helpers/protocol/route.js
+++ src/core/helpers/protocol/route.js
@@ -1,22 +1,36 @@
 import getRealRoute from '../get-real-route'
 
 function encodeQueryString (url) {
-  if (typeof url === 'string') {
-    const urls = url.split('?')
-
-    url = urls[0]
-
-    const params = [];
-    (urls[1] || '').split('&').forEach(function (pair) {
-      if (pair) {
-        const pairs = pair.split('=')
-        params.push(pairs[0] + '=' + encodeURIComponent(pairs[1]))
-      }
-    })
-
-    return params.length ? url + '?' + params.join('&') : url
+  if (typeof url !== 'string') {
+    return url
   }
-  return url
+  const index = url.indexOf('?')
+
+  if (index === -1) {
+    return url
+  }
+
+  const query = url.substr(index + 1).trim().replace(/^(\?|#|&)/, '')
+
+  if (!query) {
+    return url
+  }
+
+  url = url.substr(0, index)
+
+  const params = []
+
+  query.split('&').forEach(param => {
+    const parts = param.replace(/\+/g, ' ').split('=')
+    const key = parts.shift()
+    const val = parts.length > 0
+      ? parts.join('=')
+      : ''
+
+    params.push(key + '=' + encodeURIComponent(val))
+  })
+
+  return params.length ? url + '?' + params.join('&') : url
 }
 
 function createValidator (type) {
