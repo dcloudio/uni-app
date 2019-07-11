60bd5a1eb11b650ec0264ef90202cbfc2c282d31
qiang <guoshengqiang@live.com>
2019-07-11 21:01:05 +0800

    update: uni-h5 add src

--- build/build.js
+++ build/build.js
@@ -4,6 +4,11 @@ const {
 
 const Service = require('@vue/cli-service')
 
+const del = require('del')
+const copy = require('copy')
+const path = require('path')
+const jsonfile = require('jsonfile')
+
 const service = new Service(process.env.VUE_CLI_CONTEXT || process.cwd(), {
   inlineOptions: require('./vue.config.js')
 })
@@ -18,3 +23,30 @@ service.run('build', {
   error(err)
   process.exit(1)
 })
+
+if (process.env.UNI_WATCH === 'false') {
+  const packagePath = path.join(__dirname, `../packages/uni-${process.env.UNI_PLATFORM}`)
+  const packageJsonPath = path.join(packagePath, 'package.json')
+  del(path.join(packagePath, '{lib,src}'))
+    .then(() => {
+      copy([path.join(__dirname, '../{lib,src}/**/*')], packagePath, function (err, file) {
+        if (err) {
+          throw err
+        }
+      })
+    })
+  jsonfile.readFile(path.join(__dirname, '../package.json'))
+    .then(origin => {
+      return jsonfile.readFile(packageJsonPath)
+        .then(obj => {
+          obj.dependencies = origin.dependencies
+          return obj
+        })
+    })
+    .then(obj => {
+      return jsonfile.writeFile(packageJsonPath, obj, { spaces: 2 })
+    })
+    .catch(err => {
+      throw err
+    })
+}
