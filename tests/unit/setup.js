a291945fa006423d010d0de21f6ae18bd1934808
fxy060608 <fxy060608@gmail.com>
2019-07-12 15:01:44 +0800

    fix(test): add polyfill

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -2,51 +2,64 @@ const path = require('path')
 const glob = require('glob')
 
 global.EVENTS = [
-	'touchstart',
-	'touchmove',
-	'touchcancel',
-	'touchend',
-	'click', //tap=>click
-	'longpress',
-	'longtap',
-	'transitionend',
-	'animationstart',
-	'animationiteration',
-	'animationend',
-	'touchforcechange'
+    'touchstart',
+    'touchmove',
+    'touchcancel',
+    'touchend',
+    'click', //tap=>click
+    'longpress',
+    'longtap',
+    'transitionend',
+    'animationstart',
+    'animationiteration',
+    'animationend',
+    'touchforcechange'
 ]
 
 global.COMPONENTS = []
 
 glob.sync('../../src/core/view/components/**/*/index.vue', {
-	nodir: true,
-	cwd: __dirname
+    nodir: true,
+    cwd: __dirname
 }).forEach(file => {
-	global.COMPONENTS.push(path.basename(path.dirname(file)))
+    global.COMPONENTS.push(path.basename(path.dirname(file)))
 })
 
 glob.sync('../../src/platforms/' + process.env.UNI_PLATFORM + '/view/components/**/*/index.vue', {
-	nodir: true,
-	cwd: __dirname
+    nodir: true,
+    cwd: __dirname
 }).forEach(file => {
-	global.COMPONENTS.push(path.basename(path.dirname(file)))
+    global.COMPONENTS.push(path.basename(path.dirname(file)))
 })
 
 let lastTime = 0
 global.requestAnimationFrame = function(callback, element) {
-	const currTime = new Date().getTime()
-	const timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
-	const id = global.setTimeout(function() {
-		callback(currTime + timeToCall)
-	}, timeToCall)
-	lastTime = currTime + timeToCall
-	return id
+    const currTime = new Date().getTime()
+    const timeToCall = Math.max(0, 16.7 - (currTime - lastTime));
+    const id = global.setTimeout(function() {
+        callback(currTime + timeToCall)
+    }, timeToCall)
+    lastTime = currTime + timeToCall
+    return id
 }
 
 global.cancelAnimationFrame = function(id) {
-	clearTimeout(id)
+    clearTimeout(id)
 }
 
 require('jsdom-global')(undefined, {
-	pretendToBeVisual: true
+    pretendToBeVisual: true
 })
+//fake
+HTMLCanvasElement.prototype.getContext = function getContext() {
+    return {}
+}
+window.Date = Date
+localStorage = {
+    getItem: function(key) {
+        return this[key]
+    },
+    setItem: function(key, value) {
+        this[key] = value
+    }
+}
