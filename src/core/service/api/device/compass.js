efb93b707063d412bd2469e2e86d2edb7e3fa6b7
qiang <guoshengqiang@live.com>
2019-07-30 18:40:36 +0800

    feat: app-plus onCompassChange

---
+++ src/core/service/api/device/compass.js
@@ -0,0 +1,48 @@
+import {
+  invoke
+} from 'uni-core/service/bridge'
+
+import {
+  onMethod,
+  invokeMethod
+} from '../../platform'
+
+const callbacks = []
+
+onMethod('onCompassChange', function (res) {
+  callbacks.forEach(callbackId => {
+    invoke(callbackId, res)
+  })
+})
+
+let isEnable = false
+/**
+ * 监听加速度
+ * @param {*} callbackId
+ */
+export function onCompassChange (callbackId) {
+  // TODO 当没有 start 时，添加 on 需要主动 start?
+  callbacks.push(callbackId)
+  if (!isEnable) {
+    startCompass()
+  }
+}
+
+export function startCompass ({
+  interval // TODO
+} = {}) {
+  if (isEnable) {
+    return
+  }
+  isEnable = true
+  return invokeMethod('enableCompass', {
+    enable: true
+  })
+}
+
+export function stopCompass () {
+  isEnable = false
+  return invokeMethod('enableCompass', {
+    enable: false
+  })
+}
