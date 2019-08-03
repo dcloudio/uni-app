6075139690ec41adbf4b90677bcdf1dc7b68fa4c
qiang <guoshengqiang@live.com>
2019-08-03 17:42:20 +0800

    perf: 修改 app 端 request 实现方式

--- src/platforms/app-plus/service/api/network/request.js
+++ src/platforms/app-plus/service/api/network/request.js
@@ -1,5 +1,6 @@
 import {
-  publish
+  publish,
+  requireNativePlugin
 } from '../../bridge'
 
 const USER_AGENT =
@@ -13,49 +14,74 @@ const publishStateChange = res => {
   delete requestTasks[requestTaskId]
 }
 
-const parseResponseHeaders = headerStr => {
-  const headers = {}
-  if (!headerStr) {
-    return headers
-  }
-  const headerPairs = headerStr.split('\u000d\u000a')
-  for (let i = 0; i < headerPairs.length; i++) {
-    const headerPair = headerPairs[i]
-    const index = headerPair.indexOf('\u003a\u0020')
-    if (index > 0) {
-      const key = headerPair.substring(0, index)
-      const val = headerPair.substring(index + 2)
-      headers[key] = val
-    }
-  }
-  return headers
-}
-
 export function createRequestTaskById (requestTaskId, {
   url,
   data,
   header,
   method = 'GET'
 } = {}) {
+  const stream = requireNativePlugin('stream')
+  const headers = {}
+
   let abortTimeout
-  let xhr
-  // fixed by hxy 始终使用 plus 的 XHR
-  xhr = new plus.net.XMLHttpRequest()
-  xhr.open(method, url, true)
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
+  let aborted
+  let hasContentType = false
+  for (const name in header) {
+    if (!hasContentType && name.toLowerCase() === 'content-type') {
+      hasContentType = true
+      headers['Content-Type'] = header[name]
+    } else {
+      headers[name] = header[name]
+    }
+  }
+
+  if (!hasContentType && method === 'POST') {
+    headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
+  }
+  if (__uniConfig.crossDomain === true) {
+    headers['User-Agent'] = USER_AGENT
+  }
+  if (__uniConfig.networkTimeout.request) {
+    abortTimeout = setTimeout(() => {
+      aborted = true
+      publishStateChange({
+        requestTaskId,
+        state: 'fail',
+        statusCode: 0,
+        errMsg: 'timeout'
+      })
+    }, __uniConfig.networkTimeout.request)
+  }
+  const options = {
+    method,
+    url,
+    // weex 官方文档有误，headers 类型实际 object，用 string 类型会无响应
+    headers,
+    type: 'text'
+  }
+  if (method !== 'GET') {
+    options.body = data
+  }
+  try {
+    stream.fetch(options, ({
+      status,
+      data,
+      headers
+    }) => {
+      if (aborted) {
+        return
+      }
       if (abortTimeout) {
         clearTimeout(abortTimeout)
       }
-      xhr.onreadystatechange = null
-      const statusCode = xhr.status
-      if (statusCode) {
+      const statusCode = status
+      if (statusCode > 0) {
         publishStateChange({
           requestTaskId,
           state: 'success',
-          data: xhr.responseText,
+          data,
           statusCode,
-          header: parseResponseHeaders(xhr.getAllResponseHeaders())
+          header: headers
         })
       } else {
         publishStateChange({
@@ -65,46 +91,21 @@ export function createRequestTaskById (requestTaskId, {
           errMsg: 'abort'
         })
       }
-    }
-  }
-  let hasContentType = false
-  for (const name in header) {
-    if (header.hasOwnProperty(name)) {
-      if (!hasContentType && name.toLowerCase() === 'content-type') {
-        hasContentType = true
-        xhr.setRequestHeader('Content-Type', header[name]) // 大小写必须一致,否则部分服务器会返回invalid header name
-      } else {
-        xhr.setRequestHeader(name, header[name])
+    })
+    requestTasks[requestTaskId] = {
+      abort () {
+        aborted = true
+        if (abortTimeout) {
+          clearTimeout(abortTimeout)
+        }
+        publishStateChange({
+          requestTaskId,
+          state: 'fail',
+          statusCode: 0,
+          errMsg: 'abort'
+        })
       }
     }
-  }
-  if (!hasContentType && method === 'POST') {
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-  }
-  if (__uniConfig.crossDomain === true) {
-    xhr.setRequestHeader('User-Agent', USER_AGENT)
-  }
-
-  if (__uniConfig.networkTimeout.request) {
-    abortTimeout = setTimeout(() => {
-      xhr.onreadystatechange = null
-      xhr.abort()
-      publishStateChange({
-        requestTaskId,
-        state: 'fail',
-        data: xhr.responseText,
-        statusCode: 0,
-        errMsg: 'timeout'
-      })
-    }, __uniConfig.networkTimeout.request)
-  }
-
-  if (typeof data !== 'string' && method === 'GET') {
-    data = null
-  }
-  try {
-    xhr.send(data)
-    requestTasks[requestTaskId] = xhr
   } catch (e) {
     return {
       requestTaskId,
