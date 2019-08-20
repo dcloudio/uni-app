993fd827d4f0371b39aca158eb7e51dfa6a701da
mehaotian <490272692@qq.com>
2019-08-20 13:49:42 +0800

    feat: uni 统计

---
+++ packages/uni-stat/src/index.js
@@ -0,0 +1,45 @@
+import Stat from './stat.js';
+const stat = Stat.getInstance();
+let isHide = false
+const lifecycle = {
+  onLaunch(options) {
+    stat.report(options, this);
+  },
+  onReady() {
+    stat.ready(this);
+  },
+  onLoad(options) {
+    stat.load(options, this);
+  },
+  onShow() {
+    isHide = false
+    stat.show(this);
+  },
+  onHide() {
+    isHide = true
+    stat.hide(this);
+  },
+  onUnload() {
+    if (isHide) {
+      isHide = false
+      return
+    }
+    stat.hide(this);
+  },
+  onError(e) {
+    stat.error(e)
+  },
+  onShareAppMessage() {
+    stat.interceptShare(false)
+  }
+}
+
+function main() {
+  const Vue = require('vue');
+  (Vue.default || Vue).mixin(lifecycle);
+  uni.report = function(type, options) {
+    stat.sendEvent(type, options);
+  };
+}
+
+main();
