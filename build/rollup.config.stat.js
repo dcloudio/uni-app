4b3da1ccbe1e5b086536fb50e0d008cd3b98141a
fxy060608 <fxy060608@gmail.com>
2019-08-20 17:04:37 +0800

    chore(stat): lint

--- build/rollup.config.stat.js
+++ build/rollup.config.stat.js
@@ -1,13 +1,3 @@
-const path = require('path')
-const alias = require('rollup-plugin-alias')
-const replace = require('rollup-plugin-replace')
-const nodeResolve = require('rollup-plugin-node-resolve')
-const commonjs = require('rollup-plugin-commonjs')
-const requireContext = require('../lib/rollup-plugin-require-context')
-
-
-const resolve = dir => path.resolve(__dirname, '../', dir)
-
 module.exports = {
   input: 'packages/uni-stat/src/index.js',
   output: {
