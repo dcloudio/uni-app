99abbc4d79322ee98ae743ab49838c3b113a1537
fxy060608 <fxy060608@gmail.com>
2019-09-06 15:24:14 +0800

    feat(cli): recycle-list

--- packages/vue-cli-plugin-hbuilderx/packages/webpack-uni-nvue-loader/lib/template.js
+++ packages/vue-cli-plugin-hbuilderx/packages/webpack-uni-nvue-loader/lib/template.js
@@ -1,4 +1,7 @@
 module.exports = function(content) {
-    this.cacheable && this.cacheable()
+  this.cacheable && this.cacheable()
+  if (content.indexOf('recycle-list') === -1) {
     return `<scroll-view :scroll-y="true" :enableBackToTop="true" bubble="true" style="flex-direction:column">${content}</scroll-view>`
+  }
+  return content
 }
