e8bfd051ec475f7cfe7a66151f666fb357733392
mehaotian <490272692@qq.com>
2019-09-16 11:27:02 +0800

    fix(stat):  修改统计上报域名

--- packages/uni-stat/src/config.js
+++ packages/uni-stat/src/config.js
@@ -1,10 +1,10 @@
 import {
   version
 } from '../package.json'
-export const STAT_VERSION = version 
-export const STAT_URL = 'https://uniapp.dcloud.io/uni/stat' 
-export const STAT_H5_URL = 'https://uniapp.dcloud.io/uni/stat.gif' 
-export const STAT_KEY = 'qkTHEIegZGcL5iy3' 
-export const PAGE_PVER_TIME = 1800 
-export const APP_PVER_TIME = 300 
-export const OPERATING_TIME = 10 
+export const STAT_VERSION = version
+export const STAT_URL = 'https://tongji.dcloud.io/uni/stat'
+export const STAT_H5_URL = 'https://tongji.dcloud.io/uni/stat.gif' 
+export const STAT_KEY = 'qkTHEIegZGcL5iy3'
+export const PAGE_PVER_TIME = 1800
+export const APP_PVER_TIME = 300
+export const OPERATING_TIME = 10
