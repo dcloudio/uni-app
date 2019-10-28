c69c516bdbb3b59d23d72f42d8c434db65866ee4
fxy060608 <fxy060608@gmail.com>
2019-10-28 18:37:00 +0800

    feat(v3): init createVueComponent

---
+++ src/core/runtime/wrapper/create-vue-component/index.js
@@ -0,0 +1,14 @@
+import {
+  parseProperties
+} from './parser'
+
+export default function createVueComponent (mpComponentOptions) {
+  const vueComponentOptions = {
+    watch: {},
+    mpObservers: []
+  }
+
+  parseProperties(mpComponentOptions.properties, vueComponentOptions)
+
+  return vueComponentOptions
+}
---
+++ src/core/runtime/wrapper/create-vue-component/parser/lifetimes-parser.js
@@ -0,0 +1,5 @@
+export function parseLifetimes (lifetimes, vueComponentOptions) {
+  if (!lifetimes) {
+
+  }
+}
