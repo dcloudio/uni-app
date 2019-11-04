7a62c78d457f6fba21f28ea3880f8617858fd6e0
fxy060608 <fxy060608@gmail.com>
2019-11-04 15:45:16 +0800

    feat(h5-vue): renderList

--- packages/vue-cli-plugin-uni/packages/h5-vue/dist/vue.runtime.js
+++ packages/vue-cli-plugin-uni/packages/h5-vue/dist/vue.runtime.js
@@ -705,7 +705,13 @@
    * directives subscribing to it.
    */
   var Dep = function Dep () {
-    this.id = uid++;
+    // fixed by xxxxxx (nvue vuex)
+    /* eslint-disable no-undef */
+    if(typeof SharedObject !== 'undefined'){
+      this.id = SharedObject.uid++;
+    } else {
+      this.id = uid++;
+    }
     this.subs = [];
   };
 
@@ -2635,12 +2641,12 @@
     if (Array.isArray(val) || typeof val === 'string') {
       ret = new Array(val.length);
       for (i = 0, l = val.length; i < l; i++) {
-        ret[i] = render(val[i], i);
+        ret[i] = render(val[i], i, i, i); // fixed by xxxxxx
       }
     } else if (typeof val === 'number') {
       ret = new Array(val);
       for (i = 0; i < val; i++) {
-        ret[i] = render(i + 1, i);
+        ret[i] = render(i + 1, i, i, i); // fixed by xxxxxx
       }
     } else if (isObject(val)) {
       if (hasSymbol && val[Symbol.iterator]) {
@@ -2648,7 +2654,7 @@
         var iterator = val[Symbol.iterator]();
         var result = iterator.next();
         while (!result.done) {
-          ret.push(render(result.value, ret.length));
+          ret.push(render(result.value, ret.length, i++, i)); // fixed by xxxxxx
           result = iterator.next();
         }
       } else {
@@ -2656,7 +2662,7 @@
         ret = new Array(keys.length);
         for (i = 0, l = keys.length; i < l; i++) {
           key = keys[i];
-          ret[i] = render(val[key], key, i);
+          ret[i] = render(val[key], key, i, i); // fixed by xxxxxx
         }
       }
     }
