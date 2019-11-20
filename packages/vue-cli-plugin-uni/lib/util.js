9c91a22c9fa410910c471cb9ca73e5912730d0d8
fxy060608 <fxy060608@gmail.com>
2019-11-20 19:51:23 +0800

    feat(cli): createTemplateCacheLoader

--- packages/vue-cli-plugin-uni/lib/util.js
+++ packages/vue-cli-plugin-uni/lib/util.js
@@ -16,17 +16,16 @@ module.exports = {
     'link,meta,param,source,track,wbr'
   ),
   getPartialIdentifier () {
-    if (partialIdentifier) {
-      return partialIdentifier
-    }
-    partialIdentifier = {
-      'UNI_COMPILER_VERSION': require('../package.json').version
-    }
-    Object.keys(process.env).forEach(name => {
-      if (name.indexOf('UNI_') === 0) {
-        partialIdentifier[name] = process.env[name]
+    if (!partialIdentifier) {
+      partialIdentifier = {
+        'UNI_COMPILER_VERSION': require('../package.json').version
       }
-    })
+      Object.keys(process.env).forEach(name => {
+        if (name.indexOf('UNI_') === 0) {
+          partialIdentifier[name] = process.env[name]
+        }
+      })
+    }
     return partialIdentifier
   }
 }
