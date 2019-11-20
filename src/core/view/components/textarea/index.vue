a2265b1db9c21f4665048bbdb05d5f553e5d4c69
陈晨 <chen458679677@gmail.com>
2019-11-20 21:09:30 +0800

    fix:iOS13兼容textarea的margin

--- src/core/view/components/textarea/index.vue
+++ src/core/view/components/textarea/index.vue
@@ -122,7 +122,7 @@ export default {
       focusSync: this.focus,
       height: 0,
       focusChangeSource: '',
-      isIOS: String(navigator.platform).indexOf('iP') === 0 && String(navigator.vendor).indexOf('Apple') === 0
+      isIOS: String(navigator.platform).indexOf('iP') === 0 && String(navigator.vendor).indexOf('Apple') === 0 && String(navigator.appVersion).split('OS ')[1].split('_')[0] < 13
     }
   },
   computed: {
