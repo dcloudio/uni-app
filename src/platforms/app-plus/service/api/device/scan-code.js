c6c6f3bc129d014ee1cc94804281b1de5d79f54e
qiang <guoshengqiang@live.com>
2019-11-21 21:39:34 +0800

    fix: template

--- src/platforms/app-plus/service/api/device/scan-code.js
+++ src/platforms/app-plus/service/api/device/scan-code.js
@@ -2,6 +2,6 @@ import * as webview from './scan-code-webview'
 import * as weex from './scan-code-weex'
 
 export function scanCode (...array) {
-  const api = __uniConfig.nvueCompiler === 'uni-app' ? weex : webview
+  const api = __uniConfig.nvueCompiler !== 'weex' ? weex : webview
   return api.scanCode(...array)
 }
--- src/platforms/app-plus/service/api/location/open-location.js
+++ src/platforms/app-plus/service/api/location/open-location.js
@@ -2,6 +2,6 @@ import * as webview from './open-location-webview'
 import * as weex from './open-location-weex'
 
 export function openLocation (...array) {
-  const api = __uniConfig.nvueCompiler === 'uni-app' ? weex : webview
+  const api = __uniConfig.nvueCompiler !== 'weex' ? weex : webview
   return api.openLocation(...array)
 }
