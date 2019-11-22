7a892987e51320d0aa677a7659801f19d3338fb8
fxy060608 <fxy060608@gmail.com>
2019-11-22 17:15:37 +0800

    fix(v3): iOS timerFunc

--- packages/uni-app-plus/dist/service.runtime.esm.js
+++ packages/uni-app-plus/dist/service.runtime.esm.js
@@ -1930,7 +1930,7 @@ var timerFunc;
 // completely stops working after triggering a few times... so, if native
 // Promise is available, we will use it:
 /* istanbul ignore next, $flow-disable-line */
-if (typeof Promise !== 'undefined' && isNative(Promise)) {
+if (typeof Promise !== 'undefined' && isNative(Promise) && !isIOS) { // fixed by xxxxxx
   var p = Promise.resolve();
   timerFunc = function () {
     p.then(flushCallbacks);
@@ -1939,8 +1939,7 @@ if (typeof Promise !== 'undefined' && isNative(Promise)) {
     // microtask queue but the queue isn't being flushed, until the browser
     // needs to do some other work, e.g. handle a timer. Therefore we can
     // "force" the microtask queue to be flushed by adding an empty timer.
-    // fixed by xxxxxx
-    // if (isIOS) { setTimeout(noop); }
+    if (isIOS) { setTimeout(noop); }
   };
 } else if (!isIE && typeof MutationObserver !== 'undefined' && (
   isNative(MutationObserver) ||
@@ -6671,7 +6670,7 @@ function updateDOMListeners (oldVnode, vnode) {
   var on = vnode.data.on || {};
   var oldOn = oldVnode.data.on || {};
   target$1 = vnode.elm;
-
+  
   // fixed by xxxxxx 存储 vd
   target$1._$vd = vnode.context._$vd;
   var context = vnode.context;
