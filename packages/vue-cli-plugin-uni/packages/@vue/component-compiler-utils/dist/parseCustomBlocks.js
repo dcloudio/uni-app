bfb6cc6210a89080a5d00daa2f2ba156ba5d6f4f
fxy060608 <fxy060608@gmail.com>
2019-12-26 16:44:08 +0800

    chore(cli): add component-compiler-utils

---
+++ packages/vue-cli-plugin-uni/packages/@vue/component-compiler-utils/dist/parseCustomBlocks.js
@@ -0,0 +1,52 @@
+const {
+  getPlatformFilterTag,
+  normalizeNodeModules
+} = require('@dcloudio/uni-cli-shared/lib/platform')
+
+const FILTER_TAG = getPlatformFilterTag()
+
+module.exports = function parseCustomBlocks(descriptor, options) {
+
+  if (!descriptor.template || !FILTER_TAG || options.isAppNVue) {
+    // delete customBlocks
+    descriptor.customBlocks.length = 0
+    return descriptor
+  }
+
+  const modules = Object.create(null)
+
+  descriptor.customBlocks = descriptor.customBlocks.filter(block => {
+    if (
+      block.attrs.module &&
+      (
+        block.type === FILTER_TAG ||
+        block.attrs.lang === FILTER_TAG
+      )
+    ) {
+      modules[block.attrs.module] = block
+      return true
+    }
+    if ( // renderjs
+      options.isAppView &&
+      (
+        block.type === 'renderjs' ||
+        block.attrs.lang === 'renderjs'
+      )
+    ) {
+      return true
+    }
+  })
+
+  if (Object.keys(modules).length) {
+    const filterModules = JSON.parse(JSON.stringify(modules))
+    Object.keys(filterModules).forEach(name => {
+      const filterModule = filterModules[name]
+      if (filterModule.attrs.src) {
+        filterModule.attrs.src = normalizeNodeModules(filterModule.attrs.src)
+      }
+    })
+    descriptor.template.attrs['filter-modules'] = JSON.stringify(filterModules)
+  }
+
+  return descriptor
+}
---
+++ packages/vue-cli-plugin-uni/packages/@vue/component-compiler-utils/dist/stylePlugins/trim.d.ts
@@ -0,0 +1,3 @@
+import * as postcss from 'postcss';
+declare const _default: postcss.Plugin<unknown>;
+export default _default;
