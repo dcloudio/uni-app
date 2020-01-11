653398aa0b3e4ac497b2f7ebb7405183cf7e6c45
qiang <guoshengqiang@live.com>
2020-01-11 21:36:21 +0800

    fix: 解决 input 组件设置 top 样式导致错位的问题 fixed #1222

--- src/core/view/components/input/index.vue
+++ src/core/view/components/input/index.vue
@@ -282,10 +282,12 @@ uni-input[hidden] {
 
 .uni-input-wrapper,
 .uni-input-form {
-  display: block;
+  display: flex;
   position: relative;
   width: 100%;
   height: 100%;
+  flex-direction: column;
+  justify-content: center;
 }
 
 .uni-input-placeholder,
@@ -295,15 +297,15 @@ uni-input[hidden] {
 
 .uni-input-placeholder {
   position: absolute;
-  top: 50%;
+  top: auto !important;
   left: 0;
-  transform: translateY(-50%);
   color: gray;
   overflow: hidden;
   text-overflow: clip;
   white-space: pre;
   word-break: keep-all;
   pointer-events: none;
+  line-height: inherit;
 }
 
 .uni-input-input {
