4251ebf7fab2cf22407c40776cdedacfa74d86d7
handongxun <handongxun@dcloud.io>
2020-01-16 12:23:14 +0800

    update: app-vue 使用 <ad> 组件时提示用户开启 V3 编译

--- packages/uni-template-compiler/lib/module.js
+++ packages/uni-template-compiler/lib/module.js
@@ -12,7 +12,7 @@ module.exports = {
     warn
   }) {
     if (process.env.UNI_PLATFORM === 'app-plus' && el.tag === 'ad') {
-      warn("app-vue平台, <ad> 组件暂不支持非 V3编译, 详见: https://ask.dcloud.net.cn/article/36599")
+      warn("app-vue平台, <ad> 组件暂不支持非 V3 编译, 详见: https://ask.dcloud.net.cn/article/36599")
     }
     if (el.tag === 'slot' && !el.attrsMap['name']) {
       el.attrsList.push({
