0f32997c05b27ab9d6af620f29412cdb44493311
fxy060608 <fxy060608@gmail.com>
2020-02-15 18:00:05 +0800

    feat(v3): add confusion

--- packages/vue-cli-plugin-uni/packages/webpack-uni-app-loader/plugin/index.js
+++ packages/vue-cli-plugin-uni/packages/webpack-uni-app-loader/plugin/index.js
@@ -10,6 +10,27 @@ const {
 
 class WebpackUniAppPlugin {
   apply(compiler) {
+    if (process.UNI_CONFUSION) {
+      compiler.hooks.emit.tapPromise('webpack-uni-app-emit', compilation => {
+        return new Promise((resolve, reject) => {
+          if (compilation.assets['app-confusion.js']) { //存在加密
+            const manifestJson = JSON.parse(`${compilation.assets['manifest.json'].source()}`)
+            manifestJson.plus.confusion.resources['app-confusion.js'] = {}
+            const source = JSON.stringify(manifestJson)
+            compilation.assets['manifest.json'] = {
+              size() {
+                return Buffer.byteLength(source, 'utf8')
+              },
+              source() {
+                return source
+              }
+            }
+          }
+          resolve()
+        })
+      })
+    }
+
     compiler.hooks.invalid.tap('webpack-uni-app-invalid', (fileName, changeTime) => {
       if (fileName && typeof fileName === 'string') {
         if (fileName.indexOf('.vue') !== -1 || fileName.indexOf('.nvue') !== -1) {
