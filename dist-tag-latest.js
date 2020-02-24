0a7a55a650cb599c8bb24ee6a1468874fd5776d0
fxy060608 <fxy060608@gmail.com>
2020-02-24 14:27:39 +0800

    chore(release): ignore .DS_Store

--- dist-tag-latest.js
+++ dist-tag-latest.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const shellExec = require('shell-exec')
 
-const pkgs = fs.readdirSync(path.resolve(__dirname, 'packages'))
+const pkgs = fs.readdirSync(path.resolve(__dirname, 'packages')).filter(pkg => pkg.indexOf('.') !== 0)
 
 const version = process.argv[2]
 if (!version) {
