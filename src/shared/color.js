5f47b4a8e8c342e9fa67e85420ef1b94e96012c8
xlfsummer <20750969+xlfsummer@users.noreply.github.com>
2020-03-15 22:56:13 +0800

    refactor: 优化代码，减少不必要的判断

--- src/shared/color.js
+++ src/shared/color.js
@@ -8,20 +8,11 @@ export function hexToRgba (hex) {
     g = hex.substring(2, 4)
     b = hex.substring(4, 6)
   } else if (hex.length === 3) {
-    r = hex.substring(0, 1)
-    g = hex.substring(1, 2)
-    b = hex.substring(2, 3)
+    r = hex.substring(0, 1).repeat(2)
+    g = hex.substring(1, 2).repeat(2)
+    b = hex.substring(2, 3).repeat(2)
   } else {
     return false
-  }
-  if (r.length === 1) {
-    r += r
-  }
-  if (g.length === 1) {
-    g += g
-  }
-  if (b.length === 1) {
-    b += b
   }
   r = parseInt(r, 16)
   g = parseInt(g, 16)
