0fec7c310fe1c1fc037a7617887fb4839177182e
handongxun <handongxun@dcloud.io>
2020-03-27 18:03:39 +0800

    fix: openLocation 点击导航栏后退2次的Bug，支持ios侧滑后退

--- src/platforms/app-plus/service/api/location/open-location-weex.js
+++ src/platforms/app-plus/service/api/location/open-location-weex.js
@@ -2,14 +2,21 @@ import {
   showPage
 } from '../page.js'
 
-export function openLocation (data) {
+export function openLocation (data, callbackId) {
   showPage({
     url: '__uniappopenlocation',
     data,
     style: {
       titleNView: {
         type: 'transparent'
-      }
+      },
+      popGesture: 'close',
+      backButtonAutoControl: 'close'
+    },
+    onClose() {
+      invoke(callbackId, {
+        errMsg: 'openLocation:fail cancel'
+      })
     }
   })
   return {
