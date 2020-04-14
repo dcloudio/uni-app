e408d001ffb5917ae94f96ed1692473f73cdf4cb
qiang <guoshengqiang@live.com>
2020-04-14 21:26:51 +0800

    fix: 修复部分组件嵌套在 uni-popup 组件内渲染错误的问题

--- src/core/view/components/swiper/index.vue
+++ src/core/view/components/swiper/index.vue
@@ -1,24 +1,7 @@
 
 <script>
 import touchtrack from 'uni-mixins/touchtrack'
-
-function deepClone (vnodes, createElement) {
-  function cloneVNode (vnode) {
-    var clonedChildren = vnode.children && vnode.children.map(cloneVNode)
-    var cloned = createElement(vnode.tag, vnode.data, clonedChildren)
-    cloned.text = vnode.text
-    cloned.isComment = vnode.isComment
-    cloned.componentOptions = vnode.componentOptions
-    cloned.elm = vnode.elm
-    cloned.context = vnode.context
-    cloned.ns = vnode.ns
-    cloned.isStatic = vnode.isStatic
-    cloned.key = vnode.key
-    return cloned
-  }
-
-  return vnodes.map(cloneVNode)
-}
+import { deepClone } from 'uni-shared'
 
 export default {
   name: 'Swiper',
--- src/shared/util.js
+++ src/shared/util.js
@@ -135,3 +135,21 @@ export function looseEqual (a, b) {
     return false
   }
 }
+
+export function deepClone (vnodes, createElement) {
+  function cloneVNode (vnode) {
+    var clonedChildren = vnode.children && vnode.children.map(cloneVNode)
+    var cloned = createElement(vnode.tag, vnode.data, clonedChildren)
+    cloned.text = vnode.text
+    cloned.isComment = vnode.isComment
+    cloned.componentOptions = vnode.componentOptions
+    cloned.elm = vnode.elm
+    cloned.context = vnode.context
+    cloned.ns = vnode.ns
+    cloned.isStatic = vnode.isStatic
+    cloned.key = vnode.key
+    return cloned
+  }
+
+  return vnodes.map(cloneVNode)
+}
