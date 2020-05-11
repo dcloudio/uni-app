717486946ac2d23ee8689fa74f0329acf6f61a42
fxy060608 <fxy060608@gmail.com>
2020-05-11 17:25:26 +0800

    fix(v3): asset url

--- packages/uni-template-compiler/lib/asset-url.js
+++ packages/uni-template-compiler/lib/asset-url.js
@@ -68,10 +68,15 @@ function rewrite (attr, name, options) {
           .replace('"@/', '"/')
           .replace('"~@/', '"/')
       }
-      // h5
-      if (options.h5) {
+      // v3,h5
+      const needRequire = options.service || options.view || options.h5
+      if (needRequire) {
         attr.value = urlToRequire(attr.value.slice(1, -1))
-        if (attr.value.startsWith('require("') && options.publicPath === './') { // require
+        if (
+          options.h5 &&
+          options.publicPath === './' &&
+          attr.value.startsWith('require("')
+        ) { // require
           // h5 且 publicPath 为 ./ (仅生产模式可能为./)
           attr.value = `(${attr.value}).substr(1)`
         }
