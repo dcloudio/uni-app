db5c15ad20bf2e21f42c9c629321fe09faac63a4
WangNing <iqshen@foxmail.com>
2020-05-19 17:02:33 +0800

    修复百度小程序下mounted可能不执行的问题

--- src/platforms/mp-baidu/runtime/wrapper/util.js
+++ src/platforms/mp-baidu/runtime/wrapper/util.js
@@ -1,7 +1,9 @@
 export const mocks = ['nodeId', 'componentName', '_componentId', 'uniquePrefix']
 
 export function isPage () {
-  return !this.ownerId
+  // 百度小程序组件的id，某些情况下可能是number类型的0，不能直接return !this.ownerId 判断当前组件是否是Page
+  // 否则会导致mounted不执行
+  return typeof this.ownerId === 'undefined'
 }
 
 export function initRelation (detail) {
