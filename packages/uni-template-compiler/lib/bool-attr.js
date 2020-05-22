74d68d1b3c54433c173c62f66edc7364e1b783cb
fxy060608 <fxy060608@gmail.com>
2020-05-22 11:22:03 +0800

    fix(cli): ignore id,class,style,inline-template (bool-attr)

--- packages/uni-template-compiler/lib/bool-attr.js
+++ packages/uni-template-compiler/lib/bool-attr.js
@@ -1,5 +1,7 @@
 const dirRE = /^v-|^@|^:|^#/
 
+const PROPS = ['id', 'class', 'style', 'inline-template']
+
 module.exports = {
   preTransformNode (el) {
     if (!el.attrsList) {
@@ -7,7 +9,7 @@ module.exports = {
     }
     el.attrsList.forEach(attr => {
       if (attr.bool) {
-        if (!dirRE.test(attr.name) && attr.name !== 'inline-template') {
+        if (!dirRE.test(attr.name) && !PROPS.includes(attr.name)) {
           delete el.attrsMap[attr.name]
           attr.name = ':' + attr.name
           attr.value = 'true'
