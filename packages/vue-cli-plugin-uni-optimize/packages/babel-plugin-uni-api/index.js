8104efa8419f01b2fa7a05237c386220988291db
fxy060608 <fxy060608@gmail.com>
2020-06-01 17:08:22 +0800

    fix(h5): optimize with map

--- packages/vue-cli-plugin-uni-optimize/packages/babel-plugin-uni-api/index.js
+++ packages/vue-cli-plugin-uni-optimize/packages/babel-plugin-uni-api/index.js
@@ -1,13 +1,4 @@
-const path = require('path')
-
-const {
-  normalizePath
-} = require('@dcloudio/uni-cli-shared')
-
 process.UNI_APIS = new Set()
-
-const sourcePath = normalizePath(path.join(require.resolve('@dcloudio/uni-h5'), '../../'))
-
 module.exports = function ({
   types: t
 }) {
@@ -21,10 +12,6 @@ module.exports = function ({
             path.node.object.name === 'wx'
           )
         ) {
-          if (normalizePath(state.file.opts.filename).indexOf(sourcePath) === 0) {
-            path.stop()
-            return
-          }
           process.UNI_APIS.add(path.node.property.name || path.node.property.value)
         }
       }
