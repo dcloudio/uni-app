0bb9de28af4eba4e929b65c4a1ef97faee40f58c
wangyaqi <wangyaqi988@163.com>
2020-06-08 17:28:46 +0800

    fix: 修复createAnimation时部分动画使用rpx无效果的Bug

--- src/core/view/components/icon/index.vue
+++ src/core/view/components/icon/index.vue
@@ -28,9 +28,9 @@ export default {
   },
   methods: {
     _converPx (value) {
-      if (/\d+[ur]px$/i.test(value)) {
-        value.replace(/\d+[ur]px$/i, text => {
-          return `${uni.upx2px(parseFloat(text))}px`
+      if (/^-?\d+[ur]px$/i.test(value)) {
+        return value.replace(/(^-?\d+)[ur]px$/i, (text, num) => {
+          return `${uni.upx2px(parseFloat(num))}px`
         })
         // eslint-disable-next-line no-useless-escape
       } else if (/^-?[\d\.]+$/.test(value)) {
