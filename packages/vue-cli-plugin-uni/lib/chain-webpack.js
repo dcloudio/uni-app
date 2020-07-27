68cd6c67ec6cdecc345c6004de565691ef725103
fxy060608 <fxy060608@gmail.com>
2020-07-27 13:04:14 +0800

    refactor(vue3): __FEATURE_OPTIONS__ => __VUE_OPTIONS_API__

--- packages/vue-cli-plugin-uni/lib/chain-webpack.js
+++ packages/vue-cli-plugin-uni/lib/chain-webpack.js
@@ -91,7 +91,7 @@ module.exports = function chainWebpack (platformOptions, vueOptions, api) {
     platformOptions.chainWebpack(webpackConfig, vueOptions, api)
     // define
     const defines = {
-      __FEATURE_OPTIONS__: JSON.stringify(process.env.UNI_USING_VUE3_OPTIONS === 'true'),
+      __VUE_OPTIONS_API__: JSON.stringify(process.env.UNI_USING_VUE3_OPTIONS_API === 'true'),
       'process.env.UNI_ENV': JSON.stringify(process.env.UNI_PLATFORM),
       'process.env.UNI_CLOUD_PROVIDER': process.env.UNI_CLOUD_PROVIDER,
       'process.env.HBX_USER_TOKEN': JSON.stringify(process.env.HBX_USER_TOKEN || ''),
