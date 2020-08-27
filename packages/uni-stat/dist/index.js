6a41a2bd20a8650775de7216772aa620f6a84430
qiang <guoshengqiang@live.com>
2020-08-27 18:04:52 +0800

    build stat

--- packages/uni-stat/dist/index.js
+++ packages/uni-stat/dist/index.js
@@ -78,7 +78,8 @@ const getPlatformName = () => {
     'mp-alipay': 'ali',
     'mp-baidu': 'bd',
     'mp-toutiao': 'tt',
-    'mp-qq': 'qq'
+    'mp-qq': 'qq',
+    'mp-kuaishou': 'ks'
   };
   return platformList[process.env.VUE_APP_PLATFORM];
 };
@@ -380,12 +381,9 @@ const requestData = (done) => {
       if (report_status_code === '') {
         report_status_code = 1;
       }
-      if (report_status_code === 1) {
-        typeof done === 'function' && done({
-          enable: res.enable
-        });
-      }
-      // console.error('统计请求错误');
+      typeof done === 'function' && done({
+        enable: report_status_code
+      });
     }
   });
 };
