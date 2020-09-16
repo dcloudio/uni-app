a140b293db8edff5b5a12904df523b8635b90466
tianjiaxing <tianjiaxing2016@163.com>
2020-09-16 18:48:20 +0800

    miniprogram suppout  match-media and uni.createMediaQueryObserver

---
+++ packages/uni-cli-shared/components/uni-match-media.vue
@@ -0,0 +1,75 @@
+<template>
+  <view
+    v-show="matches"
+  > 
+    <slot />
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'UniMatchMedia',
+  props: {
+    width: {
+      type: [Number, String],
+      default: ''
+    },
+    minWidth: {
+      type: [Number, String],
+      default: ''
+    },
+    maxWidth: {
+      type: [Number, String],
+      default: ''
+    },
+    height: {
+      type: [Number, String],
+      default: ''
+    },
+    minHeight: {
+      type: [Number, String],
+      default: ''
+    },
+    maxHeight: {
+      type: [Number, String],
+      default: ''
+    },
+    orientation: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data () {
+    return {
+      matches: false
+    }
+  },
+
+  mounted() {
+    const mediaQ = uni.createMediaQueryObserver()
+    mediaQ.observe({
+        width: this.width,
+        maxWidth: this.maxWidth,
+        minWidth: this.minWidth,
+        height: this.height,
+        minHeight: this.minHeight,
+        maxHeight: this.maxHeight,
+        orientation: this.orientation
+      }, matches => {
+        this.matches = matches
+      })
+  },
+
+}
+</script>
+
+<style>
+  view {
+    display: block;
+  }
+
+  view[hidden] {
+    display: none;
+  }
+</style>
---
+++ src/platforms/mp-qq/runtime/api/index.js
@@ -0,0 +1,3 @@
+import createMediaQueryObserver from '../../../mp-weixin/helpers/create-media-query-observer'
+
+export { createMediaQueryObserver }
