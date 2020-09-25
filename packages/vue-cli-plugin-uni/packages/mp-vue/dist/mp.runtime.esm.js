f370f896ea80432bd53b7eb6c6f250e197d2523e
qiang <guoshengqiang@live.com>
2020-09-25 16:51:33 +0800

    fix: 95628713895d17fc7434f64a9271ce478a299462

--- packages/vue-cli-plugin-uni/packages/mp-vue/dist/mp.runtime.esm.js
+++ packages/vue-cli-plugin-uni/packages/mp-vue/dist/mp.runtime.esm.js
@@ -5928,7 +5928,7 @@ function internalMixin(Vue) {
   };
 
   Vue.prototype.__map = function(val, iteratee) {
-    //TODO 暂不考虑 string,number
+    //TODO 暂不考虑 string
     var ret, i, l, keys, key;
     if (Array.isArray(val)) {
       ret = new Array(val.length);
@@ -5942,6 +5942,13 @@ function internalMixin(Vue) {
       for (i = 0, l = keys.length; i < l; i++) {
         key = keys[i];
         ret[key] = iteratee(val[key], key, i);
+      }
+      return ret
+    } else if (typeof val === 'number') {
+      ret = new Array(val);
+      for (i = 0, l = val; i < l; i++) {
+        // 第一个参数暂时仍和小程序一致
+        ret[i] = iteratee(i, i);
       }
       return ret
     }
