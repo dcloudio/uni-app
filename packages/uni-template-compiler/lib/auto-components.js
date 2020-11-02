d67ae576dfd2cff6ad4cbc2f0ffb386341b6399b
fxy060608 <fxy060608@gmail.com>
2020-11-02 20:21:11 +0800

    chore(cli): add tips (easycom)

--- packages/uni-template-compiler/lib/auto-components.js
+++ packages/uni-template-compiler/lib/auto-components.js
@@ -68,7 +68,19 @@ function generateAutoComponentsCode (autoComponents, dynamic = false) {
       components.push(`'${name}': require('${source}').default`)
     }
   })
-  return `var components = {${components.join(',')}}`
+  return `var components;
+try{
+  components = {${components.join(',')}}
+}catch(e){
+  if(e.message.indexOf('Cannot find module') !== -1 && e.message.indexOf('.vue') !== -1){
+    console.error(e.message)
+    console.error('1. 排查组件名称拼写是否正确')
+    console.error('2. 排查组件是否符合 easycom 规范，文档：https://uniapp.dcloud.net.cn/collocation/pages?id=easycom')
+    console.error('3. 若组件不符合 easycom 规范，需手动引入，并在 components 中注册该组件')
+  } else {
+    throw e
+  }
+}`
 }
 
 function compileTemplate (source, options, compile) {
