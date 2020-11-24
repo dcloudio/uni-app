6e941c0d90cdeafe4db0826cdddff5fd65ec54ab
liuxiaohang <283700113@qq.com>
2020-11-24 14:56:54 +0800

    fix(h5): 修复在 微信环境里  chooseFile 默认值提示 没有应用可执行此操作

--- src/platforms/h5/service/api/media/create_input.js
+++ src/platforms/h5/service/api/media/create_input.js
@@ -1,4 +1,14 @@
 import { updateElementStyle } from 'uni-shared'
+const ALL = '*'
+
+function isWXEnv () {
+  const ua = window.navigator.userAgent.toLowerCase()
+  if (ua.match(/MicroMessenger/i) && ua.match(/MicroMessenger/i)[0] === 'micromessenger') {
+    return true
+  } else {
+    return false
+  }
+}
 
 export default function ({ count, sourceType, type, extension }) {
   const inputEl = document.createElement('input')
@@ -15,10 +25,14 @@ export default function ({ count, sourceType, type, extension }) {
   })
 
   inputEl.accept = extension.map(item => {
-    if (type !== '*') {
+    if (type !== ALL) {
       // 剔除.拼接在type后
       return `${type}/${item.replace('.', '')}`
     } else {
+      // 在微信环境里，'.jpeg,.png' 会提示没有应用可执行此操作
+      if (isWXEnv()) {
+        return '.'
+      }
       // 在后缀前方加上.
       return item.indexOf('.') === 0 ? item : `.${item}`
     }
