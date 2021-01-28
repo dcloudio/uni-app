4f5b117a18f1e96b3b046ff9c85e4eda66b2be44
DCloud_LXH <283700113@qq.com>
2021-01-28 15:22:33 +0800

    fix(h5): 修复chooseImage和chooseVideo在extendsion为空数组时表现错误

--- src/core/helpers/protocol/media/choose-video.js
+++ src/core/helpers/protocol/media/choose-video.js
@@ -20,6 +20,9 @@ export const chooseVideo = {
   },
   extension: {
     type: Array,
-    default: ['*']
+    default: ['*'],
+    validator (extension, params) {
+      if (extension.length === 0) params.extension = ['*']
+    }
   }
 }
