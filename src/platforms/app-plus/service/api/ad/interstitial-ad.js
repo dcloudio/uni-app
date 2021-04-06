4e660eec4b93fb2eb3021196e1d140b35aa68cb8
handongxun <handongxun@dcloud.io>
2021-04-06 15:17:36 +0800

    update: 新增插屏广告API uni.createInterstitialAd

--- src/platforms/app-plus/service/api/ad/interstitial-ad.js
+++ src/platforms/app-plus/service/api/ad/interstitial-ad.js
@@ -1,17 +1,11 @@
 const eventNames = [
   'load',
   'close',
-  'verify',
   'error',
   'adClicked'
 ]
 
-const ERROR_CODE_LIST = [-5001, -5002, -5003, -5004, -5005, -5006]
-const EXPIRED_TIME = 1000 * 60 * 30
-const EXPIRED_TEXT = { code: -5008, errMsg: '广告数据已过期，请重新加载' }
-const ProviderType = { CSJ: 'csj', GDT: 'gdt' }
-
-class RewardedVideoAd {
+class InterstitialAd {
   constructor (options = {}) {
     const _callbacks = this._callbacks = {}
     eventNames.forEach(item => {
@@ -22,19 +16,16 @@ class RewardedVideoAd {
       }
     })
 
-    this._preload = options.preload !== undefined ? options.preload : true
     this._isLoad = false
     this._isLoading = false
     this._adError = ''
     this._loadPromiseResolve = null
     this._loadPromiseReject = null
-    this._lastLoadTime = 0
 
-    const rewardAd = this._rewardAd = plus.ad.createRewardedVideoAd(options)
-    rewardAd.onLoad((e) => {
+    const ad = this._ad = plus.ad.createInterstitialAd(options)
+    ad.onLoad((e) => {
       this._isLoad = true
       this._isLoading = false
-      this._lastLoadTime = Date.now()
       this._dispatchEvent('load', {})
 
       if (this._loadPromiseResolve != null) {
@@ -42,43 +33,28 @@ class RewardedVideoAd {
         this._loadPromiseResolve = null
       }
     })
-    rewardAd.onClose((e) => {
+    ad.onClose((e) => {
       this._isLoad = false
       this._isLoading = false
-      if (this._preload) {
-        this._loadAd()
-      }
-      this._dispatchEvent('close', { isEnded: e.isEnded })
+      this._dispatchEvent('close', {})
     })
-    rewardAd.onVerify && rewardAd.onVerify((e) => {
-      this._dispatchEvent('verify', { isValid: e.isValid })
-    })
-    rewardAd.onError((e) => {
+    ad.onError((e) => {
       this._isLoading = false
+
       const { code, message } = e
       const data = { code: code, errMsg: message }
       this._adError = message
-      if (code === -5008) {
-        this._isLoad = false
-      }
+
       this._dispatchEvent('error', data)
-      // TODO
-      if ((code === -5005 || ERROR_CODE_LIST.index(code) === -1) && this._loadPromiseReject != null) {
+
+      if (this._loadPromiseReject != null) {
         this._loadPromiseReject(data)
         this._loadPromiseReject = null
       }
     })
-    rewardAd.onAdClicked((e) => {
+    ad.onAdClicked((e) => {
       this._dispatchEvent('adClicked', {})
     })
-
-    if (this._preload) {
-      this._loadAd()
-    }
-  }
-
-  get isExpired () {
-    return (this._lastLoadTime !== 0 && (Math.abs(Date.now() - this._lastLoadTime) > EXPIRED_TIME))
   }
 
   load () {
@@ -102,17 +78,8 @@ class RewardedVideoAd {
         return
       }
 
-      const provider = this.getProvider()
-      if (provider === ProviderType.CSJ && this.isExpired) {
-        this._isLoad = false
-        // TODO
-        this._dispatchEvent('error', EXPIRED_TEXT)
-        reject(new Error(EXPIRED_TEXT.errMsg))
-        return
-      }
-
       if (this._isLoad) {
-        this._rewardAd.show()
+        this._ad.show()
         resolve()
       } else {
         reject(new Error(this._adError))
@@ -121,17 +88,17 @@ class RewardedVideoAd {
   }
 
   getProvider () {
-    return this._rewardAd.getProvider()
+    return this._ad.getProvider()
   }
 
   destroy () {
-    this._rewardAd.destroy()
+    this._ad.destroy()
   }
 
   _loadAd () {
     this._isLoad = false
     this._isLoading = true
-    this._rewardAd.load()
+    this._ad.load()
   }
 
   _dispatchEvent (name, data) {
@@ -143,6 +110,6 @@ class RewardedVideoAd {
   }
 }
 
-export function createRewardedVideoAd (options) {
-  return new RewardedVideoAd(options)
+export function createInterstitialAd (options) {
+  return new InterstitialAd(options)
 }
