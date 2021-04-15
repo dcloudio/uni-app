d53b1754d71ee91a3315f16d4295630191d8b8df
handongxun <handongxun@dcloud.io>
2021-04-15 13:45:06 +0800

    update: 重构全屏视频和插屏视频逻辑公用一个基类减少代码量

--- src/platforms/app-plus/service/api/ad/full-screen-video-ad.js
+++ src/platforms/app-plus/service/api/ad/full-screen-video-ad.js
@@ -1,103 +1,10 @@
-const eventNames = [
-  'load',
-  'close',
-  'error',
-  'adClicked'
-]
+import {
+  AdBase
+} from './ad-base.js'
 
-class FullScreenVideoAd {
+class FullScreenVideoAd extends AdBase {
   constructor (options = {}) {
-    const _callbacks = this._callbacks = {}
-    eventNames.forEach(item => {
-      _callbacks[item] = []
-      const name = item[0].toUpperCase() + item.substr(1)
-      this[`on${name}`] = function (callback) {
-        _callbacks[item].push(callback)
-      }
-    })
-
-    this._isLoad = false
-    this._adError = ''
-    this._loadPromiseResolve = null
-    this._loadPromiseReject = null
-    this._lastLoadTime = 0
-
-    const ad = this._ad = plus.ad.createFullScreenVideoAd(options)
-    ad.onLoad((e) => {
-      this._isLoad = true
-      this._lastLoadTime = Date.now()
-      this._dispatchEvent('load', {})
-
-      if (this._loadPromiseResolve != null) {
-        this._loadPromiseResolve()
-        this._loadPromiseResolve = null
-      }
-    })
-    ad.onClose((e) => {
-      this._isLoad = false
-      this._dispatchEvent('close', { isEnded: e.isEnded })
-    })
-    ad.onError((e) => {
-      const { code, message } = e
-      const data = { code: code, errMsg: message }
-      this._adError = message
-      if (code === -5008) {
-        this._isLoad = false
-      }
-      this._dispatchEvent('error', data)
-
-      if (this._loadPromiseReject != null) {
-        this._loadPromiseReject(data)
-        this._loadPromiseReject = null
-      }
-    })
-    ad.onAdClicked((e) => {
-      this._dispatchEvent('adClicked', {})
-    })
-  }
-
-  load () {
-    return new Promise((resolve, reject) => {
-      if (this._isLoad) {
-        resolve()
-        return
-      }
-      this._loadPromiseResolve = resolve
-      this._loadPromiseReject = reject
-      this._loadAd()
-    })
-  }
-
-  show () {
-    return new Promise((resolve, reject) => {
-      if (this._isLoad) {
-        this._ad.show()
-        resolve()
-      } else {
-        reject(new Error(this._adError))
-      }
-    })
-  }
-
-  getProvider () {
-    return this._ad.getProvider()
-  }
-
-  destroy () {
-    this._ad.destroy()
-  }
-
-  _loadAd () {
-    this._isLoad = false
-    this._ad.load()
-  }
-
-  _dispatchEvent (name, data) {
-    this._callbacks[name].forEach(callback => {
-      if (typeof callback === 'function') {
-        callback(data || {})
-      }
-    })
+    super(plus.ad.createFullScreenVideoAd(options), options)
   }
 }
 
