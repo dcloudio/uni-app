4fdaf207976790bd8657164ba33d8db1e703c744
fxy060608 <fxy060608@gmail.com>
2021-05-18 16:00:29 +0800

    fix: normalize public filter

--- packages/vite-plugin-uni/src/utils/filter.ts
+++ packages/vite-plugin-uni/src/utils/filter.ts
@@ -1,10 +1,12 @@
 import path from 'path'
-
+import slash from 'slash'
 import { createFilter } from '@rollup/pluginutils'
 import { PUBLIC_DIR } from '@dcloudio/uni-cli-shared'
 
 export function createPublicFileFilter(base: string = '/') {
-  const publicDir = path.join(base, PUBLIC_DIR + '/**/*')
-  const uniModulesDir = path.join(base, 'uni_modules/*/' + PUBLIC_DIR + '/**/*')
+  const publicDir = slash(path.join(base, PUBLIC_DIR + '/**/*'))
+  const uniModulesDir = slash(
+    path.join(base, 'uni_modules/*/' + PUBLIC_DIR + '/**/*')
+  )
   return createFilter([publicDir, uniModulesDir])
 }
