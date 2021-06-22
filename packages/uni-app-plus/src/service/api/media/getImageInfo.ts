a663bb205d813e73d4a63d35318d112b57432791
tianjiaxing <tianjiaxing2016@163.com>
2021-06-22 16:39:00 +0800

    fix: getImageInfo,getVideoInfo

--- packages/uni-app-plus/src/service/api/media/getImageInfo.ts
+++ packages/uni-app-plus/src/service/api/media/getImageInfo.ts
@@ -10,12 +10,16 @@ import {
   warpPlusSuccessCallback,
   warpPlusErrorCallback,
 } from '../../../helpers/plus'
+import { TEMP_PATH } from '../constants'
 
 export const getImageInfo = defineAsyncApi<API_TYPE_GET_IMAGE_INFO>(
   API_GET_IMAGE_INFO,
   (options, { resolve, reject }) => {
+    const path = TEMP_PATH + '/download/'
     plus.io.getImageInfo(
       extend(options, {
+        savePath: path,
+        filename: path,
         success: warpPlusSuccessCallback(resolve),
         fail: warpPlusErrorCallback(reject),
       })
--- packages/uni-app-plus/src/service/api/media/getVideoInfo.ts
+++ packages/uni-app-plus/src/service/api/media/getVideoInfo.ts
@@ -1,4 +1,3 @@
-import { extend } from '@vue/shared'
 import {
   defineAsyncApi,
   API_GET_VIDEO_INFO,
@@ -6,20 +5,27 @@ import {
   GetVideoInfoOptions,
   GetVideoInfoProtocol,
 } from '@dcloudio/uni-api'
-import {
-  warpPlusSuccessCallback,
-  warpPlusErrorCallback,
-} from '../../../helpers/plus'
+import { warpPlusErrorCallback } from '../../../helpers/plus'
 
 export const getVideoInfo = <API_TYPE_GET_VIDEO_INFO>defineAsyncApi(
   API_GET_VIDEO_INFO,
   (options, { resolve, reject }) => {
-    plus.io.getVideoInfo(
-      extend(options, {
-        success: warpPlusSuccessCallback(resolve),
-        fail: warpPlusErrorCallback(reject),
-      })
-    )
+    plus.io.getVideoInfo({
+      filePath: options.src,
+      success: (data: any) => {
+        return {
+          orientation: data.orientation,
+          type: data.type,
+          duration: data.duration,
+          size: data.size,
+          height: data.height,
+          width: data.width,
+          fps: data.fps || 30,
+          bitrate: data.bitrate,
+        }
+      },
+      fail: warpPlusErrorCallback(reject),
+    })
   },
   GetVideoInfoProtocol,
   GetVideoInfoOptions
