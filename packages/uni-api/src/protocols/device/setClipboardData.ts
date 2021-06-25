a1fe2b94ba4d37ffc6bd200c63f89567c3ad5c50
tianjiaxing <tianjiaxing2016@163.com>
2021-06-25 15:01:54 +0800

    feat(App): setClipboardData、getClipboardData

--- packages/uni-api/src/protocols/device/setClipboardData.ts
+++ packages/uni-api/src/protocols/device/setClipboardData.ts
@@ -1,4 +1,6 @@
+export const API_GET_CLIPBOARD_DATA = 'getClipboardData'
 export const API_SET_CLIPBOARD_DATA = 'setClipboardData'
+export type API_TYPE_GET_CLIPBOARD_DATA = typeof uni.getClipboardData
 export type API_TYPE_SET_CLIPBOARD_DATA = typeof uni.setClipboardData
 export const SetClipboardDataOptions: ApiOptions<API_TYPE_SET_CLIPBOARD_DATA> =
   {
---
+++ packages/uni-app-plus/src/service/api/device/clipboard.ts
@@ -0,0 +1,33 @@
+import {
+  API_GET_CLIPBOARD_DATA,
+  API_SET_CLIPBOARD_DATA,
+  API_TYPE_GET_CLIPBOARD_DATA,
+  API_TYPE_SET_CLIPBOARD_DATA,
+  defineAsyncApi,
+} from '@dcloudio/uni-api'
+import { requireNativePlugin } from '../base'
+
+export const getClipboardData = defineAsyncApi<API_TYPE_GET_CLIPBOARD_DATA>(
+  API_GET_CLIPBOARD_DATA,
+  (_, { resolve, reject }) => {
+    const clipboard = requireNativePlugin('clipboard')
+    clipboard.getString((ret: any) => {
+      if (ret.result === 'success') {
+        resolve({
+          data: ret.data,
+        })
+      } else {
+        reject('getClipboardData:fail')
+      }
+    })
+  }
+)
+
+export const setClipboardData = defineAsyncApi<API_TYPE_SET_CLIPBOARD_DATA>(
+  API_SET_CLIPBOARD_DATA,
+  (options, { resolve }) => {
+    const clipboard = requireNativePlugin('clipboard')
+    clipboard.setString(options.data)
+    resolve()
+  }
+)
