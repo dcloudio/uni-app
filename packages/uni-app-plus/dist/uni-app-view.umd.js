c27d5a70d2b0ca7b96c1965250e4bdd50b68dfb4
qiang <guoshengqiang@live.com>
2021-07-05 18:11:55 +0800

    feat(App): 禁用 iOS 双击包含（滚动）手势

--- packages/uni-app-plus/dist/uni-app-view.umd.js
+++ packages/uni-app-plus/dist/uni-app-view.umd.js
@@ -2370,8 +2370,27 @@
     const { subscribe } = UniViewJSBridge;
     subscribe(VD_SYNC, onVdSync);
   }
+  function preventDoubleTap() {
+    if (String(navigator.vendor).indexOf("Apple") === 0) {
+      let firstEvent = null;
+      let timeout;
+      document.documentElement.addEventListener("click", (event) => {
+        const TIME_MAX = 450;
+        const PAGE_MAX = 44;
+        clearTimeout(timeout);
+        if (firstEvent && Math.abs(event.pageX - firstEvent.pageX) <= PAGE_MAX && Math.abs(event.pageY - firstEvent.pageY) <= PAGE_MAX && event.timeStamp - firstEvent.timeStamp <= TIME_MAX) {
+          event.preventDefault();
+        }
+        firstEvent = event;
+        timeout = setTimeout(() => {
+          firstEvent = null;
+        }, TIME_MAX);
+      });
+    }
+  }
   function initView() {
     initSubscribeHandlers();
+    preventDoubleTap();
   }
   window.UniViewJSBridge = UniViewJSBridge$1;
   function onWebviewReady() {
