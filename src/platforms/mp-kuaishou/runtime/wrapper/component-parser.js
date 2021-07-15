d6f6befaeefccdd34d88ab802f20983e8fae244f
qiang <guoshengqiang@live.com>
2021-07-15 16:07:16 +0800

    fix(mp-kuaishou): 修复快手小程序部分情况数据无法更新的问题

--- src/platforms/mp-kuaishou/runtime/wrapper/component-parser.js
+++ src/platforms/mp-kuaishou/runtime/wrapper/component-parser.js
@@ -1,5 +1,26 @@
 import parseBaseComponent from '../../../mp-weixin/runtime/wrapper/component-parser'
 
+import { isPage } from '../../../mp-weixin/runtime/wrapper/util'
+
+import {
+  fixSetDataStart,
+  fixSetDataEnd
+} from '../../../mp-weixin/runtime/wrapper/fix-set-data'
+
+const SDKVersion = __GLOBAL__.getSystemInfoSync().SDKVersion
+
 export default function parseComponent (vueComponentOptions) {
-  return parseBaseComponent(vueComponentOptions)
+  const componentOptions = parseBaseComponent(vueComponentOptions)
+  const oldAttached = componentOptions.lifetimes.attached
+  componentOptions.lifetimes.attached = function attached () {
+    if (isPage.call(this) && parseFloat(SDKVersion) <= 1.13) {
+      // 解决快手小程序页面 attached 生命周期 setData 导致数据同步异常的问题
+      fixSetDataStart(this)
+      setTimeout(() => {
+        fixSetDataEnd(this)
+      }, 0)
+    }
+    oldAttached.call(this)
+  }
+  return componentOptions
 }
