1d42ab7afe62fb4d4aad3b7cbb26df22c7e9102a
fxy060608 <fxy060608@gmail.com>
2021-08-23 18:22:58 +0800

    feat(nvue): support file-loader

--- packages/vue-cli-plugin-hbuilderx/build/webpack.nvue.conf.js
+++ packages/vue-cli-plugin-hbuilderx/build/webpack.nvue.conf.js
@@ -108,6 +108,15 @@ if (process.env.NODE_ENV === 'development') {
 // const excludeModuleReg = /node_modules(?!(\/|\\).*(weex).*)/
 
 const rules = [{
+  test: /\.(png|jpg|gif|ttf|eot|woff|woff2)$/i,
+  use: [{
+    loader: 'file-loader',
+    options: {
+      publicPath: 'assets',
+      outputPath: 'assets'
+    }
+  }]
+}, {
   test: path.resolve(process.env.UNI_INPUT_DIR, 'pages.json'),
   use: [{
     loader: '@dcloudio/webpack-uni-pages-loader'
