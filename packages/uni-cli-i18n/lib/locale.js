7f22a645ae0ec5039151b8887ccfce4399ad7dd8
handongxun <handongxun@dcloud.io>
2021-08-23 16:39:08 +0800

    fix: uni-cli-i18n local for Windows

--- packages/uni-cli-i18n/lib/locale.js
+++ packages/uni-cli-i18n/lib/locale.js
@@ -2,7 +2,7 @@ const { format } = require('./lang')
 const { osLocale } = require('os-locale-s-fix/cjs')
 
 function getLocale () {
-  return format(process.env.UNI_HBUILDERX_LANGID || osLocale.sync({ spawn: false, cache: false }) || defaultLocale)
+  return format(process.env.UNI_HBUILDERX_LANGID || osLocale.sync({ spawn: true, cache: false }) || defaultLocale)
 }
 
 module.exports = {
