15df79ab0c0875a865473ad916c72fe711c0ec06
qiang <guoshengqiang@live.com>
2021-09-26 17:55:29 +0800

    fix(App): android onHide

--- packages/uni-app-plus/src/service/framework/app/initAppLaunch.ts
+++ packages/uni-app-plus/src/service/framework/app/initAppLaunch.ts
@@ -1,6 +1,6 @@
 import { injectAppLaunchHooks } from '@dcloudio/uni-api'
 import { invokeHook } from '@dcloudio/uni-core'
-import { ON_LAUNCH, ON_SHOW } from '@dcloudio/uni-shared'
+import { ON_LAUNCH, ON_SHOW, ON_HIDE } from '@dcloudio/uni-shared'
 import { ComponentPublicInstance } from 'vue'
 
 export function initAppLaunch(appVm: ComponentPublicInstance) {
@@ -13,4 +13,10 @@ export function initAppLaunch(appVm: ComponentPublicInstance) {
   injectAppLaunchHooks(appVm.$)
   invokeHook(appVm, ON_LAUNCH, args)
   invokeHook(appVm, ON_SHOW, args)
+  // https://tower.im/teams/226535/todos/16905/
+  const getAppState = weex.requireModule('plus').getAppState
+  const appState = getAppState && Number(getAppState())
+  if (appState === 2) {
+    invokeHook(appVm, ON_HIDE, args)
+  }
 }
