936382fadf803dc5f8f3cd3f278d0014988d0a0c
mehaotian <490272692@qq.com>
2021-09-26 11:43:00 +0800

    fix(h5): 修复 rich-text 组件无法渲染 section 等标签的问题

--- packages/uni-components/src/components/rich-text/nodes-parser.js
+++ packages/uni-components/src/components/rich-text/nodes-parser.js
@@ -3,9 +3,18 @@ import { hasOwn, isPlainObject } from '@vue/shared'
 const TAGS = {
   a: '',
   abbr: '',
+  address: '',
+  article: '',
+  aside: '',
   b: '',
+  bdi: '',
+  bdo: ['dir'],
+  big: '',
   blockquote: '',
   br: '',
+  caption: '',
+  center: '',
+  cite: '',
   code: '',
   col: ['span', 'width'],
   colgroup: ['span', 'width'],
@@ -16,12 +25,15 @@ const TAGS = {
   dt: '',
   em: '',
   fieldset: '',
+  font: '',
+  footer: '',
   h1: '',
   h2: '',
   h3: '',
   h4: '',
   h5: '',
   h6: '',
+  header: '',
   hr: '',
   i: '',
   img: ['alt', 'src', 'height', 'width'],
@@ -29,21 +41,31 @@ const TAGS = {
   label: '',
   legend: '',
   li: '',
+  mark: '',
+  nav: '',
   ol: ['start', 'type'],
   p: '',
+  pre: '',
   q: '',
+  rt: '',
+  ruby: '',
+  s: '',
+  section: '',
+  small: '',
   span: '',
   strong: '',
   sub: '',
   sup: '',
   table: ['width'],
   tbody: '',
-  td: ['colspan', 'rowspan', 'height', 'width'],
+  td: ['colspan', 'height', 'rowspan', 'width'],
   tfoot: '',
-  th: ['colspan', 'rowspan', 'height', 'width'],
+  th: ['colspan', 'height', 'rowspan', 'width'],
   thead: '',
-  tr: '',
-  ul: ''
+  tr: ['colspan', 'height', 'rowspan', 'width'],
+  tt: '',
+  u: '',
+  ul: '',
 }
 const CHARS = {
   amp: '&',
@@ -51,31 +73,31 @@ const CHARS = {
   lt: '<',
   nbsp: ' ',
   quot: '"',
-  apos: "'"
+  apos: "'",
 }
 
 function decodeEntities(htmlString) {
-  return htmlString.replace(/&(([a-zA-Z]+)|(#x{0,1}[\da-zA-Z]+));/gi, function(
-    match,
-    stage
-  ) {
-    if (hasOwn(CHARS, stage) && CHARS[stage]) {
-      return CHARS[stage]
+  return htmlString.replace(
+    /&(([a-zA-Z]+)|(#x{0,1}[\da-zA-Z]+));/gi,
+    function (match, stage) {
+      if (hasOwn(CHARS, stage) && CHARS[stage]) {
+        return CHARS[stage]
+      }
+      if (/^#[0-9]{1,4}$/.test(stage)) {
+        return String.fromCharCode(stage.slice(1))
+      }
+      if (/^#x[0-9a-f]{1,4}$/i.test(stage)) {
+        return String.fromCharCode('0' + stage.slice(1))
+      }
+      const wrap = document.createElement('div')
+      wrap.innerHTML = match
+      return wrap.innerText || wrap.textContent
     }
-    if (/^#[0-9]{1,4}$/.test(stage)) {
-      return String.fromCharCode(stage.slice(1))
-    }
-    if (/^#x[0-9a-f]{1,4}$/i.test(stage)) {
-      return String.fromCharCode('0' + stage.slice(1))
-    }
-    const wrap = document.createElement('div')
-    wrap.innerHTML = match
-    return wrap.innerText || wrap.textContent
-  })
+  )
 }
 
 export default function parseNodes(nodes, parentNode) {
-  nodes.forEach(function(node) {
+  nodes.forEach(function (node) {
     if (!isPlainObject(node)) {
       return
     }
@@ -94,7 +116,7 @@ export default function parseNodes(nodes, parentNode) {
       const attrs = node.attrs
       if (isPlainObject(attrs)) {
         const tagAttrs = TAGS[tagName] || []
-        Object.keys(attrs).forEach(function(name) {
+        Object.keys(attrs).forEach(function (name) {
           let value = attrs[name]
           switch (name) {
             case 'class':
