04e92a26bf2ffcfd955b32b227ec6fd0f1222430
fxy060608 <fxy060608@gmail.com>
2021-09-27 15:15:10 +0800

    chore: update comment

--- packages/uni-h5/src/helpers/useDocumentTitle.ts
+++ packages/uni-h5/src/helpers/useDocumentTitle.ts
@@ -4,7 +4,7 @@ import { getApp } from '../framework/setup/app'
 
 export function updateDocumentTitle(title: string) {
   if (__NODE_JS__) {
-    // updateDocumentTitle 可能是异步调用，此时使用 useSSRContext 获取，可能没有 instance
+    // setNavigationBarTitle 可能是异步调用，此时使用 useSSRContext 获取，可能没有 instance
     const ssrContext = getApp().$.appContext.provides[ssrContextKey]
     if (ssrContext) {
       ssrContext[UNI_SSR_TITLE] = title
