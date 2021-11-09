9b52cda9db65745250c33653cac92ba990907543
fxy060608 <fxy060608@gmail.com>
2021-11-09 16:52:01 +0800

    workflow: size-check.yml

--- packages/uni-app/dist/uni.compiler.js
+++ packages/uni-app/dist/uni.compiler.js
@@ -2,7 +2,7 @@
 
 var index = [
     /* eslint-disable no-restricted-globals */
-    ...require('@dcloudio/uni-cloud/lib/uni.plugin.js'),
+    ...require('@dcloudio/uni-cloud/lib/uni.plugin.js').default,
     /* eslint-disable no-restricted-globals */
     ...require('@dcloudio/uni-stat/lib/uni.plugin.js'),
 ];
