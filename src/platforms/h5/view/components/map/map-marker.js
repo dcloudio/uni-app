951021cefcfee29e02ea1469652bd060265551c3
qiang <guoshengqiang@live.com>
2021-11-09 15:53:32 +0800

    feat: Map markerLabel anchorX/anchorY

--- src/platforms/h5/view/components/map/map-marker.js
+++ src/platforms/h5/view/components/map/map-marker.js
@@ -168,8 +168,8 @@ export default {
             color: labelOpt.color,
             fontSize: (labelOpt.fontSize || 14) + 'px',
             lineHeight: (labelOpt.fontSize || 14) + 'px',
-            marginLeft: (Number(labelOpt.x) || 0) + 'px',
-            marginTop: (Number(labelOpt.y) || 0) + 'px'
+            marginLeft: (Number(labelOpt.anchorX || labelOpt.x) || 0) + 'px',
+            marginTop: (Number(labelOpt.anchorY || labelOpt.y) || 0) + 'px'
           }
           if ('Label' in maps) {
             label = new maps.Label({
