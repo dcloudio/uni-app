b8090531e9d364c40982378dc0bab1572ada65cc
fxy060608 <fxy060608@gmail.com>
2021-11-17 13:30:05 +0800

    release: v3.0.0-alpha-3021320211117002

--- packages/uni-stat/dist/uni-stat.cjs.js
+++ packages/uni-stat/dist/uni-stat.cjs.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var version = "3.0.0-alpha-3021320211117001";
+var version = "3.0.0-alpha-3021320211117002";
 
 const STAT_VERSION = version;
 const STAT_URL = 'https://tongji.dcloud.io/uni/stat';
