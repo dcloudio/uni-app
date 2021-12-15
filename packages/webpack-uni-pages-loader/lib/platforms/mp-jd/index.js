3a01630a4a2b6aac4f2659c3784f028e83670535
石小磊 <shixiaolei6@jd.com>
2021-12-15 14:49:58 +0800

    update: 兼容引擎侧分包

--- packages/webpack-uni-pages-loader/lib/platforms/mp-jd/index.js
+++ packages/webpack-uni-pages-loader/lib/platforms/mp-jd/index.js
@@ -3,5 +3,9 @@ module.exports = function (pagesJson, manifestJson) {
     app,
     project
   } = require('../mp')(pagesJson, manifestJson, require('./project.config.json'))
+  //暂不支持分包，兼容引擎判断
+  if(app.content.subPackages && !app.content.subPackages.length){
+    delete app.content.subPackages;
+  }
   return [app, project]
 }
--- src/platforms/mp-jd/runtime/api/providers.js
+++ src/platforms/mp-jd/runtime/api/providers.js
@@ -1,6 +1,6 @@
 export default {
   oauth: ['jd'],
   share: ['jd'],
-  payment: [''],
-  push: ['']
+  payment: ['jd'],
+  push: ['jd']
 }
