3a9f0109ce0d758c813cc42b65d58559e629e58e
fxy060608 <fxy060608@gmail.com>
2022-01-18 13:55:13 +0800

    fix: map with tap event

---
+++ packages/uni-h5-vite/__tests__/compiler.spec.ts
@@ -0,0 +1,29 @@
+import { BindingTypes } from '@vue/compiler-core'
+import { compileTemplate } from '@vue/compiler-sfc'
+import { compilerOptions } from '../src/plugin/uni'
+const filename = 'foo.vue'
+
+function compile(source: string) {
+  return compileTemplate({
+    source,
+    filename,
+    id: filename,
+    compilerOptions: {
+      ...compilerOptions,
+      bindingMetadata: {
+        canvas: BindingTypes.SETUP_REF,
+      },
+    },
+  })
+}
+describe('h5: compiler', () => {
+  test('tap=>click', () => {
+    expect(compile('<view @tap="tap"/>').code).toContain(`onClick`)
+    expect(compile('<map @tap="tap"/>').code).toContain(`onTap`)
+  })
+  test('builtInComponents', () => {
+    expect(compile('<canvas/>').code).toContain(
+      `_resolveComponent("v-uni-canvas")`
+    )
+  })
+})
