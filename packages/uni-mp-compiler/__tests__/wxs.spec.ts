f3b62a6bd719a9d59fa59388eb65d5ebd6d85e21
fxy060608 <fxy060608@gmail.com>
2022-01-19 16:41:11 +0800

    fix(mp): v-if with wxs module (#3199)

--- packages/uni-mp-compiler/__tests__/wxs.spec.ts
+++ packages/uni-mp-compiler/__tests__/wxs.spec.ts
@@ -27,4 +27,50 @@ describe('compiler: transform wxs', () => {
       }
     )
   })
+  test('v-if', () => {
+    assert(
+      `<view v-if="test.aa()">123</view>`,
+      `<view wx:if="{{test.aa()}}">123</view>`,
+      `(_ctx, _cache) => {
+  return {}
+}`,
+      {
+        filters: ['test'],
+      }
+    )
+    assert(
+      `<view v-if="test.aa()">{{msg}}</view>`,
+      `<view wx:if="{{test.aa()}}">{{a}}</view>`,
+      `(_ctx, _cache) => {
+  return { ...{ a: _t(_ctx.msg) } }
+}`,
+      {
+        filters: ['test'],
+      }
+    )
+  })
+  test('v-if + v-else', () => {
+    assert(
+      `<view v-if="test.aa()">{{foo}}</view><view v-else>{{bar}}</view>`,
+      `<view wx:if="{{test.aa()}}">{{a}}</view><view wx:else>{{b}}</view>`,
+      `(_ctx, _cache) => {
+  return { ...{ a: _t(_ctx.foo) }, ...{ b: _t(_ctx.bar) } }
+}`,
+      {
+        filters: ['test'],
+      }
+    )
+  })
+  test('v-if + v-else-if + v-else', () => {
+    assert(
+      `<view v-if="test.aa()">{{foo}}</view><view v-else-if="test.bb()">{{foo}}</view><view v-else>{{bar}}</view>`,
+      `<view wx:if="{{test.aa()}}">{{a}}</view><view wx:elif="{{test.bb()}}">{{b}}</view><view wx:else>{{c}}</view>`,
+      `(_ctx, _cache) => {
+  return { ...{ a: _t(_ctx.foo) }, ...{ ...{ b: _t(_ctx.foo) }, ...{ c: _t(_ctx.bar) } } }
+}`,
+      {
+        filters: ['test'],
+      }
+    )
+  })
 })
