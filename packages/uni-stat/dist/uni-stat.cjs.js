f2f76235a4c2a4757db868fa05368034ce46663f
qiang <guoshengqiang@live.com>
2022-02-08 15:58:03 +0800

    release: v3.0.0-alpha-3031120220208001

--- packages/uni-stat/dist/uni-stat.cjs.js
+++ packages/uni-stat/dist/uni-stat.cjs.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var version = "3.0.0-alpha-3031120220207001";
+var version = "3.0.0-alpha-3031120220208001";
 
 const STAT_VERSION = version;
 const STAT_URL = 'https://tongji.dcloud.io/uni/stat';
--- packages/uni-stat/dist/uni-stat.es.js
+++ packages/uni-stat/dist/uni-stat.es.js
@@ -1,4 +1,4 @@
-var version = "3.0.0-alpha-3031120220207001";
+var version = "3.0.0-alpha-3031120220208001";
 
 const STAT_VERSION = version;
 const STAT_URL = 'https://tongji.dcloud.io/uni/stat';
