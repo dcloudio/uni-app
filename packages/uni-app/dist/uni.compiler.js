ae2b5f2c60e732d20e8bb54cb6909c646fdb0653
fxy060608 <fxy060608@gmail.com>
2022-02-08 15:25:53 +0800

    chore: merge

--- packages/uni-app/dist/uni.compiler.js
+++ packages/uni-app/dist/uni.compiler.js
@@ -4,7 +4,7 @@ var index = [
     /* eslint-disable no-restricted-globals */
     ...require('@dcloudio/uni-cloud/lib/uni.plugin.js').default,
     /* eslint-disable no-restricted-globals */
-    ...require('@dcloudio/uni-push/lib/uni.plugin.js'),
+    ...require('@dcloudio/uni-push/lib/uni.plugin.js')(),
     /* eslint-disable no-restricted-globals */
     ...require('@dcloudio/uni-stat/lib/uni.plugin.js'),
 ];
