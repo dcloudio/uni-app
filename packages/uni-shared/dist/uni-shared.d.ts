f83cae6717a3ad8fbe25aa8050feb3884794acd6
fxy060608 <fxy060608@gmail.com>
2022-02-17 15:58:01 +0800

    wip(app): nvue

--- packages/uni-shared/dist/uni-shared.d.ts
+++ packages/uni-shared/dist/uni-shared.d.ts
@@ -349,6 +349,7 @@ export declare interface NVueElement {
     type: string;
     ref: string;
     text?: string;
+    classList: string[];
     parentNode: NVueElement | null;
     children: Array<NVueElement>;
     previousSibling: NVueElement | null;
@@ -359,6 +360,7 @@ export declare interface NVueElement {
     insertAfter: (node: NVueElement, after: NVueElement) => void;
     setAttr: (key: string, value: any, silent?: boolean) => void;
     setAttrs: (attrs: Record<string, unknown>, silent?: boolean) => void;
+    setClassList: (classList: string[]) => void;
     setStyle: (key: string, value: any, silent?: boolean) => void;
     setStyles: (attrs: Record<string, unknown>, silent?: boolean) => void;
     addEvent: (type: string, handler: Function, args?: Array<any>) => void;
