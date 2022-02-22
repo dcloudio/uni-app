2cc7d59a4156fe27ffc9d376d43f6a7b2e343c15
fxy060608 <fxy060608@gmail.com>
2022-02-22 14:54:39 +0800

    fix(mp): v-for + component (#3263)

--- packages/uni-mp-compiler/__tests__/scopedSlot.spec.ts
+++ packages/uni-mp-compiler/__tests__/scopedSlot.spec.ts
@@ -1,30 +1,20 @@
 import { assert } from './testUtils'
 
 describe('compiler: transform scoped slots', () => {
-  test('basic', () => {
+  test('scoped + v-for + component', () => {
     assert(
-      `<view><slot :item="item" :index="index"/></view>`,
-      `<view><slot name="d"/></view>`,
+      `<MyComponent :dataList="dataList">
+  <template v-slot:default="slotProps">
+    <view>
+        <view v-for="(item, key) in dataList" :key="key">
+            <test>{{ item.title }}</test>
+        </view>
+    </view>
+  </template>
+</MyComponent>`,
+      `<my-component u-s="{{['d']}}" u-i="2a9ec0b0-0" u-p="{{b}}"><view wx:for="{{a}}" wx:for-item="slotProps" wx:key="b" slot="{{slotProps.c}}"><view><view wx:for="{{slotProps.a}}" wx:for-item="item" wx:key="c"><test u-s="{{['d']}}" u-i="{{item.b}}">{{item.a}}</test></view></view></view></my-component>`,
       `(_ctx, _cache) => {
-  return { a: _r("d", { item: _ctx.item, index: _ctx.index }) }
-}`
-    )
-  })
-  test('named slots', () => {
-    assert(
-      `<view><slot name="header" :item="item" :index="index"/></view>`,
-      `<view><slot name="header"/></view>`,
-      `(_ctx, _cache) => {
-  return { a: _r("header", { item: _ctx.item, index: _ctx.index }) }
-}`
-    )
-  })
-  test('named slots + v-if', () => {
-    assert(
-      `<view><slot v-if="ok" name="header" :item="item" :index="index"/></view>`,
-      `<view><slot wx:if="{{a}}" name="header"/></view>`,
-      `(_ctx, _cache) => {
-  return _e({ a: _ctx.ok }, _ctx.ok ? { b: _r("header", { item: _ctx.item, index: _ctx.index }) } : {})
+  return { a: _w((slotProps, s0, i0) => { return { a: _f(_ctx.dataList, (item, key, i1) => { return { a: _t(item.title), b: '2a9ec0b0-1' + '-' + i0 + '-' + i1 + ',' + '2a9ec0b0-0', c: key }; }), b: i0, c: s0 }; }, { name: 'd', path: 'a', vueId: '2a9ec0b0-0' }), b: _p({ dataList: _ctx.dataList }) }
 }`
     )
   })
