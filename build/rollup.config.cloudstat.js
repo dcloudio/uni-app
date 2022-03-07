b3ca28d1e74794e8b17cae7e294f17cf9d858288
mehaotian <490272692@qq.com>
2022-03-07 18:14:58 +0800

    fix(stat): 删除 uni-cloud-stat 包

--- build/rollup.config.cloudstat.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  input: 'packages/uni-cloud-stat/src/index.js',
-  output: {
-    file: 'packages/uni-cloud-stat/dist/index.js',
-    format: 'es'
-  },
-  external: ['vue', '../package.json'],
-  plugins: []
-}
