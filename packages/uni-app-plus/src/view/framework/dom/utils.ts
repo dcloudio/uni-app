83ba4635c87fadd4fd68374785b01e7e629436e7
fxy060608 <fxy060608@gmail.com>
2022-03-09 11:36:06 +0800

    fix(app): wxs event (#3324)

--- packages/uni-app-plus/src/view/framework/dom/utils.ts
+++ packages/uni-app-plus/src/view/framework/dom/utils.ts
@@ -1,6 +1,7 @@
 import { JSON_PROTOCOL, WXS_PROTOCOL } from '@dcloudio/uni-shared'
 import { isString } from '@vue/shared'
-import { UniCustomElement } from './components'
+import type { UniCustomElement } from './components'
+import type { UniComponent } from './components/UniComponent'
 import { invokeWxs } from './wxs'
 
 const JSON_PROTOCOL_LEN = JSON_PROTOCOL.length
@@ -19,4 +20,11 @@ export function decodeAttr(el: UniCustomElement, value: unknown) {
 
 export function isCssVar(name: string) {
   return name.indexOf('--') === 0
+}
+
+export function isUniComponent(
+  el: UniCustomElement | UniComponent
+): el is UniComponent {
+  // 不使用 instanceof 判断，因为需要引入 UniComponent，两者循环依赖
+  return !!(el as UniComponent).addWxsEvent
 }
