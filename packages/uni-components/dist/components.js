cf2f3965ff578d843e180be26a2c0715749ec622
qiang <guoshengqiang@live.com>
2022-03-25 15:13:10 +0800

    chore: build

--- packages/uni-components/dist/components.js
+++ packages/uni-components/dist/components.js
@@ -2356,7 +2356,7 @@ var PickerViewColumn = defineComponent({
       if (Date.now() - scrollToElementTime < 340) {
         return;
       }
-      const y = event.contentOffset.y;
+      const y = event.detail.contentOffset.y;
       const _current = Math.round(y / indicatorHeight.value);
       if (y % indicatorHeight.value) {
         setCurrent(_current, true, true);
