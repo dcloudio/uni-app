63dc97127eab4c90c973065e34f927e63672d5dd
fxy060608 <fxy060608@gmail.com>
2022-03-30 17:17:20 +0800

    fix(mp-alipay): onShareAppMessage (#3377)

--- src/platforms/mp-weixin/runtime/wrapper/app-base-parser.js
+++ src/platforms/mp-weixin/runtime/wrapper/app-base-parser.js
@@ -28,10 +28,6 @@ const hooks = [
   'onUnhandledRejection'
 ]
 
-if (__PLATFORM__ === 'mp-alipay') {
-  hooks.push('onShareAppMessage')
-}
-
 function initEventChannel () {
   Vue.prototype.getOpenerEventChannel = function () {
     // 微信小程序使用自身getOpenerEventChannel
