777a79f33825868f1f84fe96d00c14c662fd16a7
tianjiaxing <tianjiaxing2016@163.com>
2022-04-07 15:06:14 +0800

    fix(weixin): protocol showActionSheet, title -> alertText

--- src/platforms/mp-weixin/runtime/api/protocols.js
+++ src/platforms/mp-weixin/runtime/api/protocols.js
@@ -2,13 +2,15 @@
 import redirectTo from '../../helpers/redirect-to'
 import previewImage from '../../helpers/normalize-preview-image'
 import getSystemInfo from '../../helpers/system-info'
+import showActionSheet from '../../helpers/show-action-sheet'
 
 export const protocols = {
   redirectTo,
   // navigateTo,  // 由于在微信开发者工具的页面参数，会显示__id__参数，因此暂时关闭mp-weixin对于navigateTo的AOP
   previewImage,
   getSystemInfo,
-  getSystemInfoSync: getSystemInfo
+  getSystemInfoSync: getSystemInfo,
+  showActionSheet
 }
 export const todos = [
   'vibrate',
