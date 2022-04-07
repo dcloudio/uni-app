c1507a6484d42fe90d73febac959bc2b4da1b1a8
fxy060608 <fxy060608@gmail.com>
2022-04-07 15:50:14 +0800

    fix: Sync the provide in App.vue to app.provide (#3404)

--- packages/uni-h5-vue/dist/vue.runtime.cjs.js
+++ packages/uni-h5-vue/dist/vue.runtime.cjs.js
@@ -2724,6 +2724,10 @@ function provide(key, value) {
         }
         // TS doesn't allow symbol as index type
         provides[key] = value;
+        // 当实例为 App 时，同步到全局 provide
+        if (currentInstance.type.mpType === 'app') {
+            currentInstance.appContext.app.provide(key, value);
+        }
     }
 }
 function inject(key, defaultValue, treatDefaultAsFactory = false) {
