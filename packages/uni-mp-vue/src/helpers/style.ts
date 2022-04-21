8efbdcaaef13c308c30aa7508783832173b94354
fxy060608 <fxy060608@gmail.com>
2022-04-21 19:55:24 +0800

    fix(mp): ignore value check when stringify style (#3456)

--- packages/uni-mp-vue/src/helpers/style.ts
+++ packages/uni-mp-vue/src/helpers/style.ts
@@ -1,8 +1,19 @@
-import { isString } from '@vue/shared'
-import { normalizeStyle, stringifyStyle as stringify } from '@vue/shared'
+import { hyphenate, isString, NormalizedStyle } from '@vue/shared'
+import { normalizeStyle } from '@vue/shared'
 export function stringifyStyle(value: unknown) {
   if (isString(value)) {
     return value
   }
   return stringify(normalizeStyle(value))
+}
+
+function stringify(styles: NormalizedStyle | string | undefined): string {
+  let ret = ''
+  if (!styles || isString(styles)) {
+    return ret
+  }
+  for (const key in styles) {
+    ret += `${key.startsWith(`--`) ? key : hyphenate(key)}:${styles[key]};`
+  }
+  return ret
 }
