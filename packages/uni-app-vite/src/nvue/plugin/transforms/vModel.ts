a6834452886123f989dc8c39ab0a3f79bab39111
fxy060608 <fxy060608@gmail.com>
2022-04-21 14:35:52 +0800

    fix(nvue): v-model with input,textarea (question/143547)

--- packages/uni-app-vite/src/nvue/plugin/transforms/vModel.ts
+++ packages/uni-app-vite/src/nvue/plugin/transforms/vModel.ts
@@ -1,8 +1,33 @@
 import {
   transformModel as baseTransform,
   DirectiveTransform,
+  isStaticExp,
+  NodeTypes,
 } from '@vue/compiler-core'
-// 所有的 v-model 均走自定义组件的实现逻辑，包括 input,textarea
+const tags = ['u-input', 'u-textarea']
 export const transformModel: DirectiveTransform = (dir, node, context) => {
-  return baseTransform(dir, node, context)
+  const result = baseTransform(dir, node, context)
+  // 将 u-input,u-textarea 的 onUpdate:modelValue 事件转换为 onInput
+  if (tags.includes(node.tag) && result.props.length >= 2) {
+    const key = result.props[1].key
+    let value = result.props[1].value
+    if (value.type === NodeTypes.JS_CACHE_EXPRESSION) {
+      value = value.value
+    }
+    if (
+      isStaticExp(key) &&
+      key.content === 'onUpdate:modelValue' &&
+      value.type === NodeTypes.COMPOUND_EXPRESSION
+    ) {
+      key.content = 'onInput'
+      // 替换 $event 为 $event.detail.value
+      value.children = value.children.map((child) => {
+        if (typeof child === 'string') {
+          return child.replace(/=\s\$event/g, `= $event.detail.value`)
+        }
+        return child
+      })
+    }
+  }
+  return result
 }
