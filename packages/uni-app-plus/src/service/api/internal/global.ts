3a1ad68c6b5abe4f890e789341c5792764c17552
qiang <guoshengqiang@live.com>
2022-04-22 14:21:26 +0800

    feat(App): add requireGlobal

--- packages/uni-app-plus/src/service/api/internal/global.ts
+++ packages/uni-app-plus/src/service/api/internal/global.ts
@@ -40,3 +40,26 @@ export function restoreGlobal(
   }
   __uniConfig.serviceReady = true
 }
+
+export function requireGlobal() {
+  const list = [
+    'ArrayBuffer',
+    'Int8Array',
+    'Uint8Array',
+    'Uint8ClampedArray',
+    'Int16Array',
+    'Uint16Array',
+    'Int32Array',
+    'Uint32Array',
+    'Float32Array',
+    'Float64Array',
+    'BigInt64Array',
+    'BigUint64Array',
+  ]
+  const object: Record<string, any> = {}
+  for (let i = 0; i < list.length; i++) {
+    const key = list[i]
+    object[key] = (global as any)[key]
+  }
+  return object
+}
