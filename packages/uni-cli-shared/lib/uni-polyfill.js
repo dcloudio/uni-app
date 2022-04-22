938337a6f58ea97cdd50a197743ffbd9eaee301b
qiang <guoshengqiang@live.com>
2022-04-22 11:44:02 +0800

    feat(App): add requireGlobal
    # Conflicts:
    #	src/core/helpers/promise.js

--- packages/uni-cli-shared/lib/uni-polyfill.js
+++ packages/uni-cli-shared/lib/uni-polyfill.js
@@ -12,6 +12,18 @@ if (typeof Promise !== 'undefined' && !Promise.prototype.finally) {
     })
   }
 }
-if (uni.base64ToArrayBuffer) {
-  ArrayBuffer = uni.base64ToArrayBuffer('').constructor
+if (typeof uni !== 'undefined' && uni && uni.requireGlobal) {
+  const global = uni.requireGlobal()
+  ArrayBuffer = global.ArrayBuffer
+  Int8Array = global.Int8Array
+  Uint8Array = global.Uint8Array
+  Uint8ClampedArray = global.Uint8ClampedArray
+  Int16Array = global.Int16Array
+  Uint16Array = global.Uint16Array
+  Int32Array = global.Int32Array
+  Uint32Array = global.Uint32Array
+  Float32Array = global.Float32Array
+  Float64Array = global.Float64Array
+  BigInt64Array = global.BigInt64Array
+  BigUint64Array = global.BigUint64Array
 }
