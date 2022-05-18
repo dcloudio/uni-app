bf4f2318012f6c8e3a701216e1b41c6741efd3ee
DCloud_LXH <283700113@qq.com>
2022-05-18 15:55:07 +0800

    chore: canIUse getLaunchOptionsSync、getEnterOptionsSync

--- src/platforms/h5/helpers/can-i-use.js
+++ src/platforms/h5/helpers/can-i-use.js
@@ -5,5 +5,7 @@ function cssSupports (css) {
 export default {
   'css.var': cssSupports('--a:0'),
   'css.env': cssSupports('top:env(a)'),
-  'css.constant': cssSupports('top:constant(a)')
+  'css.constant': cssSupports('top:constant(a)'),
+  getLaunchOptionsSync: false,
+  getEnterOptionsSync: false
 }
