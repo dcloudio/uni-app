0dc17454f04641ae911a6cd06e963894f24d9dd4
handongxun <handongxun@dcloud.io>
2022-05-26 15:23:12 +0800

    feat: vue2 app 平台 manifest 增加检查Android系统webview版本配置

--- packages/webpack-uni-pages-loader/lib/platforms/app-plus/index.js
+++ packages/webpack-uni-pages-loader/lib/platforms/app-plus/index.js
@@ -451,6 +451,13 @@ module.exports = function (pagesJson, userManifestJson, isAppView) {
     }
   }
 
+  // 检查 webview 版本 || 下载 X5 后启动
+  let plusWebview = manifestJson['plus'].webView
+  if (plusWebview) {
+    manifestJson.plus['uni-app'].webView = plusWebview
+    delete manifestJson['plus'].webView
+  }
+
   // 记录编译器版本号
   appJson.compilerVersion = uniApp.compilerVersion
 
