71bb8c942ffd19f9e71bd539750873ccde5edd4b
qiang <guoshengqiang@live.com>
2022-06-22 17:34:18 +0800

    Revert "Revert "fix: 安卓适配 tabbar blurEffect""
    This reverts commit c42bfef7b7d25c06bc7b406c20e7bdc1219d3f10.

--- packages/uni-app-plus/src/service/framework/app/tabBar.ts
+++ packages/uni-app-plus/src/service/framework/app/tabBar.ts
@@ -7,8 +7,6 @@ import {
   OnTabBarMidButtonTap,
 } from '@dcloudio/uni-api'
 
-const isIOS = plus.os.name === 'iOS'
-
 let config: UniApp.TabBarOptions
 
 /**
@@ -210,7 +208,7 @@ export default {
   // tabBar是否遮挡内容区域
   get cover() {
     const array = ['extralight', 'light', 'dark']
-    return isIOS && array.indexOf(config.blurEffect as string) >= 0
+    return array.indexOf(config.blurEffect as string) >= 0
   },
   setStyle({ mask }: { mask: string }) {
     tabBar.setMask({
