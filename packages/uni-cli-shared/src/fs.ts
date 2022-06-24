48e3a6700a58f2a508d8450aa03885a2ca15ad59
fxy060608 <fxy060608@gmail.com>
2022-06-24 14:26:14 +0800

    fix: use fs-extra empty dir (#3650)

--- packages/uni-cli-shared/src/fs.ts
+++ packages/uni-cli-shared/src/fs.ts
@@ -1,17 +1,4 @@
-import fs from 'fs'
-import path from 'path'
-export function emptyDir(dir: string, skip?: string[]): void {
-  for (const file of fs.readdirSync(dir)) {
-    if (skip?.includes(file)) {
-      continue
-    }
-    const abs = path.resolve(dir, file)
-    // baseline is Node 12 so can't use rmSync :(
-    if (fs.lstatSync(abs).isDirectory()) {
-      emptyDir(abs)
-      fs.rmdirSync(abs)
-    } else {
-      fs.unlinkSync(abs)
-    }
-  }
+import { emptyDirSync } from 'fs-extra'
+export function emptyDir(dir: string): void {
+  return emptyDirSync(dir)
 }
