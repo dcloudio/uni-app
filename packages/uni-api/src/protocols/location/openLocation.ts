ac3d7bc7ee3a43190d3b331859f535ee72340ea4
zhenyuWang <13641039885@163.com>
2022-07-07 21:37:19 +0800

    fix(uni.openLocation): 修复latitude longitude 未校验必填问题

--- packages/uni-api/src/protocols/location/openLocation.ts
+++ packages/uni-api/src/protocols/location/openLocation.ts
@@ -3,6 +3,18 @@ export type API_TYPE_OPEN_LOCATION = typeof uni.openLocation
 
 export const OpenLocationOptions: ApiOptions<API_TYPE_OPEN_LOCATION> = {
   formatArgs: {
+    latitude(value, params) {
+      if (value !== 0 && !value) {
+        return 'latitude should not be empty.'
+      }
+      params.latitude = value
+    },
+    longitude(value, params) {
+      if (value !== 0 && !value) {
+        return 'longitude should not be empty.'
+      }
+      params.longitude = value
+    },
     scale(value, params) {
       value = Math.floor(value!)
       params.scale = value >= 5 && value <= 18 ? value : 18
@@ -11,14 +23,8 @@ export const OpenLocationOptions: ApiOptions<API_TYPE_OPEN_LOCATION> = {
 }
 
 export const OpenLocationProtocol: ApiProtocol<API_TYPE_OPEN_LOCATION> = {
-  latitude: {
-    type: Number,
-    required: true,
-  },
-  longitude: {
-    type: Number,
-    required: true,
-  },
+  latitude: Number,
+  longitude: Number,
   scale: Number,
   name: String,
   address: String,
