24dadeadf4d3e2fb21e4043ff2c31bd8319aa7c1
fxy060608 <fxy060608@gmail.com>
2022-07-08 18:21:49 +0800

    release: v3.0.0-alpha-3050120220706002

--- test.ts
+++ test.ts
@@ -1,3 +1,5 @@
-export function test() {
-  return { a: 1, b: [1, 2, 3] }
+export function test(): JSONObject {
+  var res = { a: 1, b: [1, 2, 3] }
+  return res
 }
+type JSONObject = Record<string, any>
