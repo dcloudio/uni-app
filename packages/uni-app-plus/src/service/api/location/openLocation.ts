9ed195e7ee109ec9fd20212e1ca1571cae141ee6
DCloud_LXH <283700113@qq.com>
2022-07-28 11:37:22 +0800

    fix(app): chooseLocation、openLocation i18n

--- packages/uni-app-plus/src/service/api/location/openLocation.ts
+++ packages/uni-app-plus/src/service/api/location/openLocation.ts
@@ -4,15 +4,19 @@ import {
   defineAsyncApi,
   OpenLocationProtocol,
   OpenLocationOptions,
+  getLocale,
 } from '@dcloudio/uni-api'
 import { showPage } from '@dcloudio/uni-core'
+import { extend } from '@vue/shared'
 
 export const openLocation = <API_TYPE_OPEN_LOCATION>defineAsyncApi(
   API_OPEN_LOCATION,
   (data, { resolve, reject }) => {
     showPage({
       url: '__uniappopenlocation',
-      data,
+      data: extend({}, data, {
+        locale: getLocale(),
+      }),
       style: {
         titleNView: {
           type: 'transparent',
