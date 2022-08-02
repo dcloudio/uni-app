ec9cfffbb88fa2a2c3e6c7da85da5761c46c2b4e
fxy060608 <fxy060608@gmail.com>
2022-08-02 17:37:22 +0800

    wip(uts): tests

--- scripts/setupJestEnv.ts
+++ scripts/setupJestEnv.ts
@@ -10,8 +10,9 @@ let instanceId = 1
       invokeSync(args: Record<string, any>, callback: unknown) {
         console.log(`invoke`, JSON.stringify(args))
         if (args.name === 'constructor') {
-          return instanceId++
+          return { type: 'return', params: instanceId++ }
         }
+        return { type: 'return', params: null }
       },
       invokeAsync(args: unknown, callback: unknown) {
         console.log(`invokeAsync`, JSON.stringify(args))
