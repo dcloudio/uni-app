dc47cd13d49140af331fda2e69c64a813b0be51e
fxy060608 <fxy060608@gmail.com>
2022-08-11 17:25:15 +0800

    wip(uts): compiler

--- packages/playground/uts/unpackage/dist/app-plus/uni_modules/test-uniplugin/app-android/index.kt
+++ packages/playground/uts/unpackage/dist/app-plus/uni_modules/test-uniplugin/app-android/index.kt
@@ -2,6 +2,9 @@ package uts.modules.testUniPlugin;
 import kotlinx.coroutines.*;
 import io.dcloud.uts.runtime.*;
 import android.util.Log;
+interface IUser {
+    fun register(name: String): Unit;
+}
 fun login(name: String, pwd: String): UtsJSONObject {
     console.log("login", "at app-android/login.uts:2");
     return object : UtsJSONObject() {
@@ -9,9 +12,10 @@ fun login(name: String, pwd: String): UtsJSONObject {
         var pwd = pwd
     };
 }
-open class User {
+open class User : IUser {
     open suspend fun login(name: String, pwd: String) = CoroutineScope(Dispatchers.Default).async {
         login(name, pwd);
         Log.info("123");
     }
+    override fun register(name: String) {}
 }
