c7e1a1a8ff6ded4d33ab9c14b08089a03081a8f3
fxy060608 <fxy060608@gmail.com>
2022-08-17 20:54:05 +0800

    release: v3.0.0-alpha-3050420220804006

--- packages/playground/uts/unpackage/dist/app/uni_modules/test-uniplugin/app-android/index.kt
+++ packages/playground/uts/unpackage/dist/app/uni_modules/test-uniplugin/app-android/index.kt
@@ -1,16 +1,19 @@
+package uts.modules.testUniPlugin;
+import kotlinx.coroutines.*;
+import io.dcloud.uts.runtime.*;
 import io.dcloud.uts.android.getResourcePath;
 import android.util.Log;
 interface IUser {
     fun register(name: String): Unit;
 }
 fun login(name: String, pwd: String): UtsJSONObject {
-    console.log("login", "at app-android/login.uts:2");
+    console.log("login", "at uni_modules/test-uniplugin/app-android/login.uts:2");
     return object : UtsJSONObject() {
         var name = name
         var pwd = pwd
     };
 }
-val __default = getResourcePath("static/logo.png");
+val __default = getResourcePath("uni_modules/test-uniplugin/static/logo.png");
 open class User : IUser {
     open suspend fun login(name: String, pwd: String) = CoroutineScope(Dispatchers.Default).async {
         login(name, pwd);
