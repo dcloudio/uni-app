41712e7ef7b32433d4ce217f8962f258c4a842f6
fxy060608 <fxy060608@gmail.com>
2022-08-17 13:54:01 +0800

    wip(uts): compiler

---
+++ packages/playground/uts/unpackage/dist/app-plus/uni_modules/test-uniplugin/app-android/index.kt
@@ -0,0 +1,23 @@
+import io.dcloud.uts.andriod.getResourcePath;
+import android.util.Log;
+interface IUser {
+    fun register(name: String): Unit;
+}
+fun login(name: String, pwd: String): UtsJSONObject {
+    console.log("login", "at app-android/login.uts:2");
+    return object : UtsJSONObject() {
+        var name = name
+        var pwd = pwd
+    };
+}
+val __default = getResourcePath("static/logo.png");
+open class User : IUser {
+    open suspend fun login(name: String, pwd: String) = CoroutineScope(Dispatchers.Default).async {
+        login(name, pwd);
+        Log.info("123");
+        Log.info(__default);
+    }
+    override fun register(name: String) {
+        Log.info(__default);
+    }
+}
