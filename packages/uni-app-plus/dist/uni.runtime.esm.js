04d7ff06fcf13e825b9a2b5ea896c20afe6b3b74
fxy060608 <fxy060608@gmail.com>
2022-08-17 18:49:02 +0800

    fix(push): getPushClientId

--- packages/uni-app-plus/dist/uni.runtime.esm.js
+++ packages/uni-app-plus/dist/uni.runtime.esm.js
@@ -11075,6 +11075,7 @@ const getLaunchOptionsSync = defineSyncApi(API_GET_LAUNCH_OPTIONS_SYNC, () => {
 let cid;
 let cidErrMsg;
 let enabled;
+let offline;
 function normalizePushMessage(message) {
     try {
         return JSON.parse(message);
@@ -11089,6 +11090,9 @@ function normalizePushMessage(message) {
 function invokePushCallback(args) {
     if (args.type === 'enabled') {
         enabled = true;
+        {
+            offline = args.offline;
+        }
     }
     else if (args.type === 'clientId') {
         cid = args.cid;
@@ -11127,6 +11131,15 @@ function invokeGetPushCidCallbacks(cid, errMsg) {
 }
 const API_GET_PUSH_CLIENT_ID = 'getPushClientId';
 const getPushClientId = defineAsyncApi(API_GET_PUSH_CLIENT_ID, (_, { resolve, reject }) => {
+    // App 端且启用离线时，使用 getClientInfoAsync 来调用
+    if (offline) {
+        plus.push.getClientInfoAsync((info) => {
+            resolve({ cid: info.clientid });
+        }, (res) => {
+            reject(res.code + ': ' + res.message);
+        });
+        return;
+    }
     Promise.resolve().then(() => {
         if (typeof enabled === 'undefined') {
             enabled = false;
--- packages/uni-push/src/plus.ts
+++ packages/uni-push/src/plus.ts
@@ -1,29 +1,10 @@
 // @ts-expect-error
 uni.invokePushCallback({
   type: 'enabled',
+  offline: true,
 })
 Promise.resolve().then(() => {
   plus.push.setAutoNotification && plus.push.setAutoNotification(false)
-  plus.push.getClientInfoAsync(
-    (info) => {
-      if (info.clientid) {
-        // @ts-expect-error
-        uni.invokePushCallback({
-          type: 'clientId',
-          cid: info.clientid,
-        })
-      }
-    },
-    (res) => {
-      // @ts-expect-error
-      uni.invokePushCallback({
-        type: 'clientId',
-        cid: '',
-        errMsg: res.code + ': ' + res.message,
-      })
-    }
-  )
-
   plus.push.addEventListener('click', (result) => {
     // @ts-expect-error
     uni.invokePushCallback({
