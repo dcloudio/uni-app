bab5dcaf3808acda6ed0b53b76994b1d338f15b4
qiang <guoshengqiang@live.com>
2022-08-23 19:57:14 +0800

    fix: pathToGlob tests

--- packages/uni-cli-shared/__tests__/pathToGlob.spec.ts
+++ packages/uni-cli-shared/__tests__/pathToGlob.spec.ts
@@ -4,9 +4,9 @@ describe('pathToGlob', () => {
   it('path in unix', () => {
     expect(pathToGlob('/test', '**/*.js')).toBe('/test/**/*.js')
     expect(pathToGlob('/test(1', '**/*.js')).toBe('/test[(]1/**/*.js')
-    expect(pathToGlob('/test(1', '**/*.js', { escape: true })).toBe(
-      '/test\\(1/**/*.js'
-    )
+    expect(
+      pathToGlob('/test(1', '**/*.js', { escape: true, windows: false })
+    ).toBe('/test\\(1/**/*.js')
   })
   it('path in windows', () => {
     expect(pathToGlob('C:\\\\test\\test', '**/*.js', { windows: true })).toBe(
