d8b7b569f30a41524dfb87fc9d4f8ca28d97cde1
zhenyuWang <13641039885@163.com>
2022-09-07 22:11:07 +0800

    refactor(h5 map):  优化map control创建方式

--- packages/uni-h5/src/view/components/map/MapControl.tsx
+++ packages/uni-h5/src/view/components/map/MapControl.tsx
@@ -1,7 +1,6 @@
-import { inject, onUnmounted, watch, PropType } from 'vue'
+import {  computed, PropType } from 'vue'
 import { getRealPath } from '@dcloudio/uni-platform'
-import { defineSystemComponent, useCustomEvent } from '@dcloudio/uni-components'
-import { Maps, Map } from './maps'
+import { defineSystemComponent } from '@dcloudio/uni-components'
 
 interface Position {
   left: number | string
@@ -12,78 +11,50 @@ interface Position {
 
 const props = {
   id: { type: [Number, String], default: '' },
-  position: { type: Object as PropType<Position>, require: true },
-  iconPath: { type: String, require: true },
+  position: { type: Object as PropType<Position>, required: true },
+  iconPath: { type: String, required: true },
   clickable: { type: [Boolean, String], default: '' },
-  rootRef: { type: Object, default: null },
+  trigger: { type: Function, required: true },
 }
 
 export type Props = Partial<Record<keyof typeof props, any>>
-type CustomEventTrigger = ReturnType<typeof useCustomEvent>
-type OnMapReadyCallback = (
-  map: Map,
-  maps: Maps,
-  trigger: CustomEventTrigger
-) => void
-type OnMapReady = (callback: OnMapReadyCallback) => void
 
 export default /*#__PURE__*/ defineSystemComponent({
   name: 'MapControl',
   props,
   setup(props) {
-    const onMapReady: OnMapReady = inject('onMapReady') as OnMapReady
-    let control: HTMLDivElement
-    function removeControl() {
-      if (control) {
-        control.remove()
-      }
-    }
-    onMapReady((_, __, trigger) => {
-      function updateControl(option: Props) {
-        removeControl()
-        addControl(option)
-      }
-      function addControl(option: Props) {
-        control = document.createElement('div')
-        const style = control.style
-        style.position = 'absolute'
-        style.width = '0'
-        style.height = '0'
-        style.top = '0'
-        style.left = '0'
-        const img = new Image()
-        img.src = getRealPath(option.iconPath)
-        img.onload = () => {
-          const position = option.position || {}
-          if (position.width) {
-            img.width = option.position.width
-          }
-          if (position.height) {
-            img.height = option.position.height
-          }
-          const style = img.style
-          style.position = 'absolute'
-          style.left = (position.left || 0) + 'px'
-          style.top = (position.top || 0) + 'px'
-          style.maxWidth = 'initial'
+    const imgPath = computed(() => getRealPath(props.iconPath!))
+    const positionStyle = computed(() => {
+      let positionStyle = `top:${props.position!.top || 0}px;left:${props.position!.left || 0}px;`
 
-          control.appendChild(img)
-          props.rootRef.value && props.rootRef.value.appendChild(control)
-        }
-        img.onclick = function ($event) {
-          if (option.clickable) {
-            trigger('controltap', $event, {
-              controlId: option.id,
-            })
-          }
-        }
+      if (props.position!.width) {
+        positionStyle += `width:${props.position!.width}px;`
       }
-      addControl(props as Props)
-      watch(props, updateControl)
+      if (props.position!.height) {
+        positionStyle += `height:${props.position!.height}px;`
+      }
+
+      return positionStyle
     })
-    onUnmounted(removeControl)
-    return () => {
-      return null
+    const handleClick = ($event: Event) => {
+      if (props.clickable) {
+        props.trigger!('controltap', $event, {
+          controlId: props.id,
+        })
+      }
     }
-  },
+
+    return () => {
+      return (
+        <div class="uni-map-control">
+        <img
+          src={imgPath.value}
+          style={positionStyle.value}
+          class="uni-map-control-icon"
+          onClick={handleClick}
+        />
+      </div>
+      )
+    }
+  }
 })
