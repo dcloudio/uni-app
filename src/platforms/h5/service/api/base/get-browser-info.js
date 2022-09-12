e74f241afd520279d878c648609194582eb78526
ichaoX <11764590+ichaoX@users.noreply.github.com>
2022-09-12 16:41:24 +0800

    fix(getBrowserInfo): osversionFind

--- src/platforms/h5/service/api/base/get-browser-info.js
+++ src/platforms/h5/service/api/base/get-browser-info.js
@@ -104,7 +104,7 @@ export function getBrowserInfo () {
     model = 'PC'
     osname = 'PC'
     deviceType = 'pc'
-    const osversionFind = ua.match(/\((.+?)\)/)[1]
+    const osversionFind = (ua.match(/\((.+?)\)/) || [])[1] || '';
 
     if (isWindows) {
       osname = 'Windows'
