81acc665b922f6a0721b918073e654fd84d55920
fxy060608 <fxy060608@gmail.com>
2022-10-08 15:41:22 +0800

    fix(mp-baidu): normalize asset file (question/154595)

--- packages/uni-cli-shared/lib/file-loader.js
+++ packages/uni-cli-shared/lib/file-loader.js
@@ -1,17 +1,16 @@
 const path = require('path')
-
-const isWin = /^win/.test(process.platform)
-
-const normalizePath = path => (isWin ? path.replace(/\\/g, '/') : path)
+const {
+  normalizeNodeModules
+} = require('./util')
 
 module.exports = {
   loader: 'file-loader',
   options: {
     publicPath (url, resourcePath, context) {
-      return '/' + normalizePath(path.relative(process.env.UNI_INPUT_DIR, resourcePath))
+      return '/' + normalizeNodeModules(path.relative(process.env.UNI_INPUT_DIR, resourcePath))
     },
     outputPath (url, resourcePath, context) {
-      return normalizePath(path.relative(process.env.UNI_INPUT_DIR, resourcePath))
+      return normalizeNodeModules(path.relative(process.env.UNI_INPUT_DIR, resourcePath))
     }
   }
 }
