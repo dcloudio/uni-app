a6f7b5a7b1378896a0954ba17a45639a91a77e1a
DCloud_LXH <283700113@qq.com>
2022-10-26 18:44:47 +0800

    feat(mp): copy theme.json use themeLocation

--- packages/uni-cli-shared/lib/theme.js
+++ packages/uni-cli-shared/lib/theme.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { parseJson } = require('./json')
 
 const {
   getJson
@@ -15,8 +16,8 @@ function parseThemeByJsonStr (jsonStr, keys, theme) {
   return jsonStr
 }
 
-function hasTheme (themeLocation = '') {
-  const themeJsonPath = path.join(process.env.UNI_INPUT_DIR, themeLocation || 'theme.json')
+function hasTheme (themeLocation = 'theme.json') {
+  const themeJsonPath = path.join(process.env.UNI_INPUT_DIR, themeLocation)
   return fs.existsSync(themeJsonPath)
 }
 
@@ -32,7 +33,7 @@ module.exports = {
   hasTheme,
   initTheme (manifestJson = {}) {
     const platform = process.env.UNI_PLATFORM
-    const themeLocation = (manifestJson[platform] || {}).themeLocation
+    const themeLocation = (manifestJson[platform] || {}).themeLocation || 'theme.json'
     if (!hasTheme(themeLocation)) {
       return
     }
@@ -40,7 +41,7 @@ module.exports = {
       return
     }
     try {
-      themeConfig = getJson(themeLocation || 'theme.json', true)
+      themeConfig = getJson(themeLocation, true)
       global.uniPlugin.defaultTheme = themeConfig.light
     } catch (e) {
       console.error(e)
@@ -59,5 +60,13 @@ module.exports = {
       return parseThemeByJsonStr(json, keys, theme)
     }
     return JSON.parse(parseThemeByJsonStr(JSON.stringify(json), keys, theme))
+  },
+  copyMiniProgramThemeJson (platformOptions, vueOptions) {
+    platformOptions.themeLocation || (platformOptions.themeLocation = 'theme.json')
+    return {
+      from: path.resolve(process.env.UNI_INPUT_DIR, platformOptions.themeLocation),
+      to: path.resolve(process.env.UNI_OUTPUT_DIR, platformOptions.themeLocation),
+      transform: content => JSON.stringify(parseJson(content.toString(), true))
+    }
   }
 }
