0997993f5ba6e0014df2fb85b8580da0b56da93d
zhenyuWang <13641039885@163.com>
2022-11-16 10:23:18 +0800

    fix(app): 修复nvue首页navigationBarTextStyle设置无效问题 (question/150485)

--- packages/uni-cli-shared/src/json/app/manifest/statusbar.ts
+++ packages/uni-cli-shared/src/json/app/manifest/statusbar.ts
@@ -2,8 +2,12 @@ export function initAppStatusbar(
   manifestJson: Record<string, any>,
   pagesJson: UniApp.PagesJson
 ) {
-  const { titleColor = '#000000', backgroundColor = '#000000' } =
-    pagesJson.globalStyle?.navigationBar || {}
+  const titleColor =
+    pagesJson.pages[0].style.navigationBar.titleColor ||
+    pagesJson.globalStyle.navigationBar.titleColor ||
+    '#ffffff'
+  const backgroundColor =
+    pagesJson.globalStyle.navigationBar.backgroundColor || '#000000'
 
   manifestJson.plus.statusbar = {
     immersed: 'supportedDevice',
