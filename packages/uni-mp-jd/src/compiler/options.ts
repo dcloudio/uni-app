bde58ad85b15ddd3acec4174d1abfbafb1b8b3fa
hexujie <hexujie@jd.com>
2022-12-21 20:28:33 +0800

    feat(uni-mp-jd): support custom-tab-bar

--- packages/uni-mp-jd/src/compiler/options.ts
+++ packages/uni-mp-jd/src/compiler/options.ts
@@ -70,7 +70,7 @@ export const options: UniMiniProgramPluginOptions = {
       targets: [
         {
           // FileWatcher这个类监听的文件，文件改动触发整体编译？编译什么？
-          src: ['project.config.json'],
+          src: ['project.config.json', 'custom-tab-bar'],
           // 输出目录
           get dest() {
             return process.env.UNI_OUTPUT_DIR
