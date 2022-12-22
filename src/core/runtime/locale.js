5077e0fc52f1ef16517d4ee3fdc724acc186d039
DCloud_LXH <283700113@qq.com>
2022-12-22 14:56:26 +0800

    fix(mp-weixin): getApp is not Function

--- src/core/runtime/locale.js
+++ src/core/runtime/locale.js
@@ -1,18 +1,21 @@
 import { normalizeLocale, LOCALE_EN } from '../helpers/i18n/index'
+import { isFn } from 'uni-shared'
 
 export function getLocale () {
   // 优先使用 $locale
-  const app = getApp({
-    allowDefault: true
-  })
-  if (app && app.$vm) {
-    return app.$vm.$locale
+  if (isFn(getApp)) {
+    const app = getApp({
+      allowDefault: true
+    })
+    if (app && app.$vm) {
+      return app.$vm.$locale
+    }
   }
   return normalizeLocale(__GLOBAL__.getSystemInfoSync().language) || LOCALE_EN
 }
 
 export function setLocale (locale) {
-  const app = getApp()
+  const app = isFn(getApp) ? getApp() : false
   if (!app) {
     return false
   }
