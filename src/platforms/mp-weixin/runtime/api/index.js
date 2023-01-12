8c278c9c4620a2025614dce98bee4a11e423a440
zhenyuWang <13641039885@163.com>
2023-01-12 15:41:24 +0800

    fix(mp-weixin): getAppBaseInfo 兼容问题

--- src/platforms/mp-weixin/runtime/api/index.js
+++ src/platforms/mp-weixin/runtime/api/index.js
@@ -6,6 +6,6 @@ export {
 }
   from 'uni-core/service/api/plugin/push'
 
-const host = wx.getAppBaseInfo().host
+const host = wx.getAppBaseInfo ? wx.getAppBaseInfo().host : wx.getSystemInfoSync().host
 export const shareVideoMessage =
 host && host.env === 'SAAASDK' ? wx.miniapp.shareVideoMessage : wx.shareVideoMessage
