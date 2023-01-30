030f28a2318271d04f273f801d6a2660bbf59b07
DCloud_LXH <283700113@qq.com>
2023-01-30 18:52:36 +0800

    feat(mp-weixin): uni.compressImage parameter compatible

--- packages/uni-mp-weixin/src/api/protocols.ts
+++ packages/uni-mp-weixin/src/api/protocols.ts
@@ -1,3 +1,5 @@
+import { MPProtocol } from '@dcloudio/uni-mp-core'
+
 export {
   redirectTo,
   previewImage,
@@ -9,3 +11,20 @@ export {
   getWindowInfo,
   getAppAuthorizeSetting,
 } from '@dcloudio/uni-mp-core'
+
+export const compressImage: MPProtocol = {
+  args(
+    fromArgs: UniApp.CompressImageOptions,
+    toArgs: WechatMiniprogram.CompressImageOption
+  ) {
+    // https://developers.weixin.qq.com/community/develop/doc/000c08940c865011298e0a43256800?highLine=compressHeight
+    if (fromArgs.compressedHeight && !toArgs.compressHeight) {
+      toArgs.compressHeight = fromArgs.compressedHeight
+    }
+    // @ts-expect-error
+    if (fromArgs.compressedWidth && !toArgs.compressWidth) {
+      // @ts-expect-error
+      toArgs.compressWidth = fromArgs.compressedWidth
+    }
+  },
+}
