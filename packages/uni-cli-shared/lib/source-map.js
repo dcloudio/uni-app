6167108aec90dabcdcab3c72b2a0b8439fcd8263
Vision_X <1334822971@qq.com>
2023-02-05 12:51:28 +0800

    fix: update incorrect sourcemap url

--- packages/uni-cli-shared/lib/source-map.js
+++ packages/uni-cli-shared/lib/source-map.js
@@ -48,7 +48,7 @@ module.exports = {
       ...args
     }
     if (filename) {
-      options.publicPath = "../"
+      options.append = "\n//# sourceMappingURL=../[url]"
       options.filename = '../.sourcemap/' + process.env.UNI_PLATFORM + '/[file].map'
     }
     return new webpack.SourceMapDevToolPlugin(options)
