f95ef3a2c61f3f444fc9baef35689997b0035c24
DCloud_LXH <283700113@qq.com>
2023-02-09 17:08:54 +0800

    chore: playground nvue test

--- packages/playground/__tests__/nvue.spec.ts
+++ packages/playground/__tests__/nvue.spec.ts
@@ -4,16 +4,13 @@ const fs = require('fs')
 const nvuePath = require('path').resolve(__dirname, '../nvue')
 
 beforeAll(async () => {
-  await execa('pnpm i', [], {
-    cwd: nvuePath,
-  })
   await execa('npm run', ['build:app'], {
     cwd: nvuePath,
   })
 })
 
 describe('nvue playground', () => {
-  jest.setTimeout(300 * 1000)
+  jest.setTimeout(50 * 1000)
   test('template scroll-view', () => {
     const s = fs.readFileSync(
       require('path').resolve(nvuePath, 'dist/build/app/pages/index/index.js'),
