0361bc0eadca818f69b4598b7d8c0513567d702d
zhenyuWang <13641039885@163.com>
2023-03-28 16:32:12 +0800

    fix(i18n): 默认语言跟随系部分语言失败及回退语言异常 (question/165884)

--- packages/uni-i18n/dist/uni-i18n.cjs.js
+++ packages/uni-i18n/dist/uni-i18n.cjs.js
@@ -1,8 +1,5 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const isArray = Array.isArray;
 const isObject = (val) => val !== null && typeof val === 'object';
 const defaultDelimiters = ['{', '}'];
 class BaseFormatter {
@@ -64,7 +61,7 @@ function parse(format, [startDelimiter, endDelimiter]) {
 function compile(tokens, values) {
     const compiled = [];
     let index = 0;
-    const mode = isArray(values)
+    const mode = Array.isArray(values)
         ? 'list'
         : isObject(values)
             ? 'named'
@@ -125,6 +122,10 @@ function normalizeLocale(locale, messages) {
         return locale;
     }
     locale = locale.toLowerCase();
+    if (locale === 'chinese') {
+        // 支付宝
+        return LOCALE_ZH_HANS;
+    }
     if (locale.indexOf('zh') === 0) {
         if (locale.indexOf('-hans') > -1) {
             return LOCALE_ZH_HANS;
@@ -137,7 +138,11 @@ function normalizeLocale(locale, messages) {
         }
         return LOCALE_ZH_HANS;
     }
-    const lang = startsWith(locale, [LOCALE_EN, LOCALE_FR, LOCALE_ES]);
+    let locales = [LOCALE_EN, LOCALE_FR, LOCALE_ES];
+    if (messages && Object.keys(messages).length > 0) {
+        locales = Object.keys(messages);
+    }
+    const lang = startsWith(locale, locales);
     if (lang) {
         return lang;
     }
@@ -419,7 +424,7 @@ function compileJsonObj(jsonObj, localeValues, delimiters) {
     return jsonObj;
 }
 function walkJsonObj(jsonObj, walk) {
-    if (isArray(jsonObj)) {
+    if (Array.isArray(jsonObj)) {
         for (let i = 0; i < jsonObj.length; i++) {
             if (walk(jsonObj, i)) {
                 return true;
