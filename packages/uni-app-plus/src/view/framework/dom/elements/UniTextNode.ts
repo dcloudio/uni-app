d650cec3b2e8ad30cb8c0931345ac26331f7dc29
fxy060608 <fxy060608@gmail.com>
2023-04-03 17:19:11 +0800

    fix(app): nested text

--- packages/uni-app-plus/src/view/framework/dom/elements/UniTextNode.ts
+++ packages/uni-app-plus/src/view/framework/dom/elements/UniTextNode.ts
@@ -1,7 +1,10 @@
-import { UniNodeJSON } from '@dcloudio/uni-shared'
+import { LINEFEED, UniNodeJSON } from '@dcloudio/uni-shared'
 import { UniNode } from './UniNode'
+import { parseText } from '@dcloudio/uni-components'
+import { UniTextElement } from './UniTextElement'
 
 export class UniTextNode extends UniNode {
+  private _text: string = ''
   constructor(
     id: number,
     parentNodeId: number,
@@ -11,5 +14,21 @@ export class UniTextNode extends UniNode {
     super(id, '#text', parentNodeId, document.createTextNode(''))
     this.init(nodeJson)
     this.insert(parentNodeId, refNodeId)
+  }
+  init(nodeJson: Partial<UniNodeJSON>) {
+    this._text = nodeJson.t || ''
+  }
+
+  setText(text: string) {
+    this._text = text
+    this.update()
+    this.updateView()
+  }
+
+  update() {
+    const {
+      $props: { space, decode },
+    } = this.$parent as UniTextElement
+    this.$.textContent = parseText(this._text, { space, decode }).join(LINEFEED)
   }
 }
