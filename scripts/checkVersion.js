91b571c07f3aa45e44a6fe579b7fbab23903d4e2
fxy060608 <fxy060608@gmail.com>
2023-04-13 16:46:55 +0800

    chore: bump typescript from 5.0.3 to 5.0.4

--- scripts/checkVersion.js
+++ scripts/checkVersion.js
@@ -19,7 +19,7 @@ const pkgs = {
     latest: '4.1.0',
   },
   pinia: {
-    latest: '2.0.33',
+    latest: '2.0.34',
   },
   'vue-i18n': {
     next: '9.1.9',
@@ -46,7 +46,7 @@ const pkgs = {
     latest: '3.4.0',
   },
   typescript: {
-    latest: '5.0.3',
+    latest: '5.0.4',
   },
 }
 
