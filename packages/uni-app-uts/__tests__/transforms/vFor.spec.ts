6fb591fd78006a81c330540bdaf029b6f207eb3a
zhenyuWang <13641039885@163.com>
2023-05-04 15:34:23 +0800

    test(uts): vIf vFor

--- packages/uni-app-uts/__tests__/transforms/vFor.spec.ts
+++ packages/uni-app-uts/__tests__/transforms/vFor.spec.ts
@@ -3,21 +3,21 @@ import { assert } from '../testUtils'
 describe('compiler:v-for', () => {
   test('template v-for', () => {
     assert(
-      `<view v-for="item in 10" :key="item">text</view>`,
+      `<text v-for="item in 10" :key="item">text</text>`,
       `createElementVNode(Fragment, null, RenderHelpers.renderList(10, (item, _key_, _index_):VNode => {
-  return createElementVNode("view", new Map<string,any>([["key", item]]), "text")
+  return createElementVNode("text", new Map<string,any>([["key", item]]), "text")
 }), 64 /* STABLE_FRAGMENT */)`
     )
     assert(
-      `<view v-for="(item, index) in [1,2,3]" :key="index">text</view>`,
+      `<text v-for="(item, index) in [1,2,3]" :key="index">text</text>`,
       `createElementVNode(Fragment, null, RenderHelpers.renderList([1,2,3], (item, index, _index_):VNode => {
-  return createElementVNode("view", new Map<string,any>([["key", index]]), "text")
+  return createElementVNode("text", new Map<string,any>([["key", index]]), "text")
 }), 64 /* STABLE_FRAGMENT */)`
     )
     assert(
-      `<view v-for="(item, key, index) in {a:'a',b:'b'}" :key="index">text</view>`,
+      `<text v-for="(item, key, index) in {a:'a',b:'b'}" :key="index">text</text>`,
       `createElementVNode(Fragment, null, RenderHelpers.renderList({a:'a',b:'b'}, (item, key, index):VNode => {
-  return createElementVNode("view", new Map<string,any>([["key", index]]), "text")
+  return createElementVNode("text", new Map<string,any>([["key", index]]), "text")
 }), 64 /* STABLE_FRAGMENT */)`
     )
   })
--- packages/uni-app-uts/__tests__/transforms/vIf.spec.ts
+++ packages/uni-app-uts/__tests__/transforms/vIf.spec.ts
@@ -10,20 +10,20 @@ describe('compiler:v-if', () => {
   test('template v-if + v-else', () => {
     assert(
       `
-        <view v-if="a">v-if</view>
-        <view v-else>v-else</view>
+        <text v-if="a">v-if</text>
+        <text v-else>v-else</text>
       `,
-      `isTrue(_ctx.a)\n  ? createElementVNode("view", new Map<string,any>([["key", 0]]), "v-if")\n  : createElementVNode("view", new Map<string,any>([["key", 1]]), "v-else")`
+      `isTrue(_ctx.a)\n  ? createElementVNode("text", new Map<string,any>([["key", 0]]), "v-if")\n  : createElementVNode("text", new Map<string,any>([["key", 1]]), "v-else")`
     )
   })
   test('template v-if + v-else-if + v-else', () => {
     assert(
       `
-        <view v-if="a">v-if</view>
-        <view v-else-if="a">v-else-if</view>
-        <view v-else>v-else</view>
+        <text v-if="a">v-if</text>
+        <text v-else-if="a">v-else-if</text>
+        <text v-else>v-else</text>
       `,
-      `isTrue(_ctx.a)\n  ? createElementVNode("view", new Map<string,any>([["key", 0]]), "v-if")\n  : isTrue(_ctx.a)\n    ? createElementVNode("view", new Map<string,any>([["key", 1]]), "v-else-if")\n    : createElementVNode("view", new Map<string,any>([["key", 2]]), "v-else")`
+      `isTrue(_ctx.a)\n  ? createElementVNode("text", new Map<string,any>([["key", 0]]), "v-if")\n  : isTrue(_ctx.a)\n    ? createElementVNode("text", new Map<string,any>([["key", 1]]), "v-else-if")\n    : createElementVNode("text", new Map<string,any>([["key", 2]]), "v-else")`
     )
   })
 })
