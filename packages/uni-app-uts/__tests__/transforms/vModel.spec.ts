04e7e1d785b61d46baa489b95c10d33459f50a55
zhenyuWang <13641039885@163.com>
2023-06-01 19:57:57 +0800

    wip(uts): vModel

--- packages/uni-app-uts/__tests__/transforms/vModel.spec.ts
+++ packages/uni-app-uts/__tests__/transforms/vModel.spec.ts
@@ -1,26 +1,92 @@
 import { assert } from '../testUtils'
 
-describe('compiler:v-model', () => {
-  test('input v-model', () => {
+describe('compiler: transform v-model', () => {
+  test('simple expression', () => {
     assert(
-      `<input type="text" v-model="title" />`,
+      `<input v-model="model" />`,
       `createElementVNode("input", new Map<string, any | null>([
-  ["type", "text"],
-  ["modelValue", _ctx.title],
-  ["onInput", ($event: InputEvent): any => {_ctx.title = $event.detail.value;
+  ["modelValue", _ctx.model],
+  ["onInput", ($event: InputEvent): any => {_ctx.model = $event.detail.value;
 return $event.detail.value;}]
 ]), null, 40 /* PROPS, HYDRATE_EVENTS */, ["modelValue", "onInput"])`
     )
   })
 
-  test('textarea v-model', () => {
+  // #2426
+  test('simple expression (with multilines)', () => {
     assert(
-      `<textarea v-model="title" />`,
-      `createElementVNode("textarea", new Map<string, any | null>([
-  ["modelValue", _ctx.title],
-  ["onInput", ($event: InputEvent): any => {_ctx.title = $event.detail.value;
+      `<input v-model="\nmodel.\nfoo\n" />`,
+      `createElementVNode("input", new Map<string, any | null>([
+  ["modelValue", \n_ctx.model.
+foo
+],
+  ["onInput", ($event: InputEvent): any => {
+_ctx.model.
+foo
+ = $event.detail.value;
 return $event.detail.value;}]
 ]), null, 40 /* PROPS, HYDRATE_EVENTS */, ["modelValue", "onInput"])`
+    )
+  })
+
+  test('compound expression', () => {
+    assert(
+      `<input v-model="model[index]" />`,
+      `createElementVNode(\"input\", new Map<string, any | null>([
+  [\"modelValue\", _ctx.model[_ctx.index]],
+  [\"onInput\", ($event: InputEvent): any => {_ctx.model[_ctx.index] = $event.detail.value;
+return $event.detail.value;}]
+]), null, 40 /* PROPS, HYDRATE_EVENTS */, [\"modelValue\", \"onInput\"])`
+    )
+  })
+
+  test('with argument', () => {
+    assert(
+      `<Foo v-model:title="model" />`,
+      `createVNode(_component_Foo, new Map<string, any | null>([
+  ["title", _ctx.model],
+  ["onUpdate:title", $event => ((_ctx.model) = $event)]
+]), null, 8 /* PROPS */, ["title", "onUpdate:title"])`
+    )
+  })
+
+  test('with dynamic argument', () => {
+    assert(
+      `<input v-model:[value]="model" />`,
+      `createElementVNode(\"input\", normalizeProps(new Map<string, any | null>([
+  [_ctx.value, _ctx.model],
+  [\"onUpdate:\" + _ctx.value, $event => ((_ctx.model) = $event)]
+])), null, 16 /* FULL_PROPS */)`
+    )
+  })
+  test('with modifier lazy', () => {
+    assert(
+      `<input v-model.lazy="model" />`,
+      `createElementVNode(\"input\", new Map<string, any | null>([
+  [\"modelValue\", _ctx.model],
+  [\"onBlur\", ($event: InputBlurEvent): any => {_ctx.model = $event.detail.value;
+return $event.detail.value;}]
+]), null, 40 /* PROPS, HYDRATE_EVENTS */, [\"modelValue\", \"onBlur\"])`
+    )
+  })
+  test('with modifier number', () => {
+    assert(
+      `<input v-model.number="model" />`,
+      `createElementVNode(\"input\", new Map<string, any | null>([
+  [\"modelValue\", _ctx.model],
+  [\"onInput\", ($event: InputEvent): any => {_ctx.model = _looseToNumber($event.detail.value);
+return _looseToNumber($event.detail.value);}]
+]), null, 40 /* PROPS, HYDRATE_EVENTS */, [\"modelValue\", \"onInput\"])`
+    )
+  })
+  test('with modifier trim', () => {
+    assert(
+      `<input v-model.trim="model" />`,
+      `createElementVNode(\"input\", new Map<string, any | null>([
+  [\"modelValue\", _ctx.model],
+  [\"onInput\", ($event: InputEvent): any => {_ctx.model = $event.detail.value.trim();
+return $event.detail.value.trim();}]
+]), null, 40 /* PROPS, HYDRATE_EVENTS */, [\"modelValue\", \"onInput\"])`
     )
   })
 })
