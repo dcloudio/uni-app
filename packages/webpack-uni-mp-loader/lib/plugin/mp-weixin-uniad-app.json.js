9787bf9a35fa4614331932185d1202261ab62276
hdx <hdx@dcloud.io>
2023-06-16 10:14:12 +0800

    uni-ad: 新增Web源，web激励视频

--- packages/webpack-uni-mp-loader/lib/plugin/mp-weixin-uniad-app.json.js
+++ packages/webpack-uni-mp-loader/lib/plugin/mp-weixin-uniad-app.json.js
@@ -1,24 +1,32 @@
 const UNI_PLUGINS = [{
   name: 'uni-ad',
-  version: '1.1.1',
+  version: '1.1.7',
   provider: 'wxf72d316417b6767f'
 },
 {
   name: 'coral-adv',
-  version: '1.0.15',
+  version: '1.0.18',
   provider: 'wx0e203209e27b1e66'
 }
 ]
 
+const {
+  getManifestJson,
+} = require('@dcloudio/uni-cli-shared/lib/manifest.js')
+
 module.exports = function (appJson, useAD) {
-  const plugins = appJson.plugins || {}
-  for (const key in plugins) {
-    const provider = plugins[key].provider
-    if (provider) {
-      const exists = UNI_PLUGINS.find((item) => item.provider === provider)
-      if (exists) {
-        console.error('应用的uni-ad配置不正确，请直接在页面中引入uni-ad广告组件，无需单独引入插件。')
-        process.exit(-1)
+  const manifestJson = getManifestJson()
+  const manifestJsonWxNode = manifestJson['mp-weixin']
+  if (manifestJsonWxNode) {
+    const plugins = manifestJsonWxNode.plugins || {}
+    for (const key in plugins) {
+      const provider = plugins[key].provider
+      if (provider) {
+        const exists = UNI_PLUGINS.find((item) => item.provider === provider)
+        if (exists) {
+          console.error('应用的uni-ad配置不正确，请直接在页面中引入uni-ad广告组件，无需单独引入插件。')
+          process.exit(-1)
+        }
       }
     }
   }
