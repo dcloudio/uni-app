f330ad5dcba8c11594296c5688719bce3901c8e5
zhenyuWang <13641039885@163.com>
2023-07-31 13:56:46 +0800

    wip(uts): objectStringToMapString

--- packages/uni-app-uts/src/plugins/uvue/compiler/utils.ts
+++ packages/uni-app-uts/src/plugins/uvue/compiler/utils.ts
@@ -33,73 +33,82 @@ function stringToData(str: string): object | any[] | string {
   return str
 }
 
-function stringToObject(str: string) {
-  let result: Record<string, any> = {}
-  // split key value
-  let preIndex = 0
+function stringToObject(str: string): object {
+  const result: Record<string, any> = {}
+  let preIndex = -1
+
   for (let i = 0; i < str.length; i++) {
-    let char = str[i]
-    // find key
+    const char = str[i]
     if (char === ':') {
-      let key = str.substring(preIndex, i).trim()
-      preIndex = i + 1
-      // find value
-      let num = 0
-      for (let j = i + 1; j < str.length; j++) {
-        let char2 = str[j]
-        if (isLeftBracket(char2)) {
-          num++
-        } else if (isRightBracket(char2)) {
-          num--
-        }
-        if (char2 === ',' && num === 0) {
-          let value = removeExtraQuotationMarks(
-            str.substring(preIndex, j).trim()
-          )
-          result[key] = isComplexExpressionString(value)
-            ? stringToData(value)
-            : value
-          preIndex = j + 1
-          i = j
-          break
-        }
-        if (j === str.length - 1 && num === 0) {
-          let value = removeExtraQuotationMarks(
-            str.substring(preIndex, str.length).trim()
-          )
-          result[key] = isComplexExpressionString(value)
-            ? stringToData(value)
-            : value
-          i = j
-          break
-        }
-      }
+      const key = str.substring(preIndex + 1, i).trim()
+      const { value, endIndex } = findObjectValueInString(i + 1, str)
+      result[key] = value
+      i = endIndex
+      preIndex = endIndex
     }
   }
 
   return result
 }
 
-function stringToArray(str: string) {
-  let result = []
-  // split item
-  let preIndex = 0
+function findObjectValueInString(
+  startIndex: number,
+  str: string
+): { value: any; endIndex: number } {
   let num = 0
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i]
+
+  for (let i = startIndex; i < str.length; i++) {
+    const char = str[i]
     if (isLeftBracket(char)) {
       num++
     } else if (isRightBracket(char)) {
       num--
     }
     if (char === ',' && num === 0) {
-      let item = removeExtraQuotationMarks(str.substring(preIndex, i).trim())
-      result.push(isComplexExpressionString(item) ? stringToData(item) : item)
-      preIndex = i + 1
+      const value = removeExtraQuotationMarks(
+        str.substring(startIndex, i).trim()
+      )
+      return {
+        value: isComplexExpressionString(value) ? stringToData(value) : value,
+        endIndex: i,
+      }
     }
     if (i === str.length - 1 && num === 0) {
-      let item = removeExtraQuotationMarks(
-        str.substring(preIndex, str.length).trim()
+      const value = removeExtraQuotationMarks(
+        str.substring(startIndex, str.length).trim()
+      )
+      return {
+        value: isComplexExpressionString(value) ? stringToData(value) : value,
+        endIndex: i,
+      }
+    }
+  }
+
+  return { value: '', endIndex: startIndex }
+}
+
+function stringToArray(str: string): any[] {
+  const result = []
+  let preIndex = -1
+  let num = 0
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i]
+    if (isLeftBracket(char)) {
+      num++
+    } else if (isRightBracket(char)) {
+      num--
+    }
+    if (char === ',' && num === 0) {
+      const item = removeExtraQuotationMarks(
+        str.substring(preIndex + 1, i).trim()
+      )
+      result.push(isComplexExpressionString(item) ? stringToData(item) : item)
+      preIndex = i
+    }
+    if (i === str.length - 1 && num === 0) {
+      const item = removeExtraQuotationMarks(
+        str.substring(preIndex + 1, str.length).trim()
       )
       item &&
         result.push(isComplexExpressionString(item) ? stringToData(item) : item)
@@ -110,15 +119,8 @@ function stringToArray(str: string) {
 }
 
 function convertObjectToMapString(obj: Record<any, any>): string {
-  let result = ''
   const keys = Object.keys(obj)
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i]
-    if (obj.hasOwnProperty(key)) {
-      result +=
-        getKeyValueString(key, obj[key]) + (i === keys.length - 1 ? '' : ', ')
-    }
-  }
+  const result = keys.map((key) => getKeyValueString(key, obj[key])).join(', ')
   return `new Map<string, any | null>(${result ? `[${result}]` : ''})`
 }
 
@@ -135,7 +137,7 @@ function getValueString(value: any): string {
       : `'${value}'`
   }
   if (Array.isArray(value)) {
-    return getArrayString(value)
+    return `[${value.map((item) => getValueString(item)).join(', ')}]`
   }
   if (isObject(value)) {
     return convertObjectToMapString(value)
@@ -143,56 +145,44 @@ function getValueString(value: any): string {
   return value
 }
 
-function getArrayString(arr: any[]): string {
-  let result = ''
-  arr.forEach((item, index) => {
-    result +=
-      index !== arr.length - 1
-        ? `${getValueString(item)}, `
-        : getValueString(item)
-  })
-  return `[${result}]`
-}
-
 function removeMapWrap(content: string): string {
-  if (content === 'new Map<string, any | null>()') {
-    return ''
-  }
   const mapPrefixLength = 29 // new Map<string, any | null>([
-  return content.substring(mapPrefixLength, content.length - 2)
+  return content === 'new Map<string, any | null>()'
+    ? ''
+    : content.substring(mapPrefixLength, content.length - 2)
 }
 
-function hasExtraQuotationMarks(str: string) {
+function hasExtraQuotationMarks(str: string): boolean {
   return str.startsWith("'") || str.endsWith("'")
 }
 
-function hasExtraStartQuotationMarks(str: string) {
-  return str.startsWith("'") || str.startsWith('"')
+function hasExtraStartQuotationMarks(str: string): boolean {
+  return /^['"].*['"]$/.test(str)
 }
 
-function removeStartAndEndChar(str: string) {
+function removeStartAndEndChar(str: string): string {
   return str.substring(1, str.length - 1)
 }
 
-function removeExtraQuotationMarks(str: string) {
+function removeExtraQuotationMarks(str: string): string {
   if (hasExtraStartQuotationMarks(str)) {
     return removeStartAndEndChar(str)
   }
   return str
 }
 
-function isComplexExpressionString(str: string) {
-  return str.includes('{') || str.includes('[')
+function isComplexExpressionString(str: string): boolean {
+  return /[{[]/.test(str)
 }
 
-function isBooleanString(str: string) {
+function isBooleanString(str: string): boolean {
   return str === 'true' || str === 'false'
 }
 
-function isLeftBracket(char: string) {
+function isLeftBracket(char: string): boolean {
   return char === '[' || char === '{' || char === '('
 }
 
-function isRightBracket(char: string) {
+function isRightBracket(char: string): boolean {
   return char === ']' || char === '}' || char === ')'
 }
