1c11b5bddb5e4eb766458359f6e7741e3fb4c124
zhenyuWang <13641039885@163.com>
2023-09-08 15:26:52 +0800

    wip(uts): uniCompileVersion

--- packages/uni-app-uts/src/plugins/manifestJson.ts
+++ packages/uni-app-uts/src/plugins/manifestJson.ts
@@ -57,7 +57,7 @@ export class UniAppConfig extends AppConfig {
     override appid: string = "${manifestJson.appid || ''}"
     override versionName: string = "${manifestJson.versionName || ''}"
     override versionCode: string = "${manifestJson.versionCode || ''}"
-    override uniCompilerVersion: string = "${
+    override uniCompileVersion: string = "${
       process.env.UNI_COMPILER_VERSION || ''
     }"
     // override tabBar = __uniTabBar
