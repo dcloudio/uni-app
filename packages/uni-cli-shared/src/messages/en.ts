a5317e95f0a794dd2b10feab783f2f820af55984
fxy060608 <fxy060608@gmail.com>
2023-09-26 20:45:08 +0800

    wip(uts): compiler

--- packages/uni-cli-shared/src/messages/en.ts
+++ packages/uni-cli-shared/src/messages/en.ts
@@ -3,6 +3,8 @@ export default {
   compiling: 'Compiling...',
   'dev.performance':
     'Please note that in running mode, due to log output, sourcemap, and uncompressed source code, the performance and package size are not as good as release mode.',
+  'dev.exclusion':
+    'Please configure the antivirus software to set up an exclusion list for scanning, reducing system resource consumption. [详情](https://uniapp.dcloud.net.cn/uni-app-x/compiler/#add-an-exclusion)',
   'dev.performance.nvue':
     'Especially the sourcemap of app-nvue has a greater impact',
   'dev.performance.mp':
