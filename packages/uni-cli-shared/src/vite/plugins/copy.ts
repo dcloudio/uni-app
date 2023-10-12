064d9e6d8a03eb6050a84dee53550a98dfe48241
zhenyuWang <13641039885@163.com>
2023-10-12 18:15:29 +0800

    fix: 修复自动化测试文件拷贝丢失问题

--- packages/uni-cli-shared/src/vite/plugins/copy.ts
+++ packages/uni-cli-shared/src/vite/plugins/copy.ts
@@ -54,8 +54,11 @@ export function uniViteCopyPlugin({
                   ...watchOptions,
                 },
                 (watcher) => {
-                  if (process.env.NODE_ENV !== 'development') {
-                    // 生产模式下，延迟 close，否则会影响 chokidar 初始化的 add 等事件
+                  if (
+                    process.env.NODE_ENV !== 'development' ||
+                    process.env.UNI_AUTOMATOR_CONFIG
+                  ) {
+                    // 生产或自动化测试模式下，延迟 close，否则会影响 chokidar 初始化的 add 等事件
                     setTimeout(() => {
                       watcher.close().then(() => resolve(void 0))
                     }, 2000)
