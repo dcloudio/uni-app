71b0a43555763c75cdd38f34f2465618f78ade5a
fxy060608 <fxy060608@gmail.com>
2023-10-18 16:57:41 +0800

    wip(uts): Generate class name using the same algorithm.

---
+++ packages/uni-app-uts/__tests__/utils.spec.ts
@@ -0,0 +1,15 @@
+import { genClassName } from '../src/plugins/utils'
+
+describe('uts:utils', () => {
+  test(`genClassName`, () => {
+    expect(genClassName('@dcloudio-unicloud-db')).toBe('GenDcloudioUnicloudDb')
+    expect(genClassName('.uvue-test')).toBe('GenUvueTest')
+    expect(genClassName('123-test')).toBe('Gen123Test')
+    expect(genClassName('components-test_123-test')).toBe(
+      'GenComponentsTest123Test'
+    )
+    expect(genClassName('components-test_b-test')).toBe(
+      'GenComponentsTestBTest'
+    )
+  })
+})
