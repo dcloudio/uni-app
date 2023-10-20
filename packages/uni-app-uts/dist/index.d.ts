70447431ac7a57b9ea6776d74ab353beca6df5bf
fxy060608 <fxy060608@gmail.com>
2023-10-20 14:33:11 +0800

    wip(uts): iOS

--- packages/uni-app-uts/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: (import("vite").Plugin | import("rollup").Plugin[])[];
-export default _default;
-export { genClassName } from './plugins/android/utils';
-export { transformVue } from './plugins/android/uvue/index';
--- packages/uni-app-uts/src/index.ts
+++ packages/uni-app-uts/src/index.ts
@@ -1,9 +1,10 @@
 import { initAndroid, initIOS } from './plugins'
 
-export default process.env.UNI_APP_X === 'true' &&
-process.env.UNI_UTS_PLATFORM === 'app-android'
-  ? initAndroid()
-  : initIOS()
+export default () => {
+  return process.env.UNI_UTS_PLATFORM === 'app-android'
+    ? initAndroid()
+    : initIOS()
+}
 
 export { genClassName } from './plugins/android/utils'
 export { transformVue } from './plugins/android/uvue/index'
