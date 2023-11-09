350d2afc27cdaef6a2e89da672a773863b879029
fxy060608 <fxy060608@gmail.com>
2023-11-09 14:00:21 +0800

    wip(uts): uts2js

--- packages/uni-uts-v1/lib/javascript/lib/runtime/index.d.ts
+++ packages/uni-uts-v1/lib/javascript/lib/runtime/index.d.ts
@@ -1,38 +1,36 @@
-declare function isInstanceOf(value: any, type: Function): boolean
+declare function isInstanceOf(value: any, type: Function): boolean;
 
 declare const JSON_2: {
-  parse: (
-    text: string,
-    reviver: (this: any, key: string, value: any) => any
-  ) => any | null
-  parseArray(text: string): Array<any> | null
-  parseObject(text: string): UTSJSONObject | null
-  stringify: (value: any) => string
-}
+    parse: (text: string, reviver: (this: any, key: string, value: any) => any) => any | null;
+    parseArray(text: string): Array<any> | null;
+    parseObject(text: string): UTSJSONObject | null;
+    stringify: (value: any) => string;
+};
 export { JSON_2 as JSON }
 
 export declare const UTS: {
-  isInstanceOf: typeof isInstanceOf
-}
+    isInstanceOf: typeof isInstanceOf;
+};
 
 export declare class UTSJSONObject {
-  [key: string]: any
-  private _content
-  constructor(content?: Record<string, any>)
-  toJSON(): Record<string, any>
-  private _getValue
-  get(key: string): any | null
-  set(key: string, value: any): void
-  getAny(key: string): any | null
-  getString(key: string): string | null
-  getNumber(key: string): number | null
-  getBoolean(key: string): boolean | null
-  getJSON(key: string): UTSJSONObject | null
-  getArray<T = any>(key: string): Array<T> | null
-  toMap(): Map<string, any>
-  forEach(callback: (value: any, key: string) => void): void
+    [key: string]: any;
+    private _content;
+    constructor(content?: Record<string, any>);
+    toJSON(): Record<string, any>;
+    private _getValue;
+    get(key: string): any | null;
+    set(key: string, value: any): void;
+    getAny(key: string): any | null;
+    getString(key: string): string | null;
+    getNumber(key: string): number | null;
+    getBoolean(key: string): boolean | null;
+    getJSON(key: string): UTSJSONObject | null;
+    getArray<T = any>(key: string): Array<T> | null;
+    toMap(): Map<string, any>;
+    forEach(callback: (value: any, key: string) => void): void;
 }
 
-export * from 'tslib'
 
-export {}
+export * from "tslib";
+
+export { }
--- packages/uni-uts-v1/lib/typescript/lib/lib/lib.es2018.intl.d.ts
+++ packages/uni-uts-v1/lib/typescript/lib/lib/lib.es2018.intl.d.ts
@@ -13,83 +13,59 @@ See the Apache Version 2.0 License for specific language governing permissions
 and limitations under the License.
 ***************************************************************************** */
 
+
 /// <reference no-default-lib="true"/>
 
 declare namespace Intl {
-  // http://cldr.unicode.org/index/cldr-spec/plural-rules#TOC-Determining-Plural-Categories
-  type LDMLPluralRule = 'zero' | 'one' | 'two' | 'few' | 'many' | 'other'
-  type PluralRuleType = 'cardinal' | 'ordinal'
 
-  interface PluralRulesOptions {
-    localeMatcher?: 'lookup' | 'best fit' | undefined
-    type?: PluralRuleType | undefined
-    minimumIntegerDigits?: number | undefined
-    minimumFractionDigits?: number | undefined
-    maximumFractionDigits?: number | undefined
-    minimumSignificantDigits?: number | undefined
-    maximumSignificantDigits?: number | undefined
-  }
+    // http://cldr.unicode.org/index/cldr-spec/plural-rules#TOC-Determining-Plural-Categories
+    type LDMLPluralRule = "zero" | "one" | "two" | "few" | "many" | "other";
+    type PluralRuleType = "cardinal" | "ordinal";
 
-  interface ResolvedPluralRulesOptions {
-    locale: string
-    pluralCategories: LDMLPluralRule[]
-    type: PluralRuleType
-    minimumIntegerDigits: number
-    minimumFractionDigits: number
-    maximumFractionDigits: number
-    minimumSignificantDigits?: number
-    maximumSignificantDigits?: number
-  }
+    interface PluralRulesOptions {
+        localeMatcher?: "lookup" | "best fit" | undefined;
+        type?: PluralRuleType | undefined;
+        minimumIntegerDigits?: number | undefined;
+        minimumFractionDigits?: number | undefined;
+        maximumFractionDigits?: number | undefined;
+        minimumSignificantDigits?: number | undefined;
+        maximumSignificantDigits?: number | undefined;
+    }
 
-  interface PluralRules {
-    resolvedOptions(): ResolvedPluralRulesOptions
-    select(n: number): LDMLPluralRule
-  }
+    interface ResolvedPluralRulesOptions {
+        locale: string;
+        pluralCategories: LDMLPluralRule[];
+        type: PluralRuleType;
+        minimumIntegerDigits: number;
+        minimumFractionDigits: number;
+        maximumFractionDigits: number;
+        minimumSignificantDigits?: number;
+        maximumSignificantDigits?: number;
+    }
 
-  const PluralRules: {
-    new (locales?: string | string[], options?: PluralRulesOptions): PluralRules
-    (locales?: string | string[], options?: PluralRulesOptions): PluralRules
+    interface PluralRules {
+        resolvedOptions(): ResolvedPluralRulesOptions;
+        select(n: number): LDMLPluralRule;
+    }
 
-    supportedLocalesOf(
-      locales: string | string[],
-      options?: { localeMatcher?: 'lookup' | 'best fit' }
-    ): string[]
-  }
+    const PluralRules: {
+        new (locales?: string | string[], options?: PluralRulesOptions): PluralRules;
+        (locales?: string | string[], options?: PluralRulesOptions): PluralRules;
 
-  // We can only have one definition for 'type' in TypeScript, and so you can learn where the keys come from here:
-  type ES2018NumberFormatPartType =
-    | 'literal'
-    | 'nan'
-    | 'infinity'
-    | 'percent'
-    | 'integer'
-    | 'group'
-    | 'decimal'
-    | 'fraction'
-    | 'plusSign'
-    | 'minusSign'
-    | 'percentSign'
-    | 'currency'
-    | 'code'
-    | 'symbol'
-    | 'name'
-  type ES2020NumberFormatPartType =
-    | 'compact'
-    | 'exponentInteger'
-    | 'exponentMinusSign'
-    | 'exponentSeparator'
-    | 'unit'
-    | 'unknown'
-  type NumberFormatPartTypes =
-    | ES2018NumberFormatPartType
-    | ES2020NumberFormatPartType
+        supportedLocalesOf(locales: string | string[], options?: { localeMatcher?: "lookup" | "best fit" }): string[];
+    };
 
-  interface NumberFormatPart {
-    type: NumberFormatPartTypes
-    value: string
-  }
+    // We can only have one definition for 'type' in TypeScript, and so you can learn where the keys come from here:
+    type ES2018NumberFormatPartType = "literal" | "nan" | "infinity" | "percent" | "integer" | "group" | "decimal" | "fraction" | "plusSign" | "minusSign" | "percentSign" | "currency" | "code" | "symbol" | "name";
+    type ES2020NumberFormatPartType = "compact" | "exponentInteger" | "exponentMinusSign" | "exponentSeparator" | "unit" | "unknown";
+    type NumberFormatPartTypes = ES2018NumberFormatPartType | ES2020NumberFormatPartType;
 
-  interface NumberFormat {
-    formatToParts(number?: number | bigint): NumberFormatPart[]
-  }
+    interface NumberFormatPart {
+        type: NumberFormatPartTypes;
+        value: string;
+    }
+
+    interface NumberFormat {
+        formatToParts(number?: number | bigint): NumberFormatPart[];
+    }
 }
