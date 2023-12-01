9022f221fa1927cf6de616a78d6eed7ca1d76445
yurj26 <1816387074@qq.com>
2023-12-01 16:49:32 +0800

    fix: cli项目`@vue/compiler-core`环境不对的问题

--- packages/vite-plugin-uni/bin/uni.js
+++ packages/vite-plugin-uni/bin/uni.js
@@ -19,4 +19,11 @@ if (debugIndex > 0) {
     }
   }
 }
+// uni-cli-shared 引入了 @vue/compiler-core，需要提前设置NODE_ENV，才能保证 @vue/compiler-core 加载的是对应环境的代码
+if (
+  process.argv[2] === 'build' &&
+  !['-w', '--watch'].includes(process.argv[3])
+) {
+  process.env.NODE_ENV = 'production'
+}
 require('../dist/cli/index.js')
