a327db875dee5251973f7559fbc14a1383117bbb
wangyaqi <wangyaqi988@163.com>
2024-01-03 14:57:50 +0800

    fix(uni-app-x web): 修复vite类型丢失的Bug

--- packages/uni-uts-v1/lib/tsconfig/env.d.ts
+++ packages/uni-uts-v1/lib/tsconfig/env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="vite/client" />
+/// <reference types="../../../../../../uniapp-cli-vite/node_modules/vite/client" />
 /// <reference types="../../../../../../hbuilderx-language-services/builtin-dts/uts-types/common/index.d.ts" />
 /// <reference types="../../../../../../hbuilderx-language-services/builtin-dts/uniappx/node_modules/@vue/global.d.ts" />
 /// <reference types="../../../../../../hbuilderx-language-services/builtin-dts/uniappx/node_modules/@dcloudio/uni-app-x/types/index.d.ts" />
