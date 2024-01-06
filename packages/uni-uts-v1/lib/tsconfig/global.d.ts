e0d6a93e9288e92d595b83af0b65b441ab41e0f6
wangyaqi <wangyaqi988@163.com>
2024-01-06 12:57:54 +0800

    feat(uni-app-x web): 移除对app端vue相关类型依赖

---
+++ packages/uni-uts-v1/lib/tsconfig/global.d.ts
@@ -0,0 +1,169 @@
+import {
+  VueAppContext as VueAppContextOrigin,
+  createAppContext as createAppContextOrigin,
+  VueAppConfig as VueAppConfigOrigin,
+  VueApp as VueAppOrigin,
+  definePlugin as definePluginOrigin,
+  createApp as createAppOrigin,
+  createSSRApp as createSSRAppOrigin,
+  withDirectives as withDirectivesOrigin,
+  VNode as VNodeOrigin,
+  guardReactiveProps as guardReactivePropsOrigin,
+  createVNode as createVNodeOrigin,
+  createElementVNode as createElementVNodeOrigin,
+  createCommentVNode as createCommentVNodeOrigin,
+  isVNode as isVNodeOrigin,
+  queuePostFlushCb as queuePostFlushCbOrigin,
+  watchEffect as watchEffectOrigin,
+  watch as watchOrigin,
+  provide as provideOrigin,
+  inject as injectOrigin,
+  currentInstance as currentInstanceOrigin,
+  getCurrentInstance as getCurrentInstanceOrigin,
+  CreateVueComponent as CreateVueComponentOrigin,
+  CreateVueAppComponent as CreateVueAppComponentOrigin,
+  VueAppComponent as VueAppComponentOrigin,
+  VueComponent as VueComponentOrigin,
+  ComponentInternalInstance as ComponentInternalInstanceOrigin,
+  getComponentName as getComponentNameOrigin,
+  withCtx as withCtxOrigin,
+  render as renderOrigin,
+  h as hOrigin,
+  padStyleMapOf as padStyleMapOfOrigin,
+  normalizeCssStyles as normalizeCssStylesOrigin,
+  injectHook as injectHookOrigin,
+  injectHookWithArg as injectHookWithArgOrigin,
+  onBeforeCreate as onBeforeCreateOrigin,
+  onCreated as onCreatedOrigin,
+  onBeforeMount as onBeforeMountOrigin,
+  onMounted as onMountedOrigin,
+  onBeforeUpdate as onBeforeUpdateOrigin,
+  onUpdated as onUpdatedOrigin,
+  onBeforeUnmount as onBeforeUnmountOrigin,
+  onUnmounted as onUnmountedOrigin,
+  vShow as vShowOrigin,
+  vModelDynamic as vModelDynamicOrigin,
+  withModifiers as withModifiersOrigin,
+  RenderHelpers as RenderHelpersOrigin,
+  resolveComponent as resolveComponentOrigin,
+  resolveEasyComponent as resolveEasyComponentOrigin,
+  resolveDynamicComponent as resolveDynamicComponentOrigin,
+  toHandlers as toHandlersOrigin,
+  renderSlot as renderSlotOrigin,
+  withMemo as withMemoOrigin,
+  isMemoSame as isMemoSameOrigin,
+  resolveCache as resolveCacheOrigin,
+  normalizePropsOptions as normalizePropsOptionsOrigin,
+  toDisplayString as toDisplayStringOrigin,
+  Directive as DirectiveOrigin,
+  WatchOptions as WatchOptionsOrigin,
+  VueComponentOptions as VueComponentOptionsOrigin,
+  ComponentPublicInstance as ComponentPublicInstanceOrigin,
+  EffectScope as EffectScopeOrigin,
+  ReactiveEffect as ReactiveEffectOrigin,
+  WritableComputedOptions as WritableComputedOptionsOrigin,
+  computed as computedOrigin,
+  effect as effectOrigin,
+  isReactive as isReactiveOrigin,
+  isReadonly as isReadonlyOrigin,
+  isRef as isRefOrigin,
+  reactive as reactiveOrigin,
+  ref as refOrigin,
+  shallowReactive as shallowReactiveOrigin,
+  shallowRef as shallowRefOrigin,
+  stop as stopOrigin,
+  toRaw as toRawOrigin,
+  unref as unrefOrigin,
+  PropType as PropTypeOrigin,
+  SlotsType as SlotsTypeOrigin
+} from '@vue/runtime-core'
+
+declare global {
+  const VueAppContext: typeof VueAppContextOrigin
+  type VueAppContext = VueAppContextOrigin
+  const createAppContext: typeof createAppContextOrigin
+  const VueAppConfig: typeof VueAppConfigOrigin
+  type VueAppConfig = VueAppConfigOrigin
+  const VueApp: typeof VueAppOrigin
+  type VueApp = VueAppOrigin
+  const definePlugin: typeof definePluginOrigin
+  const createApp: typeof createAppOrigin
+  const createSSRApp: typeof createSSRAppOrigin
+  const withDirectives: typeof withDirectivesOrigin
+  const VNode: typeof VNodeOrigin
+  type VNode = VNodeOrigin
+  const guardReactiveProps: typeof guardReactivePropsOrigin
+  const createVNode: typeof createVNodeOrigin
+  const createElementVNode: typeof createElementVNodeOrigin
+  const createCommentVNode: typeof createCommentVNodeOrigin
+  const isVNode: typeof isVNodeOrigin
+  const queuePostFlushCb: typeof queuePostFlushCbOrigin
+  const watchEffect: typeof watchEffectOrigin
+  const watch: typeof watchOrigin
+  const provide: typeof provideOrigin
+  const inject: typeof injectOrigin
+  const currentInstance: typeof currentInstanceOrigin
+  const getCurrentInstance: typeof getCurrentInstanceOrigin
+  const CreateVueComponent: typeof CreateVueComponentOrigin
+  type CreateVueComponent = CreateVueComponentOrigin
+  const CreateVueAppComponent: typeof CreateVueAppComponentOrigin
+  type CreateVueAppComponent = CreateVueAppComponentOrigin
+  const VueAppComponent: typeof VueAppComponentOrigin
+  type VueAppComponent = VueAppComponentOrigin
+  const VueComponent: typeof VueComponentOrigin
+  type VueComponent = VueComponentOrigin
+  const ComponentInternalInstance: typeof ComponentInternalInstanceOrigin
+  type ComponentInternalInstance = ComponentInternalInstanceOrigin
+  const getComponentName: typeof getComponentNameOrigin
+  const withCtx: typeof withCtxOrigin
+  const render: typeof renderOrigin
+  const h: typeof hOrigin
+  const padStyleMapOf: typeof padStyleMapOfOrigin
+  const normalizeCssStyles: typeof normalizeCssStylesOrigin
+  const injectHook: typeof injectHookOrigin
+  const injectHookWithArg: typeof injectHookWithArgOrigin
+  const onBeforeCreate: typeof onBeforeCreateOrigin
+  const onCreated: typeof onCreatedOrigin
+  const onBeforeMount: typeof onBeforeMountOrigin
+  const onMounted: typeof onMountedOrigin
+  const onBeforeUpdate: typeof onBeforeUpdateOrigin
+  const onUpdated: typeof onUpdatedOrigin
+  const onBeforeUnmount: typeof onBeforeUnmountOrigin
+  const onUnmounted: typeof onUnmountedOrigin
+  const vShow: typeof vShowOrigin
+  const vModelDynamic: typeof vModelDynamicOrigin
+  const withModifiers: typeof withModifiersOrigin
+  const RenderHelpers: typeof RenderHelpersOrigin
+  type RenderHelpers = RenderHelpersOrigin
+  const resolveComponent: typeof resolveComponentOrigin
+  const resolveEasyComponent: typeof resolveEasyComponentOrigin
+  const resolveDynamicComponent: typeof resolveDynamicComponentOrigin
+  const toHandlers: typeof toHandlersOrigin
+  const renderSlot: typeof renderSlotOrigin
+  const withMemo: typeof withMemoOrigin
+  const isMemoSame: typeof isMemoSameOrigin
+  const resolveCache: typeof resolveCacheOrigin
+  const normalizePropsOptions: typeof normalizePropsOptionsOrigin
+  const toDisplayString: typeof toDisplayStringOrigin
+  type Directive = DirectiveOrigin
+  type WatchOptions = WatchOptionsOrigin
+  type VueComponentOptions = VueComponentOptionsOrigin
+  type ComponentPublicInstance = ComponentPublicInstanceOrigin
+  const EffectScope: typeof EffectScopeOrigin
+  const ReactiveEffect: typeof ReactiveEffectOrigin
+  type WritableComputedOptions<T> = WritableComputedOptionsOrigin<T>
+  const computed: typeof computedOrigin
+  const effect: typeof effectOrigin
+  const isReactive: typeof isReactiveOrigin
+  const isReadonly: typeof isReadonlyOrigin
+  const isRef: typeof isRefOrigin
+  const reactive: typeof reactiveOrigin
+  const ref: typeof refOrigin
+  const shallowReactive: typeof shallowReactiveOrigin
+  const shallowRef: typeof shallowRefOrigin
+  const stop: typeof stopOrigin
+  const toRaw: typeof toRawOrigin
+  const unref: typeof unrefOrigin
+  type PropType<T> = PropTypeOrigin<T>
+  type SlotsType<T> = SlotsTypeOrigin<T>
+}
