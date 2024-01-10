865b581ce56f78daa9d2cc2b80ce1369bc231759
wangyaqi <wangyaqi988@163.com>
2024-01-10 15:04:03 +0800

    feat(uni-app-x web): style.setProperty支持传入rpx单位

--- packages/uni-components/src/helpers/UniElement.ts
+++ packages/uni-components/src/helpers/UniElement.ts
@@ -1,3 +1,13 @@
+function transformRpx(value: string) {
+  if (/-?\d+[ur]px/gi.test(value)) {
+    return value.replace(/(-?\d+)[ur]px/gi, (text, num) => {
+      return `${uni.upx2px(parseFloat(num))}px`
+    })
+    // eslint-disable-next-line no-useless-escape
+  }
+  return value
+}
+
 export class UniElement extends HTMLElement {
   private _props: Record<string, any> = {}
   constructor() {
@@ -16,5 +26,28 @@ export class UniElement extends HTMLElement {
     return name in this._props
       ? this._props[name] + ''
       : super.getAttribute(qualifiedName) || null
+  }
+
+  get style() {
+    const originalStyle = super.style
+    // @ts-ignore
+    if (originalStyle.__patchRpx__) {
+      return originalStyle
+    }
+    // @ts-ignore
+    originalStyle.__patchRpx__ = true
+    const originalSetProperty = originalStyle.setProperty.bind(originalStyle)
+    super.style.setProperty = function (
+      property: string,
+      value: string | null,
+      priority?: string
+    ) {
+      return originalSetProperty(
+        property,
+        value ? transformRpx(value + '') : value,
+        priority || undefined
+      )
+    }
+    return super.style
   }
 }
