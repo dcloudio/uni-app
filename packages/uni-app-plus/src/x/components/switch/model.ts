6ab834524d53ff6523290c53710ba7399d419808
jixinbao <jixinbao@dcloud.io>
2024-01-25 16:46:15 +0800

    wip(x): 补充 checkbox switch 组件

---
+++ packages/uni-app-plus/src/x/components/switch/model.ts
@@ -0,0 +1,7 @@
+/// <reference types="@dcloudio/uni-app-x/types/native-global" />
+
+export const SWITCH_NAME = 'Switch'
+export const SWITCH_NAME_ROOT_ELEMENT = 'uni-switch-element'
+
+// export class UniSwitchElement extends UniElement {}
+export class UniSwitchElement extends UniElementImpl {}
