dce17e83f5cdc73feca1ad9a8cccc17f5bcd516a
fxy060608 <fxy060608@gmail.com>
2024-01-30 16:01:29 +0800

    feat(uvue): web 平台 index.html 增加 main.js 的检测提示

--- packages/uni-h5-vite/src/plugin/transformIndexHtml/index.ts
+++ packages/uni-h5-vite/src/plugin/transformIndexHtml/index.ts
@@ -3,26 +3,26 @@ import type { Plugin } from 'vite'
 import { parseManifestJsonOnce } from '@dcloudio/uni-cli-shared'
 
 export function createTransformIndexHtml(): Plugin['transformIndexHtml'] {
-  let checked = false
+  let warned = false
   return async function (html) {
     const manifestJson = parseManifestJsonOnce(process.env.UNI_INPUT_DIR)
     const title = manifestJson.h5?.title || manifestJson.name || ''
     const isX = process.env.UNI_APP_X === 'true'
     if (isX) {
-      if (!checked) {
-        checked = true
-        // 兼容旧版本模板
-        const mainJs = ` src="/main.js"`
-        const mainTs = ` src="/main.ts"`
-        const main = ` src="/main"`
-        let oldMain = ''
-        if (html.includes(mainJs)) {
-          oldMain = mainJs
-        } else if (html.includes(mainTs)) {
-          oldMain = mainTs
-        }
-        if (oldMain) {
-          html = html.replace(oldMain, main)
+      // 兼容旧版本模板
+      const mainJs = ` src="/main.js"`
+      const mainTs = ` src="/main.ts"`
+      const main = ` src="/main"`
+      let oldMain = ''
+      if (html.includes(mainJs)) {
+        oldMain = mainJs
+      } else if (html.includes(mainTs)) {
+        oldMain = mainTs
+      }
+      if (oldMain) {
+        html = html.replace(oldMain, main)
+        if (!warned) {
+          warned = true
           console.warn(
             `当前项目根目录 index.html 未兼容 uni-app x 的 web 平台，请将里边的${oldMain} 调整为${main}。`
           )
