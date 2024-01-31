1936dd9850c9cc5d5d617cff19be86f847f0b12c
fxy060608 <fxy060608@gmail.com>
2024-01-31 17:43:09 +0800

    chore(uts): update renderComponentSlot

--- packages/uni-app/src/vue.ts
+++ packages/uni-app/src/vue.ts
@@ -3,8 +3,8 @@ import type { Slots } from 'vue'
 export function renderComponentSlot(
   slots: Slots,
   name: string,
-  props: any = null
-): any {
+  props: any | null = null
+): any | null {
   if (slots[name]) {
     return slots[name]!(props)
   }
