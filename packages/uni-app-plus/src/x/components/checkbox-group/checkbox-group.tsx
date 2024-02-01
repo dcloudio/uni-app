349f793b9a1f6a154be8bdaa00b17f3dc7af4279
jixinbao <jixinbao@dcloud.io>
2024-02-01 19:31:47 +0800

    feat(x): ios 完善 checkbox-group

--- packages/uni-app-plus/src/x/components/checkbox-group/checkbox-group.tsx
+++ packages/uni-app-plus/src/x/components/checkbox-group/checkbox-group.tsx
@@ -8,15 +8,14 @@ import {
   UniCheckboxGroupChangeEvent,
   UniCheckboxGroupElement,
 } from './model'
-import {
-  ComponentInternalInstance,
-  ComponentPublicInstance,
-  getCurrentInstance,
-  onMounted,
-  defineExpose,
-  ref,
-} from 'vue'
+import { getCurrentInstance, onMounted, ref } from 'vue'
 import { $dispatch } from '../../utils'
+
+interface CheckboxInfo {
+  name: string
+  checked: boolean
+  setCheckboxChecked: (checked: boolean) => void
+}
 
 export default /*#__PURE__*/ defineBuiltInComponent({
   name: CHECKBOX_GROUP_NAME,
@@ -29,55 +28,49 @@ export default /*#__PURE__*/ defineBuiltInComponent({
   emits: ['change'],
   setup(props, { emit, expose, slots }) {
     // data
-    const $checkboxList = ref<ComponentPublicInstance[]>([])
+    const $checkboxList = ref<CheckboxInfo[]>([])
     const uniCheckboxGroupElementRef = ref<UniCheckboxGroupElement | null>(null)
+    const instance = getCurrentInstance()
 
-    let instance: ComponentInternalInstance | null = null
-
-    instance = getCurrentInstance()
-
-    const _checkboxGroupUpdateHandler = (
-      vm: ComponentPublicInstance,
-      type: string
-    ) => {
+    /**
+     * 子 checkbox mounted 触发
+     */
+    const _checkboxGroupUpdateHandler = (info: CheckboxInfo, type: string) => {
       if (type == 'add') {
-        $checkboxList.value.push(vm)
+        $checkboxList.value.push(info)
       } else {
-        const index = $checkboxList.value.indexOf(vm)
+        const index = $checkboxList.value.findIndex((i) => i.name === info.name)
         if (index !== -1) {
           $checkboxList.value.splice(index, 1)
         }
       }
     }
-    const _changeHandler = () => {
+
+    /**
+     * checkbox change 触发
+     */
+    const _changeHandler = (info: CheckboxInfo) => {
+      // set
+      $checkboxList.value.forEach((i) => {
+        if (i.name === info.name) {
+          i.checked = info.checked
+        }
+      })
       emit('change', new UniCheckboxGroupChangeEvent(_getValue()))
     }
 
-    // 目前不能用
-    defineExpose({
-      _checkboxGroupUpdateHandler,
-      _changeHandler,
-    })
-
     const _getValue = () => {
       let valueArray: string[] = []
-      $checkboxList.value.forEach((vm) => {
-        const data = vm.$data as any
-        if (data.get('checkboxChecked') as boolean) {
-          valueArray.push(data.get('checkboxValue') as string)
+      $checkboxList.value.forEach((info) => {
+        if (info.checked) {
+          valueArray.push(info.name)
         }
       })
       return valueArray
     }
     const _setValue = (valueArray: string[]) => {
-      $checkboxList.value.forEach((vm) => {
-        const data = vm.$data as any
-        const value = data.get('checkboxValue') as string
-        if (valueArray.indexOf(value) !== -1) {
-          data.set('checkboxChecked', true)
-        } else {
-          data.set('checkboxChecked', false)
-        }
+      $checkboxList.value.forEach((info) => {
+        info.setCheckboxChecked(valueArray.includes(info.name))
       })
     }
 
@@ -91,8 +84,8 @@ export default /*#__PURE__*/ defineBuiltInComponent({
         uniCheckboxGroupElementRef.value._setValue = _setValue
         uniCheckboxGroupElementRef.value._initialValue = _getValue()
 
+        // for form
         const ctx = instance.proxy
-
         $dispatch(
           ctx,
           'Form',
