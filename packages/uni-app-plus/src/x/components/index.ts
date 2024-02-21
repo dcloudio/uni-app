9bb01dd62c40a760b7650a502d19649ceafd2da7
hdx <hdx@dcloud.io>
2024-02-21 18:10:18 +0800

    feat(x-ios): 注释对 slider 的引用，使用原生 slider

--- packages/uni-app-plus/src/x/components/index.ts
+++ packages/uni-app-plus/src/x/components/index.ts
@@ -1,4 +1,4 @@
-export * as Slider from './slider/slider'
+// export * as Slider from './slider/slider'
 export * as Button from './button/button'
 export * as Checkbox from './checkbox/checkbox'
 export * as CheckboxGroup from './checkbox-group/checkbox-group'
