29d5d2088874f6d28ce96fea57ccbc9d61bd4482
liuyanwei20 <liuyanwei20@jd.com>
2024-02-27 18:17:02 +0800

    fix(mp-jd): 支持配置subPackages项

--- packages/uni-mp-jd/lib/uni.config.js
+++ packages/uni-mp-jd/lib/uni.config.js
@@ -11,7 +11,7 @@ module.exports = {
       style: '.jxss',
       template: '.jxml'
     },
-    subPackages: false,
+    subPackages: true,
     project: 'project.config.json'
   },
   copyWebpackOptions (platformOptions, vueOptions) {
