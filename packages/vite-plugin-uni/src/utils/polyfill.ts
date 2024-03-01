f371ab017fce58e032b2924bf534b86e46beb5dd
fxy060608 <fxy060608@gmail.com>
2024-03-01 14:36:35 +0800

    fix: 重构 esbuild initTSConfck

--- packages/vite-plugin-uni/src/utils/polyfill.ts
+++ packages/vite-plugin-uni/src/utils/polyfill.ts
@@ -33,6 +33,7 @@ export function rewriteCompilerSfcParse() {
 }
 
 /**
+ * 已废弃，交由 rewriteExistsSyncHasRootFile 实现，因为新的 vite 版本在 configResolved 中重写已经晚了
  * 解决 HBuilderX 项目未包含 package.json 时，initTSConfck 可能导致查找过慢，或递归目录时权限不足报错
  * 即：未包含 package.json 时，直接移除 initTSConfck 相关逻辑
  * @param inputDir
