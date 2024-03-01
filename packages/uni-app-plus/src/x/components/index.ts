70b927b02788ee2e7e48cbebe1189fde6c4dd22d
hdx <hdx@dcloud.io>
2024-03-01 17:46:02 +0800

    feat(x:ios): 注释前端封装的 button，使用原生提供的 button

--- packages/uni-app-plus/src/x/components/index.ts
+++ packages/uni-app-plus/src/x/components/index.ts
@@ -1,5 +1,5 @@
 // export * as Slider from './slider/slider'
-export * as Button from './button/button'
+// export * as Button from './button/button'
 export * as Checkbox from './checkbox/checkbox'
 export * as CheckboxGroup from './checkbox-group/checkbox-group'
 export * as Radio from './radio/radio'
