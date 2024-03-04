ac36b2b2f34e6d30be3825b1709f38a14cdff137
qiang <guoshengqiang@live.com>
2024-03-04 15:11:06 +0800

    fix(x): 避免 iOS 文本解析遇到非法控制字符报错

--- packages/uni-app-uts/src/plugins/utils.ts
+++ packages/uni-app-uts/src/plugins/utils.ts
@@ -72,8 +72,13 @@ export function createUniOptions(
                 } else if (node.type === 2) {
                   const parent = context.parent
                   if (parent && parent.type === 1 && parent.tag === 'text') {
-                    // 解析文本节点换行
-                    node.content = JSON.parse(`"${node.content}"`)
+                    // 解析文本节点转义，暂时仅处理换行
+                    node.content = node.content.replace(
+                      /[\\]+n/g,
+                      function (match) {
+                        return JSON.parse(`"${match}"`)
+                      }
+                    )
                   }
                 }
               },
