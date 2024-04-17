cd0f4e54987e4a7194b10a44b68be85f4311e22f
qiang <guoshengqiang@live.com>
2024-04-17 17:34:31 +0800

    fix: build with node17+

--- build/build.js
+++ build/build.js
@@ -13,6 +13,22 @@ const {
   generateApiManifest
 } = require('./manifest')
 
+const crypto = require('crypto')
+
+/**
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ */
+try {
+  crypto.createHash('md4')
+} catch (e) {
+  // console.warn('Crypto "MD4" is not supported anymore by this Node.js version');
+  const origCreateHash = crypto.createHash
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === 'md4' ? 'md5' : alg, opts)
+  }
+}
+
 const fixInnerHTML = require('./fixInnerHTML')
 
 const service = new Service(process.env.VUE_CLI_CONTEXT || process.cwd(), {
