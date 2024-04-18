02035c22fb4caa79d65edef43b73f2b76f715fcf
jixinbao <jixinbao@dcloud.io>
2024-04-18 15:35:22 +0800

    feat(x): ios 子组件样式集成页面

--- packages/uni-app-plus/src/x/framework/app/initComponentInstance.ts
+++ packages/uni-app-plus/src/x/framework/app/initComponentInstance.ts
@@ -10,6 +10,19 @@ export function initComponentInstance(app: App) {
       if ((instance.type as any).mpType === 'app') {
         return
       }
+      if (instance.root === instance) {
+        // 页面
+      } else {
+        // 自定义组件，接受页面传递的样式
+        // @ts-expect-error
+        instance.type.styles = [
+          // @ts-expect-error
+          ...(instance.type.styles ?? []),
+          // @ts-expect-error
+          ...(instance.root.type.styles ?? []),
+        ]
+        // 和 android 不同，后续样式在 parseStyleSheet 进一步处理
+      }
       const pageId = instance.root.attrs.__pageId
       vm.$nativePage = getNativeApp().pageManager.findPageById(pageId + '')
     },
