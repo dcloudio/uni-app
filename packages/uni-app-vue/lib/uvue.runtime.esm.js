1ac28e541ded8567c598e747cec0cbf553ff8cd0
jixinbao <jixinbao@dcloud.io>
2024-04-24 14:54:48 +0800

    chore(x): 同步 app-x 产物

--- packages/uni-app-vue/lib/uvue.runtime.esm.js
+++ packages/uni-app-vue/lib/uvue.runtime.esm.js
@@ -9043,6 +9043,9 @@ function parseStyleDecl(prop, value) {
   return expandStyle(prop, value);
 }
 
+function isSame(a, b) {
+  return isString(a) && isString(b) || typeof a === "number" && typeof b === "number" ? a == b : a === b;
+}
 function patchStyle(el, prev, next) {
   if (!next) {
     return;
@@ -9057,9 +9060,9 @@ function patchStyle(el, prev, next) {
     const style = getExtraStyle(el);
     for (const key in prev) {
       const _key = camelize(key);
-      if (next[_key] == null) {
+      if (next[key] == null) {
         const value = classStyle != null && classStyle.has(_key) ? classStyle.get(_key) : "";
-        parseStyleDecl(key, value).forEach((value2, key2) => {
+        parseStyleDecl(_key, value).forEach((value2, key2) => {
           batchedStyles.set(key2, value2);
           style == null ? void 0 : style.delete(key2);
         });
@@ -9067,7 +9070,8 @@ function patchStyle(el, prev, next) {
     }
     for (const key in next) {
       const value = next[key];
-      {
+      const prevValue = prev[key];
+      if (!isSame(prevValue, value)) {
         parseStyleDecl(camelize(key), value).forEach(
           (value2, key2) => {
             batchedStyles.set(key2, value2);
