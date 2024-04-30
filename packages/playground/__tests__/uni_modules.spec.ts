f56f51980f6b12a2d79f0287a09537e3732dc87f
fxy060608 <fxy060608@gmail.com>
2024-04-30 17:42:41 +0800

    wip: uni_modules 编译模式

--- packages/playground/__tests__/uni_modules.spec.ts
+++ packages/playground/__tests__/uni_modules.spec.ts
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import fs from 'fs-extra'
 import path from 'path'
 import execa from 'execa'
 import { sync } from 'fast-glob'
@@ -32,13 +32,16 @@ describe('uni_modules playground', () => {
       //   'build:mp-weixin',
     ],
   }
+  const distDir = path.resolve(projectDir, 'dist')
+  if (fs.existsSync(distDir)) {
+    fs.emptyDirSync(distDir)
+  }
   Object.keys(modes).forEach((mode) => {
     const scripts = modes[mode]
     scripts.forEach((script) => {
       test(`${mode} ${script}`, async () => {
         const outDir = path.resolve(
-          projectDir,
-          'dist',
+          distDir,
           'build',
           mode,
           script.replace('build:', '')
@@ -49,6 +52,7 @@ describe('uni_modules playground', () => {
             ...process.env,
             UNI_OUTPUT_DIR: outDir,
             UNI_COMPILE_TARGET: 'uni_modules',
+            UNI_APP_X: mode === 'uni-app-x' ? 'true' : 'false',
           },
         })
         sync('**/*', { cwd: outDir, absolute: true }).forEach((file) => {
