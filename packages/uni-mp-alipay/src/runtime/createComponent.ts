776418de379451712e1cbaf44b06654f420fb6e7
jixinbao <jixinbao@dcloud.io>
2024-05-07 11:38:46 +0800

    feat(mp-alipay): 支付宝小程序支持用户传递 options

--- packages/uni-mp-alipay/src/runtime/createComponent.ts
+++ packages/uni-mp-alipay/src/runtime/createComponent.ts
@@ -85,7 +85,9 @@ function initVm(
 export function initCreateComponent() {
   return function createComponent(vueOptions: ComponentOptions) {
     vueOptions = vueOptions.default || vueOptions
-    const mpComponentOptions: tinyapp.ComponentOptions = {
+    const mpComponentOptions: tinyapp.ComponentOptions & {
+      options: any
+    } = {
       props: initComponentProps(vueOptions.props),
       didMount() {
         const createComponent = (parent?: ComponentPublicInstance) => {
@@ -115,6 +117,7 @@ export function initCreateComponent() {
         __l: handleLink,
         triggerEvent,
       },
+      options: vueOptions.options || {},
     }
     if (__VUE_OPTIONS_API__) {
       mpComponentOptions.data = initData(vueOptions)
