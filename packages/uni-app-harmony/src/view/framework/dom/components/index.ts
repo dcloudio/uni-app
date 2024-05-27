2f65226b27f63c46e800892de10cde878f981de1
DCloud_LXH <283700113@qq.com>
2024-05-27 11:16:35 +0800

    feat(harmony): image component wip: getRealPath

--- packages/uni-app-harmony/src/view/framework/dom/components/index.ts
+++ packages/uni-app-harmony/src/view/framework/dom/components/index.ts
@@ -2,10 +2,12 @@ import { UniTextNode } from '@dcloudio/uni-app-plus/view/framework/dom/elements/
 import { UniComment } from '@dcloudio/uni-app-plus/view/framework/dom/elements/UniComment'
 import { UniViewElement } from '@dcloudio/uni-app-plus/view/framework/dom/elements/UniViewElement'
 import { UniButton } from '@dcloudio/uni-app-plus/view/framework/dom/components/UniButton'
+import { UniImage } from '@dcloudio/uni-app-plus/view/framework/dom/components/UniImage'
 
 export const BuiltInComponents = {
   '#text': UniTextNode,
   '#comment': UniComment,
   VIEW: UniViewElement,
   BUTTON: UniButton,
+  IMAGE: UniImage,
 }
