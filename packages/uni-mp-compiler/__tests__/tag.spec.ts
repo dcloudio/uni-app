df47afe558f4d3517fe7c4d149c5fb941b9180b9
jixinbao <jixinbao@dcloud.io>
2024-05-28 17:25:09 +0800

    test(mp): 模板标签编译测试自闭合、成对标签

--- packages/uni-mp-compiler/__tests__/tag.spec.ts
+++ packages/uni-mp-compiler/__tests__/tag.spec.ts
@@ -4,9 +4,18 @@ import { assert } from './testUtils'
 describe('compiler: transform tag', () => {
   test('html', () => {
     Object.keys(HTML_TO_MINI_PROGRAM_TAGS).forEach((htmlTag) => {
+      // 自闭合
       assert(
         `<${htmlTag}/>`,
         `<${HTML_TO_MINI_PROGRAM_TAGS[htmlTag]}/>`,
+        `(_ctx, _cache) => {
+  return {}
+}`
+      )
+      // 成对标签
+      assert(
+        `<${htmlTag}></${htmlTag}>`,
+        `<${HTML_TO_MINI_PROGRAM_TAGS[htmlTag]}></${HTML_TO_MINI_PROGRAM_TAGS[htmlTag]}>`,
         `(_ctx, _cache) => {
   return {}
 }`
