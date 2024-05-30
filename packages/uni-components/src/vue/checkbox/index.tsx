ab4c4bbe17de37046bfee9a836cc1b5404550e3d
hdx <hdx@dcloud.io>
2024-05-30 16:58:07 +0800

    feat(x:components): 重命名属性 foregroundColor 为 foreColor, activeForegroundColor 为 activeForeColor

--- packages/uni-components/src/vue/checkbox/index.tsx
+++ packages/uni-components/src/vue/checkbox/index.tsx
@@ -57,7 +57,7 @@ const props = {
     default: '',
   },
   // 图标颜色,同color,优先级大于iconColor
-  foregroundColor: {
+  foreColor: {
     type: String,
     default: '',
   },
@@ -210,7 +210,7 @@ export default /*#__PURE__*/ defineBuiltInComponent({
                     ICON_PATH_SUCCESS_NO_CIRCLE,
                     props.disabled
                       ? '#ADADAD'
-                      : props.foregroundColor || props.iconColor || props.color,
+                      : props.foreColor || props.iconColor || props.color,
                     22
                   )
                 : ''}
