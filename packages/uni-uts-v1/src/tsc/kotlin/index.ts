91483c7aa5fdec21733afa342e64e71b8a2b9d9d
fxy060608 <fxy060608@gmail.com>
2024-06-11 17:08:35 +0800

    chore: merge

--- packages/uni-uts-v1/src/tsc/kotlin/index.ts
+++ packages/uni-uts-v1/src/tsc/kotlin/index.ts
@@ -1,6 +1,7 @@
+import path from 'path'
 import { extend, isFunction } from '@vue/shared'
 import type { RPT2Options } from 'rollup-plugin-typescript2'
-import { createBasicUtsOptions } from '../utils/options'
+import { isInHBuilderX } from '../../shared'
 interface UTS2KotlinOptions extends Omit<RPT2Options, 'transformers'> {
   inputDir: string
   sourcemap?: boolean
@@ -11,9 +12,33 @@ interface UTS2KotlinOptions extends Omit<RPT2Options, 'transformers'> {
 type uts2kotlin = (options: UTS2KotlinOptions) => import('rollup').Plugin[]
 
 export const uts2kotlin: uts2kotlin = (options) => {
-  extend(options, createBasicUtsOptions(options.inputDir))
+  extend(options, { clean: true })
+  // TODO 开发阶段禁用缓存
   if (isFunction(globalThis.uts2kotlin)) {
     return globalThis.uts2kotlin(options)
+  }
+  if (!options.tsconfig) {
+    options.tsconfig = path.resolve(
+      process.env.UNI_INPUT_DIR,
+      '../tsconfig.json'
+    )
+  }
+  if (!options.typescript) {
+    options.typescript = require('../../../lib/typescript')
+  }
+  if (isInHBuilderX()) {
+    options.tsconfigOverride = {
+      compilerOptions: {
+        typeRoots: [
+          options.inputDir,
+          path.resolve(
+            process.env.UNI_HBUILDERX_PLUGINS,
+            'uniapp-cli-vite',
+            'node_modules'
+          ),
+        ],
+      },
+    }
   }
   return require('../../../lib/kotlin').uts2kotlin(options)
 }
