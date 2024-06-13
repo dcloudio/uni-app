ba77dc8deb07128899615d6c676a71388c3abec0
fxy060608 <fxy060608@gmail.com>
2024-06-13 20:58:09 +0800

    chore: 同步 uts

--- packages/uni-uts-v1/src/tsc/index.ts
+++ packages/uni-uts-v1/src/tsc/index.ts
@@ -1,17 +1,20 @@
+import type { CompilerOptions, EmitResult } from 'typescript'
+
 export { uts2js } from './javascript'
 export { runUTS2KotlinDev } from './kotlin'
 
-import type { CompilerOptions, EmitResult } from 'typescript'
-
 export interface UTSEmitDeclarationOptions {
-  typescript: typeof import('typescript')
+  typescript?: typeof import('typescript')
   inputDir: string
   rootFiles: string[]
-  compilerOptions: CompilerOptions
+  compilerOptions?: CompilerOptions
 }
 
 export function emitDeclaration(
   options: UTSEmitDeclarationOptions
 ): EmitResult {
-  return require('../../../lib/uts').emitDeclaration(options)
+  return require('../../lib/uts').emitDeclaration({
+    typescript: require('../../lib/typescript'),
+    ...options,
+  })
 }
