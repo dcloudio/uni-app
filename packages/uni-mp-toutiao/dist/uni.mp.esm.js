33c969e0c4db9382cc0c2c13ffa351663f8b37a4
fxy060608 <fxy060608@gmail.com>
2024-06-14 14:14:46 +0800

    chore: build

--- packages/uni-mp-toutiao/dist/uni.mp.esm.js
+++ packages/uni-mp-toutiao/dist/uni.mp.esm.js
@@ -439,7 +439,7 @@ function initDefaultProps(options, isBehavior = false) {
     }
     if (options.behaviors) {
         // wx://form-field
-        if (options.behaviors.includes('__GLOBAL__://form-field')) {
+        if (options.behaviors.includes('tt' + '://form-field')) {
             if (!options.properties || !options.properties.name) {
                 properties.name = {
                     type: null,
@@ -637,7 +637,8 @@ function initBehaviors(vueOptions) {
     const behaviors = [];
     if (isArray(vueBehaviors)) {
         vueBehaviors.forEach((behavior) => {
-            behaviors.push(behavior.replace('uni://', '__GLOBAL__://'));
+            // 这里的 global 应该是个变量
+            behaviors.push(behavior.replace('uni://', 'tt' + '://'));
             if (behavior === 'uni://form-field') {
                 if (isArray(vueProps)) {
                     vueProps.push('name');
--- packages/uni-quickapp-webview/dist/uni.mp.esm.js
+++ packages/uni-quickapp-webview/dist/uni.mp.esm.js
@@ -417,7 +417,7 @@ function initDefaultProps(options, isBehavior = false) {
     }
     if (options.behaviors) {
         // wx://form-field
-        if (options.behaviors.includes('__GLOBAL__://form-field')) {
+        if (options.behaviors.includes('qa' + '://form-field')) {
             if (!options.properties || !options.properties.name) {
                 properties.name = {
                     type: null,
@@ -596,7 +596,8 @@ function initBehaviors(vueOptions) {
     const behaviors = [];
     if (isArray(vueBehaviors)) {
         vueBehaviors.forEach((behavior) => {
-            behaviors.push(behavior.replace('uni://', '__GLOBAL__://'));
+            // 这里的 global 应该是个变量
+            behaviors.push(behavior.replace('uni://', 'qa' + '://'));
             if (behavior === 'uni://form-field') {
                 if (isArray(vueProps)) {
                     vueProps.push('name');
