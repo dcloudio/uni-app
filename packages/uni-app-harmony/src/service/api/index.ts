7c0ad0d93a4555892fa895122c7a74d2577ac6a7
DCloud_LXH <283700113@qq.com>
2024-06-18 12:11:16 +0800

    feat(harmony): build api

--- packages/uni-app-harmony/src/service/api/index.ts
+++ packages/uni-app-harmony/src/service/api/index.ts
@@ -18,5 +18,14 @@ export {
   getLocale,
   onLocaleChange,
   createCanvasContext,
+  createSelectorQuery,
+  canvasGetImageData,
+  canvasPutImageData,
+  canvasToTempFilePath,
 } from '@dcloudio/uni-api'
+export {
+  pageScrollTo,
+  setNavigationBarColor,
+  setNavigationBarTitle,
+} from '@dcloudio/uni-app-plus/service/api'
 export { requireUTSPlugin, registerUTSPlugin } from './plugin/uts'
