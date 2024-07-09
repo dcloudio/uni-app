934aea3a14ab9cfb6f6f5daffa62830dfa0ff453
王亚琪 <wangyaqi988@163.com>
2024-07-09 14:49:07 +0800

    feat(uts2kt): 增加kotlin类型信息

---
+++ packages/uni-uts-v1/lib/kotlin/types/env.d.ts
@@ -0,0 +1,5 @@
+import "@dcloudio/virtual-modules/hbuilderx-language-services/builtin-dts/uts-types/common/index.d.ts"
+// import "@dcloudio/virtual-modules/hbuilderx-language-services/builtin-dts/uts-types/app-android/index.d.ts"
+import "@dcloudio/virtual-modules/hbuilderx-language-services/builtin-dts/common/HBuilderX.d.ts"
+import "@dcloudio/virtual-modules/hbuilderx-language-services/builtin-dts/uniappx/node_modules/@dcloudio/uni-app-x/types/index.d.ts"
+import "@dcloudio/virtual-modules/hbuilderx-language-services/builtin-dts/uniappx/node_modules/@vue/global.d.ts"
