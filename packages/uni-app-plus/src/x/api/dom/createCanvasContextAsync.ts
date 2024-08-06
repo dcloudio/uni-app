52b4f154516860f0b1148f3945e050194da2d528
jixinbao <jixinbao@dcloud.io>
2024-08-06 15:46:43 +0800

    refactor: 优化类型导出

--- packages/uni-app-plus/src/x/api/dom/createCanvasContextAsync.ts
+++ packages/uni-app-plus/src/x/api/dom/createCanvasContextAsync.ts
@@ -2,125 +2,12 @@ import { defineAsyncApi } from '@dcloudio/uni-api'
 import type { ComponentPublicInstance } from 'vue'
 import { getCurrentPage } from '@dcloudio/uni-core'
 import { isVueComponent } from './createSelectorQuery'
-
-// type API_TYPE_CREATE_CANVAS_CONTEXT_ASYNC = typeof uni.createCanvasContext
-
-type CreateCanvasContextAsyncSuccessCallback = any
-type CreateCanvasContextAsyncFailCallback = any
-type CreateCanvasContextAsyncCompleteCallback = any
-
-export type CreateCanvasContextAsyncOptions = {
-  /**
-   * canvas 元素的 id 属性
-   * @uniPlatform {
-   *  "app": {
-   *    "android": {
-   *        "osVer": "5.0",
-   *        "uniVer": "x",
-   *        "unixVer": "4.25"
-   *      },
-   *      "ios": {
-   *          "osVer": "10.0",
-   *          "uniVer": "x",
-   *          "unixVer": "4.25"
-   *      }
-   *  },
-   *  "web": {
-   *      "uniVer": "x",
-   *      "unixVer": "4.25"
-   *  }
-   * }
-   */
-  id: string.IDString
-  /**
-   * 组件或页面实例
-   * @uniPlatform {
-   *  "app": {
-   *    "android": {
-   *        "osVer": "5.0",
-   *        "uniVer": "x",
-   *        "unixVer": "4.25"
-   *      },
-   *      "ios": {
-   *          "osVer": "10.0",
-   *          "uniVer": "x",
-   *          "unixVer": "4.25"
-   *      }
-   *  },
-   *  "web": {
-   *      "uniVer": "x",
-   *      "unixVer": "4.25"
-   *  }
-   * }
-   */
-  component?: ComponentPublicInstance
-  /**
-   * 接口调用成功的回调函数
-   * @uniPlatform {
-   *  "app": {
-   *    "android": {
-   *        "osVer": "5.0",
-   *        "uniVer": "x",
-   *        "unixVer": "4.25"
-   *      },
-   *      "ios": {
-   *          "osVer": "10.0",
-   *          "uniVer": "x",
-   *          "unixVer": "4.25"
-   *      }
-   *  },
-   *  "web": {
-   *      "uniVer": "x",
-   *      "unixVer": "4.25"
-   *  }
-   * }
-   */
-  success?: CreateCanvasContextAsyncSuccessCallback
-  /**
-   * 接口调用失败的回调函数
-   * @uniPlatform {
-   *  "app": {
-   *    "android": {
-   *        "osVer": "5.0",
-   *        "uniVer": "x",
-   *        "unixVer": "4.25"
-   *      },
-   *      "ios": {
-   *          "osVer": "10.0",
-   *          "uniVer": "x",
-   *          "unixVer": "4.25"
-   *      }
-   *  },
-   *  "web": {
-   *      "uniVer": "x",
-   *      "unixVer": "4.25"
-   *  }
-   * }
-   */
-  fail?: CreateCanvasContextAsyncFailCallback
-  /**
-   * 接口调用结束的回调函数（调用成功、失败都会执行）
-   * @uniPlatform {
-   *  "app": {
-   *    "android": {
-   *        "osVer": "5.0",
-   *        "uniVer": "x",
-   *        "unixVer": "4.25"
-   *      },
-   *      "ios": {
-   *          "osVer": "10.0",
-   *          "uniVer": "x",
-   *          "unixVer": "4.25"
-   *      }
-   *  },
-   *  "web": {
-   *      "uniVer": "√",
-   *      "unixVer": "4.25"
-   *  }
-   * }
-   */
-  complete?: CreateCanvasContextAsyncCompleteCallback
-}
+export type {
+  CreateCanvasContextAsyncSuccessCallback,
+  CreateCanvasContextAsyncFailCallback,
+  CreateCanvasContextAsyncCompleteCallback,
+  CreateCanvasContextAsyncOptions,
+} from '@dcloudio/uni-app-x/types/uni'
 
 export interface CanvasContext {
   getContext(type: string): CanvasRenderingContext2D
@@ -162,11 +49,10 @@ export const createCanvasContextAsync = defineAsyncApi(
 
     resolve({
       getContext: element.getContext.bind(element),
+      // @ts-expect-error waiting for uni-app-x type update
       toDataURL: element.toDataURL.bind(element),
-      // @ts-expect-error 类型不匹配？
-      createImage(): Image {
-        return new Image()
-      },
+      // @ts-expect-error waiting for uni-app-x type update
+      createImage: element.createImage.bind(element),
     })
   }
 )
