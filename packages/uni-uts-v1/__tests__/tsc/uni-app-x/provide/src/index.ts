ba375981026dafd09343512108d0dfa84b73878f
fxy060608 <fxy060608@gmail.com>
2024-08-09 13:59:43 +0800

    wip(x-android): tsc

--- packages/uni-uts-v1/__tests__/tsc/uni-app-x/provide/src/index.ts
+++ packages/uni-uts-v1/__tests__/tsc/uni-app-x/provide/src/index.ts
@@ -6,3 +6,24 @@ defineComponent({
     }
   },
 })
+const __sfc__ = defineComponent({
+  inject: {
+    str: {
+      type: String,
+      default: 'str',
+    },
+  },
+  data() {
+    return {
+      a: '',
+    }
+  },
+  render() {
+    this.str
+  },
+})
+
+export function render(this: InstanceType<typeof __sfc__>) {
+  const _ctx = this
+  _ctx.str
+}
