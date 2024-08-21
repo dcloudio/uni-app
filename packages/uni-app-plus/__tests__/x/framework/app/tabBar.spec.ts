423f17b49272f9db90b73197a41922c0937434d9
jixinbao <jixinbao@dcloud.io>
2024-08-21 16:31:47 +0800

    fix(x): 补充相关测试

--- packages/uni-app-plus/__tests__/x/framework/app/tabBar.spec.ts
+++ packages/uni-app-plus/__tests__/x/framework/app/tabBar.spec.ts
@@ -1,9 +1,11 @@
 import { fixBorderStyle } from '../../../../src/x/framework/app/tabBar'
 
 describe('test tabBar', () => {
-  it('test fixBorderStyle', () => {
+  it('定义存在', () => {
     expect(fixBorderStyle).toBeDefined()
+  })
 
+  it('设置 borderStyle', () => {
     const tabBarConfig = new Map([
       ['borderStyle', 'white'],
       ['borderColor', undefined],
@@ -18,6 +20,15 @@ describe('test tabBar', () => {
     fixBorderStyle(tabBarConfig2)
     expect(tabBarConfig2.get('borderStyle')).toBe('rgba(0, 0, 0, 0.33)')
 
+    const tabBarConfig5 = new Map([
+      ['borderStyle', 'red'],
+      ['borderColor', undefined],
+    ])
+    fixBorderStyle(tabBarConfig5)
+    expect(tabBarConfig5.get('borderStyle')).toBe('rgba(0, 0, 0, 0.33)')
+  })
+
+  it('borderColor 优先级高', () => {
     const tabBarConfig3 = new Map([
       ['borderStyle', 'black'],
       ['borderColor', 'red'],
@@ -31,12 +42,14 @@ describe('test tabBar', () => {
     ])
     fixBorderStyle(tabBarConfig4)
     expect(tabBarConfig4.get('borderStyle')).toBe('blue')
+  })
 
-    const tabBarConfig5 = new Map([
-      ['borderStyle', 'red'],
+  it('都不设置', () => {
+    const tabBarConfig6 = new Map([
+      ['borderStyle', undefined],
       ['borderColor', undefined],
     ])
-    fixBorderStyle(tabBarConfig5)
-    expect(tabBarConfig5.get('borderStyle')).toBe('rgba(0, 0, 0, 0.33)')
+    fixBorderStyle(tabBarConfig6)
+    expect(tabBarConfig6.get('borderStyle')).toBe(undefined)
   })
 })
