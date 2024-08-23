51fe9f2867710d987895684631d1167492abaff9
zhenyuWang <13641039885@163.com>
2024-08-23 17:46:04 +0800

    wip(x-ios): 实现 dialogPage eventBus

--- packages/uni-app-plus/src/x/framework/page/dialogPage.ts
+++ packages/uni-app-plus/src/x/framework/page/dialogPage.ts
@@ -1,9 +1,20 @@
+import {
+  type EmitterEmit,
+  type EmitterOff,
+  type EmitterOn,
+  type EmitterOnce,
+  EventBus,
+} from '@dcloudio/uni-api'
 import type { ComponentPublicInstance } from 'vue'
 
 export class DialogPage {
   route: string = ''
   component?: any
   $getParentPage: () => ComponentPublicInstance | null
+  $on: EmitterOn
+  $once: EmitterOnce
+  $off: EmitterOff
+  $emit: EmitterEmit
   $disableEscBack: boolean = false
   $vm?: ComponentPublicInstance
 
@@ -19,6 +30,11 @@ export class DialogPage {
     this.route = route
     this.component = component
     this.$getParentPage = $getParentPage
+    const { $on, $once, $emit, $off } = new EventBus()
+    this.$on = $on
+    this.$once = $once
+    this.$off = $off
+    this.$emit = $emit
   }
 }
 
