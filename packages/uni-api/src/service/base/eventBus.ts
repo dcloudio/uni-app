2aaaaf1d761f70df2d1eed50f63e3f72ab8d7723
zhenyuWang <13641039885@163.com>
2024-08-26 16:35:40 +0800

    refactor(eventBus): 支持摇树

--- packages/uni-api/src/service/base/eventBus.ts
+++ packages/uni-api/src/service/base/eventBus.ts
@@ -31,49 +31,63 @@ export type EmitterOff = (
   callback?: (result: any) => void
 ) => void
 export type EmitterEmit = (eventName: string, param?: any) => void
+
 export class EventBus {
   emitter = new Emitter()
-  $on = defineSyncApi<API_TYPE_ON>(
-    API_ON,
-    (name, callback): EventStopHandler => {
-      this.emitter.on(name, callback)
-
-      return () => this.emitter.off(name, callback)
-    },
-    OnProtocol
-  )
-  $once = defineSyncApi<API_TYPE_ONCE>(
-    API_ONCE,
-    (name, callback): EventStopHandler => {
-      this.emitter.once(name, callback)
-
-      return () => this.emitter.off(name, callback)
-    },
-    OnceProtocol
-  )
-  $off = defineSyncApi<API_TYPE_OFF>(
-    API_OFF,
-    (name, callback) => {
-      if (!name) {
-        this.emitter.e = {}
-        return
-      }
-      if (!isArray(name)) name = [name]
-      name.forEach((n) => this.emitter.off(n, callback))
-    },
-    OffProtocol
-  )
-  $emit = defineSyncApi<API_TYPE_EMIT>(
-    API_EMIT,
-    (name, ...args: any[]) => {
-      this.emitter.emit(name, ...args)
-    },
-    EmitProtocol
-  )
+  $on = (name, callback) => {
+    this.emitter.on(name, callback)
+  }
+  $once = (name, callback) => {
+    this.emitter.once(name, callback)
+  }
+  $off = (name, callback) => {
+    if (!name) {
+      this.emitter.e = {}
+      return
+    }
+    if (!isArray(name)) name = [name]
+    name.forEach((n) => this.emitter.off(n, callback))
+  }
+  $emit = (name, ...args: any[]) => {
+    this.emitter.emit(name, ...args)
+  }
 }
 
 const eventBus = new EventBus()
-export const $on = eventBus.$on
-export const $once = eventBus.$once
-export const $off = eventBus.$off
-export const $emit = eventBus.$emit
+export const $on = defineSyncApi<API_TYPE_ON>(
+  API_ON,
+  (name, callback): EventStopHandler => {
+    eventBus.$on(name, callback)
+
+    return () => eventBus.$off(name, callback)
+  },
+  OnProtocol
+)
+export const $once = defineSyncApi<API_TYPE_ONCE>(
+  API_ONCE,
+  (name, callback): EventStopHandler => {
+    eventBus.$once(name, callback)
+
+    return () => eventBus.$off(name, callback)
+  },
+  OnceProtocol
+)
+export const $off = defineSyncApi<API_TYPE_OFF>(
+  API_OFF,
+  (name, callback) => {
+    if (!name) {
+      eventBus.emitter.e = {}
+      return
+    }
+    if (!isArray(name)) name = [name]
+    name.forEach((n) => eventBus.$off(n, callback))
+  },
+  OffProtocol
+)
+export const $emit = defineSyncApi<API_TYPE_EMIT>(
+  API_EMIT,
+  (name, ...args: any[]) => {
+    eventBus.$emit(name, ...args)
+  },
+  EmitProtocol
+)
