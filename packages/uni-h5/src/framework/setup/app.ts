47adf8e88da3a217deb04c5a5dcd4a7bfd86020c
zhenyuWang <13641039885@163.com>
2024-08-29 11:54:09 +0800

    wip(x-web): 重构 UniApp

--- packages/uni-h5/src/framework/setup/app.ts
+++ packages/uni-h5/src/framework/setup/app.ts
@@ -7,11 +7,61 @@ import {
   initService,
   initView,
 } from '@dcloudio/uni-core'
+import {
+  type EmitterEmit,
+  type EmitterOff,
+  type EmitterOn,
+  type EmitterOnce,
+  EventBus,
+} from '@dcloudio/uni-api'
 
 let appVm: ComponentPublicInstance
+let $uniApp: UniApp
+if (__X__) {
+  class UniApp {
+    on: EmitterOn
+    once: EmitterOnce
+    off: EmitterOff
+    emit: EmitterEmit
+    constructor() {
+      const eventBus = new EventBus()
+      this.on = (eventName: string, callback: (result: any) => void) => {
+        eventBus.$on(eventName, callback)
+      }
+      this.once = (eventName: string, callback: (result: any) => void) => {
+        eventBus.$once(eventName, callback)
+      }
+      this.off = (
+        eventName?: string | string[],
+        callback?: (result: any) => void
+      ) => {
+        eventBus.$off(eventName, callback)
+      }
+      this.emit = (eventName: string, ...args: any[]) => {
+        eventBus.$emit(eventName, ...args)
+      }
+    }
+
+    get vm() {
+      return appVm
+    }
+    get $vm() {
+      return appVm
+    }
+    get globalData() {
+      return appVm?.globalData || {}
+    }
+  }
+  // @ts-expect-error
+  $uniApp = new UniApp()
+}
 
 export function getApp() {
-  return appVm
+  if (__X__) {
+    return $uniApp
+  } else {
+    return appVm
+  }
 }
 
 export function initApp(vm: ComponentPublicInstance) {
@@ -25,11 +75,11 @@ export function initApp(vm: ComponentPublicInstance) {
   })
 
   const app = appVm.$.appContext.app
-  if (!app.component(AsyncLoadingComponent.name)) {
-    app.component(AsyncLoadingComponent.name, AsyncLoadingComponent)
+  if (!app.component(AsyncLoadingComponent.name!)) {
+    app.component(AsyncLoadingComponent.name!, AsyncLoadingComponent)
   }
-  if (!app.component(AsyncErrorComponent.name)) {
-    app.component(AsyncErrorComponent.name, AsyncErrorComponent)
+  if (!app.component(AsyncErrorComponent.name!)) {
+    app.component(AsyncErrorComponent.name!, AsyncErrorComponent)
   }
   initAppVm(appVm)
   defineGlobalData(appVm)
