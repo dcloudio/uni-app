33a7c30dea2007ff4c3384f6da494042f0e31ca7
hdx <hdx@dcloud.io>
2024-09-03 18:00:31 +0800

    feat(ad): 支持支付宝小程序

--- packages/uni-cli-shared/components/uniad.vue
+++ packages/uni-cli-shared/components/uniad.vue
@@ -3,7 +3,6 @@
     <uniad-plugin
       class="uniad-plugin"
       :adpid="adpid"
-      :unit-id="unitId"
       @load="_onmpload"
       @close="_onmpclose"
       @error="_onmperror"
@@ -12,7 +11,12 @@
 </template>
 
 <script>
-import adMixin from './ad.mixin.mp.js'
+// #ifdef MP-WEIXIN
+import adMixin from "./ad.mixin.mp-weixin.js"
+// #endif
+// #ifdef MP-ALIPAY
+import adMixin from "./ad.mixin.mp-alipay.js"
+// #endif
 export default {
   name: 'Uniad',
   mixins: [adMixin],
---
+++ packages/webpack-uni-mp-loader/lib/plugin/mp-alipay-uniad-app.json.js
@@ -0,0 +1,45 @@
+const UNI_PLUGINS = [{
+    name: 'uni-ad',
+    version: '*',
+    provider: '2021004169623603'
+  }
+]
+
+const {
+  getManifestJson
+} = require('@dcloudio/uni-cli-shared/lib/manifest.js')
+
+module.exports = function(appJson, useAD) {
+  if (!useAD) {
+    return
+  }
+
+  const manifestJson = getManifestJson()
+  const manifestJsonWxNode = manifestJson['mp-alipay']
+
+  if (!appJson.plugins) {
+    appJson.plugins = {}
+  }
+  for (let i = 0; i < UNI_PLUGINS.length; i++) {
+    const { name, version, provider } = UNI_PLUGINS[i]
+    appJson.plugins[name] = {
+      version,
+      provider
+    }
+  }
+
+  if (!appJson.usingComponents) {
+    appJson.usingComponents = {}
+  }
+  if (!appJson.usingComponents['uniad-plugin']) {
+    appJson.usingComponents['uniad-plugin'] = 'plugin://uni-ad/ad'
+  }
+
+  if (!appJson.window) {
+    appJson.window = {}
+  }
+  // 信息流需要添加此配置
+  if (!appJson.window['enableInPageRender']) {
+    appJson.window['enableInPageRender'] = 'YES'
+  }
+}
