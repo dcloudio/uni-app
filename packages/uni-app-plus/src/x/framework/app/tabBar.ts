5079044da921fc094c90659fb5c4bb80966c32b9
jixinbao <jixinbao@dcloud.io>
2024-09-05 17:50:21 +0800

    fix(x): 创建 tabBar 时候传递当前页面方向，默认为竖屏

--- packages/uni-app-plus/src/x/framework/app/tabBar.ts
+++ packages/uni-app-plus/src/x/framework/app/tabBar.ts
@@ -74,6 +74,10 @@ function init() {
   const list = getTabList()
   const style = new Map<string, any | null>()
   style.set('navigationStyle', 'custom')
+  style.set(
+    'pageOrientation',
+    __uniConfig.globalStyle?.pageOrientation ?? 'portrait'
+  )
   const page = getPageManager().createPage('tabBar', 'tabBar', style)
   const document = page.createDocument(
     new NodeData(
