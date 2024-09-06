f1f49b4fe5a6866c2571a9c50d1f311d7fc9026f
DCloud_UNI_BFC <baofuchun@dcloud.io>
2024-09-06 16:55:07 +0800

    fix(h5): 页面切换pageStyle样式修复

--- packages/uni-h5/src/service/api/ui/setPageMeta.ts
+++ packages/uni-h5/src/service/api/ui/setPageMeta.ts
@@ -2,25 +2,38 @@ import type { ComponentPublicInstance } from 'vue'
 import type { SetPageMetaOptions } from '@dcloudio/uni-api'
 
 export function setCurrentPageMeta(
-  _page: ComponentPublicInstance | null,
+  page: ComponentPublicInstance | null,
   { pageStyle, rootFontSize }: SetPageMetaOptions
 ) {
-  if (__PLATFORM__ === 'h5' && _page) {
+  // h5端 page-meta.vue组件触发时，
+  // 仅仅将pageStyle，rootFontSize设置到$page.meta里面
+  // 页面切换onPageShow逻辑会更新pageStyle，rootFontSize
+  if (__PLATFORM__ === 'h5' && page) {
     if (pageStyle) {
-      _page.$page.meta.pageStyle = pageStyle
+      page.$page.meta.pageStyle = pageStyle
     }
     if (rootFontSize) {
-      _page.$page.meta.rootFontSize = rootFontSize
+      page.$page.meta.rootFontSize = rootFontSize
     }
+    return
   }
+  const pageElm = document.querySelector('uni-page-body') || document.body
+  if (__PLATFORM__ === 'h5' && pageElm === document.body) {
+    console.warn('uni-page-body 获取失败')
+  }
+
   if (pageStyle) {
-    const pageElm = document.querySelector('uni-page-body') || document.body
     pageElm.setAttribute('style', pageStyle)
+  } else {
+    pageElm.removeAttribute('style')
   }
-  if (
-    rootFontSize &&
-    document.documentElement.style.fontSize !== rootFontSize
-  ) {
+
+  if (document.documentElement.style.fontSize === rootFontSize) {
+    return
+  }
+  if (!rootFontSize) {
+    document.documentElement.style.removeProperty('font-size')
+  } else {
     document.documentElement.style.fontSize = rootFontSize
   }
 }
