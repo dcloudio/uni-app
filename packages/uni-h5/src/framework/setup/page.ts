f6c986b3275f8c13bfb31f6db89865b1b8e59b2e
DCloud_UNI_BFC <baofuchun@dcloud.io>
2024-09-06 19:41:11 +0800

    feat: 新增 onPageActivated 阶段

--- packages/uni-h5/src/framework/setup/page.ts
+++ packages/uni-h5/src/framework/setup/page.ts
@@ -480,15 +480,21 @@ function updateCurPageAttrs(pageMeta: UniApp.PageRouteMeta) {
     }
   }
 }
+// 触发页面page-meta.vue的设置
 function updatePageMeta(pageMeta: UniApp.PageRouteMeta) {
-  // 触发页面page-meta.vue的设置
-  nextTick(() => {
-    setCurrentPageMeta(null, {
-      pageStyle: pageMeta.pageStyle,
-      rootFontSize: pageMeta.rootFontSize,
-    })
+  setCurrentPageMeta(null, {
+    pageStyle: pageMeta.pageStyle,
+    rootFontSize: pageMeta.rootFontSize,
   })
 }
+// 页面mounted和activeated时触发
+export function onPageActivated(
+  instance: ComponentInternalInstance,
+  pageMeta: UniApp.PageRouteMeta
+) {
+  updatePageMeta(pageMeta)
+}
+
 export function onPageShow(
   instance: ComponentInternalInstance,
   pageMeta: UniApp.PageRouteMeta
@@ -496,8 +502,10 @@ export function onPageShow(
   updateBodyScopeId(instance)
   updateCurPageCssVar(pageMeta)
   updateCurPageAttrs(pageMeta)
-  updatePageMeta(pageMeta)
   initPageScrollListener(instance, pageMeta)
+  nextTick(() => {
+    onPageActivated(instance, pageMeta)
+  })
 }
 
 export function onPageReady(instance: ComponentInternalInstance) {
