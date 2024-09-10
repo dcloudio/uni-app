7d5056d5388d3a281c788b43e6ef32bb1029bf60
qiang <guoshengqiang@live.com>
2024-09-10 15:58:16 +0800

    fix(harmony): embed 组件传递 visibility 参数

--- packages/uni-app-harmony/src/view/components/embed/index.tsx
+++ packages/uni-app-harmony/src/view/components/embed/index.tsx
@@ -1,4 +1,11 @@
-import { type PropType, computed, watch } from 'vue'
+import {
+  type PropType,
+  computed,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+  watch,
+} from 'vue'
 import { defineBuiltInComponent } from '@dcloudio/uni-components'
 import { invokeHarmonyChannel } from '../../plus'
 
@@ -27,10 +34,24 @@ export default /*#__PURE__*/ defineBuiltInComponent({
   props,
   setup(props, { expose, attrs }) {
     const elId = String(index++)
+    // 通过 IntersectionObserver 来触发补充 onNativeEmbedLifecycleChange
+    const elRef = ref<HTMLElement | null>(null)
+    // Visible:0 Hidden:1 None:2
+    const visibility = ref(0)
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      visibility.value = entries[0].intersectionRatio > 0 ? 0 : 2
+    })
+    onMounted(() => {
+      intersectionObserver.observe(elRef.value!)
+    })
+    onBeforeUnmount(() => {
+      intersectionObserver.disconnect()
+    })
     const src = computed(() => {
       const on: string[] = []
       const options = Object.assign({}, props.options, {
         on: on,
+        visibility: visibility.value,
       })
       Object.keys(attrs).forEach((key) => {
         if (/^on[A-Z]/.test(key)) {
@@ -54,6 +75,7 @@ export default /*#__PURE__*/ defineBuiltInComponent({
     expose(exposed)
     return () => (
       <embed
+        ref={elRef}
         el-id={elId}
         type={`native/${props.tag}`}
         src={srcValue}
