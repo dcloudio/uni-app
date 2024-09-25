4af24861179475ab3f7e5300fea446e794707302
王亚琪 <wangyaqi988@163.com>
2024-09-25 17:13:25 +0800

    chore: build uni-app-harmony

--- packages/uni-app-harmony/dist/uni.compiler.js
+++ packages/uni-app-harmony/dist/uni.compiler.js
@@ -140,11 +140,19 @@ function getRelatedProviders(inputDir) {
             const relatedProviders = sdkConfigs[service];
             for (const name in relatedProviders) {
                 if (Object.prototype.hasOwnProperty.call(relatedProviders, name)) {
-                    const providerName = ProviderNameMap[name];
-                    providers.push({
-                        service,
-                        name: providerName || name,
-                    });
+                    const providerConf = relatedProviders[name];
+                    if (!providerConf) {
+                        continue;
+                    }
+                    if (!providerConf.__platform__ ||
+                        (Array.isArray(providerConf.__platform__) &&
+                            providerConf.__platform__.includes('harmonyos'))) {
+                        const providerName = ProviderNameMap[name];
+                        providers.push({
+                            service,
+                            name: providerName || name,
+                        });
+                    }
                 }
             }
         }
