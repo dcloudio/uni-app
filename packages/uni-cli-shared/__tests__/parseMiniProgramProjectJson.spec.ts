beb94a95d0967a2f9b7b145aa3468cd4f702af2a
jixinbao <jixinbao@dcloud.io>
2024-10-08 15:18:41 +0800

    test(cli-shared): 测试 mp 不同平台的 manifest.json 配置生成逻辑

--- packages/uni-cli-shared/__tests__/parseMiniProgramProjectJson.spec.ts
+++ packages/uni-cli-shared/__tests__/parseMiniProgramProjectJson.spec.ts
@@ -5,15 +5,51 @@
 
 import { parseMiniProgramProjectJson } from '../src/json/mp/project'
 import { recursive } from 'merge'
+
 // xhs 的默认配置
 import mpXhsConfig from '../../uni-mp-xhs/src/compiler/project.config.json'
-const userManifestJSON = {
+import mpWeixinConfig from '../../uni-mp-weixin/src/compiler/project.config.json'
+import mpAlipayConfig from '../../uni-mp-alipay/src/compiler/mini.project.json'
+import mpBaiduConfig from '../../uni-mp-baidu/src/compiler/project.swan.json'
+
+const userManifestJSON: any = {
   name: 'uni-app',
   appid: '',
   description: '自定义描述',
   versionName: '1.0.0',
   versionCode: '100',
   transformPx: false,
+  'uni-app-x': {},
+  /* 快应用特有相关 */
+  quickapp: {},
+  /* 小程序特有相关 */
+  'mp-weixin': {
+    appid: '',
+    setting: {
+      urlCheck: false,
+    },
+    usingComponents: true,
+  },
+  'mp-alipay': {
+    usingComponents: true,
+    enableAppxNg: false,
+    component2: false,
+    enableNodeModuleBabelTransform: false,
+  },
+  'mp-baidu': {
+    usingComponents: true,
+    appid: 'test-baidu',
+    setting: {
+      autoAudits: true,
+      urlCheck: true,
+    },
+  },
+  'mp-toutiao': {
+    usingComponents: true,
+  },
+  uniStatistics: {
+    enable: false,
+  },
   'mp-xhs': {
     appid: '自定义 appid',
     libVersion: '3.105.1',
@@ -44,8 +80,6 @@ const userManifestJSON = {
   },
   vueVersion: '3',
 }
-const platfrom = 'mp-xhs'
-
 const pagesJson = {
   pages: [
     {
@@ -63,31 +97,122 @@ const pagesJson = {
 } as any
 
 describe('parseMiniProgramProjectJson', () => {
-  it('测试自定义 manifest.json', () => {
-    const options = {
-      template: mpXhsConfig,
-      pagesJson,
-    }
+  it('测试自定义 manifest.json - xhs', () => {
+    const platfrom = 'mp-xhs'
 
     const projectJson = parseMiniProgramProjectJson(
       JSON.stringify(userManifestJSON),
       platfrom,
-      options
+      {
+        template: mpXhsConfig,
+        pagesJson,
+      }
     )
 
     expect(projectJson).toEqual({
       description: mpXhsConfig.description,
-      appid: userManifestJSON['mp-xhs'].appid,
+      appid: userManifestJSON[platfrom].appid,
       packOptions: mpXhsConfig.packOptions,
       setting: recursive(
         true,
         mpXhsConfig.setting,
-        userManifestJSON['mp-xhs'].setting
+        userManifestJSON[platfrom].setting
       ),
       compileType: mpXhsConfig.compileType,
-      libVersion: userManifestJSON['mp-xhs'].libVersion,
+      libVersion: userManifestJSON[platfrom].libVersion || '',
       projectname: userManifestJSON.name,
       condition: mpXhsConfig.condition,
+    })
+  })
+
+  it('测试自定义 manifest.json - wx', () => {
+    const platfrom = 'mp-weixin'
+
+    const projectJson = parseMiniProgramProjectJson(
+      JSON.stringify(userManifestJSON),
+      platfrom,
+      {
+        template: mpWeixinConfig,
+        pagesJson,
+      }
+    )
+
+    expect(projectJson).toEqual({
+      description: mpWeixinConfig.description,
+      appid: userManifestJSON[platfrom].appid || 'touristappid',
+      packOptions: mpXhsConfig.packOptions,
+      setting: recursive(
+        true,
+        mpXhsConfig.setting,
+        userManifestJSON[platfrom].setting
+      ),
+      compileType: mpXhsConfig.compileType,
+      projectname: userManifestJSON.name,
+      libVersion: (userManifestJSON[platfrom] as any)?.libVersion || '',
+      condition: mpXhsConfig.condition,
+    })
+  })
+
+  it('测试自定义 manifest.json - alipay', () => {
+    // mpAlipayConfig
+    const platfrom = 'mp-alipay'
+
+    const userPlatformConfig: any = userManifestJSON[platfrom]
+
+    const projectJson = parseMiniProgramProjectJson(
+      JSON.stringify(userManifestJSON),
+      platfrom,
+      {
+        template: mpAlipayConfig,
+        pagesJson,
+      }
+    )
+
+    expect(projectJson).toEqual({
+      appid: userPlatformConfig.appid ?? mpAlipayConfig.appid,
+      component2: userPlatformConfig.component2 ?? mpAlipayConfig.component2,
+      enableAppxNg:
+        userPlatformConfig.enableAppxNg ?? mpAlipayConfig.enableAppxNg,
+      enableNodeModuleBabelTransform:
+        userPlatformConfig.enableNodeModuleBabelTransform ??
+        mpAlipayConfig.enableNodeModuleBabelTransform,
+      projectname: userManifestJSON.name,
+    })
+  })
+
+  it('测试自定义 manifest.json - baidu', () => {
+    // mpBaiduConfig
+    const platfrom = 'mp-baidu'
+
+    const userPlatformConfig: any = userManifestJSON[platfrom]
+
+    const projectJson = parseMiniProgramProjectJson(
+      JSON.stringify(userManifestJSON),
+      platfrom,
+      {
+        template: mpBaiduConfig,
+        pagesJson,
+      }
+    )
+
+    expect(projectJson).toEqual({
+      appid: userPlatformConfig.appid,
+      projectname: userManifestJSON.name,
+      'compilation-args': {
+        common: {
+          babelSetting: {
+            ignore: [],
+          },
+          enhance: true,
+          ignorePrefixCss: false,
+        },
+        selected: -3,
+      },
+      host: 'baiduboxapp',
+      setting: {
+        autoAudits: true,
+        urlCheck: true,
+      },
     })
   })
 })
