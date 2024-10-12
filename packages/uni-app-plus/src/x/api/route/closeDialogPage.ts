df0f3bad66d77d1c2ef4014ef68f61bd992f6d7d
zhenyuWang <13641039885@163.com>
2024-10-12 10:25:21 +0800

    refactor(x-ios): 调整 dialogPage options 参数赋值

--- packages/uni-app-plus/src/x/api/route/closeDialogPage.ts
+++ packages/uni-app-plus/src/x/api/route/closeDialogPage.ts
@@ -1,56 +1,7 @@
 import { ON_SHOW } from '@dcloudio/uni-shared'
 import { invokeHook } from '@dcloudio/uni-core'
 import { closeNativeDialogPage } from './utils'
-
-/**
- *
- * 文档: []()
- */
-type CloseDialogPageSuccess = AsyncApiResult
-type CloseDialogPageFail = AsyncApiResult
-type CloseDialogPageComplete = AsyncApiResult
-interface CloseDialogPageOptions {
-  /**
-   * 窗口显示的动画类型
-   * - auto: 自动选择动画效果
-   * - none: 无动画效果
-   * - slide-out-right: 横向向右侧滑出屏幕动画
-   * - slide-out-left: 横向向左侧滑出屏幕动画
-   * - slide-out-top: 竖向向上侧滑出屏幕动画
-   * - slide-out-bottom: 竖向向下侧滑出屏幕动画
-   * - fade-out: 从不透明到透明逐渐隐藏动画
-   * - zoom-in: 从大逐渐缩小关闭动画
-   * - zoom-fade-in: 从大逐渐缩小并且从不透明到透明逐渐隐藏关闭动画
-   */
-  animationType?:
-    | 'auto'
-    | 'none'
-    | 'slide-out-right'
-    | 'slide-out-left'
-    | 'slide-out-top'
-    | 'slide-out-bottom'
-    | 'fade-out'
-    | 'zoom-in'
-    | 'zoom-fade-in'
-  /**
-   * 页面间通信接口，用于监听被打开页面发送到当前页面的数据
-   */
-  events?: any
-
-  dialogPage?: UniDialogPage
-  /**
-   * 接口调用成功的回调函数
-   */
-  success?: (result: CloseDialogPageSuccess) => void
-  /**
-   * 接口调用失败的回调函数
-   */
-  fail?: (result: CloseDialogPageFail) => void
-  /**
-   * 接口调用结束的回调函数（调用成功、失败都会执行）
-   */
-  complete?: (result: CloseDialogPageComplete) => void
-}
+import type { CloseDialogPageOptions } from '@dcloudio/uni-app-x/types/uni'
 
 export const closeDialogPage = (options?: CloseDialogPageOptions) => {
   const currentPages = getCurrentPages() as UniPage[]
@@ -113,6 +64,7 @@ function triggerFailCallback(
     4,
     `openDialogPage: fail, ${errMsg}`
   )
+  // @ts-expect-error
   options?.fail?.(failOptions)
   options?.complete?.(failOptions)
 }
