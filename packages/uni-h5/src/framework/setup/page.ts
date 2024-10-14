c8c1c72ba869b1f0856da0c231eb6a66da7f2b25
zhenyuWang <13641039885@163.com>
2024-10-14 16:50:59 +0800

    fix(web): 修复编译报错问题

--- packages/uni-h5/src/framework/setup/page.ts
+++ packages/uni-h5/src/framework/setup/page.ts
@@ -40,8 +40,9 @@ import type {
   UniPage,
 } from '@dcloudio/uni-app-x/types/page'
 import { setCurrentPageMeta } from '../../service/api/ui/setPageMeta'
+//#if _X_ && !_NODE_JS_
 import { closeDialogPage } from '../../service/api'
-
+//#endif
 const SEP = '$$'
 
 const currentPagesMap = new Map<string, ComponentPublicInstance>()
@@ -177,13 +178,15 @@ export const redirectToPagesBeforeEntryPages: RedirectToPage[] = []
 export const reLaunchPagesBeforeEntryPages: ReLaunchPage[] = []
 let escBackPageNum = 0
 function handleEscKeyPress(event) {
-  if (event.key === 'Escape') {
-    const currentPage = getCurrentPage()
-    // @ts-expect-error
-    const dialogPages = currentPage.getDialogPages()
-    const dialogPage = dialogPages[dialogPages.length - 1]
-    if (!dialogPage.$disableEscBack) {
-      closeDialogPage({ dialogPage })
+  if (__X__ && !__NODE_JS__) {
+    if (event.key === 'Escape') {
+      const currentPage = getCurrentPage()
+      // @ts-expect-error
+      const dialogPages = currentPage.getDialogPages()
+      const dialogPage = dialogPages[dialogPages.length - 1]
+      if (!dialogPage.$disableEscBack) {
+        closeDialogPage({ dialogPage })
+      }
     }
   }
 }
@@ -247,7 +250,7 @@ export function removePage(routeKey: string, removeRouteCaches = true) {
   const pageVm = currentPagesMap.get(routeKey) as ComponentPublicInstance
   if (__X__) {
     const dialogPages = (pageVm.$page as UniPage).getDialogPages()
-    for (let i = dialogPages.length - 1;i >= 0;i--) {
+    for (let i = dialogPages.length - 1; i >= 0; i--) {
       closeDialogPage({ dialogPage: dialogPages[i] })
     }
   }
