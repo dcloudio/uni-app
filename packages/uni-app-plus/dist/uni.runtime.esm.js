0bf12a5f5265e2ed7daf9e4f4e4a7899a2ddea0e
DCloud_LXH <283700113@qq.com>
2024-10-18 18:11:14 +0800

    chore(harmony): build

--- packages/uni-app-plus/dist/uni.runtime.esm.js
+++ packages/uni-app-plus/dist/uni.runtime.esm.js
@@ -16243,6 +16243,33 @@ const createLivePusherContext = defineSyncApi(API_CREATE_LIVE_PUSHER_CONTEXT, (i
     return new LivePusherContextVue(id, vm.$page.id);
 }, CreateLivePusherContextProtocol);
 
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, [])).next());
+    });
+}
+
 const PI = 3.1415926535897932384626;
 const a = 6378245.0;
 const ee = 0.00669342162296594323;
@@ -16776,8 +16803,73 @@ function createFactory(component) {
     };
 }
 
-// @ts-nocheck
-// TODO 优化此处代码，此页面无对应的css
+function tencentMapPlaceSearch(options) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // @ts-expect-error access uni
+        const cloud = uni.__uniCloud;
+        if (!cloud) {
+            throw new Error('使用uni.chooseLocation且使用腾讯地图时，需搭配uniCloud使用，详情请参考：https://uniapp.dcloud.net.cn/api/location/location.html#chooselocation');
+        }
+        const { keyword, latitude, longitude, pageIndex, pageSize, secure } = options;
+        const obj = cloud.importObject('uni-map-co', {
+            customUI: true,
+            secretMethods: secure
+                ? {
+                    chooseLocation: 'both',
+                }
+                : {},
+        });
+        let pois = [];
+        if (keyword) {
+            const res = yield obj.chooseLocation({
+                action: 'search',
+                data: {
+                    keyword: '酒店',
+                    location: {
+                        lat: latitude,
+                        lng: longitude,
+                    },
+                    radius: 1000,
+                    auto_extend: 1,
+                    get_subpois: 0,
+                    orderby: 'weight',
+                    page_index: pageIndex,
+                    page_size: pageSize,
+                },
+                provider: 'qqmap',
+                needOriginalResult: true,
+            });
+            pois = res.result.data || [];
+        }
+        else {
+            const res = yield obj.chooseLocation({
+                action: 'location2address',
+                data: {
+                    location: `${latitude},${longitude}`,
+                    get_poi: 1,
+                    poi_options: {
+                        page_index: pageIndex,
+                        page_size: pageSize,
+                    },
+                },
+                provider: 'qqmap',
+                needOriginalResult: true,
+            });
+            pois = res.result.result.pois || [];
+        }
+        return pois;
+    });
+}
+function subscribeMapPlaceSearch() {
+    registerServiceMethod('mapPlaceSearch', (args, resolve) => {
+        tencentMapPlaceSearch(args)
+            .then(resolve)
+            .catch((err) => {
+            console.error(err);
+            resolve({ errMsg: 'mapPlaceSearch:fail' });
+        });
+    });
+}
 const LocationPickerPage = {
     data() {
         return {
@@ -21035,6 +21127,7 @@ function initSubscribeHandlers() {
             publishHandler(ON_WEBVIEW_READY, {}, 1);
         }
         subscribeGetLocation();
+        subscribeMapPlaceSearch();
     }
 }
 
