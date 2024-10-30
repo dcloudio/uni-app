7ad5a988f1d238a58237c307c75e1c802406a258
mehaotian <490272692@qq.com>
2024-10-30 19:37:50 +0800

    fix(stat): 统计新增私有云，修复获取系统信息 system 时可能是undefined的情况。

--- packages/uni-stat/dist/uni-stat.cjs.js
+++ packages/uni-stat/dist/uni-stat.cjs.js
@@ -1,9 +1,18 @@
 'use strict';
 
 /**
- * 获取系统信息
+ * 老版本兼容，系统信息
  */
 const sys = uni.getSystemInfoSync();
+// /**
+//  * app基础信息
+//  */
+// export const baseInfo = uni.getAppBaseInfo()
+
+// /**
+//  * 设备相关
+//  */
+// export const deviceInfo = uni.getDeviceInfo()
 
 // 访问开始即启动小程序，访问结束结分为：进入后台超过5min、在前台无任何操作超过30min、在新的来源打开小程序；
 const STAT_VERSION = process.env.UNI_COMPILER_VERSION;
@@ -276,7 +285,7 @@ const get_splicing = (data) => {
 /**
  * 获取页面url，不包含参数
  */
-const get_route$1 = (pageVm) => {
+const get_route = (pageVm) => {
   let _self = pageVm || get_page_vm();
   if (get_platform_name() === 'bd') {
     let mp_route = _self.$mp && _self.$mp.page && _self.$mp.page.is;
@@ -729,6 +738,7 @@ const get_residence_time = (type) => {
 };
 
 const eport_Interval = get_report_Interval(OPERATING_TIME);
+
 // 统计数据默认值
 let statData = {
   uuid: get_uuid(), // 设备标识
@@ -746,7 +756,7 @@ let statData = {
   tt: '',
   brand: sys.brand || '', // 手机品牌
   md: sys.model, // 手机型号
-  sv: sys.system.replace(/(Android|iOS)\s/, ''), // 手机系统版本
+  sv: '', // 手机系统版本
   mpsdk: sys.SDKVersion || '', // x程序 sdk version
   mpv: sys.version || '', // 小程序平台版本 ，如微信、支付宝
   lang: sys.language, // 语言
@@ -756,6 +766,11 @@ let statData = {
   sw: sys.screenWidth, // screenWidth 屏幕宽度
   sh: sys.screenHeight, // screenHeight 屏幕高度
 };
+
+// 获取手机版本
+if (sys.system){
+  statData.sv = sys.system.replace(/(Android|iOS)\s/, '');
+}
 class Report {
   constructor() {
     // 页面实例
@@ -877,6 +892,19 @@ class Report {
           cst: 2,
         };
         this.sendReportRequest(options);
+      } else {
+        // 在没有超过时限的时候 ，判断场景值 ，如果是场景值发生了变化，则需要上报应用启动数据
+        // 目前只有微信小程序生效
+        const scene = get_scene();
+        if (scene !== this.statData.sc) {
+          let lastPageRoute = uni.getStorageSync('_STAT_LAST_PAGE_ROUTE');
+          let options = {
+            path: lastPageRoute,
+            scene: scene,
+            cst: 2,
+          };
+          this.sendReportRequest(options);
+        }
       }
       // 状态重置
       this.__licationHide = false;
@@ -922,7 +950,7 @@ class Report {
     };
 
     const route = get_page_route(self);
-    const routepath = get_route$1(self);
+    const routepath = get_route(self);
 
     this._navigationBarTitle.config = get_page_name(routepath);
     // 表示应用触发 ，页面切换不触发之后的逻辑
@@ -983,7 +1011,7 @@ class Report {
     let query = is_opt ? '?' + JSON.stringify(options.query) : '';
     const last_time = get_last_visit_time();
     // 非老用户
-    if(last_time !== 0 || !last_time){
+    if (last_time !== 0 || !last_time) {
       const odid = get_odid();
       // 1.0 处理规则
       {
@@ -1062,7 +1090,7 @@ class Report {
     let routepath = '';
 
     try {
-      routepath = get_route$1();
+      routepath = get_route();
     } catch (error) {
       const launch_options = dbGet('__launch_options');
       routepath = launch_options.path;
