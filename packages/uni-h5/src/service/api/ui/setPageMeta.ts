8e786eafd9d3b6bd71fa4b9816a6ed1681f542c9
王亚琪 <wangyaqi988@163.com>
2024-10-31 17:06:07 +0800

    Revert "fix: 根据key判断是否更新和存储page-meta信息"
    This reverts commit 89e35e12343b9e8bf9bc7b6d068b20bb976caae9.

--- packages/uni-h5/src/service/api/ui/setPageMeta.ts
+++ packages/uni-h5/src/service/api/ui/setPageMeta.ts
@@ -3,28 +3,19 @@ import type { SetPageMetaOptions } from '@dcloudio/uni-api'
 
 export function setCurrentPageMeta(
   page: ComponentPublicInstance | null,
-  options: SetPageMetaOptions
+  { pageStyle, rootFontSize }: SetPageMetaOptions
 ) {
-  const { pageStyle, rootFontSize } = options
-  // h5端 page-meta.vue组件触发时setPageMeta时, 需要将pageStyle和rootFontSize存储到page.$page.meta
+  // h5端 page-meta.vue组件触发时setPageMeta时，仅仅将pageStyle，rootFontSize设置到$page.meta里面
+  // 页面切换onPageShow逻辑会更新pageStyle，rootFontSize
   if (__PLATFORM__ === 'h5' && page) {
-    if (hasKey(options, 'pageStyle')) {
+    if (pageStyle) {
       page.$page.meta.pageStyle = pageStyle
     }
-    if (hasKey(options, 'rootFontSize')) {
+    if (rootFontSize) {
       page.$page.meta.rootFontSize = rootFontSize
     }
+    return
   }
-
-  if (hasKey(options, 'pageStyle')) {
-    setPageStyle(pageStyle)
-  }
-  if (hasKey(options, 'rootFontSize')) {
-    setRootFontSize(rootFontSize)
-  }
-}
-
-const setPageStyle = (pageStyle: string | undefined | null) => {
   const pageElm = document.querySelector('uni-page-body') || document.body
   if (__PLATFORM__ === 'h5' && pageElm === document.body) {
     console.warn('uni-page-body 获取失败')
@@ -35,9 +26,7 @@ const setPageStyle = (pageStyle: string | undefined | null) => {
   } else {
     pageElm.removeAttribute('style')
   }
-}
 
-const setRootFontSize = (rootFontSize: string | undefined | null) => {
   if (document.documentElement.style.fontSize === rootFontSize) {
     return
   }
@@ -48,11 +37,4 @@ const setRootFontSize = (rootFontSize: string | undefined | null) => {
     document.documentElement.style.removeProperty('font-size')
     document.documentElement.removeAttribute('root-font-size')
   }
-}
-
-function hasKey<T extends object, K extends keyof T>(
-  obj: T,
-  key: K
-): obj is T & Record<K, unknown> {
-  return obj.hasOwnProperty(key)
 }
