4e5b9a10ecae019eb07f24d42a4a8d7a850fff8d
jixinbao <jixinbao@dcloud.io>
2024-11-05 18:06:41 +0800

    feat(mp-harmony): 适配 harmony requestPayment

--- packages/uni-mp-harmony/src/api/protocols.ts
+++ packages/uni-mp-harmony/src/api/protocols.ts
@@ -6,3 +6,10 @@ export {
 } from '@dcloudio/uni-mp-core'
 import { navigateTo as _navigateTo } from '@dcloudio/uni-mp-core'
 export const navigateTo = _navigateTo()
+
+export const requestPayment = {
+  name: 'requestPayment',
+  args: {
+    orderInfo: 'orderStr',
+  },
+}
