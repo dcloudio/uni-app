a3158c219d9faaa0920f44ec1af2f7e78c9bbef5
王亚琪 <wangyaqi988@163.com>
2024-11-09 17:08:44 +0800

    fix(web): 修复因getApp调整引发的调用报错

--- packages/uni-h5/src/helpers/useDocumentTitle.ts
+++ packages/uni-h5/src/helpers/useDocumentTitle.ts
@@ -1,17 +1,13 @@
-import {
-  type ComponentPublicInstance,
-  onActivated,
-  ssrContextKey,
-  watchEffect,
-} from 'vue'
+import { onActivated, ssrContextKey, watchEffect } from 'vue'
 import { ON_NAVIGATION_BAR_CHANGE, UNI_SSR_TITLE } from '@dcloudio/uni-shared'
 import { getApp } from '../framework/setup/app'
 
 export function updateDocumentTitle(title: string) {
   if (__NODE_JS__) {
     // setNavigationBarTitle 可能是异步调用，此时使用 useSSRContext 获取，可能没有 instance
-    const ssrContext = (getApp() as ComponentPublicInstance).$.appContext
-      .provides[ssrContextKey]
+    const ssrContext = (__X__ ? getApp().$vm : getApp()).$.appContext.provides[
+      ssrContextKey
+    ]
     if (ssrContext) {
       ssrContext[UNI_SSR_TITLE] = title
     }
