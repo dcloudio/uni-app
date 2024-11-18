42e81cc88bbe286be9e863a962c8e192588685a0
fxy060608 <fxy060608@gmail.com>
2024-11-18 16:52:27 +0800

    fix(x): 修复 utssdk 加密 + easycom 组件

--- packages/uni-cli-shared/src/easycom.ts
+++ packages/uni-cli-shared/src/easycom.ts
@@ -240,12 +240,14 @@ function initAutoScanEasycom(
   }
   const is_uni_modules =
     path.basename(path.resolve(dir, '../..')) === 'uni_modules'
-  const is_encrypt_uni_modules = // uni_modules模式不需要此逻辑
+  const is_easycom_encrypt_uni_modules = // uni_modules模式不需要此逻辑
     process.env.UNI_COMPILE_TARGET !== 'uni_modules' &&
     is_uni_modules &&
-    fs.existsSync(path.resolve(dir, '../encrypt'))
+    // 前端加密插件，不能包含utssdk目录
+    fs.existsSync(path.resolve(dir, '../encrypt')) &&
+    !fs.existsSync(path.resolve(dir, '../utssdk'))
   const uni_modules_plugin_id =
-    is_encrypt_uni_modules && path.basename(path.resolve(dir, '..'))
+    is_easycom_encrypt_uni_modules && path.basename(path.resolve(dir, '..'))
   fs.readdirSync(dir).forEach((name) => {
     const folder = path.resolve(dir, name)
     if (!isDir(folder)) {
@@ -257,7 +259,7 @@ function initAutoScanEasycom(
     for (let i = 0; i < extensions.length; i++) {
       const ext = extensions[i]
       if (files.includes(name + ext)) {
-        easycoms[`^${name}$`] = is_encrypt_uni_modules
+        easycoms[`^${name}$`] = is_easycom_encrypt_uni_modules
           ? normalizePath(
               path.join(
                 rootDir,
