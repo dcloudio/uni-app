04bae2d0e421acc7d8dc2927b394c9106e648d3a
zhenyuWang <13641039885@163.com>
2024-11-18 21:17:23 +0800

    refactor(x-ios): 代码优化

--- packages/uni-app-plus/src/service/framework/page/setup.ts
+++ packages/uni-app-plus/src/service/framework/page/setup.ts
@@ -1,9 +1,8 @@
-import { getCurrentPage, initPageVm, invokeHook } from '@dcloudio/uni-core'
+import { initPageVm, invokeHook } from '@dcloudio/uni-core'
 import {
   EventChannel,
   ON_READY,
   ON_UNLOAD,
-  SYSTEM_DIALOG_PAGE_PATH_STARTER,
   formatLog,
 } from '@dcloudio/uni-shared'
 import {
@@ -14,8 +13,10 @@ import {
   onMounted,
 } from 'vue'
 import type { VuePageComponent } from './define'
-import { addCurrentPage, getPage$BasePage } from './getCurrentPages'
-import { OPEN_DIALOG_PAGE } from '../../../x/constants'
+import { addCurrentPage } from './getCurrentPages'
+//#if _X_
+import { setupXPage } from '../../../x/framework/page/setup'
+//#endif
 
 export function setupPage(component: VuePageComponent) {
   const oldSetup = component.setup
@@ -31,106 +32,19 @@ export function setupPage(component: VuePageComponent) {
     const pageVm = instance.proxy!
     initPageVm(pageVm, __pageInstance as Page.PageInstance['$page'])
     if (__X__) {
-      instance.$dialogPages = []
-      let uniPage: UniPage
-      if (
-        (__pageInstance as Page.PageInstance['$page']).openType ===
-        OPEN_DIALOG_PAGE
-      ) {
-        const currentPage = getCurrentPage() as unknown as UniPage
-        if (
-          (__pagePath as string).startsWith(SYSTEM_DIALOG_PAGE_PATH_STARTER)
-        ) {
-          const systemDialogPages = currentPage.vm.$systemDialogPages
-          uniPage = systemDialogPages[systemDialogPages.length - 1]
-        } else {
-          uniPage = new UniDialogPageImpl()
-        }
-        uniPage.getElementById = (
-          id: string.IDString | string
-        ): UniElement | null => {
-          const currentPage = getCurrentPage() as unknown as UniPage
-          if (currentPage !== uniPage.getParentPage()) {
-            return null
-          }
-          const containerNode = pageVm.$el?._parent
-          if (containerNode == null) {
-            console.warn('bodyNode is null')
-            return null
-          }
-          return containerNode.querySelector(`#${id}`)
-        }
-      } else {
-        uniPage = new UniNormalPageImpl()
-        uniPage.getElementById = (
-          id: string.IDString | string
-        ): UniElement | null => {
-          const currentPage = getCurrentPage() as unknown as UniPage
-          if (currentPage !== uniPage) {
-            return null
-          }
-          const bodyNode = pageVm.$el?.parentNode
-          if (bodyNode == null) {
-            console.warn('bodyNode is null')
-            return null
-          }
-          return bodyNode.querySelector(`#${id}`)
-        }
-      }
-      pageVm.$basePage = pageVm.$page as Page.PageInstance['$page']
-      pageVm.$page = uniPage
-      uniPage.route = pageVm.$basePage.route
-      // @ts-expect-error
-      uniPage.optionsByJS = pageVm.$basePage.options
-      Object.defineProperty(uniPage, 'options', {
-        get: function () {
-          return new UTSJSONObject(pageVm.$basePage.options)
-        },
-      })
-      uniPage.vm = pageVm
-      uniPage.$vm = pageVm
-      uniPage.getParentPage = () => {
-        // @ts-expect-error
-        const parentPage = uniPage.getParentPageByJS()
-        return parentPage || null
-      }
-
-      uniPage.getPageStyle = (): UTSJSONObject => {
-        // @ts-expect-error
-        const pageStyle = uniPage.getPageStyleByJS()
-        return new UTSJSONObject(pageStyle)
-      }
-      uniPage.$getPageStyle = (): UTSJSONObject => {
-        return uniPage.getPageStyle()
-      }
-
-      uniPage.setPageStyle = (styles: UTSJSONObject) => {
-        // @ts-expect-error
-        uniPage.setPageStyleByJS(styles)
-      }
-      uniPage.$setPageStyle = (styles: UTSJSONObject) => {
-        uniPage.setPageStyle(styles)
-      }
-
-      uniPage.getAndroidView = () => null
-      uniPage.getIOSView = () => null
-      uniPage.getHTMLElement = () => null
-
-      if (getPage$BasePage(pageVm).openType !== OPEN_DIALOG_PAGE) {
-        addCurrentPageWithInitScope(
-          __pageId as number,
-          pageVm,
-          __pageInstance as Page.PageInstance['$page']
-        )
-      }
+      setupXPage(
+        instance,
+        __pageInstance as Page.PageInstance['$page'],
+        pageVm,
+        __pageId as number,
+        __pagePath as string
+      )
     } else {
       addCurrentPageWithInitScope(
         __pageId as number,
         pageVm,
         __pageInstance as Page.PageInstance['$page']
       )
-    }
-    if (!__X__) {
       onMounted(() => {
         nextTick(() => {
           // onShow被延迟，故onReady也同时延迟
@@ -140,19 +54,6 @@ export function setupPage(component: VuePageComponent) {
       })
       onBeforeUnmount(() => {
         invokeHook(pageVm, ON_UNLOAD)
-      })
-    } else {
-      onMounted(() => {
-        const rootElement = pageVm.$el?._parent
-        if (rootElement) {
-          rootElement._page = pageVm.$page
-        }
-      })
-      onBeforeUnmount(() => {
-        const rootElement = pageVm.$el?._parent
-        if (rootElement) {
-          rootElement._page = null
-        }
       })
     }
     if (oldSetup) {
@@ -201,7 +102,7 @@ export function initScope(
   return vm
 }
 
-function addCurrentPageWithInitScope(
+export function addCurrentPageWithInitScope(
   pageId: number,
   pageVm: ComponentPublicInstance,
   pageInstance: Page.PageInstance['$page']
