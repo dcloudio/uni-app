bb680fc7d0ee89a98903e2086c6727d1a0d30ef9
王亚琪 <wangyaqi988@163.com>
2024-12-04 20:21:56 +0800

    fix(mp): 修复language属性为undefined时getAppBaseInfo接口报错的Bug

--- packages/uni-mp-core/src/api/protocols/enhanceSystemInfo.ts
+++ packages/uni-mp-core/src/api/protocols/enhanceSystemInfo.ts
@@ -114,7 +114,7 @@ export function populateParameters(
   }
 
   // hostLanguage
-  const hostLanguage = language.replace(/_/g, '-')
+  const hostLanguage = (language || '').replace(/_/g, '-')
 
   // wx.getAccountInfoSync
 
