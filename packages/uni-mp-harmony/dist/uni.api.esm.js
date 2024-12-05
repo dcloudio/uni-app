62161a4284f43186fcd9fe890182ef46196c0ac3
fxy060608 <153647646@qq.com>
2024-12-05 13:35:06 +0800

    chore: 修改依赖版本

--- packages/uni-mp-harmony/dist/uni.api.esm.js
+++ packages/uni-mp-harmony/dist/uni.api.esm.js
@@ -450,7 +450,7 @@ let isIOS = false;
 let deviceWidth = 0;
 let deviceDPR = 0;
 function checkDeviceWidth() {
-    const { platform, pixelRatio, windowWidth } = getBaseSystemInfo();
+    const { windowWidth, pixelRatio, platform } = getBaseSystemInfo();
     deviceWidth = windowWidth;
     deviceDPR = pixelRatio;
     isIOS = platform === 'ios';
@@ -988,7 +988,7 @@ function populateParameters(fromRes, toRes) {
     // SDKVersion
     let _SDKVersion = SDKVersion;
     // hostLanguage
-    const hostLanguage = language.replace(/_/g, '-');
+    const hostLanguage = (language || '').replace(/_/g, '-');
     // wx.getAccountInfoSync
     const parameters = {
         appId: process.env.UNI_APP_ID,
