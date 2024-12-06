73012d1ea4dd35b2a9f429bbeb71ff51e0c3e9c9
zhenyuWang <13641039885@163.com>
2024-12-06 16:37:55 +0800

    refactor(x-ios): closeDialogPage 调整默认关闭动画类型为 auto

--- packages/uni-app-plus/src/x/api/route/closeDialogPage.ts
+++ packages/uni-app-plus/src/x/api/route/closeDialogPage.ts
@@ -30,7 +30,7 @@ export const closeDialogPage = (options?: CloseDialogPageOptions) => {
         parentDialogPages.splice(index, 1)
         closeNativeDialogPage(
           dialogPage,
-          options?.animationType || 'none',
+          options?.animationType || 'auto',
           options?.animationDuration || ANI_DURATION
         )
         if (index > 0 && index === parentDialogPages.length) {
@@ -55,7 +55,7 @@ export const closeDialogPage = (options?: CloseDialogPageOptions) => {
     for (let i = dialogPages.length - 1; i >= 0; i--) {
       closeNativeDialogPage(
         dialogPages[i],
-        options?.animationType || 'none',
+        options?.animationType || 'auto',
         options?.animationDuration || ANI_DURATION
       )
       if (i > 0) {
