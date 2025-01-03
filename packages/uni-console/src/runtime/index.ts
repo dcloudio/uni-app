187adb5a974942b9204a884cb78e38dbbd7a5db0
fxy060608 <fxy060608@gmail.com>
2025-01-03 17:16:37 +0800

    wip(console): 调整日志换行打印

--- packages/uni-console/src/runtime/index.ts
+++ packages/uni-console/src/runtime/index.ts
@@ -23,9 +23,13 @@ export function initRuntimeSocketService(): Promise<boolean> {
         restoreError()
         restoreConsole()
         originalConsole.error(
-          wrapError(
-            '开发模式下日志通道建立 socket 连接失败。\n如果是小程序平台，请勾选不校验合法域名配置。\n如果是运行到真机，请确认手机与电脑处于同一网络。'
-          )
+          wrapError('开发模式下日志通道建立 socket 连接失败。')
+        )
+        originalConsole.error(
+          wrapError('如果是小程序平台，请勾选不校验合法域名配置。')
+        )
+        originalConsole.error(
+          wrapError('如果是运行到真机，请确认手机与电脑处于同一网络。')
         )
         return false
       }
