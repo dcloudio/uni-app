a2ec79d061bbf6438532e321a313592faddc7546
jixinbao <jixinbao@dcloud.io>
2025-02-17 16:02:29 +0800

    feat(mp-vue): build mp-vue

--- packages/uni-mp-vue/dist-x/vue.runtime.esm.js
+++ packages/uni-mp-vue/dist-x/vue.runtime.esm.js
@@ -5562,6 +5562,165 @@ function hyphenateCssProperty(str) {
     return hyphenate(str);
 }
 
+// TODO App端实现未继承自EventTarget，如果后续App端调整此处也需要同步调整
+class UniAnimation {
+    constructor(id, scope, keyframes, options = {}) {
+        var _a;
+        this._playState = '';
+        this.parsedKeyframes = [];
+        this.options = {};
+        this.onfinish = null;
+        this.oncancel = null;
+        this.id = id;
+        this.scope = scope;
+        this.options = typeof options === 'number' ? { duration: options } : options; //as KeyframeAnimationOptions
+        if (((_a = this.options) === null || _a === void 0 ? void 0 : _a.iterations) === Infinity) {
+            this.options.iterations = -1;
+        }
+        this.parsedKeyframes = coverAnimateToStyle(keyframes, options);
+        this.onfinish = () => { };
+        this.oncancel = () => { };
+    }
+    get playState() {
+        return this._playState;
+    }
+    get currentTime() {
+        throw new Error('currentTime not implemented.');
+    }
+    cancel() {
+        // this.scope.setData({
+        //   ['$eA.' + this.id]: JSON.stringify({
+        //     id: this.id,
+        //     playState: 'cancel',
+        //     keyframes: this.parsedKeyframes,
+        //     options: this.options,
+        //   }),
+        // })
+        throw new Error('cancel not implemented.');
+    }
+    finish() {
+        throw new Error('finish not implemented.');
+    }
+    pause() {
+        throw new Error('pause not implemented.');
+    }
+    play() {
+        this.scope.setData({
+            ['$eA.' + this.id]: JSON.stringify({
+                id: this.id,
+                playState: 'running',
+                keyframes: this.parsedKeyframes,
+                options: this.options,
+            }),
+        });
+    }
+}
+// 小程序中的 this.animate 不支持 color 等属性，keyframes 结构不同，需要手动转换
+// 该用 wxs 的 requestAnimationFrame 实现，需要手动实现缓动函数、解析 keyframes，对产物体积有影响
+// 该用 wxs 配合
+function normalizeKeyframes(keyframes) {
+    // 如果关键帧
+    // 数组为空，返回空数组
+    if (keyframes.length === 0) {
+        return [];
+    }
+    // hyphenate
+    keyframes.forEach((kf) => {
+        Object.keys(kf).forEach((key) => {
+            const newKey = hyphenate(key);
+            if (key !== newKey) {
+                kf[newKey] = kf[key];
+                delete kf[key];
+            }
+        });
+    });
+    // 记录已有的 offset 位置
+    const existingOffsets = keyframes
+        .map((kf, index) => ({
+        index,
+        offset: kf.offset,
+    }))
+        .filter((item) => item.offset !== undefined);
+    // 如果没有已有的 offset，均匀分配
+    if (existingOffsets.length === 0) {
+        for (let i = 0; i < keyframes.length; i++) {
+            keyframes[i].offset = i / (keyframes.length - 1);
+        }
+        return keyframes;
+    }
+    // 处理第一个关键帧
+    if (existingOffsets[0].index > 0) {
+        const firstOffset = existingOffsets[0].offset / existingOffsets[0].index;
+        for (let i = 0; i < existingOffsets[0].index; i++) {
+            keyframes[i].offset = firstOffset * i;
+        }
+    }
+    // 处理中间的关键帧
+    for (let i = 0; i < existingOffsets.length - 1; i++) {
+        const startOffset = existingOffsets[i].offset;
+        const endOffset = existingOffsets[i + 1].offset;
+        const diffFrames = existingOffsets[i + 1].index - existingOffsets[i].index;
+        if (diffFrames !== 1) {
+            const step = (endOffset - startOffset) / diffFrames;
+            for (let j = 1; j <= diffFrames; j++) {
+                keyframes[existingOffsets[i].index + j].offset = startOffset + j * step;
+            }
+        }
+    }
+    // 处理最后一个关键帧
+    if (existingOffsets[existingOffsets.length - 1].index <
+        keyframes.length - 1) {
+        const lastOffset = existingOffsets[existingOffsets.length - 1].offset;
+        const numFrames = keyframes.length - existingOffsets[existingOffsets.length - 1].index;
+        const step = (1 - lastOffset) / (numFrames - 1);
+        for (let i = 0; i < numFrames; i++) {
+            keyframes[existingOffsets[existingOffsets.length - 1].index + i].offset =
+                lastOffset + i * step;
+        }
+    }
+    // 保留 五位小数
+    return keyframes.map((kf) => {
+        kf.offset = Number(kf.offset.toFixed(5));
+        return kf;
+    });
+}
+function coverAnimateToStyle(keyframes, options) {
+    const duration = (options === null || options === void 0 ? void 0 : options.duration) || 0;
+    // Handle object format with array values
+    if (!Array.isArray(keyframes)) {
+        const propertyNames = Object.keys(keyframes);
+        const arrayLength = keyframes[propertyNames[0]].length;
+        const frames = Array.from({ length: arrayLength }, (_, i) => {
+            const frame = {};
+            propertyNames.forEach((prop) => {
+                frame[prop] = keyframes[prop][i];
+            });
+            return frame;
+        });
+        return coverAnimateToStyle(frames, options);
+    }
+    // fill offset
+    const frames = normalizeKeyframes(keyframes);
+    return frames.map((frame, index) => {
+        var _a;
+        const currentOffset = frame.offset;
+        let stepDuration;
+        const prevOffset = ((_a = frames[index - 1]) === null || _a === void 0 ? void 0 : _a.offset) || 0;
+        const currentDuration = Math.round(duration * (currentOffset - prevOffset));
+        const crrentOffsetStartTime = Math.round(duration * prevOffset);
+        stepDuration = currentDuration;
+        const result = frame;
+        // delete result.offset
+        return Object.assign({}, result, {
+            // ...result,
+            offset: undefined,
+            transition: `all ${stepDuration}ms ease`,
+            _duration: stepDuration,
+            _startTime: crrentOffsetStartTime,
+        });
+    });
+}
+
 /**
  * event.target、event.currentTarget也是UniElement实例，可能不含id
  */
@@ -5682,6 +5841,22 @@ class UniElement {
     }
     setAttribute(name, value) {
         console.warn(`Miniprogram does not support UniElement.setAttribute(${name}, value)`);
+    }
+    animate(keyframes, options) {
+        if (!this.id) {
+            throw new Error('animate is only supported on elements with id');
+        }
+        const root = this.$vm.$root;
+        const scope = root && root.$scope;
+        if (!scope) {
+            throw new Error(`animate is only supported on elements in page`);
+        }
+        if (!keyframes) {
+            throw new Error('animate keyframes is required');
+        }
+        const animation = new UniAnimation(this.id, scope, keyframes, options);
+        animation.play();
+        return animation;
     }
     $destroy() {
         if (this.style) {
