22488bcefed8272f8f1f456521aff205342f2302
fxy060608 <fxy060608@gmail.com>
2025-02-24 15:20:36 +0800

    wip(app-harmony): expose isolateUniModules

--- packages/uni-app-harmony/src/compiler/index.ts
+++ packages/uni-app-harmony/src/compiler/index.ts
@@ -1,6 +1,8 @@
 import appVite from '@dcloudio/uni-app-vite'
 import { initUniAppXHarmonyPlugin } from '@dcloudio/uni-app-uts'
 import { uniAppHarmonyPlugin } from './plugin'
+import ExternalModulesX from './external-modules-x.json'
+export const isolateUniModules = ExternalModulesX
 export default [
   process.env.UNI_APP_X === 'true' ? initUniAppXHarmonyPlugin : appVite,
   uniAppHarmonyPlugin,
