20391364e80768a0cda06fc2da633b4888a1862f
fxy060608 <fxy060608@gmail.com>
2025-02-24 15:22:28 +0800

    chore(app-harmony): rename

--- packages/uni-app-harmony/src/compiler/index.ts
+++ packages/uni-app-harmony/src/compiler/index.ts
@@ -2,7 +2,7 @@ import appVite from '@dcloudio/uni-app-vite'
 import { initUniAppXHarmonyPlugin } from '@dcloudio/uni-app-uts'
 import { uniAppHarmonyPlugin } from './plugin'
 import ExternalModulesX from './external-modules-x.json'
-export const isolateUniModules = ExternalModulesX
+export const externalModulesX = ExternalModulesX
 export default [
   process.env.UNI_APP_X === 'true' ? initUniAppXHarmonyPlugin : appVite,
   uniAppHarmonyPlugin,
