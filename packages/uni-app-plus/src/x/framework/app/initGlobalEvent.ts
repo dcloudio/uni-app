2cc2fac0cac01b4521f3812e9ce864a26c215af6
fxy060608 <fxy060608@gmail.com>
2025-03-01 12:01:32 +0800

    wip(x-harmony): 补充dialogPage返回逻辑

--- packages/uni-app-plus/src/x/framework/app/initGlobalEvent.ts
+++ packages/uni-app-plus/src/x/framework/app/initGlobalEvent.ts
@@ -1,10 +1,53 @@
 import type { IApp } from '@dcloudio/uni-app-x/types/native'
+import type { UniDialogPage } from '@dcloudio/uni-app-x/types/page'
+import { ON_BACK_PRESS } from '@dcloudio/uni-shared'
+import { getCurrentPage, invokeHook } from '@dcloudio/uni-core'
 import { backbuttonListener } from '../../../service/framework/app/utils'
 import { ON_BACK_BUTTON } from '../../constants'
 
+import { closeDialogPage } from '../../api/route/closeDialogPage'
+
 export function initGlobalEvent(app: IApp) {
   app.addKeyEventListener(ON_BACK_BUTTON, () => {
+    // 目前app-ios和app-harmony均会执行此逻辑，但是app-ios理论上始终不会触发以下dialogPage逻辑
+    const currentPage = getCurrentPage() as unknown as UniPage
+    if (
+      currentPage &&
+      (currentPage.vm.$systemDialogPages.length > 0 ||
+        currentPage.getDialogPages().length > 0)
+    ) {
+      const systemDialogPages = currentPage.vm.$systemDialogPages
+      const dialogPages = currentPage.getDialogPages()
+
+      const lastSystemDialog = systemDialogPages[systemDialogPages.length - 1]
+      const lastDialog = dialogPages[dialogPages.length - 1]
+
+      if (!systemDialogPages.length) {
+        handleDialogPageBack(lastDialog as UniDialogPage)
+      } else if (!dialogPages.length) {
+        handleDialogPageBack(lastSystemDialog as UniDialogPage)
+      } else {
+        handleDialogPageBack(
+          parseInt(lastDialog.vm!.$nativePage!.pageId) >
+            parseInt(lastSystemDialog.vm!.$nativePage!.pageId)
+            ? lastDialog
+            : lastSystemDialog
+        )
+      }
+    }
     backbuttonListener()
     return true
   })
+}
+
+function handleDialogPageBack(dialogPage: UniDialogPage) {
+  const onBackPressRes = invokeHook(dialogPage.vm, ON_BACK_PRESS, {
+    from: 'navigateBack',
+  })
+  if (onBackPressRes !== true) {
+    closeDialogPage({
+      dialogPage,
+      animationType: 'auto',
+    })
+  }
 }
