0c707d23ff87b88dbc706ec47faef6e0762adca0
jixinbao <jixinbao@dcloud.io>
2025-03-11 20:15:28 +0800

    fix(nvue-styler): CSS border 展开区分 nvue ask206548
    (cherry picked from commit 3c7f4ab81293079aeec6f4686a31c9075d7236bc)

--- packages/uni-nvue-styler/src/expand/border.ts
+++ packages/uni-nvue-styler/src/expand/border.ts
@@ -92,3 +92,46 @@ export function createTransformBorder(
     }
   }
 }
+
+export function createTransformBorderNvue(
+  options: NormalizeOptions
+): TransformDecl {
+  return (decl) => {
+    const { prop, value, important, raws, source } = decl
+    const splitResult = value.replace(/\s*,\s*/g, ',').split(/\s+/)
+    const result = [
+      /^[\d\.]+\S*|^(thin|medium|thick)$/,
+      /^(solid|dashed|dotted|none)$/,
+      /\S+/,
+    ].map((item) => {
+      const index = splitResult.findIndex((str) => item.test(str))
+      return index < 0 ? null : splitResult.splice(index, 1)[0]
+    })
+    if (splitResult.length) {
+      return [decl]
+    }
+    return [
+      createDecl(
+        prop + borderWidth,
+        (result[0] || (options.type === 'uvue' ? 'medium' : '0')).trim(),
+        important,
+        raws,
+        source
+      ),
+      createDecl(
+        prop + borderStyle,
+        (result[1] || (options.type === 'uvue' ? 'none' : 'solid')).trim(),
+        important,
+        raws,
+        source
+      ),
+      createDecl(
+        prop + borderColor,
+        (result[2] || '#000000').trim(),
+        important,
+        raws,
+        source
+      ),
+    ]
+  }
+}
