9e6369af039e3fa5e2609730394265d57906005f
hdx <hdx@dcloud.io>
2025-03-14 18:02:51 +0800

    feat(x:auto-import): 新增类型 UniPointerEvent

--- packages/uni-uts-v1/src/arkts.ts
+++ packages/uni-uts-v1/src/arkts.ts
@@ -104,6 +104,7 @@ export function getArkTSAutoImports(isX = false): AutoImportOptions {
       ['UniElementImpl'],
       ['document'],
       ['UniPageImpl'],
+      ['UniPointerEvent'],
       ['UniTouchEvent'],
       ['UniFormControlElement'],
       ['UniNativeViewElementImpl'],
