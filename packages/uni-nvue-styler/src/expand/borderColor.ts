fcaad7f0a0434fd39e3367409cc3dfced79fef47
qiang <guoshengqiang@live.com>
2025-03-28 16:53:58 +0800

    chore: revert to dev

--- packages/uni-nvue-styler/src/expand/borderColor.ts
+++ packages/uni-nvue-styler/src/expand/borderColor.ts
@@ -111,35 +111,3 @@ export const transformBorderColorNvue = (decl: Declaration): Declaration[] => {
     createDecl(borderLeft + property, splitResult[3], important, raws, source),
   ]
 }
-
-export const transformBorderColorNvue: TransformDecl = (decl) => {
-  const { prop, value, important, raws, source } = decl
-  let property = hyphenate(prop).split('-')[1]
-  if (!__NODE_JS__) {
-    property = capitalize(property)
-  }
-  const splitResult = value.replace(/\s*,\s*/g, ',').split(/\s+/)
-  switch (splitResult.length) {
-    case 1:
-      return [decl]
-    case 2:
-      splitResult.push(splitResult[0], splitResult[1])
-      break
-    case 3:
-      splitResult.push(splitResult[1])
-      break
-  }
-
-  return [
-    createDecl(borderTop + property, splitResult[0], important, raws, source),
-    createDecl(borderRight + property, splitResult[1], important, raws, source),
-    createDecl(
-      borderBottom + property,
-      splitResult[2],
-      important,
-      raws,
-      source
-    ),
-    createDecl(borderLeft + property, splitResult[3], important, raws, source),
-  ]
-}
