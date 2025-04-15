dd0848c10f918141304bd942b1f64b6fbfbe7ec2
fxy060608 <fxy060608@gmail.com>
2025-04-15 15:03:18 +0800

    wip(sourcemap): uvue

--- packages/uni-cli-shared/src/vite/plugins/uts/uvue.ts
+++ packages/uni-cli-shared/src/vite/plugins/uts/uvue.ts
@@ -1,12 +1,42 @@
-import type { Plugin } from 'vite'
+import type { Plugin, ResolvedConfig } from 'vite'
+import MagicString from 'magic-string'
 import { isVueSfcFile } from '../../../vue'
+import { withSourcemap } from '../../utils/utils'
 
+export function replaceScriptLang(
+  id: string,
+  code: string,
+  sourcemap: boolean
+) {
+  const magicString = new MagicString(code)
+  magicString.replace(/<script([^>]*)>/gi, (match, attributes) => {
+    // 如果 <script> 标签中没有 lang 属性，添加 lang="uts"
+    if (!/lang=["']?[^"']*["']?/.test(attributes)) {
+      return `<script${attributes} lang="uts">`
+    }
+    // 否则，将现有的 lang 属性替换为 lang="uts"
+    return match.replace(/lang=["']?ts["']?/, 'lang="uts"')
+  })
+
+  if (magicString.hasChanged()) {
+    return {
+      code: magicString.toString(),
+      map: sourcemap
+        ? magicString.generateMap({
+            hires: true,
+          })
+        : null,
+    }
+  }
+}
 export function uniUTSUVueJavaScriptPlugin(options = {}): Plugin {
   process.env.UNI_UTS_USING_ROLLUP = 'true'
+  let resolvedConfig: ResolvedConfig
   return {
     name: 'uni:uts-uvue',
     enforce: 'pre',
     configResolved(config) {
+      resolvedConfig = config
       // 移除自带的 esbuild 处理 ts 文件
       const index = config.plugins.findIndex((p) => p.name === 'vite:esbuild')
       if (index > -1) {
@@ -18,17 +48,7 @@ export function uniUTSUVueJavaScriptPlugin(options = {}): Plugin {
       if (!isVueSfcFile(id)) {
         return
       }
-      return {
-        code: code.replace(/<script([^>]*)>/gi, (match, attributes) => {
-          // 如果 <script> 标签中没有 lang 属性，添加 lang="uts"
-          if (!/lang=["']?[^"']*["']?/.test(attributes)) {
-            return `<script${attributes} lang="uts">`
-          }
-          // 否则，将现有的 lang 属性替换为 lang="uts"
-          return match.replace(/lang=["']?ts["']?/, 'lang="uts"')
-        }),
-        map: null,
-      }
+      return replaceScriptLang(id, code, withSourcemap(resolvedConfig))
     },
   }
 }
