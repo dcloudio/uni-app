878fed102591e94e7ebcb66f1761a6dfe2fb3a39
fxy060608 <fxy060608@gmail.com>
2025-04-15 19:07:56 +0800

    wip(sourcemap): 还原sourcemap

--- packages/uni-cli-shared/src/vite/plugins/uts/uvue.ts
+++ packages/uni-cli-shared/src/vite/plugins/uts/uvue.ts
@@ -1,38 +1,12 @@
-import type { Plugin, ResolvedConfig, TransformResult } from 'vite'
-import MagicString from 'magic-string'
+import type { Plugin } from 'vite'
 import { isVueSfcFile } from '../../../vue'
-import { withSourcemap } from '../../utils/utils'
 
-export function replaceScriptLang(
-  id: string,
-  code: string,
-  sourcemap: boolean
-): TransformResult | undefined {
-  const magicString = new MagicString(code)
-  magicString.replace(/<script([^>]*)>/gi, (match, attributes) => {
-    // 如果 <script> 标签中没有 lang 属性，添加 lang="uts"
-    if (!/lang=["']?[^"']*["']?/.test(attributes)) {
-      return `<script${attributes} lang="uts">`
-    }
-    // 否则，将现有的 lang 属性替换为 lang="uts"
-    return match.replace(/lang=["']?ts["']?/, 'lang="uts"')
-  })
-
-  if (magicString.hasChanged()) {
-    return {
-      code: magicString.toString(),
-      map: sourcemap ? { mappings: '' } : null,
-    }
-  }
-}
 export function uniUTSUVueJavaScriptPlugin(options = {}): Plugin {
   process.env.UNI_UTS_USING_ROLLUP = 'true'
-  let resolvedConfig: ResolvedConfig
   return {
     name: 'uni:uts-uvue',
     enforce: 'pre',
     configResolved(config) {
-      resolvedConfig = config
       // 移除自带的 esbuild 处理 ts 文件
       const index = config.plugins.findIndex((p) => p.name === 'vite:esbuild')
       if (index > -1) {
@@ -44,7 +18,17 @@ export function uniUTSUVueJavaScriptPlugin(options = {}): Plugin {
       if (!isVueSfcFile(id)) {
         return
       }
-      return replaceScriptLang(id, code, withSourcemap(resolvedConfig))
+      return {
+        code: code.replace(/<script([^>]*)>/gi, (match, attributes) => {
+          // 如果 <script> 标签中没有 lang 属性，添加 lang="uts"
+          if (!/lang=["']?[^"']*["']?/.test(attributes)) {
+            return `<script${attributes} lang="uts">`
+          }
+          // 否则，将现有的 lang 属性替换为 lang="uts"
+          return match.replace(/lang=["']?ts["']?/, 'lang="uts"')
+        }),
+        map: { mappings: '' },
+      }
     },
   }
 }
@@ -72,7 +56,7 @@ export function uniUVueTypeScriptPlugin(options = {}): Plugin {
           // 否则，将现有的 lang 属性替换为 lang="uts"
           return match.replace(/lang=["']?uts["']?/, 'lang="ts"')
         }),
-        map: null,
+        map: { mappings: '' },
       }
     },
   }
