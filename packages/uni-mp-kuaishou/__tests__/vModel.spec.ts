26923e79ecf1a6ce2368184e2a27be6487da0af4
Qiang <2631640352@qq.com>
2025-05-14 11:30:02 +0800

    支持快手小程序动态事件 (#5450)
    * fix(mp): 修复快手小程序不支持动态事件绑定
    
    * fix(mp): 调整快手小程序事件的单元测试
    
    ---------
    
    Co-authored-by: wuxianqiang <wuxianqiang@kuaishou.com>

--- packages/uni-mp-kuaishou/__tests__/vModel.spec.ts
+++ packages/uni-mp-kuaishou/__tests__/vModel.spec.ts
@@ -4,18 +4,18 @@ describe('mp-kuaishou: transform v-model', () => {
   test(`component v-model`, () => {
     assert(
       `<Comp v-model="model" />`,
-      `<comp ks:if="{{b}}" u-i="2a9ec0b0-0" bind:__l="__l" eO="{{a}}" bindupdateModelValue="__e" u-p="{{b}}"/>`,
+      `<comp ks:if="{{b}}" u-i="2a9ec0b0-0" bind:__l="__l" bindupdateModelValue="{{a}}" u-p="{{b}}"/>`,
       `(_ctx, _cache) => {
-  return { a: _j({ 'updateModelValue': _o($event => _ctx.model = $event) }), b: _p({ modelValue: _ctx.model }) }
+  return { a: _o($event => _ctx.model = $event), b: _p({ modelValue: _ctx.model }) }
 }`
     )
   })
   test(`component v-model with cache`, () => {
     assert(
       `<Comp v-model="model" />`,
-      `<comp ks:if="{{b}}" u-i="2a9ec0b0-0" bind:__l="__l" eO="{{a}}" bindupdateModelValue="__e" u-p="{{b}}"/>`,
+      `<comp ks:if="{{b}}" u-i="2a9ec0b0-0" bind:__l="__l" bindupdateModelValue="{{a}}" u-p="{{b}}"/>`,
       `(_ctx, _cache) => {
-  return { a: _j({ 'updateModelValue': _o($event => _ctx.model = $event) }), b: _p({ modelValue: _ctx.model }) }
+  return { a: _o($event => _ctx.model = $event), b: _p({ modelValue: _ctx.model }) }
 }`,
       {
         cacheHandlers: true,
@@ -25,32 +25,32 @@ describe('mp-kuaishou: transform v-model', () => {
   test(`input,textarea v-model`, () => {
     assert(
       `<input v-model="model" />`,
-      `<input data-e-o="{{a}}" value="{{b}}" bindinput="__e"/>`,
+      `<input value="{{a}}" bindinput="{{b}}"/>`,
       `(_ctx, _cache) => {
-  return { a: { 'input': _o($event => _ctx.model = $event.detail.value) }, b: _ctx.model }
+  return { a: _ctx.model, b: _o($event => _ctx.model = $event.detail.value) }
 }`
     )
     assert(
       `<textarea v-model="model" />`,
-      `<textarea data-e-o="{{a}}" value="{{b}}" bindinput="__e"/>`,
+      `<textarea value="{{a}}" bindinput="{{b}}"/>`,
       `(_ctx, _cache) => {
-  return { a: { 'input': _o($event => _ctx.model = $event.detail.value) }, b: _ctx.model }
+  return { a: _ctx.model, b: _o($event => _ctx.model = $event.detail.value) }
 }`
     )
   })
   test(`input v-model + v-on`, () => {
     assert(
       `<input @input="input" v-model="model" />`,
-      `<input bindinput="__e" data-e-o="{{a}}" value="{{b}}"/>`,
+      `<input bindinput="{{a}}" value="{{b}}"/>`,
       `(_ctx, _cache) => {
-  return { a: { 'input': _o([$event => _ctx.model = $event.detail.value, _ctx.input]) }, b: _ctx.model }
+  return { a: _o([$event => _ctx.model = $event.detail.value, _ctx.input]), b: _ctx.model }
 }`
     )
     assert(
       `<input v-model="model" @input="input" />`,
-      `<input bindinput="__e" data-e-o="{{a}}" value="{{b}}"/>`,
+      `<input bindinput="{{a}}" value="{{b}}"/>`,
       `(_ctx, _cache) => {
-  return { a: { 'input': _o([$event => _ctx.model = $event.detail.value, _ctx.input]) }, b: _ctx.model }
+  return { a: _o([$event => _ctx.model = $event.detail.value, _ctx.input]), b: _ctx.model }
 }`
     )
   })
