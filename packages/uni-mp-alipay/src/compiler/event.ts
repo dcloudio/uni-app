2e9eed6b540f324fde39f1da7674632d6ff9edec
chouchouji <1305974212@qq.com>
2025-05-22 14:52:33 +0800

    docs: add comment

--- packages/uni-mp-alipay/src/compiler/event.ts
+++ packages/uni-mp-alipay/src/compiler/event.ts
@@ -8,6 +8,7 @@ export const event: MiniProgramCompilerOptions['event'] = {
       name = 'tap'
     }
     name = eventMap[name] || name
+    // 处理支付宝支持捕获 https://opendocs.alipay.com/mini/framework/events#%E4%BA%8B%E4%BB%B6%E7%9A%84%E6%8D%95%E8%8E%B7%E9%98%B6%E6%AE%B5
     return `${isCapture ? 'capture-' : ''}${
       isCatch ? 'catch' : 'on'
     }${capitalize(customizeEvent(name))}`
