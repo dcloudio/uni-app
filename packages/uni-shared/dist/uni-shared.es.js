cb98f9889be9cfed6d186fb3839ce031c5059643
chouchouji <1305974212@qq.com>
2025-06-04 11:06:24 +0800

    chore: build shared

--- packages/uni-shared/dist/uni-shared.es.js
+++ packages/uni-shared/dist/uni-shared.es.js
@@ -136,6 +136,8 @@ const UVUE_BUILT_IN_TAGS = [
     'share-element',
     'cover-view',
     'cover-image',
+    // custom element
+    'match-media',
 ];
 const UVUE_WEB_BUILT_IN_TAGS = [
     'list-view',
