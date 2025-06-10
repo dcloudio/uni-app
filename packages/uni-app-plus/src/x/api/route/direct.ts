e0576c2e8690dd0243df32b0bf851b53ae40b160
zhenyuWang <13641039885@163.com>
2025-06-10 16:27:05 +0800

    fix(x-ios): 修复某些情况下热更后返回首页白屏问题

--- packages/uni-app-plus/src/x/api/route/direct.ts
+++ packages/uni-app-plus/src/x/api/route/direct.ts
@@ -1,14 +1,12 @@
 import type { ComponentPublicInstance } from 'vue'
-import { getRealPath } from '../../framework/route'
 import { reLaunch } from './reLaunch'
-import { parseUrl } from '@dcloudio/uni-shared'
+import { getCurrentPages } from '../../../service/framework/page'
 
 // 是否处于直达页面
 export function isDirectPage(page: ComponentPublicInstance): boolean {
   return (
     !!__uniConfig.realEntryPagePath &&
-    getRealPath(page.$basePage.route, true) ===
-      getRealPath(parseUrl(__uniConfig.entryPagePath!).path, true)
+    (getCurrentPages() as UniPage[])[0]?.vm === page
   )
 }
 // 重新启动到首页
