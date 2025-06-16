27dafdca9e53eb1ab4bd4a446b2b50cb6e529290
chouchouji <jibinbin@dcloud.io>
2025-06-16 15:42:38 +0800

    fix(mp-alipay): showActionSheet 支持 itemColor 属性

--- src/platforms/mp-alipay/runtime/api/protocols.js
+++ src/platforms/mp-alipay/runtime/api/protocols.js
@@ -200,7 +200,7 @@ const protocols = { // 需要做转换的 API 列表
     name: 'showActionSheet',
     args: {
       itemList: 'items',
-      itemColor: false
+      itemColor: my.canIUse('showActionSheet.object.itemColor') ? 'itemColor' : false
     },
     returnValue: {
       index: 'tapIndex'
