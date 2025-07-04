a2c306779deb13e1c3621f205872650bf81d7d64
hdx <hdx@dcloud.io>
2025-07-04 11:23:15 +0800

    feat(ad): 优化show方法即使没有调用load也可显示

--- packages/uni-components/lib/uniad/uniad.vue
+++ packages/uni-components/lib/uniad/uniad.vue
@@ -11,7 +11,7 @@
     />
     <!-- #ifdef MP-WEIXIN -->
     <ad-custom v-if="userwx" :unit-id="userUnitId" class="uni-ad-custom" :class="[customFullscreen]" @load="_onmpload" @error="_onmperror"></ad-custom>
-    <uniad-plugin-wx v-if="wxchannel" class="uniad-plugin-wx" @load="_onmpload" @error="_onwxchannelerror"></uniad-plugin-wx>
+    <uniad-plugin-wx v-if="wxchannel" class="uniad-plugin-wx" :class="[customFullscreen]" @load="_onmpload" @error="_onwxchannelerror"></uniad-plugin-wx>
     <!-- #endif -->
   </view>
 </template>
@@ -32,6 +32,7 @@ export default {
 
 <style>
 .uni-ad-custom-fullscreen {
+  display: flex;
   height: 100vh;
 }
 </style>
