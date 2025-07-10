b99b169424bc7254f54409e98a7cec115b69f023
ywenhao <37856060+ywenhao@users.noreply.github.com>
2025-07-10 10:31:52 +0800

    feat: 只针对h5生效

--- packages/uni-components/src/helpers/scroller/Scroller.ts
+++ packages/uni-components/src/helpers/scroller/Scroller.ts
@@ -216,7 +216,11 @@ export class Scroller {
         const e = Date.now()
         const i = (e - this._scroll._startTime) / 1e3
         const r = this._scroll.x(i)
-        this._position = r < -this._extent ? -this._extent : r
+        if (__PLATFORM__ === 'h5') {
+          this._position = r < -this._extent ? -this._extent : r
+        } else {
+          this._position = r
+        }
         this.updatePosition()
         const o = this._scroll.dx(i)
         if (
