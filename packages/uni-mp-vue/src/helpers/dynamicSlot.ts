36d380e000033c4aae8471439ef83c1aee500afc
chouchouji <1305974212@qq.com>
2025-07-22 14:50:39 +0800

    refactor(mp):  拆分 dynamicSlotName 函数处理逻辑

--- packages/uni-mp-vue/src/helpers/dynamicSlot.ts
+++ packages/uni-mp-vue/src/helpers/dynamicSlot.ts
@@ -1,5 +1,6 @@
 import { isString } from '@vue/shared'
 import { dynamicSlotName } from '@dcloudio/uni-shared'
+import { type ComponentInternalInstance, getCurrentInstance } from 'vue'
 /**
  * quickapp-webview 不能使用 default 作为插槽名称，故统一转换 default 为 d
  * @param names
@@ -7,7 +8,29 @@ import { dynamicSlotName } from '@dcloudio/uni-shared'
  */
 export function dynamicSlot(names: string | string[], key?: string | number) {
   if (isString(names)) {
-    return dynamicSlotName(names, key)
+    return normalizeSlotName(names, key)
   }
-  return names.map((name) => dynamicSlotName(name, key))
+  return names.map((name) => normalizeSlotName(name, key))
+}
+
+function normalizeSlotName(name: string, key?: string | number) {
+  const slotName = dynamicSlotName(name)
+  if (key === undefined) {
+    return slotName
+  }
+
+  const instance = getCurrentInstance() as ComponentInternalInstance & {
+    ctx: { $scope: { _$vueId: string } }
+  }
+  let isScopedSlot = false
+  let parent = instance.parent
+  while (parent) {
+    const invokers = (parent as any).$ssi
+    if (invokers && invokers[instance.ctx.$scope._$vueId]) {
+      isScopedSlot = true
+      break
+    }
+    parent = parent.parent
+  }
+  return slotName + (isScopedSlot ? `-${key}` : '')
 }
