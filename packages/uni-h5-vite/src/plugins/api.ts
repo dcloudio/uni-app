18c4ce15187195da509cbb30696fa69c461f065a
DCloud_LXH <283700113@qq.com>
2025-08-01 16:42:57 +0800

    feat(web): 调整编译、运行阶段自动注入 sdk 逻辑

--- packages/uni-h5-vite/src/plugins/api.ts
+++ packages/uni-h5-vite/src/plugins/api.ts
@@ -1,6 +1,7 @@
 import type { Plugin, ViteDevServer } from 'vite'
 import { AliYunCloudAuthWebSDK } from '../utils'
 import { normalizePath } from '@dcloudio/uni-cli-shared'
+import type { OutputChunk } from 'rollup'
 
 let enableFacialRecognition = false
 
@@ -12,11 +13,12 @@ function setEnableFacialRecognition(enable: boolean) {
   enableFacialRecognition = enable
 }
 
+function checkGetMetaInfo(code: string) {
+  return code.includes('window.getMetaInfo')
+}
+
 function checkFacialRecognition(code: string) {
-  return (
-    code.includes('getFacialRecognitionMetaInfo') ||
-    code.includes('window.getMetaInfo')
-  )
+  return code.includes('getFacialRecognitionMetaInfo')
 }
 
 export function uniApiPlugin(): Plugin {
@@ -24,11 +26,12 @@ export function uniApiPlugin(): Plugin {
   const inputDir = normalizePath(process.env.UNI_INPUT_DIR)
   return {
     name: 'uni:h5-api',
-    enforce: 'post',
+    enforce: 'pre',
     configureServer(server) {
       viteServer = server
     },
     transform(code, id) {
+      if (!viteServer) return
       // 通过transform阶段识别，仅判断inputDir内部的文件，避免框架文件影响
       if (
         !isEnableFacialRecognition() &&
@@ -36,12 +39,24 @@ export function uniApiPlugin(): Plugin {
       ) {
         if (checkFacialRecognition(code)) {
           setEnableFacialRecognition(true)
-          if (viteServer) {
-            // 开发模式触发重新刷新
-            viteServer.ws.send({
-              type: 'full-reload',
-              path: '*',
-            })
+          // 开发模式触发重新刷新
+          viteServer.hot.send({
+            type: 'full-reload',
+            path: '*',
+          })
+        }
+      }
+    },
+    generateBundle(_options, bundle) {
+      if (viteServer) return
+      if (!isEnableFacialRecognition()) {
+        const filesNames = Object.keys(bundle)
+        for (const fileName of filesNames) {
+          const chunk = bundle[fileName] as OutputChunk
+          if (chunk && chunk.type === 'chunk' && chunk.code) {
+            setEnableFacialRecognition(
+              checkFacialRecognition(chunk.code) || checkGetMetaInfo(chunk.code)
+            )
           }
         }
       }
