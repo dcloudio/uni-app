113535689219cd5519196db61ecb288735393868
DCloud_LXH <283700113@qq.com>
2025-08-02 16:10:31 +0800

    wip(x-mp-weixin): worker

--- packages/uni-mp-weixin/src/x/api/index.ts
+++ packages/uni-mp-weixin/src/x/api/index.ts
@@ -4,3 +4,17 @@ import * as shims from '../../api/shims'
 import * as protocols from '../../api/protocols'
 export const wx = initWx()
 export default initUni(shims, protocols, wx)
+
+declare const worker: UniNamespace.Worker
+export class WorkerTaskImpl {
+  constructor() {
+    worker.onMessage = (e) => {
+      this.onMessage(e.data)
+    }
+  }
+  entry(): void {}
+  onMessage(message: Object): void {}
+  postMessage(message: Object): void {
+    worker.postMessage(message)
+  }
+}
