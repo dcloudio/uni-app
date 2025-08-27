9a605a05184620bc217a3979b857344fba707b93
chouchouji <1305974212@qq.com>
2025-08-27 19:11:50 +0800

    refactor(mp-xhs): 移除无用的代码

--- packages/uni-mp-xhs/src/runtime/util.ts
+++ packages/uni-mp-xhs/src/runtime/util.ts
@@ -1,11 +1,9 @@
-import { hasOwn, isFunction, isString } from '@vue/shared'
+import { hasOwn, isFunction } from '@vue/shared'
 
-import {
-  type ComponentInternalInstance,
-  type ComponentOptions,
-  type ComponentPublicInstance,
-  type Ref,
-  isRef,
+import type {
+  ComponentInternalInstance,
+  ComponentOptions,
+  ComponentPublicInstance,
 } from 'vue'
 
 // @ts-expect-error EMPTY_OBJ 不能从 @vue/shared 中引入，从 vue 中导入，保持一致
@@ -27,8 +25,6 @@ export type MPComponentInstance = tinyapp.IComponentInstance<
   Record<string, any>,
   any
 >
-
-export const isComponent2 = xhs.canIUse('component2')
 
 export const mocks = ['nodeId', 'componentId', 'componentPath']
 
@@ -60,70 +56,6 @@ export function initSpecialMethods(
         }
       }
     })
-  }
-}
-
-export function handleRef(this: MPComponentInstance, ref: MPComponentInstance) {
-  if (!ref) {
-    return
-  }
-  const refName = ref.props.uR // data-ref
-  const refInForName = ref.props.uRIF // data-ref-in-for
-  if (!refName && !refInForName) {
-    return
-  }
-  const instance = this.$vm.$
-  const refs =
-    instance.refs === EMPTY_OBJ ? (instance.refs = {}) : instance.refs
-
-  const { setupState } = instance
-  const refValue = ref.$vm
-  if (refName) {
-    if (isString(refName)) {
-      refs[refName] = refValue
-      if (hasOwn(setupState, refName)) {
-        setupState[refName] = refValue
-      }
-    } else {
-      setRef(refName, refValue, refs, setupState)
-    }
-  } else if (refInForName) {
-    if (isString(refInForName)) {
-      ;(refs[refInForName] || (refs[refInForName] = [])).push(refValue)
-    } else {
-      setRef(refInForName, refValue, refs, setupState)
-    }
-  }
-}
-
-type VNodeRef =
-  | string
-  | Ref
-  | ((ref: object | null, refs: Record<string, any>) => void)
-
-type TemplateRef = {
-  r: VNodeRef
-  k?: string // setup ref key
-  f?: boolean // refInFor marker
-}
-
-function isTemplateRef(opts: unknown): opts is TemplateRef {
-  return !!(opts && (opts as TemplateRef).r)
-}
-
-function setRef(
-  ref: Ref | ((ref: object | null, refs: Record<string, any>) => void),
-  refValue: ComponentPublicInstance,
-  refs: Record<string, unknown>,
-  setupState: Data
-) {
-  if (isRef(ref)) {
-    ref.value = refValue
-  } else if (isFunction(ref)) {
-    const templateRef = ref(refValue, refs)
-    if (isTemplateRef(templateRef)) {
-      setTemplateRef(templateRef, refValue, setupState)
-    }
   }
 }
 
