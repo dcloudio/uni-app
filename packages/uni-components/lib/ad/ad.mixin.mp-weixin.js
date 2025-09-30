4d6ad92eceb003ed4c02c737e1541ee2c0f70182
hdx <hdx@dcloud.io>
2025-09-30 15:25:51 +0800

    fix(ad-rewarded-video): 代运营模式作用域插槽没有错误信息的问题#0

--- packages/uni-components/lib/ad/ad.mixin.mp-weixin.js
+++ packages/uni-components/lib/ad/ad.mixin.mp-weixin.js
@@ -321,6 +321,7 @@ export default {
 
       this._wxRewardedAd.onError(err => {
         this.loading = false
+        this.errorMessage = JSON.stringify(err)
         this._dispatchEvent(EventType.Error, err);
       });
 
@@ -329,6 +330,10 @@ export default {
         if (res.isEnded && this._hasCallback()) {
           this._callServer()
         }
+      });
+
+      this._wxRewardedAd.load().then(() => {
+      }).catch((_) => {
       });
 
       this.loading = true
@@ -353,6 +358,7 @@ export default {
 
       this._wxInterstitialAd.onError(err => {
         this.loading = false
+        this.errorMessage = JSON.stringify(err)
         this._dispatchEvent(EventType.Error, err);
       });
 
