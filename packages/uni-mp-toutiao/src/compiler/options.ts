6915d3d1b3001e2a4c2c6e5aa2ab6ff472035eee
chouchouji <1305974212@qq.com>
2025-10-09 20:41:05 +0800

    fix(mp-toutiao): 修复 mask 组件编译属性丢失的 Bug

--- packages/uni-mp-toutiao/src/compiler/options.ts
+++ packages/uni-mp-toutiao/src/compiler/options.ts
@@ -32,6 +32,7 @@ export const customElements = [
   'clue-order-form',
   'shop-follow-card',
   'match-media',
+  'mask',
   ...getNativeTags(process.env.UNI_INPUT_DIR, process.env.UNI_PLATFORM),
 ]
 
