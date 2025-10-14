3b33cd98878aef731bafa9f6ef7221aba785fb5c
DCloud_LXH <283700113@qq.com>
2025-10-14 15:37:02 +0800

    feat(h5): 优化 dev 构建时对 aliyun sdk 的引入

--- packages/vue-cli-plugin-uni/packages/webpack-html-inject-aliyun-plugin/index.js
+++ packages/vue-cli-plugin-uni/packages/webpack-html-inject-aliyun-plugin/index.js
@@ -2,8 +2,6 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 class WebpackHtmlInjectAliYunPlugin {
-  isEnableFacialRecognition = false;
-  isInserted = false;
   AliYunCloudAuthWebSDK = '';
 
   constructor (AliYunCloudAuthWebSDK) {
@@ -13,14 +11,13 @@ class WebpackHtmlInjectAliYunPlugin {
   apply (compiler) {
     if (webpack.version[0] > 4) {
       compiler.hooks.compilation.tap('WebpackHtmlInjectAliYunPlugin', compilation => {
-        compilation.hooks.processAssets.tap(
-          {
-            name: 'WebpackHtmlInjectAliYunPlugin',
-            stage: webpack.Compilation.PROCESS_ASSETS_STAGE_DEV_TOOLING
-          },
-          _ => {
-            this.analyze(compilation)
-          }
+        compilation.hooks.processAssets.tap({
+          name: 'WebpackHtmlInjectAliYunPlugin',
+          stage: webpack.Compilation.PROCESS_ASSETS_STAGE_DEV_TOOLING
+        },
+        _ => {
+          this.analyze(compilation)
+        }
         )
       })
     } else {
@@ -34,49 +31,42 @@ class WebpackHtmlInjectAliYunPlugin {
   }
 
   analyze (compilation) {
-    if (this.isInserted) {
-      return
-    }
-    if (!this.isEnableFacialRecognition) {
-      const assets = compilation.assets
-      const chunkNames = Object.keys(assets).filter(name =>
-        name.endsWith('.js') && !name.includes('chunk-vendors')
-      )
-      chunkNames.forEach(name => {
-        if (!this.isEnableFacialRecognition) {
-          const asset = assets[name]
-          const source = asset.source()
-
-          this.isEnableFacialRecognition = source.includes('uni.getFacialRecognitionMetaInfo')
-        }
-      })
-    }
-
-    if (!this.isEnableFacialRecognition) return
+    const assets = compilation.assets
+    const chunkNames = Object.keys(assets).filter(name =>
+      name.endsWith('.js') && !name.includes('chunk-vendors')
+    )
+    const findChunkName = chunkNames.find(name => {
+      const asset = assets[name]
+      const source = asset.source()
+      return source.includes('getFacialRecognitionMetaInfo') || source.includes('window.getMetaInfo')
+    })
+    if (!findChunkName) return
 
     if (compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration) {
       compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration.tapAsync(
         'WebpackHtmlInjectAliYunPlugin',
         (data, cb) => {
-          if (!this.isInserted) {
+          const index = data.assets.js.indexOf(this.AliYunCloudAuthWebSDK)
+          if (index === -1) {
             data.assets.js.unshift(this.AliYunCloudAuthWebSDK)
-            this.isInserted = true
-            cb(null, data)
           }
+          cb(null, data)
         }
       )
-    } else if (!this.isInserted) {
+    } else {
       const hooks = HtmlWebpackPlugin.getHooks(compilation)
       if (hooks) {
         hooks.beforeAssetTagGeneration.tapAsync(
           'WebpackHtmlInjectAliYunPlugin',
           (data, cb) => {
-            data.assets.js.unshift(this.AliYunCloudAuthWebSDK)
+            const index = data.assets.js.indexOf(this.AliYunCloudAuthWebSDK)
+            if (index === -1) {
+              data.assets.js.unshift(this.AliYunCloudAuthWebSDK)
+            }
             cb(null, data)
           }
         )
       }
-      this.isInserted = true
     }
   }
 }
