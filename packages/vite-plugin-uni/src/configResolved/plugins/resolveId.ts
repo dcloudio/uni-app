dc5619ffcc0c8e5672cb06bcb15f8f93b7e6b625
ruan-cat <1219043956@qq.com>
2025-10-16 19:54:23 +0800

    perf(vite-plugin-uni): 优化 vue-router 的导入语法，直接用 vue-router.mjs 代替 vue-router.esm-bundler.js

--- packages/vite-plugin-uni/src/configResolved/plugins/resolveId.ts
+++ packages/vite-plugin-uni/src/configResolved/plugins/resolveId.ts
@@ -18,20 +18,8 @@ try {
     ) >= 3
 } catch (error) {}
 
-let isVueRouterVersion5 = false
-try {
-  isVueRouterVersion5 =
-    Number(
-      fs
-        .readJSONSync(path.join(resolveBuiltIn('vue-router/package.json')))
-        .version.split('.')[0]
-    ) >= 5
-} catch (error) {}
-
 const BUILT_IN_MODULES = {
-  'vue-router': isVueRouterVersion5
-    ? 'dist/vue-router.mjs'
-    : 'dist/vue-router.esm-bundler.js',
+  'vue-router': 'dist/vue-router.mjs',
   vuex: 'dist/vuex.esm-bundler.js',
   'vue-i18n': 'dist/vue-i18n.esm-bundler.js',
   '@dcloudio/uni-app': 'dist/uni-app.es.js',
