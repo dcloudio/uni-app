3e3087f3e668e7065cc774571adeb82c0a0f82d3
jixinbao <jixinbao@dcloud.io>
2025-10-17 20:05:53 +0800

    chore: 修复 eslint/jest 环境报错

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,21 @@
+/**
+ * @type {import('jest').Config}
+ */
 module.exports = {
-    globals: {
-        __DEV__: true
-    },
-    coverageDirectory: 'coverage',
-    coverageReporters: ['html', 'lcov', 'text'],
-    collectCoverageFrom: ['packages/*/src/**/*.js'],
-    moduleFileExtensions: ['js', 'json'],
-    moduleNameMapper: {
-        '^@dcloudio/(.*?)$': '<rootDir>/packages/$1'
-    },
-    rootDir: __dirname,
-    testMatch: ['<rootDir>/packages/**/__tests__/**/*spec.(t|j)s']
+  globals: {
+    __DEV__: true
+  },
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'lcov', 'text'],
+  collectCoverageFrom: ['packages/*/src/**/*.js'],
+  moduleFileExtensions: ['js', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@dcloudio/(.*?)$': '<rootDir>/packages/$1'
+  },
+  rootDir: __dirname,
+  testMatch: ['<rootDir>/packages/**/__tests__/**/*spec.(t|j)s']
 }
+
