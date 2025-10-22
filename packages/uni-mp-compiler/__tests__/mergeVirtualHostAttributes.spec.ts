6e1578f8a3affb3aa65c4afa4092aa35e6856af2
chouchouji <1305974212@qq.com>
2025-10-22 18:01:05 +0800

    test(x-mp-weixin): 更新测试用例

--- packages/uni-mp-compiler/__tests__/mergeVirtualHostAttributes.spec.ts
+++ packages/uni-mp-compiler/__tests__/mergeVirtualHostAttributes.spec.ts
@@ -246,5 +246,14 @@ describe('complier: options with mergeVirtualHostAttributes', () => {
 }`,
       options
     )
+    assert(
+      `<custom-view><view/></custom-view>`,
+      `<custom-view u-s=\"{{['d']}}\" style=\"{{'--status-bar-height:' + b + ';' + (virtualHostStyle || '')}}\" virtualHostStyle=\"{{'--status-bar-height:' + b + ';' + (virtualHostStyle || '')}}\" u-i=\"2a9ec0b0-0\" id=\"{{a}}\" virtualHostId=\"{{a}}\" u-p=\"{{c||''}}\" class=\"{{[virtualHostClass]}}\" virtualHostClass=\"{{[virtualHostClass]}}\" hidden=\"{{virtualHostHidden || false}}\" virtualHostHidden=\"{{virtualHostHidden || false}}\"><view/></custom-view>`,
+      `(_ctx, _cache) => { "raw js"
+  const __returned__ = { a: _gei(_ctx, ''), b: \`\${_ctx.u_s_b_h}px\`, c: _p({ id: _gei(_ctx, '') }) }
+  return __returned__
+}`,
+      optionsX
+    )
   })
 })
