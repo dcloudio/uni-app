05c6fb69fa4c2735e81742ab4e1c59402441a9b6
wangyaqi <wangyaqi988@163.com>
2025-10-24 17:05:57 +0800

    feat: 支付宝云服务空间增加endpoint初始化参数

--- packages/uni-cloud/src/uni.plugin.ts
+++ packages/uni-cloud/src/uni.plugin.ts
@@ -177,6 +177,7 @@ function initUniCloudEnv() {
               spaceAppId: space.spaceAppId,
               accessKey: space.accessKey,
               secretKey: space.secretKey,
+              endpoint: space.apiEndpoint,
             }
           }
           case 'tencent':
