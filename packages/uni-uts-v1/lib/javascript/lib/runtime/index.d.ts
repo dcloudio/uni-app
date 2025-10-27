78ec229930cce29d167bc46b94b3f6b1ee58e74c
wangyaqi <wangyaqi988@163.com>
2025-10-27 16:41:32 +0800

    chore: 同步uts相关代码

--- packages/uni-uts-v1/lib/javascript/lib/runtime/index.d.ts
+++ packages/uni-uts-v1/lib/javascript/lib/runtime/index.d.ts
@@ -1,16 +1,3 @@
-import { UniError } from '@dcloudio/uni-shared';
-import { UTS } from '@dcloudio/uni-shared';
-import { UTSJSONObject } from '@dcloudio/uni-shared';
-import { UTSValueIterable } from '@dcloudio/uni-shared';
-
-export { UniError }
-
-export { UTS }
-
-export { UTSJSONObject }
-
-export { UTSValueIterable }
-
 
 export * from "tslib";
 
