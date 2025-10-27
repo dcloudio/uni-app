a065dc278e33d1838f9f5ac293d3d7e86fb09207
chouchouji <1305974212@qq.com>
2025-10-27 14:35:20 +0800

    fix(mp-weixin): 过滤空 class

--- packages/uni-mp-vue/src/helpers/virtualHost.ts
+++ packages/uni-mp-vue/src/helpers/virtualHost.ts
@@ -1,10 +1,14 @@
 export function parseVirtualHostClass(className: string | string[]) {
   if (Array.isArray(className)) {
-    return className.map((name) => `^${name}`).join(' ')
+    return className
+      .filter(Boolean)
+      .map((name) => `^${name}`)
+      .join(' ')
   }
   if (typeof className === 'string') {
     return className
       .split(/\s+/)
+      .filter(Boolean)
       .map((name) => `^${name}`)
       .join(' ')
   }
