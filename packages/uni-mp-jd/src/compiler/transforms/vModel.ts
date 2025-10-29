cdd975c3800fa5662014394d19ea029968eca1df
chouchouji <1305974212@qq.com>
2025-10-29 14:35:52 +0800

    fix: update v-model match function to use type assertion for ComponentNode

--- packages/uni-mp-jd/src/compiler/transforms/vModel.ts
+++ packages/uni-mp-jd/src/compiler/transforms/vModel.ts
@@ -1,10 +1,12 @@
 import { createTransformModel } from '@dcloudio/uni-cli-shared'
 import { transformModel as baseTransformModel } from '@dcloudio/uni-mp-compiler'
+import type { ComponentNode } from '@vue/compiler-core'
+
 /**
  * 京东小程序 input 事件不支持动态事件，故 v-model 也需要调整
  */
 export const transformModel = createTransformModel(baseTransformModel, {
-  match: (node, context) => {
+  match: (node): node is ComponentNode => {
     if (node.tag === 'input' || node.tag === 'textarea') {
       return true
     }
--- packages/uni-mp-kuaishou/src/compiler/transforms/vModel.ts
+++ packages/uni-mp-kuaishou/src/compiler/transforms/vModel.ts
@@ -3,11 +3,13 @@ import {
   matchTransformModel,
 } from '@dcloudio/uni-cli-shared'
 import { transformModel as baseTransformModel } from '@dcloudio/uni-mp-compiler'
+import type { ComponentNode } from '@vue/compiler-core'
+
 /**
  * 快手小程序的自定义组件，不支持动态事件绑定，故 v-model 也需要调整，其中 input、textarea 也不支持
  */
 export const transformModel = createTransformModel(baseTransformModel, {
-  match: (node, context) => {
+  match: (node, context): node is ComponentNode => {
     if (node.tag === 'input' || node.tag === 'textarea') {
       return true
     }
