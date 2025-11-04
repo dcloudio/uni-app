2585b5093e1ebfcc12f2792b928c7dfd7df69577
DCloud_LXH <283700113@qq.com>
2025-11-04 19:05:35 +0800

    chore: syncExtComponentFile

--- scripts/syncExtComponentFile.js
+++ scripts/syncExtComponentFile.js
@@ -41,82 +41,87 @@ function syncExtComponentFile(apiDirs) {
     var uniComponentsVuePath = path_1.default.resolve(uniComponentsPath, './src/vue');
     var uniComponentsLibXPath = path_1.default.resolve(uniComponentsPath, './lib-x');
     var uniComponentsStyleXPath = path_1.default.resolve(uniComponentsPath, './style-x');
-    apiDirs.forEach(function (apiDir) {
-        var extComponentDir = apiDir;
-        Object.keys(syncExtComponents).forEach(function (componentName) {
-            (0, fast_glob_1.sync)(path_1.default.join(extComponentDir, "./".concat(componentName, "/package.json"))).forEach(function (packageJsonPath) {
-                var packageJson = fs_extra_1.default.readJsonSync(packageJsonPath, { encoding: 'utf-8' });
-                var componentOptions = syncExtComponents[componentName];
-                var platforms = Object.keys(componentOptions);
-                platforms.forEach(function (platform) {
-                    var _a;
-                    var syncExtComponentOption = componentOptions[platform];
-                    if (((_a = packageJson['uni_modules']) === null || _a === void 0 ? void 0 : _a.components[platform]) === true && syncExtComponentOption) {
-                        var componentPath_1 = path_1.default.resolve(packageJsonPath, '../components');
-                        (0, fast_glob_1.sync)(path_1.default.join(componentPath_1, '**')).forEach(function (filePath) {
-                            var _a, _b, _c, _d, _e, _f, _g, _h, _j;
-                            var _k = path_1.default.parse(path_1.default.relative(componentPath_1, filePath)), buildInComponentName = _k.name, ext = _k.ext, dir = _k.dir;
-                            switch (ext) {
-                                case '.vue': {
-                                    var originCode = fs_extra_1.default.readFileSync(filePath, { encoding: 'utf-8' });
-                                    if (syncExtComponentOption.name) {
-                                        var vueCode = (0, uni_preprocess_1.preprocess)((0, uni_preprocess_1.preprocess)(originCode, { type: 'html', context: syncExtComponentOption.preContext }).code, { type: 'js', context: syncExtComponentOption.preContext }).code;
-                                        var sfcParseResult = (0, compiler_sfc_1.parse)(vueCode);
-                                        if (sfcParseResult.errors.length) {
-                                            console.error("[uni-h5 (syncExtComponentFile)] ".concat(componentName, " parse ").concat(filePath, " error:"), sfcParseResult.errors);
-                                            return;
-                                        }
-                                        else {
-                                            var descriptor = sfcParseResult.descriptor;
-                                            var scriptSetupContent = ((_a = descriptor.scriptSetup) === null || _a === void 0 ? void 0 : _a.content) || '';
-                                            var scriptContent = ((_b = descriptor.script) === null || _b === void 0 ? void 0 : _b.content) || '';
-                                            var parseScriptContent = { import: '', content: '' };
-                                            if (scriptSetupContent.length) {
-                                                var regex = /^\s*(import[\s\S]*?from\s+['"][\s\S]*?['"];?)/gm;
-                                                var imports = [];
-                                                var execResult = void 0;
-                                                while (execResult = regex.exec(scriptSetupContent)) {
-                                                    var importStr = execResult[1];
-                                                    imports.push(importStr);
-                                                    scriptSetupContent = scriptSetupContent.replace(execResult[0], '');
-                                                    regex.lastIndex = 0;
+    try {
+        apiDirs.forEach(function (apiDir) {
+            var extComponentDir = apiDir;
+            Object.keys(syncExtComponents).forEach(function (componentName) {
+                (0, fast_glob_1.sync)(path_1.default.join(extComponentDir, "./".concat(componentName, "/package.json"))).forEach(function (packageJsonPath) {
+                    var packageJson = fs_extra_1.default.readJsonSync(packageJsonPath, { encoding: 'utf-8' });
+                    var componentOptions = syncExtComponents[componentName];
+                    var platforms = Object.keys(componentOptions);
+                    platforms.forEach(function (platform) {
+                        var _a;
+                        var syncExtComponentOption = componentOptions[platform];
+                        if (((_a = packageJson['uni_modules']) === null || _a === void 0 ? void 0 : _a.components[platform]) === true && syncExtComponentOption) {
+                            var componentPath_1 = path_1.default.resolve(packageJsonPath, '../components');
+                            (0, fast_glob_1.sync)(path_1.default.join(componentPath_1, '**')).forEach(function (filePath) {
+                                var _a, _b, _c, _d, _e, _f, _g, _h, _j;
+                                var _k = path_1.default.parse(path_1.default.relative(componentPath_1, filePath)), buildInComponentName = _k.name, ext = _k.ext, dir = _k.dir;
+                                switch (ext) {
+                                    case '.vue': {
+                                        var originCode = fs_extra_1.default.readFileSync(filePath, { encoding: 'utf-8' });
+                                        if (syncExtComponentOption.name) {
+                                            var vueCode = (0, uni_preprocess_1.preprocess)((0, uni_preprocess_1.preprocess)(originCode, { type: 'html', context: syncExtComponentOption.preContext }).code, { type: 'js', context: syncExtComponentOption.preContext }).code;
+                                            var sfcParseResult = (0, compiler_sfc_1.parse)(vueCode);
+                                            if (sfcParseResult.errors.length) {
+                                                console.error("[uni-h5 (syncExtComponentFile)] ".concat(componentName, " parse ").concat(filePath, " error:"), sfcParseResult.errors);
+                                                return;
+                                            }
+                                            else {
+                                                var descriptor = sfcParseResult.descriptor;
+                                                var scriptSetupContent = ((_a = descriptor.scriptSetup) === null || _a === void 0 ? void 0 : _a.content) || '';
+                                                var scriptContent = ((_b = descriptor.script) === null || _b === void 0 ? void 0 : _b.content) || '';
+                                                var parseScriptContent = { import: '', content: '' };
+                                                if (scriptSetupContent.length) {
+                                                    var regex = /^\s*(import[\s\S]*?from\s+['"][\s\S]*?['"];?)/gm;
+                                                    var imports = [];
+                                                    var execResult = void 0;
+                                                    while (execResult = regex.exec(scriptSetupContent)) {
+                                                        var importStr = execResult[1];
+                                                        imports.push(importStr);
+                                                        scriptSetupContent = scriptSetupContent.replace(execResult[0], '');
+                                                        regex.lastIndex = 0;
+                                                    }
+                                                    parseScriptContent.import = imports.join('\n');
+                                                    parseScriptContent.content = scriptSetupContent;
                                                 }
-                                                parseScriptContent.import = imports.join('\n');
-                                                parseScriptContent.content = scriptSetupContent;
+                                                else if (scriptContent.length) {
+                                                    parseScriptContent.content = scriptContent;
+                                                }
+                                                var finalVueCode = createVueSFCCode(syncExtComponentOption.name, ((_c = descriptor.template) === null || _c === void 0 ? void 0 : _c.content) || '', parseScriptContent);
+                                                fs_extra_1.default.outputFileSync(path_1.default.resolve(uniComponentsVuePath, "".concat((_d = syncExtComponentOption.dir) !== null && _d !== void 0 ? _d : dir, "/").concat((_e = syncExtComponentOption.index) !== null && _e !== void 0 ? _e : buildInComponentName, ".vue")), finalVueCode);
+                                                fs_extra_1.default.outputFileSync(path_1.default.resolve(uniComponentsStyleXPath, "".concat((_f = syncExtComponentOption.css) !== null && _f !== void 0 ? _f : buildInComponentName, ".css")), descriptor.styles.map(function (styleBlock) { return styleBlock.content; }).join('\n'));
                                             }
-                                            else if (scriptContent.length) {
-                                                parseScriptContent.content = scriptContent;
-                                            }
-                                            var finalVueCode = createVueSFCCode(syncExtComponentOption.name, ((_c = descriptor.template) === null || _c === void 0 ? void 0 : _c.content) || '', parseScriptContent);
-                                            fs_extra_1.default.outputFileSync(path_1.default.resolve(uniComponentsVuePath, "".concat((_d = syncExtComponentOption.dir) !== null && _d !== void 0 ? _d : dir, "/").concat((_e = syncExtComponentOption.index) !== null && _e !== void 0 ? _e : buildInComponentName, ".vue")), finalVueCode);
-                                            fs_extra_1.default.outputFileSync(path_1.default.resolve(uniComponentsStyleXPath, "".concat((_f = syncExtComponentOption.css) !== null && _f !== void 0 ? _f : buildInComponentName, ".css")), descriptor.styles.map(function (styleBlock) { return styleBlock.content; }).join('\n'));
                                         }
+                                        // libX 暂只支持 .vue 文件
+                                        var libX = syncExtComponentOption.libX;
+                                        if (libX) {
+                                            var vueCode = (0, uni_preprocess_1.preprocess)((0, uni_preprocess_1.preprocess)(originCode, { type: 'html', context: libX.preContext }).code, { type: 'js', context: libX.preContext }).code;
+                                            fs_extra_1.default.outputFileSync(path_1.default.resolve(uniComponentsLibXPath, "".concat((_g = libX.dir) !== null && _g !== void 0 ? _g : buildInComponentName, "/").concat((_h = libX.index) !== null && _h !== void 0 ? _h : buildInComponentName, ".vue")), vueCode);
+                                        }
+                                        break;
                                     }
-                                    // libX 暂只支持 .vue 文件
-                                    var libX = syncExtComponentOption.libX;
-                                    if (libX) {
-                                        var vueCode = (0, uni_preprocess_1.preprocess)((0, uni_preprocess_1.preprocess)(originCode, { type: 'html', context: libX.preContext }).code, { type: 'js', context: libX.preContext }).code;
-                                        fs_extra_1.default.outputFileSync(path_1.default.resolve(uniComponentsLibXPath, "".concat((_g = libX.dir) !== null && _g !== void 0 ? _g : buildInComponentName, "/").concat((_h = libX.index) !== null && _h !== void 0 ? _h : buildInComponentName, ".vue")), vueCode);
+                                    case '.ts':
+                                    case '.js':
+                                    case '.uts': {
+                                        var _l = path_1.default.parse(filePath), name_1 = _l.name, ext_1 = _l.ext;
+                                        var originCode = fs_extra_1.default.readFileSync(filePath, { encoding: 'utf-8' });
+                                        var preCode = (0, uni_preprocess_1.preprocess)(originCode, {
+                                            type: 'js',
+                                            context: syncExtComponentOption.preContext
+                                        }).code;
+                                        fs_extra_1.default.outputFileSync(path_1.default.resolve(uniComponentsVuePath, "".concat((_j = syncExtComponentOption.dir) !== null && _j !== void 0 ? _j : dir, "/").concat(name_1, ".ts")), preCode);
+                                        break;
                                     }
-                                    break;
                                 }
-                                case '.ts':
-                                case '.js':
-                                case '.uts': {
-                                    var _l = path_1.default.parse(filePath), name_1 = _l.name, ext_1 = _l.ext;
-                                    var originCode = fs_extra_1.default.readFileSync(filePath, { encoding: 'utf-8' });
-                                    var preCode = (0, uni_preprocess_1.preprocess)(originCode, {
-                                        type: 'js',
-                                        context: syncExtComponentOption.preContext
-                                    }).code;
-                                    fs_extra_1.default.outputFileSync(path_1.default.resolve(uniComponentsVuePath, "".concat((_j = syncExtComponentOption.dir) !== null && _j !== void 0 ? _j : dir, "/").concat(name_1, ".ts")), preCode);
-                                    break;
-                                }
-                            }
-                        });
-                    }
+                            });
+                        }
+                    });
                 });
             });
         });
-    });
+    }
+    catch (error) {
+        console.error('[uni-h5 (syncExtComponentFile)] sync ext component file error:', error);
+    }
 }
--- scripts/syncExtComponentFile.ts
+++ scripts/syncExtComponentFile.ts
@@ -65,94 +65,97 @@ export function syncExtComponentFile(apiDirs: string[]) {
   const uniComponentsVuePath = path.resolve(uniComponentsPath, './src/vue')
   const uniComponentsLibXPath = path.resolve(uniComponentsPath, './lib-x')
   const uniComponentsStyleXPath = path.resolve(uniComponentsPath, './style-x')
-  apiDirs.forEach((apiDir) => {
-    const extComponentDir = apiDir
-    Object.keys(syncExtComponents).forEach(componentName => {
-      sync(path.join(extComponentDir, `./${componentName}/package.json`)).forEach(packageJsonPath => {
-        const packageJson = fs.readJsonSync(packageJsonPath, { encoding: 'utf-8' })
+  try {
+    apiDirs.forEach((apiDir) => {
+      const extComponentDir = apiDir
+      Object.keys(syncExtComponents).forEach(componentName => {
+        sync(path.join(extComponentDir, `./${componentName}/package.json`)).forEach(packageJsonPath => {
+          const packageJson = fs.readJsonSync(packageJsonPath, { encoding: 'utf-8' })
 
-        const componentOptions = syncExtComponents[componentName]
-        const platforms = Object.keys(componentOptions)
-        platforms.forEach(platform => {
-          let syncExtComponentOption = componentOptions[platform as 'web' | 'mp-weixin']
-          if (packageJson['uni_modules']?.components[platform] === true && syncExtComponentOption) {
+          const componentOptions = syncExtComponents[componentName]
+          const platforms = Object.keys(componentOptions)
+          platforms.forEach(platform => {
+            let syncExtComponentOption = componentOptions[platform as 'web' | 'mp-weixin']
+            if (packageJson['uni_modules']?.components[platform] === true && syncExtComponentOption) {
 
-            const componentPath = path.resolve(packageJsonPath, '../components')
-            sync(path.join(componentPath, '**')).forEach(filePath => {
-              const { name: buildInComponentName, ext, dir } = path.parse(path.relative(componentPath, filePath))
-              switch (ext) {
-                case '.vue': {
-                  const originCode = fs.readFileSync(filePath, { encoding: 'utf-8' })
-                  if (syncExtComponentOption.name) {
-                    let vueCode = preprocess(
-                      preprocess(originCode, { type: 'html', context: syncExtComponentOption.preContext }).code,
-                      { type: 'js', context: syncExtComponentOption.preContext }
-                    ).code
-                    const sfcParseResult = parse(vueCode)
-                    if (sfcParseResult.errors.length) {
-                      console.error(`[uni-h5 (syncExtComponentFile)] ${componentName} parse ${filePath} error:`, sfcParseResult.errors)
-                      return
-                    } else {
-                      const descriptor = sfcParseResult.descriptor
-                      let scriptSetupContent = descriptor.scriptSetup?.content || ''
-                      let scriptContent = descriptor.script?.content || ''
-                      const parseScriptContent: ScriptContent = { import: '', content: '' }
-                      if (scriptSetupContent.length) {
-                        const regex = /^\s*(import[\s\S]*?from\s+['"][\s\S]*?['"];?)/gm
+              const componentPath = path.resolve(packageJsonPath, '../components')
+              sync(path.join(componentPath, '**')).forEach(filePath => {
+                const { name: buildInComponentName, ext, dir } = path.parse(path.relative(componentPath, filePath))
+                switch (ext) {
+                  case '.vue': {
+                    const originCode = fs.readFileSync(filePath, { encoding: 'utf-8' })
+                    if (syncExtComponentOption.name) {
+                      let vueCode = preprocess(
+                        preprocess(originCode, { type: 'html', context: syncExtComponentOption.preContext }).code,
+                        { type: 'js', context: syncExtComponentOption.preContext }
+                      ).code
+                      const sfcParseResult = parse(vueCode)
+                      if (sfcParseResult.errors.length) {
+                        console.error(`[uni-h5 (syncExtComponentFile)] ${componentName} parse ${filePath} error:`, sfcParseResult.errors)
+                        return
+                      } else {
+                        const descriptor = sfcParseResult.descriptor
+                        let scriptSetupContent = descriptor.scriptSetup?.content || ''
+                        let scriptContent = descriptor.script?.content || ''
+                        const parseScriptContent: ScriptContent = { import: '', content: '' }
+                        if (scriptSetupContent.length) {
+                          const regex = /^\s*(import[\s\S]*?from\s+['"][\s\S]*?['"];?)/gm
 
-                        const imports = []
-                        let execResult
-                        while (execResult = regex.exec(scriptSetupContent)) {
-                          const importStr = execResult[1]
-                          imports.push(importStr)
-                          scriptSetupContent = scriptSetupContent.replace(execResult[0], '')
-                          regex.lastIndex = 0
+                          const imports = []
+                          let execResult
+                          while (execResult = regex.exec(scriptSetupContent)) {
+                            const importStr = execResult[1]
+                            imports.push(importStr)
+                            scriptSetupContent = scriptSetupContent.replace(execResult[0], '')
+                            regex.lastIndex = 0
+                          }
+                          parseScriptContent.import = imports.join('\n')
+                          parseScriptContent.content = scriptSetupContent
+                        } else if (scriptContent.length) {
+                          parseScriptContent.content = scriptContent
                         }
-                        parseScriptContent.import = imports.join('\n')
-                        parseScriptContent.content = scriptSetupContent
-                      } else if (scriptContent.length) {
-                        parseScriptContent.content = scriptContent
+                        const finalVueCode = createVueSFCCode(syncExtComponentOption.name!, descriptor.template?.content || '', parseScriptContent)
+                        fs.outputFileSync(path.resolve(uniComponentsVuePath, `${syncExtComponentOption.dir ?? dir}/${syncExtComponentOption.index ?? buildInComponentName}.vue`), finalVueCode)
+                        fs.outputFileSync(path.resolve(uniComponentsStyleXPath, `${syncExtComponentOption.css ?? buildInComponentName}.css`), descriptor.styles.map(styleBlock => styleBlock.content).join('\n'))
                       }
-                      const finalVueCode = createVueSFCCode(syncExtComponentOption.name!, descriptor.template?.content || '', parseScriptContent)
-                      fs.outputFileSync(path.resolve(uniComponentsVuePath, `${syncExtComponentOption.dir ?? dir}/${syncExtComponentOption.index ?? buildInComponentName}.vue`), finalVueCode)
-                      fs.outputFileSync(path.resolve(uniComponentsStyleXPath, `${syncExtComponentOption.css ?? buildInComponentName}.css`), descriptor.styles.map(styleBlock => styleBlock.content).join('\n'))
                     }
+                    // libX 暂只支持 .vue 文件
+                    const libX = syncExtComponentOption.libX
+                    if (libX) {
+                      let vueCode = preprocess(
+                        preprocess(originCode, { type: 'html', context: libX.preContext }).code,
+                        { type: 'js', context: libX.preContext }
+                      ).code
+                      fs.outputFileSync(path.resolve(uniComponentsLibXPath, `${libX.dir ?? buildInComponentName}/${libX.index ?? buildInComponentName}.vue`), vueCode)
+                    }
+                    break;
                   }
-                  // libX 暂只支持 .vue 文件
-                  const libX = syncExtComponentOption.libX
-                  if (libX) {
-                    let vueCode = preprocess(
-                      preprocess(originCode, { type: 'html', context: libX.preContext }).code,
-                      { type: 'js', context: libX.preContext }
+                  case '.ts':
+                  case '.js':
+                  case '.uts': {
+                    const { name, ext } = path.parse(filePath)
+                    const originCode = fs.readFileSync(filePath, { encoding: 'utf-8' })
+                    const preCode = preprocess(
+                      originCode,
+                      {
+                        type: 'js',
+                        context: syncExtComponentOption.preContext
+                      }
                     ).code
-                    fs.outputFileSync(path.resolve(uniComponentsLibXPath, `${libX.dir ?? buildInComponentName}/${libX.index ?? buildInComponentName}.vue`), vueCode)
+                    fs.outputFileSync(
+                      path.resolve(uniComponentsVuePath, `${syncExtComponentOption.dir ?? dir}/${name}.ts`),
+                      preCode
+                    )
+                    break;
                   }
-                  break;
                 }
-                case '.ts':
-                case '.js':
-                case '.uts': {
-                  const { name, ext } = path.parse(filePath)
-                  const originCode = fs.readFileSync(filePath, { encoding: 'utf-8' })
-                  const preCode = preprocess(
-                    originCode,
-                    {
-                      type: 'js',
-                      context: syncExtComponentOption.preContext
-                    }
-                  ).code
-                  fs.outputFileSync(
-                    path.resolve(uniComponentsVuePath, `${syncExtComponentOption.dir ?? dir}/${name}.ts`),
-                    preCode
-                  )
-                  break;
-                }
-              }
-            })
-          }
+              })
+            }
+          })
         })
       })
     })
-
-  })
+  } catch (error) {
+    console.error('[uni-h5 (syncExtComponentFile)] sync ext component file error:', error)
+  }
 }
