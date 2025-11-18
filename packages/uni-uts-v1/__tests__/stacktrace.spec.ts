a74745aff90d7b3a29dfdcef212e0013f7df8c76
wangyaqi <wangyaqi988@163.com>
2025-11-18 16:52:37 +0800

    fix: 移除未使用的导入

--- packages/uni-uts-v1/__tests__/stacktrace.spec.ts
+++ packages/uni-uts-v1/__tests__/stacktrace.spec.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import {
   parseCompileStacktrace,
-  parseCppStacktrace,
+  // parseCppStacktrace,
   parseUTSArkTSPluginStacktrace,
   parseUTSKotlinStacktrace,
   parseUTSSwiftPluginStacktrace,
