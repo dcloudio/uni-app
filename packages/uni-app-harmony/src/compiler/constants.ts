eacacffefeae73a2a7ba5d2d1f6d0c76e5bd87db
DCloud_LXH <283700113@qq.com>
2025-12-08 16:43:12 +0800

    feat(harmony): dom2 编译 uni-video module

--- packages/uni-app-harmony/src/compiler/constants.ts
+++ packages/uni-app-harmony/src/compiler/constants.ts
@@ -8,7 +8,6 @@ export const ExtApiBlackListX = ['uni-pullDownRefresh']
 export const ExtApiBlackListDom2 = [
   ...ExtApiBlackListX,
   'uni-createWebviewContext',
-  'uni-video',
 ]
 export const ExtApiBlackList = [
   'uni-loadFontFace',
