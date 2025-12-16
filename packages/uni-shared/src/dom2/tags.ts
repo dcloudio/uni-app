21789419084fcc41c5754e3d472ec4a81987dd3d
fxy060608 <fxy060608@gmail.com>
2025-12-16 11:39:30 +0800

    wip(dom2): 新增 nested-scroll-header、nested-scroll-body

--- packages/uni-shared/src/dom2/tags.ts
+++ packages/uni-shared/src/dom2/tags.ts
@@ -1,4 +1,12 @@
-const APP_NATIVE_TAGS = ['view', 'text', 'image', 'scroll-view', 'native-view']
+const APP_NATIVE_TAGS = [
+  'view',
+  'text',
+  'image',
+  'scroll-view',
+  'native-view',
+  'nested-scroll-header',
+  'nested-scroll-body',
+]
 export function isDom2AppHarmonyNativeTag(tag: string) {
   return APP_NATIVE_TAGS.includes(tag)
 }
