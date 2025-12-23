44f70b1fb8b845f487842e343fe6a32dd959d99a
uni <uni@uni-macmini.local>
2025-12-23 13:58:54 +0800

    chore(dev): update framework #0

--- packages/uni-app-vue/dist/uvue.runtime.esm.prod.js
+++ packages/uni-app-vue/dist/uvue.runtime.esm.prod.js
@@ -4145,6 +4145,8 @@ extend(/* @__PURE__ */Object.create(null), {
   $refs: i => i.refs,
   $parent: i => getPublicInstance(i.parent),
   $root: i => getPublicInstance(i.root),
+  // fixed by xxxxxx
+  $page: i => i.page,
   $emit: i => i.emit,
   $options: i => resolveMergedOptions(i),
   $forceUpdate: i => i.f || (i.f = () => {
@@ -7345,6 +7347,9 @@ function createComponentInstance(vnode, parent, suspense) {
   }
   instance.root = parent ? parent.root : instance;
   instance.emit = emit.bind(null, instance);
+  if (parent) {
+    instance.page = parent.root.page;
+  }
   if (vnode.ce) {
     vnode.ce(instance);
   }
