4b0afd95bf76b6972f9e1afe545e0e6450d86bb7
DCloud_LXH <283700113@qq.com>
2025-12-26 19:22:31 +0800

    fix(web): 手机连接 wifi 在鸿蒙 Web 组件中 getNetworkType 获取为 none

--- src/platforms/h5/service/api/device/network-info.js
+++ src/platforms/h5/service/api/device/network-info.js
@@ -3,6 +3,7 @@ const {
 } = UniServiceJSBridge
 
 const callbackIds = []
+const NONE = 'none'
 
 function changeHandler () {
   const {
@@ -11,7 +12,7 @@ function changeHandler () {
   callbackIds.forEach(callbackId => {
     invoke(callbackId, {
       errMsg: 'onNetworkStatusChange:ok',
-      isConnected: networkType !== 'none',
+      isConnected: networkType !== NONE,
       networkType
     })
   })
@@ -51,14 +52,19 @@ export function getNetworkType () {
   const connection = navigator.connection || navigator.webkitConnection
   let networkType = 'unknown'
   if (connection) {
+    const effectiveType = connection.effectiveType
     networkType = connection.type
-    if (networkType === 'cellular' && connection.effectiveType) {
-      networkType = connection.effectiveType.replace('slow-', '')
-    } else if (!['none', 'wifi'].includes(networkType)) {
+    if (networkType === 'cellular' && effectiveType) {
+      networkType = effectiveType.replace('slow-', '')
+    } else if ((!networkType || networkType === NONE) && effectiveType) {
+      // mac/ios 不支持，h5 模式下可能会因为用户隐私的原因，无法获取到具体网络状态
+      // 在鸿蒙 Web 组件中 networkType 为 none 但是 effectiveType 有值
+      networkType = effectiveType
+    } else if (![NONE, 'wifi'].includes(networkType)) {
       networkType = 'unknown'
     }
   } else if (navigator.onLine === false) {
-    networkType = 'none'
+    networkType = NONE
   }
   return {
     errMsg: 'getNetworkType:ok',
