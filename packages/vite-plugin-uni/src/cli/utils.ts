d0c90f932d508baf816d17e89fc51f1559dbbb15
fxy060608 <fxy060608@gmail.com>
2026-01-09 15:01:43 +0800

    feat(dom2): 调整 styleIsolationVersion 位置

--- packages/vite-plugin-uni/src/cli/utils.ts
+++ packages/vite-plugin-uni/src/cli/utils.ts
@@ -248,7 +248,7 @@ export function initEnv(
     ) {
       process.env.UNI_APP_X_TSC = 'false'
     }
-    if (manifestJson['styleIsolationVersion'] === '2') {
+    if (manifestJson['uni-app-x']?.['styleIsolationVersion'] === '2') {
       process.env.UNI_APP_STYLE_ISOLATION_VERSION = '2'
     }
   } catch (e) {}
