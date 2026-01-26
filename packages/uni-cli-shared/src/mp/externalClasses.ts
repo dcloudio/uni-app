6c2fd535f4e3af03d74f2d935355c068a005f9f4
schizobulia <2833324528@qq.com>
2026-01-26 18:09:30 +0800

    feat: 优化externalClass的生成逻辑，静态ExternalClasses时只给对应class添加page

--- packages/uni-cli-shared/src/mp/externalClasses.ts
+++ packages/uni-cli-shared/src/mp/externalClasses.ts
@@ -13,6 +13,49 @@ const externalClassesCache = new Map<
   { mtime: number; classes: string[] }
 >()
 
+/**
+ * 页面使用的 externalClasses 信息
+ * - staticClasses: 静态绑定的 class 值集合，如 my-class="foo" 中的 "foo"
+ * - hasDynamic: 是否存在动态绑定，如 :my-class="bar"
+ */
+export interface PageExternalClassesInfo {
+  staticClasses: Set<string>
+  hasDynamic: boolean
+}
+
+const pageExternalClassesCache = new Map<string, PageExternalClassesInfo>()
+
+export function findPageExternalClasses(filename: string) {
+  return pageExternalClassesCache.get(filename)
+}
+
+export function updatePageExternalClasses(
+  filename: string,
+  info: PageExternalClassesInfo
+) {
+  pageExternalClassesCache.set(filename, info)
+}
+
+export function addPageExternalClasses(
+  filename: string,
+  staticClasses: string[],
+  hasDynamic: boolean
+) {
+  let info = pageExternalClassesCache.get(filename)
+  if (!info) {
+    info = { staticClasses: new Set(), hasDynamic: false }
+    pageExternalClassesCache.set(filename, info)
+  }
+  staticClasses.forEach((cls) => info!.staticClasses.add(cls))
+  if (hasDynamic) {
+    info.hasDynamic = true
+  }
+}
+
+export function clearPageExternalClasses(filename: string) {
+  pageExternalClassesCache.delete(filename)
+}
+
 export function hasExternalClasses(code: string) {
   return code.includes('externalClasses')
 }
