a575fecf2c02e7d73e98334a248c4eaf50b6fbe4
fxy060608 <fxy060608@gmail.com>
2026-01-30 19:31:13 +0800

    fix(x-mp): 修复 全局样式对组件可能不生效的bug

--- packages/uni-mp-vue/src/helpers/virtualHost.ts
+++ packages/uni-mp-vue/src/helpers/virtualHost.ts
@@ -1,18 +1,29 @@
-import { isArray, isString } from '@vue/shared'
+import { normalizeClass } from '@dcloudio/uni-shared'
+
+function patchClassList(classList: string[]) {
+  const patchedClassList: string[] = []
+  classList.forEach((className) => {
+    className = className.trim()
+    if (!className) {
+      return
+    }
+    if (__X_STYLE_ISOLATION__) {
+      // 需要兼容原始类名，因为该class可能是全局样式定义的，当组件配置为app时，目前的方案
+      // 是底层配置isolated+@import app.wxss来实现的，需要兼容这种情况
+      const originalClassName = className.replace(/^\^/g, '')
+      if (!patchedClassList.includes(originalClassName)) {
+        patchedClassList.push(originalClassName)
+      }
+    }
+    const patchedClassName = '^' + className
+    if (!patchedClassList.includes(patchedClassName)) {
+      patchedClassList.push(patchedClassName)
+    }
+  })
+  return patchedClassList
+}
 
 export function parseVirtualHostClass(className: string | string[]) {
-  if (isArray(className)) {
-    return className
-      .filter(Boolean)
-      .map((name) => `^${name}`)
-      .join(' ')
-  }
-  if (isString(className)) {
-    return className
-      .split(/\s+/)
-      .filter(Boolean)
-      .map((name) => `^${name}`)
-      .join(' ')
-  }
-  return ''
+  className = normalizeClass(className)
+  return patchClassList(className.split(/\s+/)).join(' ')
 }
