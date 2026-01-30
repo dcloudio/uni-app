ae1863cb9e90333572dbfb5dcdd0787073ad7f20
uni@uni-macmini.local <uni@uni-macmini.local>
2026-01-30 20:06:02 +0800

    chore(alpha): update framework #0

--- packages/uni-mp-vue/dist-x/vue.runtime.esm.js
+++ packages/uni-mp-vue/dist-x/vue.runtime.esm.js
@@ -6683,21 +6683,31 @@ function genUniElementId(_ctx, idBinding, genId) {
     return genIdWithVirtualHost(_ctx, idBinding) || genId || '';
 }
 
+function patchClassList(classList) {
+    const patchedClassList = [];
+    classList.forEach((className) => {
+        className = className.trim();
+        if (!className) {
+            return;
+        }
+        if (__X_STYLE_ISOLATION__) {
+            // 需要兼容原始类名，因为该class可能是全局样式定义的，当组件配置为app时，目前的方案
+            // 是底层配置isolated+@import app.wxss来实现的，需要兼容这种情况
+            const originalClassName = className.replace(/^\^/g, '');
+            if (!patchedClassList.includes(originalClassName)) {
+                patchedClassList.push(originalClassName);
+            }
+        }
+        const patchedClassName = '^' + className;
+        if (!patchedClassList.includes(patchedClassName)) {
+            patchedClassList.push(patchedClassName);
+        }
+    });
+    return patchedClassList;
+}
 function parseVirtualHostClass(className) {
-    if (isArray(className)) {
-        return className
-            .filter(Boolean)
-            .map((name) => `^${name}`)
-            .join(' ');
-    }
-    if (isString(className)) {
-        return className
-            .split(/\s+/)
-            .filter(Boolean)
-            .map((name) => `^${name}`)
-            .join(' ');
-    }
-    return '';
+    className = normalizeClass(className);
+    return patchClassList(className.split(/\s+/)).join(' ');
 }
 
 function setupDevtoolsPlugin() {
