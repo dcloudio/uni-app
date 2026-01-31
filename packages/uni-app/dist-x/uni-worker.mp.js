23b670581d63435d1aa7a9095d641e2522d8f400
uni@uni-macmini.local <uni@uni-macmini.local>
2026-01-31 11:18:53 +0800

    chore(alpha): update framework #0

--- packages/uni-app/dist-x/uni-worker.mp.js
+++ packages/uni-app/dist-x/uni-worker.mp.js
@@ -213,6 +213,10 @@
         }
         constructor() { }
         static initProps(options, metadata, isJSONParse = false) {
+            // 为了性能，非JSON.parse场景直接返回
+            if (!isJSONParse) {
+                return options;
+            }
             const obj = {};
             if (!metadata.fields) {
                 return obj;
