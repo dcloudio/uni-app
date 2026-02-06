287eabbb50058784cf109090a9cc792a5ef1b332
chouchouji <1305974212@qq.com>
2026-02-06 20:42:12 +0800

    refactor(mp-toutiao): 添加条件编译

--- packages/uni-mp-toutiao/src/runtime/componentLifetimes.ts
+++ packages/uni-mp-toutiao/src/runtime/componentLifetimes.ts
@@ -59,25 +59,27 @@ export function initLifetimes({
       mpInstance.route = mpInstance.__route__
     }
 
-    // 在创建 Vue 组件实例之前，先查找父组件，以支持组合式 API 的 inject
-    if (mpType === 'component') {
-      const webviewId = mpInstance.__webviewId__ + ''
-      const vuePid = relationOptions.vuePid
+    if (__PLATFORM__ === 'mp-toutiao') {
+      // 在创建 Vue 组件实例之前，先查找父组件，以支持组合式 API 的 inject
+      if (mpType === 'component') {
+        const webviewId = mpInstance.__webviewId__ + ''
+        const vuePid = relationOptions.vuePid
 
-      let parentVm
-      if (vuePid) {
-        // 尝试从页面查找父组件
-        const pageVm = instances[webviewId + '_0'] as ComponentPublicInstance
-        if (pageVm) {
-          parentVm = findVmByVueId(pageVm, vuePid)
+        let parentVm
+        if (vuePid) {
+          // 尝试从页面查找父组件
+          const pageVm = instances[webviewId + '_0'] as ComponentPublicInstance
+          if (pageVm) {
+            parentVm = findVmByVueId(pageVm, vuePid)
+          }
+        } else {
+          // 如果 vuePid 不存在，则认为当前组件的父是页面
+          parentVm = instances[webviewId + '_0']
         }
-      } else {
-        // 如果 vuePid 不存在，则认为当前组件的父是页面
-        parentVm = instances[webviewId + '_0']
-      }
 
-      if (parentVm) {
-        relationOptions.parent = parentVm
+        if (parentVm) {
+          relationOptions.parent = parentVm
+        }
       }
     }
 
