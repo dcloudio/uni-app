47471051b15b308ea7b09b30057c51dbd10a1550
uni@uni-macmini.local <uni@uni-macmini.local>
2026-03-16 15:18:00 +0800

    chore(dev): update framework #0

--- packages/uni-mp-vue/dist-x/vue.runtime.esm.js
+++ packages/uni-mp-vue/dist-x/vue.runtime.esm.js
@@ -6690,14 +6690,14 @@ function patchClassList(classList) {
         if (!className) {
             return;
         }
-        if (__X_STYLE_ISOLATION__) {
-            // 需要兼容原始类名，因为该class可能是全局样式定义的，当组件配置为app时，目前的方案
-            // 是底层配置isolated+@import app.wxss来实现的，需要兼容这种情况
-            const originalClassName = className.replace(/^\^/g, '');
-            if (!patchedClassList.includes(originalClassName)) {
-                patchedClassList.push(originalClassName);
-            }
+        // if (__X_STYLE_ISOLATION__) {
+        // 需要兼容原始类名，因为该class可能是全局样式定义的，当组件配置为app时，目前的方案
+        // 是底层配置isolated+@import app.wxss来实现的，需要兼容这种情况
+        const originalClassName = className.replace(/^\^/g, '');
+        if (!patchedClassList.includes(originalClassName)) {
+            patchedClassList.push(originalClassName);
         }
+        // }
         const patchedClassName = '^' + className;
         if (!patchedClassList.includes(patchedClassName)) {
             patchedClassList.push(patchedClassName);
