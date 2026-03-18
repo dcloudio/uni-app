f9f7debd9d37473410f6f944d70909fa3126c943
DCloud_LXH <283700113@qq.com>
2026-03-18 15:13:42 +0800

    fix: EditorContext 类型定义错误

--- packages/uni-api/src/service/context/editor.ts
+++ packages/uni-api/src/service/context/editor.ts
@@ -81,10 +81,10 @@ export class EditorContext implements UniApp.EditorContext {
   blur(options: UniApp.CommonOptions) {
     this._exec('blur', options)
   }
-  getSelectionText(options: UniApp.CommonOptions) {
+  getSelectionText(options: UniApp.EditorContextGetSelectionTextOptions) {
     this._exec('getSelectionText', options)
   }
-  scrollIntoView(options: UniApp.EditorContextGetSelectionTextOptions) {
+  scrollIntoView(options: UniApp.CommonOptions) {
     this._exec('scrollIntoView', options)
   }
   private _exec(method: string, options?: any) {
