17d9fcab3d3074e195d9244ced4a3f939f96094b
wangyaqi <wangyaqi988@163.com>
2026-03-25 13:24:32 +0800

    chore: 同步uts编译器

--- packages/uni-uts-v1/lib/uni-x/dist/compiler.d.ts
+++ packages/uni-uts-v1/lib/uni-x/dist/compiler.d.ts
@@ -39,7 +39,7 @@ interface CreateTransformerOptions {
     disableUTSBooleanConversion?: boolean;
     sharedData?: {
         resolveFieldMeta(name: string): {
-            fieldId: string;
+            fieldId: number;
         };
     };
 }
