a633d20109c15f4f16b30c9cffab372618c1dcd7
qiang <guoshengqiang@live.com>
2026-04-16 17:54:46 +0800

    fix(ios): 修复 iOS26 以上 WebView 环境中横屏 rpx 计算错误的问题 question/215260

--- src/platforms/h5/service/api/device/get-window-info.js
+++ src/platforms/h5/service/api/device/get-window-info.js
@@ -1,15 +1,17 @@
 import getWindowOffset from 'uni-platform/helpers/get-window-offset'
+import getWindowWidth from 'uni-platform/helpers/get-window-width'
 import safeAreaInsets from 'safe-area-insets'
 
 export function getWindowInfo () {
   var screen = window.screen
   var pixelRatio = window.devicePixelRatio
-  // 横屏时 iOS 获取的屏幕宽高颠倒，进行纠正
-  const screenFix = /^Apple/.test(navigator.vendor) && typeof window.orientation === 'number'
-  const landscape = screenFix && Math.abs(window.orientation) === 90
-  var screenWidth = screenFix ? Math[landscape ? 'max' : 'min'](screen.width, screen.height) : screen.width
-  var screenHeight = screenFix ? Math[landscape ? 'min' : 'max'](screen.height, screen.width) : screen.height
-  var windowWidth = Math.min(window.innerWidth, document.documentElement.clientWidth, screenWidth) || screenWidth
+  const isApple = /^Apple/.test(navigator.vendor)
+  // 横屏时 iOS 获取的屏幕宽高颠倒，进行纠正，iOS26 以上在 WebView 中 window.orientation 与 screen.orientation 可能不准确
+  const screenFix = isApple && window.matchMedia('(orientation:landscape)').matches
+  var screenWidth = screenFix ? Math.max(screen.width, screen.height) : screen.width
+  var screenHeight = screenFix ? Math.min(screen.height, screen.width) : screen.height
+  // 目前仅在 iOS 直接使用平台 API，其他仍使用历史遗留的兼容方案
+  var windowWidth = isApple ? getWindowWidth() : Math.min(getWindowWidth(), document.documentElement.clientWidth, screenWidth) || screenWidth
   var windowHeight = window.innerHeight
   var statusBarHeight = safeAreaInsets.top
 
