16a0aeb98d0cf3722524696e926d2289d5bedab2
wangyaqi <wangyaqi988@163.com>
2026-04-23 14:47:32 +0800

    fix(android): 修复蒸汽模式通道参数nested设置错误的Bug

--- packages/uni-app-plus/src/service/api/plugin/uts.ts
+++ packages/uni-app-plus/src/service/api/plugin/uts.ts
@@ -73,6 +73,7 @@ export function normalizeArg(
   }
 ) {
   arg = toRaw(arg)
+  const isVaporAndroid = __VAPOR__ && isUTSAndroid()
   if (typeof arg === 'function') {
     let id: number
     if (keepAlive) {
@@ -90,12 +91,14 @@ export function normalizeArg(
     return arg.map((item) => normalizeArg(item, callbacks, keepAlive, context))
     // 为啥还要额外判断了isUniElement?，isPlainObject不是包含isUniElement的逻辑吗？为了避免出bug，保留此逻辑
   } else if (arg instanceof ArrayBuffer) {
+    // android dom2 js引擎支持直接传递 ArrayBuffer
+    // nested在dom2安卓表示当前调用参数包含ArrayBuffer等不可序列化对象，不管是不是顶层参数
+    if (isVaporAndroid) {
+      context.nested = true
+      return arg
+    }
     if (context.depth > 0) {
       context.nested = true
-    }
-    // android dom2 js引擎支持直接传递 ArrayBuffer
-    if (__VAPOR__ && isUTSAndroid()) {
-      return arg
     }
     return serializeArrayBuffer(arg)
   } else if (isPlainObject(arg) || isUniElement(arg)) {
@@ -105,7 +108,7 @@ export function normalizeArg(
       : undefined
     const el = uniElement || componentPublicInstanceUniElement
     if (el) {
-      if (context.depth > 0) {
+      if (context.depth > 0 || isVaporAndroid) {
         context.nested = true
       }
       return serializeUniElement(
