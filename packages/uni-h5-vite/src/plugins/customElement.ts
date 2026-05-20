71e8c1647540c0dc4d66e98de116c41aa19b8952
fxy060608 <fxy060608@gmail.com>
2026-05-20 12:56:40 +0800

    perf(h5-vite): 减少 custom-element 插件无关 transform 调用

--- packages/uni-h5-vite/src/plugins/customElement.ts
+++ packages/uni-h5-vite/src/plugins/customElement.ts
@@ -9,37 +9,40 @@ const H5_COMPONENTS_PATH = '@dcloudio/uni-h5'
 export function uniCustomElementPlugin(): Plugin {
   return {
     name: 'uni:h5-custom-element',
-    transform(code, id) {
-      if (!isVueSfcFile(id)) {
-        return
-      }
-      if (!code.includes('$UniCustomElement$')) {
-        return
-      }
-      const importSpecifiers: Set<string> = new Set()
-      code = code.replace(
-        /['|"]\$UniCustomElement\$([\w|-]+)['|"]/g,
-        (_, name) => {
-          if (!UVUE_WEB_BUILT_IN_CUSTOM_ELEMENTS.includes(name)) {
-            return _
-          }
-          const elementName = capitalize(camelize(name))
-          const localName = '_' + elementName + 'Element'
-          importSpecifiers.add(`${elementName} as ${localName}`)
-          return localName
+    transform: {
+      filter: { id: /\.(vue|uvue)(\?|$)/, code: /\$UniCustomElement\$/ },
+      handler(code, id) {
+        if (!isVueSfcFile(id)) {
+          return
         }
-      )
+        if (!code.includes('$UniCustomElement$')) {
+          return
+        }
+        const importSpecifiers: Set<string> = new Set()
+        code = code.replace(
+          /['|"]\$UniCustomElement\$([\w|-]+)['|"]/g,
+          (_, name) => {
+            if (!UVUE_WEB_BUILT_IN_CUSTOM_ELEMENTS.includes(name)) {
+              return _
+            }
+            const elementName = capitalize(camelize(name))
+            const localName = '_' + elementName + 'Element'
+            importSpecifiers.add(`${elementName} as ${localName}`)
+            return localName
+          }
+        )
 
-      if (importSpecifiers.size) {
-        code =
-          `import {${Array.from(importSpecifiers).join(
-            ','
-          )}} from "${H5_COMPONENTS_PATH}";` + code
-      }
-      return {
-        code,
-        map: null,
-      }
+        if (importSpecifiers.size) {
+          code =
+            `import {${Array.from(importSpecifiers).join(
+              ','
+            )}} from "${H5_COMPONENTS_PATH}";` + code
+        }
+        return {
+          code,
+          map: null,
+        }
+      },
     },
   }
 }
