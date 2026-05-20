41ed27b2341edf468d53cd9f4ae16f3867e345a2
fxy060608 <fxy060608@gmail.com>
2026-05-20 13:05:43 +0800

    perf(h5-vite): 减少 post-vue 插件无关 transform 调用

--- packages/uni-h5-vite/src/plugins/postVue.ts
+++ packages/uni-h5-vite/src/plugins/postVue.ts
@@ -9,35 +9,41 @@ export function uniPostVuePlugin(): Plugin {
     name: 'uni:post-vue',
     apply: 'serve',
     enforce: 'post',
-    async transform(code, id) {
-      const { filename, query } = parseVueRequest(id)
-      if (query.vue) {
-        return
-      }
-      if (!EXTNAME_VUE.includes(path.extname(filename))) {
-        return
-      }
-      if (!WXS_RE.test(code)) {
-        return
-      }
-      const hmrId = parseHmrId(code)
-      if (!hmrId) {
-        return
-      }
-      // TODO 内部解决 @vitejs/plugin-vue 自定义块外链热刷的问题
-      // https://github.com/vitejs/vite/blob/main/packages/plugin-vue/src/main.ts#L387
-      // 没有增加 src=descriptor.id
-      // 包含外链 wxs,renderjs
-      code = code.replace(
-        /vue&type=(wxs|renderjs)&index=([0-9]+)&src&/gi,
-        (_, type, index) => {
-          return `vue&type=${type}&index=${index}&src=${hmrId}&`
+    transform: {
+      filter: {
+        id: /\.(?:vue|nvue|uvue)(?:$|\?(?!vue(?:&|=|$)))/,
+        code: WXS_RE,
+      },
+      async handler(code, id) {
+        const { filename, query } = parseVueRequest(id)
+        if (query.vue) {
+          return
         }
-      )
-      return {
-        code: code, // 暂不提供sourcemap,意义不大
-        map: null,
-      }
+        if (!EXTNAME_VUE.includes(path.extname(filename))) {
+          return
+        }
+        if (!WXS_RE.test(code)) {
+          return
+        }
+        const hmrId = parseHmrId(code)
+        if (!hmrId) {
+          return
+        }
+        // TODO 内部解决 @vitejs/plugin-vue 自定义块外链热刷的问题
+        // https://github.com/vitejs/vite/blob/main/packages/plugin-vue/src/main.ts#L387
+        // 没有增加 src=descriptor.id
+        // 包含外链 wxs,renderjs
+        code = code.replace(
+          /vue&type=(wxs|renderjs)&index=([0-9]+)&src&/gi,
+          (_, type, index) => {
+            return `vue&type=${type}&index=${index}&src=${hmrId}&`
+          }
+        )
+        return {
+          code: code, // 暂不提供sourcemap,意义不大
+          map: null,
+        }
+      },
     },
   }
 }
