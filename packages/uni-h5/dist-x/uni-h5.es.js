6c25e96266cc4c984b785963459510625fc0c33d
uni@uni-macmini.local <uni@uni-macmini.local>
2026-05-22 11:55:36 +0800

    chore(alpha): update framework #0

--- packages/uni-h5/dist-x/uni-h5.es.js
+++ packages/uni-h5/dist-x/uni-h5.es.js
@@ -9252,10 +9252,13 @@ function getTheme() {
 function getBrowserInfo() {
   let osname;
   let osversion = "0";
-  let model = "";
+  let model;
   let deviceType = "phone";
+  let platform = "";
+  let deviceBrand;
   const language = navigator.language;
   if (isIOS) {
+    deviceBrand = "iPhone";
     osname = "iOS";
     const osversionFind = ua.match(/OS\s([\w_]+)\slike/);
     if (osversionFind) {
@@ -9311,6 +9314,7 @@ function getBrowserInfo() {
       }
     }
   } else if (isIPadOS) {
+    deviceBrand = "iPad";
     model = "iPad";
     osname = "iOS";
     deviceType = "pad";
@@ -9354,7 +9358,8 @@ function getBrowserInfo() {
         osversion += ` x${framework[1]}`;
       }
     } else if (isMac) {
-      osname = "mac";
+      osname = "macos";
+      platform = "mac";
       const _osversion = osversionFind && osversionFind.match(/Mac OS X (.+)/) || "";
       if (osversion) {
         osversion = _osversion[1].replace(/_/g, ".");
@@ -9373,20 +9378,22 @@ function getBrowserInfo() {
       }
     }
   } else if (isHarmony) {
-    osname = isHarmony2in1 ? "ohos_pc" : "Harmony";
+    deviceBrand = "HUAWEI";
+    osname = "harmonyos";
     deviceType = isHarmony2in1 ? "pc" : isHarmonyTablet ? "pad" : "phone";
     const osversionFind = ua.match(/OpenHarmony\s([\d\.]+)/);
     if (osversionFind) {
       osversion = osversionFind[1];
     }
-    model = "";
+    model = void 0;
   } else {
     osname = "Other";
     osversion = "0";
     deviceType = "unknown";
   }
   const system = `${osname} ${osversion}`;
-  const platform = osname.toLowerCase();
+  if (!platform)
+    platform = osname.toLowerCase();
   let browserName = "";
   let browserVersion = String(IEVersion());
   if (browserVersion !== "-1") {
@@ -9419,8 +9426,8 @@ function getBrowserInfo() {
     deviceOrientation = Math.abs(window.orientation) === 90 ? "landscape" : "portrait";
   }
   return {
-    deviceBrand: void 0,
-    brand: void 0,
+    deviceBrand,
+    brand: deviceBrand,
     deviceModel: model,
     deviceOrientation,
     model,
--- packages/uni-mp-harmony/dist/uni.api.esm.js
+++ packages/uni-mp-harmony/dist/uni.api.esm.js
@@ -974,21 +974,40 @@ function addSafeAreaInsets(fromRes, toRes) {
     }
 }
 function getOSInfo(system, platform) {
+    /**
+     * system 枚举值说明：
+     *
+     * weixin: 操作系统及版本
+     * qq: 操作系统及版本
+     * kuaishou: 操作系统及版本
+     *
+     * alipay、dingding: 系统版本
+     * baidu: 操作系统版本
+     * toutiao/douyin: 操作系统版本
+     * jd: 操作系统版本
+     * harmony: 操作系统版本
+     *
+     * lark: 文档无此字段
+     */
     let osName = '';
     let osVersion = '';
     if (platform &&
-        ("mp-harmony" === 'mp-baidu')) {
+        ("mp-harmony" === 'mp-harmony')) {
         osName = platform;
         osVersion = system;
+        system = `${osName} ${osVersion}`;
     }
     else {
-        osName = system.split(' ')[0] || platform;
+        {
+            osName = system.split(' ')[0] || platform;
+        }
         osVersion = system.split(' ')[1] || '';
     }
     osName = osName.toLowerCase();
     switch (osName) {
         case 'harmony': // alipay
-        case 'ohos': // weixin
+        case 'ohos': // weixin harmony
+        case 'openharmonyos': // weixin 由 HarmonyOS 改为了 OpenHarmonyOS
         case 'openharmony': // feishu
             osName = 'harmonyos';
             break;
@@ -1006,13 +1025,38 @@ function getOSInfo(system, platform) {
     return {
         osName,
         osVersion,
+        system,
     };
+}
+function getPlatform(platform) {
+    /**
+     * platform 枚举值说明：
+     *
+     * weixin：ios、android、windows、mac、ohos、ohos_pc、devtools
+     * alipay、dingding：Android，iOS / iPhone OS，Harmony
+     * harmony: 固定 ohos
+     *
+     * toutiao: Android，iOS 无 harmony 平台，暂不处理
+     * lark: 'pc' | 'mobile' | 'android' | 'ios', 无 harmony 平台，暂不处理
+     *
+     * baidu：无相关描述
+     * qq: 无相关描述
+     * kuaishou: 无相关描述
+     * jd: 无相关描述
+     */
+    platform = platform.toLowerCase();
+    {
+        if (platform === 'ohos') {
+            platform = 'harmonyos';
+        }
+    }
+    return platform;
 }
 function populateParameters(fromRes, toRes) {
     const { brand = '', model = '', system = '', language = '', theme, version, platform, fontSizeSetting, SDKVersion, pixelRatio, deviceOrientation, } = fromRes;
     // const isQuickApp = "mp-harmony".indexOf('quickapp-webview') !== -1
     // osName osVersion
-    const { osName, osVersion } = getOSInfo(system, platform);
+    const { osName, osVersion, system: updatedSystem, } = getOSInfo(system, platform);
     let hostVersion = version;
     // deviceType
     let deviceType = getGetDeviceType(fromRes, model);
@@ -1054,6 +1098,8 @@ function populateParameters(fromRes, toRes) {
         hostFontSizeSetting: fontSizeSetting,
         windowTop: 0,
         windowBottom: 0,
+        platform: getPlatform(platform),
+        system: updatedSystem,
         // TODO
         osLanguage: undefined,
         osTheme: undefined,
@@ -1069,6 +1115,7 @@ function populateParameters(fromRes, toRes) {
     extend(toRes, parameters);
 }
 function getGetDeviceType(fromRes, model) {
+    const platform = fromRes.platform || '';
     // deviceType
     let deviceType = fromRes.deviceType || 'phone';
     {
@@ -1076,6 +1123,8 @@ function getGetDeviceType(fromRes, model) {
             ipad: 'pad',
             windows: 'pc',
             mac: 'pc',
+            linux: 'pc',
+            pc: 'pc',
         };
         const deviceTypeMapsKeys = Object.keys(deviceTypeMaps);
         const _model = model.toLowerCase();
@@ -1085,6 +1134,11 @@ function getGetDeviceType(fromRes, model) {
                 deviceType = deviceTypeMaps[_m];
                 break;
             }
+        }
+    }
+    {
+        if (platform === 'ohos_pc') {
+            deviceType = 'pc';
         }
     }
     return deviceType;
