e62a8093451543c610577d1eebcb6499aa7aff26
chouchouji <1305974212@qq.com>
2026-06-11 16:42:21 +0800

    feat(mp-weixin): 支持拷贝 ai 相关的文件

--- packages/uni-mp-weixin/src/compiler/options.ts
+++ packages/uni-mp-weixin/src/compiler/options.ts
@@ -18,6 +18,7 @@ import {
 } from '@dcloudio/uni-mp-vite'
 import { transformAd } from './transforms/transformAd'
 import { transformLoading } from '../x/compiler/transforms/transformLoading'
+import { getMiniProgramAIPaths } from './utils'
 
 import source from './project.config.json'
 
@@ -168,6 +169,10 @@ export const options: UniMiniProgramPluginOptions = {
             'functional-pages',
             'project.private.config.json',
             projectConfigFilename,
+            ...getMiniProgramAIPaths(
+              process.env.UNI_INPUT_DIR,
+              process.env.UNI_PLATFORM
+            ),
           ],
           get dest() {
             return process.env.UNI_OUTPUT_DIR
---
+++ packages/uni-mp-weixin/src/compiler/utils.ts
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import path from 'path'
+import { parseManifestJsonOnce } from '@dcloudio/uni-cli-shared'
+import { isArray } from '@vue/shared'
+
+interface AgentConfig {
+  instruction?: string
+  skills?: Array<{ path: string; name: string; description: string }>
+}
+
+export function getMiniProgramAIPaths(
+  inputDir: string,
+  platform: UniApp.PLATFORM
+) {
+  const manifestJson = parseManifestJsonOnce(inputDir).manifest
+  const config = manifestJson[platform]
+  if (!config?.agent) {
+    return []
+  }
+
+  const agentConfig: AgentConfig = config.agent
+  const paths: string[] = []
+  const instruction = agentConfig.instruction
+  if (instruction && fs.existsSync(path.resolve(inputDir, instruction))) {
+    paths.push(instruction)
+  }
+
+  const skills = agentConfig.skills
+  if (isArray(skills) && skills.length > 0) {
+    skills.forEach((skill) => {
+      if (skill.path && fs.existsSync(path.resolve(inputDir, skill.path))) {
+        paths.push(skill.path)
+      }
+    })
+  }
+
+  return paths
+}
