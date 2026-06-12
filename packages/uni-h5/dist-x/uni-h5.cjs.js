b00c47d4ae40cf69deaa67a6cc787a170acc2342
uni@uni-macmini.local <uni@uni-macmini.local>
2026-06-12 11:38:24 +0800

    chore(alpha): update framework #0

--- packages/uni-h5/dist-x/uni-h5.cjs.js
+++ packages/uni-h5/dist-x/uni-h5.cjs.js
@@ -14350,6 +14350,7 @@ const getAppBaseInfo = /* @__PURE__ */ defineSyncApi(
         appVersionCode: __uniConfig.appVersionCode,
         appLanguage: getLocale ? getLocale() : language,
         enableDebug: false,
+        packagename: __uniConfig.appId,
         hostSDKVersion: void 0,
         hostPackageName: void 0,
         hostFontSizeSetting: void 0,
