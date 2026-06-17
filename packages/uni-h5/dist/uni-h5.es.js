71e7538bd0d94115a03d4e884a6bd699a78cbf2b
git_robot <uni@uni-macmini.local>
2026-06-17 16:50:09 +0800

    chore(alpha): update framework #0

--- packages/uni-h5/dist/uni-h5.es.js
+++ packages/uni-h5/dist/uni-h5.es.js
@@ -19593,7 +19593,6 @@ const getAppBaseInfo = /* @__PURE__ */ defineSyncApi(
         appVersionCode: __uniConfig.appVersionCode,
         appLanguage: getLocale ? getLocale() : language,
         enableDebug: false,
-        packagename: __uniConfig.appId,
         hostSDKVersion: void 0,
         hostPackageName: void 0,
         hostFontSizeSetting: void 0,
