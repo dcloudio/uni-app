3bd15e710541e690130b5111ee61823d0c7c4b2e
fxy060608 <fxy060608@gmail.com>
2026-06-22 11:29:58 +0800

    refactor(dom2): 移除 disableStaticStyle 逻辑

--- packages/uni-cli-shared/src/dom2/vue.ts
+++ packages/uni-cli-shared/src/dom2/vue.ts
@@ -12,35 +12,19 @@ export function initVueTemplateCompilerExtraOptions(descriptor: SFCDescriptor) {
   const rootScrollView = parseUniXPageOptions(filename)
   const componentType =
     rootScrollView || isUniPageFile(filename) ? 'page' : 'component'
-  const isDevX =
-    process.env.UNI_HX_VERSION_DEV === 'true' &&
-    process.env.UNI_APP_X === 'true'
   const isDynamic = process.env.UNI_APP_X_DOM2_DYNAMIC === 'true'
-  let disableStaticStyle = false
-  if (
-    isDevX &&
-    // 动态渲染时，仍旧使用静态样式，避免性能问题。
-    !isDynamic &&
-    process.env.NODE_ENV === 'development'
-  ) {
-    if (process.env.UNI_UTS_PLATFORM === 'app-harmony') {
-      // 开发版本、开发模式下，非鸿蒙release模式打包
-      disableStaticStyle = process.env.UNI_APP_HARMONY_RUN_MODE !== 'release'
-    }
-  }
   const helper = requireUniHelpers()
   return {
     root: normalizePath(process.env.UNI_INPUT_DIR),
     platform: process.env.UNI_UTS_PLATFORM,
     componentType,
-    filename: filename,
+    filename,
     relativeFilename,
     helper,
     enableRootScrollViewTransform: true,
     // 仅页面透传 rootScrollView，避免非页面组件误触发 ROOT 自动包裹逻辑。
     rootScrollView: componentType === 'page' ? rootScrollView : undefined,
     scriptCppBlocks: (descriptor as any).scriptCppBlocks,
-    disableStaticStyle,
     onVueTemplateCompileLog(type: 'warn' | 'error', error: CompilerError) {
       return onVueTemplateCompileLog(
         type,
