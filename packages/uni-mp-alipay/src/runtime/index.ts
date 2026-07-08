be406820c04add25994fe8235f19b5665ff36ffb
chouchouji <1305974212@qq.com>
2026-07-08 12:03:56 +0800

    feat(mp-alipay): 支持独立分包

--- packages/uni-mp-alipay/src/runtime/index.ts
+++ packages/uni-mp-alipay/src/runtime/index.ts
@@ -2,6 +2,7 @@ import { EventChannel } from '@dcloudio/uni-shared'
 
 import {
   initCreateApp,
+  initCreateIndependentSubpackageApp,
   initCreatePluginApp,
   initCreateSubpackageApp,
 } from '@dcloudio/uni-mp-core'
@@ -17,9 +18,12 @@ export const createPage = initCreatePage()
 export const createComponent = initCreateComponent()
 export const createPluginApp = initCreatePluginApp(parseAppOptions)
 export const createSubpackageApp = initCreateSubpackageApp(parseAppOptions)
+export const createIndependentSubpackageApp =
+  initCreateIndependentSubpackageApp()
 ;(my as any).EventChannel = EventChannel
 ;(my as any).createApp = createApp
 ;(my as any).createPage = createPage
 ;(my as any).createComponent = createComponent
 ;(my as any).createPluginApp = createPluginApp
 ;(my as any).createSubpackageApp = createSubpackageApp
+;(my as any).createIndependentSubpackageApp = createIndependentSubpackageApp
