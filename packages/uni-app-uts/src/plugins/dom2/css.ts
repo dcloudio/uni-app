f340f492b5b0a4d369e94da884860b69600069e8
fxy060608 <fxy060608@gmail.com>
2026-08-10 14:36:10 +0800

    feat(dom2): 默认启用 animation 编译 #31169

--- packages/uni-app-uts/src/plugins/dom2/css.ts
+++ packages/uni-app-uts/src/plugins/dom2/css.ts
@@ -1,5 +1,3 @@
-import fs from 'fs'
-import path from 'path'
 import type { Plugin, ResolvedConfig } from 'vite'
 
 import {
@@ -32,13 +30,7 @@ import { DOM2_CSS_CACHE_MAP, isVue } from '../utils'
 
 const CSS_FILE_ID_MAP = new Map<string, string>()
 
-export function isAnimationEnabled() {
-  const inputDir = process.env.UNI_INPUT_DIR
-  return !!inputDir && fs.existsSync(path.resolve(inputDir, '.animation'))
-}
-
 export function uniAppCssPrePlugin(): Plugin {
-  const enableAnimation = isAnimationEnabled()
   const name = 'uni:app-uvue-css-pre'
   const mainPath = resolveMainPathOnce(process.env.UNI_INPUT_DIR)
   const appUVuePath = resolveAppVue(process.env.UNI_INPUT_DIR)
@@ -81,7 +73,6 @@ export function uniAppCssPrePlugin(): Plugin {
             platform: process.env.UNI_UTS_PLATFORM,
             helper: requireUniHelpers(),
             output,
-            enableAnimation,
           })
           if (isDom2 && fontFaces?.length) {
             const id = CSS_FILE_ID_MAP.get(filename)
@@ -184,7 +175,6 @@ export function uniAppCssPrePlugin(): Plugin {
 }
 
 export function uniAppCssPlugin(): Plugin {
-  const enableAnimation = isAnimationEnabled()
   let resolvedConfig: ResolvedConfig
   const { parseCss } = require('@dcloudio/compiler-vapor-dom2')
   return {
@@ -211,7 +201,6 @@ export function uniAppCssPlugin(): Plugin {
         platform: process.env.UNI_UTS_PLATFORM,
         helper: requireUniHelpers(),
         output,
-        enableAnimation,
       })
       let cssSourceMap: SourceMapInput | undefined
       if (messages.find((m) => m.type === 'warning')) {
