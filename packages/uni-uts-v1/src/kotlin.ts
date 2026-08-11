60d7efc333406c49640d5563896e895a4caf6de1
wangyaqi <wangyaqi988@163.com>
2026-08-11 16:52:39 +0800

    feat: 蒸汽模式编译uts插件增加UTSAutoRegister导入

--- packages/uni-uts-v1/src/kotlin.ts
+++ packages/uni-uts-v1/src/kotlin.ts
@@ -566,7 +566,10 @@ const DEFAULT_IMPORTS_VUE_X = [
 const DEFAULT_IMPORTS_VUE_X_DOM2 = []
 
 const DEFAULT_IMPORTS_X = ['io.dcloud.uniapp.runtime.*']
-const DEFAULT_IMPORTS_X_DOM2 = ['io.dcloud.uniappxv.runtime.*']
+const DEFAULT_IMPORTS_X_DOM2 = [
+  'io.dcloud.uniappxv.runtime.*',
+  'io.dcloud.uts.autoregister.UTSAutoRegister',
+]
 
 export async function compile(
   filename: string,
