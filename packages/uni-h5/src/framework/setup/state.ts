ce51b0c1d6f63b05c6a30128c1d64940c4b4ef56
DCloud_LXH <283700113@qq.com>
2026-08-18 11:30:52 +0800

    feat(web): custom-tab-bar 支持 theme.json

--- packages/uni-h5/src/framework/setup/state.ts
+++ packages/uni-h5/src/framework/setup/state.ts
@@ -1,13 +1,30 @@
 import { reactive } from 'vue'
 import { initTabBarI18n } from '@dcloudio/uni-core'
+import { parseTheme, useTheme } from '../../helpers/theme'
 
-let tabBar: UniApp.TabBarOptions | undefined
+let _tabBar: UniApp.TabBarOptions | undefined
 
 export function useTabBar() {
-  if (!tabBar) {
-    tabBar =
+  if (!_tabBar) {
+    _tabBar =
       __uniConfig.tabBar &&
       reactive<UniApp.TabBarOptions>(initTabBarI18n(__uniConfig.tabBar))
   }
+
+  const tabBar = useTheme(_tabBar!, () => {
+    const tabBarStyle = parseTheme(_tabBar!)
+    tabBar.backgroundColor = tabBarStyle.backgroundColor
+    tabBar.borderStyle = tabBarStyle.borderStyle
+    tabBar.color = tabBarStyle.color
+    tabBar.selectedColor = tabBarStyle.selectedColor
+    tabBar.blurEffect = tabBarStyle.blurEffect
+    tabBar.midButton = tabBarStyle.midButton
+    if (tabBarStyle.list && tabBarStyle.list.length) {
+      tabBarStyle.list.forEach((item, index) => {
+        tabBar.list[index].iconPath = item.iconPath
+        tabBar.list[index].selectedIconPath = item.selectedIconPath
+      })
+    }
+  })
   return tabBar
 }
