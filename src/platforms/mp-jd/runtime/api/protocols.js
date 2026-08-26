66fd1a8bab3270618387d988c076a3eb6eb1edb7
DCloud_LXH <283700113@qq.com>
2026-08-26 14:48:48 +0800

    feat(mp): 增强 getWindowInfo、getAppBaseInfo、getDeviceInfo

--- src/platforms/mp-jd/runtime/api/protocols.js
+++ src/platforms/mp-jd/runtime/api/protocols.js
@@ -3,6 +3,9 @@
 // import previewImage from '../../../mp-weixin/helpers/normalize-preview-image'
 import getSystemInfo from '../../../mp-weixin/helpers/system-info'
 // import getUserProfile from '../../../mp-weixin/helpers/get-user-profile'
+import getAppBaseInfo from '../../../mp-weixin/helpers/get-app-base-info'
+import getWindowInfo from '../../../mp-weixin/helpers/get-window-info'
+import getDeviceInfo from '../../../mp-weixin/helpers/get-device-info'
 
 // 需要做转换的 API 列表
 export const protocols = {
@@ -10,8 +13,17 @@ export const protocols = {
   // redirectTo,
   // previewImage,
   getSystemInfo,
-  getSystemInfoSync: getSystemInfo
-  // getUserProfile
+  getSystemInfoSync: getSystemInfo,
+  // getUserProfile,
+  getAppBaseInfo: Object.assign({}, getAppBaseInfo, {
+    name: __GLOBAL__.canIUse('getAppBaseInfo') ? 'getAppBaseInfo' : 'getSystemInfoSync'
+  }),
+  getWindowInfo: Object.assign({}, getWindowInfo, {
+    name: __GLOBAL__.canIUse('getWindowInfo') ? 'getWindowInfo' : 'getSystemInfoSync'
+  }),
+  getDeviceInfo: Object.assign({}, getDeviceInfo, {
+    name: __GLOBAL__.canIUse('getDeviceInfo') ? 'getDeviceInfo' : 'getSystemInfoSync'
+  })
 }
 
 // 不支持的 API 列表
