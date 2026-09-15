140f70b8708b292f036c4a5d04522f4fcd7ad28d
chouchouji <1305974212@qq.com>
2026-09-15 19:25:20 +0800

    fix(web): sync the scoped hash of uni-app element to body

--- packages/uni-h5/src/framework/components/layout/index.tsx
+++ packages/uni-h5/src/framework/components/layout/index.tsx
@@ -38,6 +38,7 @@ import { hasOwn } from '@vue/shared'
 
 import TabBar from './tabBar'
 import { usePageRoute } from '../../setup/provide'
+import { syncScopeIdsToBody } from '../../../helpers/scope'
 
 type KeepAliveRoute = ReturnType<typeof useKeepAliveRoute>
 
@@ -64,6 +65,9 @@ export default /*#__PURE__*/ defineSystemComponent({
     const showTabBar = (__UNI_FEATURE_TABBAR__ &&
       useShowTabBar(emit)) as ComputedRef<boolean>
     const clazz = useAppClass(showTabBar)
+    onMounted(() => {
+      syncScopeIdsToBody(rootRef.value, document.body)
+    })
     globalLayoutState = layoutState
     return () => {
       const layoutTsx = createLayoutTsx(
