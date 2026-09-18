41c5a3a0a36b8fd30c436d8f12b5583ee05ff617
git_robot <uni@uni-macmini.local>
2026-09-18 17:53:45 +0800

    chore(dev): update framework #0

--- packages/uni-mp-alipay/dist/uni.compiler.js
+++ packages/uni-mp-alipay/dist/uni.compiler.js
@@ -314,6 +314,9 @@ const options = {
         copyOptions: {
             assets: uniCliShared.createCopyComponentDirs(COMPONENTS_DIR),
             targets: [
+                ...(process.env.UNI_APP_X === 'true'
+                    ? [uniCliShared.createMiniProgramUasmCopyTarget('mp-alipay')]
+                    : []),
                 ...(process.env.UNI_MP_PLUGIN ? [uniCliShared.copyMiniProgramPluginJson] : []),
                 {
                     src: ['customize-tab-bar', 'preload.json', 'sitemap.json'],
