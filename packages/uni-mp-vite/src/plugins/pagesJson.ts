3c1c3bbfd82e6f14e25cf1243878acf08e875951
chouchouji <1305974212@qq.com>
2026-09-20 20:13:43 +0800

    feat(mp): support subpackage component handling

--- packages/uni-mp-vite/src/plugins/pagesJson.ts
+++ packages/uni-mp-vite/src/plugins/pagesJson.ts
@@ -160,7 +160,11 @@ export function uniPagesJsonPlugin(
         console.warn(M['dev.watching.restart.independentSubPackages'])
         process.exit(0)
       }
-      addMiniProgramAppJson(normalizedAppJson)
+      addMiniProgramAppJson(normalizedAppJson, {
+        subPackages:
+          !!parseManifestJsonOnce(inputDir)[platform]?.optimization
+            ?.subPackages,
+      })
       allPagePaths = []
       Object.keys(pageJsons).forEach((name) => {
         if (isNormalPage(name)) {
