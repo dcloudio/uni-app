e33e88922fa99316c40ad1f5d66145986f5f2fdc
wangyaqi <wangyaqi988@163.com>
2026-09-23 17:18:21 +0800

    fix: 修复worker内使用项目下的uni_module插件编译报错的Bug
    https://issues.dcloud.net.cn/pages/issues/detail?id=33251

--- packages/uni-app-harmony/src/compiler/workers.ts
+++ packages/uni-app-harmony/src/compiler/workers.ts
@@ -1,9 +1,25 @@
 import path from 'path'
+import fs from 'fs'
 import {
   getWorkers,
   resolveUTSCompiler,
   uvueOutDir,
 } from '@dcloudio/uni-cli-shared'
+
+/**
+ * 临时放在此处，其他地方没有这种需求
+ * 所有含utssdk的uni_modules都可能是worker的依赖
+ */
+function resolveAllUniModules(): string[] {
+  const inputDir = process.env.UNI_INPUT_DIR
+  const uniModulesDir = path.resolve(inputDir, 'uni_modules')
+  return fs
+    .readdirSync(uniModulesDir)
+    .filter((module) =>
+      fs.statSync(path.resolve(uniModulesDir, module, 'utssdk')).isDirectory()
+    )
+}
+
 export async function buildWorkers() {
   const workers = getWorkers()
   if (!Object.keys(workers).length) {
@@ -18,6 +34,7 @@ export async function buildWorkers() {
       rootDir,
       outDir: process.env.UNI_OUTPUT_DIR,
       footer: `;(new ${workers[workPath]}()).entry()`,
+      uni_modules: resolveAllUniModules(),
     })
     if (result && result.error) {
       throw parseUTSSyntaxError(result.error, process.env.UNI_INPUT_DIR)
